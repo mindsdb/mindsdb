c68e0357eaefd9f84f7c4f6322385ca72b80fe4b
Jorge Torres <jorge.torres.maldonado@gmail.com>
2018-09-21 07:34:27 -0400

    isntall script update, support dev mode

--- install.sh
+++ install.sh
@@ -2,19 +2,36 @@ rm -rf build
 rm -rf dist
 rm -rf mindsdb.egg-info
 # python3 setup.py --help-commands
-# python3 setup.py develop --uninstall
-python3 setup.py clean
-python3 setup.py build
-#python3 setup.py install
-python3 setup.py sdist bdist_wheel
 
-echo "Do you want to publish this version (yes/no)?"
+echo "mode (prod/dev)?"
 
-read publish
+read mode
 
-if [ "$publish" = "yes" ]; then
-    echo "Publishing mindsdb to Pypi"
-    python3 -m twine upload dist/*
+if [ "$mode" = "prod" ]; then
+
+    python3 setup.py develop --uninstall
+    python3 setup.py clean
+    python3 setup.py build
+    #python3 setup.py install
+    python3 setup.py sdist bdist_wheel
+
+    echo "Do you want to publish this version (yes/no)?"
+
+    read publish
+
+    if [ "$publish" = "yes" ]; then
+        echo "Publishing mindsdb to Pypi"
+        python3 -m twine upload dist/*
+    fi
+
+
 fi
+
+if [ "$mode" = "dev" ]; then
+    pip3 uninstall mindsdb
+    python3 setup.py develop --uninstall
+    python3 setup.py develop
+fi
+
 
 

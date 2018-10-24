89c43554e06b2ba3dce893cd04959423a334fbee
Jorge Torres <jorge.torres.maldonado@gmail.com>
2018-10-24 17:16:07 -0400

    time series example complete

--- docs/examples/time_series/predict.py
+++ docs/examples/time_series/predict.py
@@ -13,8 +13,8 @@ In order to to this we have a dataset "data_sources/home_rentals.csv"
 from mindsdb import *
 
 # Here we use the model to make predictions (NOTE: You need to run train.py first)
-result = MindsDB().predict(predict='Main_Engine_Fuel_Consumption_MT_day', model_name='fuel', from_data = 'fuel.csv')
+result = MindsDB().predict(predict='Main_Engine_Fuel_Consumption_MT_day', model_name='fuel', from_data = 'fuel_predict.csv')
 
 # you can now print the results
 print('The predicted main engine fuel consumption')
-print(result)
+print(result.predicted_values)

66c835c3ed73a45d8a2c8bdacc7f8d5c74f700b0
Jorge Torres <jorge.torres.maldonado@gmail.com>
2018-12-14 10:37:55 -0500

    change column name so it has a space or special character " "

--- docs/examples/basic/train.py
+++ docs/examples/basic/train.py
@@ -15,7 +15,7 @@ from mindsdb import *
 # We tell mindsDB what we want to learn and from what data
 MindsDB().learn(
     from_data="home_rentals.csv", #https://raw.githubusercontent.com/mindsdb/mindsdb/master/docs/examples/basic/home_rentals.csv", # the path to the file where we can learn from
-    predict='rental_price', # the column we want to learn to predict given all the data in the file
+    predict='rental price', # the column we want to learn to predict given all the data in the file
     model_name='home_rentals' # the name of this model
 )
 

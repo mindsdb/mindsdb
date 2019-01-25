b01aedb6c79e9c23554c264e30235c1a32de4797
duanzhihua <duanzhihua@189.cn>
2019-01-25 21:56:46 +0800

    fix the example

--- docs/examples/nlp/train.py
+++ docs/examples/nlp/train.py
@@ -1,10 +1,11 @@
 from mindsdb import *
 
 # We tell mindsDB what we want to learn and from what data
-MindsDB().read_csv(filepath="real_estate_description.xlsx" ,delimiter='\t' ,encoding='utf-8')\
+MindsDB().read_csv(filepath="shakes.train.numLines.csv" ,delimiter=',' ,encoding='GB18030')\
     .learn(
-    from_file="real_estate_description.xlsx", # the path to the file where we can learn from
-    predict='number_of_rooms', # the column we want to learn to predict given all the data in the file
+    #from_file="real_estate_description.xlsx", # the path to the file where we can learn from
+    #predict='number_of_rooms', # the column we want to learn to predict given all the data in the file
+    predict='行项目数',
     model_name='real_estate_desc' # the name of this model
 )
 

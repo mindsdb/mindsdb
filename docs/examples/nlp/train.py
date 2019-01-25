b2f26ef0ffdcdcf1150b97908e1bac6d59df37a5
duanzhihua <duanzhihua@189.cn>
2019-01-25 21:50:49 +0800

    add data source encoding

--- docs/examples/nlp/train.py
+++ docs/examples/nlp/train.py
@@ -1,7 +1,8 @@
 from mindsdb import *
 
 # We tell mindsDB what we want to learn and from what data
-MindsDB().learn(
+MindsDB().read_csv(filepath="real_estate_description.xlsx" ,delimiter='\t' ,encoding='utf-8')\
+    .learn(
     from_file="real_estate_description.xlsx", # the path to the file where we can learn from
     predict='number_of_rooms', # the column we want to learn to predict given all the data in the file
     model_name='real_estate_desc' # the name of this model

3fa25407f34d5c5afa91a36212001eeb520d5838
George3d6 <george@cerebralab.com>
2019-02-11 21:52:41 +0000

    added ComplementNB and missing features based accuracy

--- mindsdb/libs/ml_models/probabilistic_validator.py
+++ mindsdb/libs/ml_models/probabilistic_validator.py
@@ -1,4 +1,5 @@
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB, ComplementNB
+from mindsdb.libs.constants.mindsdb import NULL_VALUES
 import numpy as np
 
 """
@@ -10,26 +11,15 @@ functions to work with vectors of features and labels (i.e. multiple rows at onc
 This could increase efficiency
 """
 class ProbabilisticValidator():
-    _smoothing_factor = 0.2
+    _smoothing_factor = 1
     _value_bucket_probabilities = {}
     _probabilistic_model = None
-    _use_features = None
 
-    def __init__(self, value_stats, use_features=False):
-        self._use_features = use_features
 
-        if self._use_features:
-            self._probabilistic_model = MultinomialNB(alpha=self._smoothing_factor)
+    def __init__(self):
+        self._probabilistic_model = ComplementNB(alpha=self._smoothing_factor)
+        #self._probabilistic_model = GaussianNB()
 
-        for bucket in value_stats['histogram']:
-            if value_stats['histogram'][bucket] > 0:
-                self._value_bucket_probabilities[bucket] = {
-                    'True': self._smoothing_factor , 'False': self._smoothing_factor, 'nr_observations': self._smoothing_factor
-                }
-            else:
-                self._value_bucket_probabilities[bucket] = {
-                    'True': 0 , 'False': 0, 'nr_observations': 0.00001
-                }
 
     # For contignous values we want to use a bucket in the histogram to get a discrete label
     @staticmethod
@@ -37,68 +27,56 @@ class ProbabilisticValidator():
         # @TODO Not implemented
         return value
 
+    @staticmethod
+    def _features_missing(feature):
+        return int(not (feature in NULL_VALUES))
+
     def register_observation(self, features, real_value, predicted_value):
         real_value_b = self._get_value_bucket(real_value, None)
         predicted_value_b = self._get_value_bucket(predicted_value, None)
 
+        feature_existence = list(map(self._features_missing,features))
         correct_prediction = real_value_b == predicted_value_b
 
-        if self._use_features:
-            features_b = list(map(lambda x: self._get_value_bucket(x, None), features))
-            features_b.append(predicted_value_b)
-            self._probabilistic_model.partial_fit(np.array(features_b).reshape(1,-1), np.array([correct_prediction]) ,classes=[False, True])
-        else:
-            self._value_bucket_probabilities[predicted_value_b][str(correct_prediction)] += 1
-            self._value_bucket_probabilities[predicted_value_b]['nr_observations'] += 1
+        X = feature_existence
+        X.append(predicted_value_b)
+        Y = [correct_prediction]
+
+        print(Y, X)
+        #exit()
+
+        self._probabilistic_model.partial_fit(np.array(X).reshape(1,-1), Y, classes=[True, False])
 
 
     def evaluate_prediction_accuracy(self, features, predicted_value):
         predicted_value_b = self._get_value_bucket(predicted_value, None)
 
-        if self._use_features:
-            features_b = list(map(lambda x: self._get_value_bucket(x, None), features))
-            features_b.append(predicted_value_b)
-            """
-            # Note: probabilities are rturned for the sorted vector of classes
-            So, in thise case, since True(1) > False(0) the second value in the vecotr
-            is the probability predicted for 'True' (i.e. chance of our prediction being true)
-            """
-            return self._probabilistic_model.predict_proba(np.array(features_b).reshape(1,-1))[0][1]
-        else:
-            try:
-                return self._value_bucket_probabilities[predicted_value_b]['True'] / self._value_bucket_probabilities[predicted_value_b]['nr_observations']
-            except:
-                return 0
+        feature_existence = list(map(self._features_missing,features))
+
+        X = feature_existence
+        X.append(predicted_value_b)
+        print(X)
+        return self._probabilistic_model.predict_proba(np.array(X).reshape(1,-1))[0][1]
 
 
 if __name__ == "__main__":
     feature_rows = [
-        [1,2,3]
+        [None,2,3]
         ,[2,2,3]
-        ,[1,2,6]
-        ,[0,3,3]
+        ,[1,None,6]
+        ,[0,3,None]
         ,[2,0,1]
     ]
 
     values = [2,2,2,3,5]
     predictions = [2,2,2,3,2]
 
-    value_col_stats = {'histogram': {
-        1: 111
-        ,2: 212
-        ,3: 17
-        ,4: 2
-        ,5: 6
-        ,6: 0
-        ,7: 0
-    }}
-
-    pbv = ProbabilisticValidator(value_col_stats, use_features=True)
+    pbv = ProbabilisticValidator()
 
     for i in range(len(feature_rows)):
         pbv.register_observation(feature_rows[i],values[i], predictions[i])
 
     print(pbv.evaluate_prediction_accuracy([1,2,3], 2))
-    print(pbv.evaluate_prediction_accuracy([1,2,3], 3))
-    print(pbv.evaluate_prediction_accuracy([1,2,6], 5))
-    print(pbv.evaluate_prediction_accuracy([22,12,61], 101))
+    print(pbv.evaluate_prediction_accuracy([1,None,3], 3))
+    print(pbv.evaluate_prediction_accuracy([None,0,2], 5))
+    print(pbv.evaluate_prediction_accuracy([22,12,None], 101))

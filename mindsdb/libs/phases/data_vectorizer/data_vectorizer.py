2d64c4d198830facad256fa91130cc8e1bb662da
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-02-12 03:39:46 -0800

    data vectorizer

--- mindsdb/libs/phases/data_vectorizer/data_vectorizer.py
+++ mindsdb/libs/phases/data_vectorizer/data_vectorizer.py
@@ -20,7 +20,7 @@ from mindsdb.libs.constants.mindsdb import *
 from mindsdb.libs.phases.base_module import BaseModule
 from collections import OrderedDict
 from mindsdb.libs.helpers.norm_denorm_helpers import norm, norm_buckets
-from mindsdb.libs.helpers.text_helpers import hashtext, clean_float, tryCastToNumber
+from mindsdb.libs.helpers.text_helpers import hashtext, clean_float, cast_string_to_python_type
 from mindsdb.libs.data_types.transaction_metadata import TransactionMetadata
 
 
@@ -28,206 +28,66 @@ class DataVectorizer(BaseModule):
 
     phase_name = PHASE_DATA_VECTORIZATION
 
-    def _getRowExtraVector(self, ret, column_name, col_row_index, distances):
-
-        predict_columns = self.train_meta_data.model_predict_columns
-
-        desired_total = self.train_meta_data.window_size
-        batch_height = len(ret[column_name])
-        remaining_row_count = batch_height - (col_row_index +1)
-
-
-        harvest_count = desired_total if desired_total < remaining_row_count else remaining_row_count
-        empty_count = desired_total - harvest_count
-        empty_vector_len = (
-                len(ret[column_name][col_row_index])
-                + sum( [ len(ret[predict_col_name][0]) for predict_col_name in predict_columns])
-                + 1) * empty_count # this is the width of the padding
-
-
-        row_extra_vector = []
-
-        for i in range(harvest_count):
-            try:
-                row_extra_vector += ret[column_name][col_row_index + i + 1]
-                row_extra_vector += [distances[col_row_index + i+1]]
-
-                # append the target values before:
-                for predict_col_name in predict_columns:
-                     row_extra_vector += [clean_float(v) for v in ret[predict_col_name][col_row_index + i + 1]]
-            except Exception as e:
-                log.error(traceback.format_exc())
-                log.error('something is not right, seems like we got here with np arrays and they should not be!')
-                raise e
-
-        if empty_count > 0:
-            # complete with empty
-            row_extra_vector += [0] *  empty_vector_len
-
-        return row_extra_vector
-
-
-
 
     def run(self):
-        self.train_meta_data = TransactionMetadata()
-        self.train_meta_data.setFromDict(self.transaction.persistent_model_metadata.train_metadata)
 
-        group_by = self.train_meta_data.model_group_by
-        order_by = self.train_meta_data.model_order_by
-        is_time_series = self.transaction.metadata.model_is_time_series
-
-        group_by_indexes = None
-
-        # if order_by this means its a time series
-        if group_by and len(group_by) > 0:
-            group_by_indexes = [self.transaction.input_data.columns.index(group_by_column) for group_by_column in group_by]
-
+        column_stats = self.transaction.persistent_model_metadata.column_stats
         # this is a template of how we store columns
         column_packs_template = OrderedDict()
-
         # create a template of the column packs
         for i, column_name in enumerate(self.transaction.input_data.columns):
             column_packs_template[column_name] = []
 
         if self.transaction.metadata.type == TRANSACTION_LEARN:
-            groups = [
+            subsets = [
                 {
                     'name': 'test',
-                    'target_set': self.transaction.model_data.test_set,
+                    'subset_pointer': self.transaction.model_data.test_set,
                     'map': self.transaction.model_data.test_set_map,
                     'indexes': self.transaction.input_data.test_indexes
                 },
                 {
                     'name': 'train',
-                    'target_set': self.transaction.model_data.train_set,
+                    'subset_pointer': self.transaction.model_data.train_set,
                     'map': self.transaction.model_data.train_set_map,
                     'indexes': self.transaction.input_data.train_indexes
                 },
                 {
                     'name': 'validation',
-                    'target_set': self.transaction.model_data.validation_set,
+                    'subset_pointer': self.transaction.model_data.validation_set,
                     'map': self.transaction.model_data.validation_set_map,
                     'indexes': self.transaction.input_data.validation_indexes
                 }
             ]
         else:
-            groups = [
+            subsets = [
                 {
                     'name': 'predict',
-                    'target_set': self.transaction.model_data.predict_set,
+                    'subset_pointer': self.transaction.model_data.predict_set,
                     'map': self.transaction.model_data.predict_set_map,
-                    'indexes': range(0,len(self.transaction.input_data.data_array)) # TODO: measure impact of this
+                    'indexes': self.transaction.input_data.all_indexes
                 }
             ]
 
         # iterate over all groups and populate tensors by columns
 
-        for group in groups:
+        for subset in subsets:
 
-            target_set = group['target_set'] # for ease use a pointer
+            subset_pointer = subset['subset_pointer'] # for ease use a pointer
 
-            # iterate over all indexes taht belong to this group
-            for input_row_index in group['indexes']:
-                row = self.transaction.input_data.data_array[input_row_index] # extract the row from input data
-                map = group['map']
+            # iterate over all indexes that belong to this subset
+            for subset_group_by_hash in subset['indexes']:
 
-                if is_time_series:
-                    if group_by_indexes in [None]:
-                        group_by_hash = KEY_NO_GROUP_BY
-                    else:
-                        group_by_hash = hashtext(':'.join([row[group_by_index] for group_by_index in group_by_indexes]))
+                subset_pointer[subset_group_by_hash] = copy.deepcopy(column_packs_template)
 
-                else:
-                    group_by_hash = KEY_NO_GROUP_BY
-
-                # if the set group has not been initiated add a new one
-                if group_by_hash not in target_set:
-                    target_set[group_by_hash] = copy.deepcopy(column_packs_template)
-                    map[group_by_hash] = {}
-
-
-                # Now populate into the group_hash column pile
-                for column_index, cell_value in enumerate(row):
-
-                    column_name = self.transaction.input_data.columns[column_index]
-
-                    value = tryCastToNumber(cell_value)
-                    stats = self.transaction.persistent_model_metadata.column_stats[column_name]
-
-                    # TODO: Provide framework for custom nom functions
-                    # TODO: FIX norm allways add column for is null
-                    normalized = norm(value=value, cell_stats=stats) # this should return a vector representation already normalized
-
-                    # keep track of where it came from in the input data inc ase we need to go back
-                    position = len(target_set[group_by_hash][column_name])
-                    map[group_by_hash][position] = input_row_index
-
-                    # append normalized vector to column tensor
-                    target_set[group_by_hash][column_name] += [normalized]
-
-                    if self.transaction.persistent_model_metadata.column_stats[column_name][KEYS.DATA_TYPE] in [DATA_TYPES.NUMERIC, DATA_TYPES.DATE] and column_name in self.train_meta_data.model_predict_columns:
-                        column_name_expanded = EXTENSION_COLUMNS_TEMPLATE.format(column_name=column_name)
-                        if column_name_expanded not in target_set[group_by_hash]:
-                            target_set[group_by_hash][column_name_expanded] = []
-
-                        normalized_buckets = norm_buckets(value=value, cell_stats=stats)
-                        target_set[group_by_hash][column_name_expanded] += [normalized_buckets]
-
-
-
-            # turn into numpy arrays:
-            for group_by_hash in target_set:
-
-                distances = None
-
-                # if we have a group by and order by calculate a distances vector for each data point in this batch
-                if self.train_meta_data.model_group_by is not None and self.train_meta_data.model_order_by is not None:
-                    distances = []
-                    batch_height = len(target_set[group_by_hash][self.train_meta_data.model_group_by])
-
-                    # create a vector for the top distance
-
-                    for j in range(batch_height):
-                        order_by_bottom_vector = np.array(
-                            list(itertools.chain.from_iterable(
-                                [target_set[group_by_hash][order_by_col][j] for order_by_col in
-                                 self.train_meta_data.model_order_by]
-                            ))
-                        )
-                        if j == 0:
-                            order_by_top_vector = order_by_bottom_vector
-                        else:
-                            order_by_top_vector = np.array(list(itertools.chain.from_iterable(
-                                [target_set[group_by_hash][order_by_col][j - 1] for order_by_col in
-                                 self.train_meta_data.model_order_by])))
-                        # create a vector for the current row
-
-                        # calculate distance and append to distances
-                        distance = float(np.linalg.norm(order_by_top_vector - order_by_bottom_vector))
-                        distances.append(distance)
-
-                # Append the time series data to each column
-                # NOTE: we want to make sure that the self.train_meta_data.model_predict_columns are the first in being converted into vectors
-                #       the reason for this is that if there is a time series query then we will want to add the history of the target value (see self._getRowExtraVector)
-                columns_in_order = self.train_meta_data.model_predict_columns + [column_name for column_name in target_set[group_by_hash] if column_name not in self.train_meta_data.model_predict_columns]
-
-                for column_name in columns_in_order:
-
-                    # if there is a group by and order by and this is not a column to be predicted, append history vector
-                    # TODO: Encode the history vector if possible
-                    non_groupable_columns = self.train_meta_data.model_predict_columns + [self.train_meta_data.model_group_by] +  self.train_meta_data.model_order_by
-                    # NOTE: since distances is only not None if there is a group by this is only evaluated for group by queries
-                    if distances is not None and column_name not in non_groupable_columns:
-                        # for each row create a vector of history and append to it
-                        prev = 0
-                        for col_row_index, col_row in enumerate(target_set[group_by_hash][column_name]):
-                            row_extra_vector = self._getRowExtraVector(target_set[group_by_hash], column_name, col_row_index, distances)
-                            target_set[group_by_hash][column_name][col_row_index] = target_set[group_by_hash][column_name][col_row_index]+ row_extra_vector
-
-
-                    target_set[group_by_hash][column_name] = np.array(target_set[group_by_hash][column_name])
-
+                for input_row_index in subset['indexes'][subset_group_by_hash]:
+                    row = self.transaction.input_data.data_array[input_row_index]  # extract the row from input data
+                    for column_name in subset_pointer[subset_group_by_hash]:
+                        column_index = self.transaction.input_data.columns.index(column_name)
+                        cell_value = row[column_index]
+                        value = cast_string_to_python_type(cell_value)
+                        normalized_value = norm(value=value, cell_stats=column_stats[column_name])  # this should return a vector representation already normalized
+                        subset_pointer[subset_group_by_hash][column_name] += [normalized_value]
 
 
         return []
@@ -237,12 +97,13 @@ def test():
     from mindsdb.libs.controllers.mindsdb_controller import MindsDBController as MindsDB
 
     mdb = MindsDB()
+
+    # We tell mindsDB what we want to learn and from what data
     mdb.learn(
-        from_query='select * from position_target_table',
-        group_by='id',
-        order_by=['max_time_rec'],
-        predict='position',
-        model_name='mdsb_model',
+        from_data="https://raw.githubusercontent.com/mindsdb/mindsdb/master/docs/examples/basic/home_rentals.csv",
+        # the path to the file where we can learn from, (note: can be url)
+        predict='rental_price',  # the column we want to learn to predict given all the data in the file
+        model_name='home_rentals',  # the name of this model
         breakpoint=PHASE_DATA_VECTORIZATION
     )
 

74f33e7b1e7506e75032f1041b10821e5e44f6a7
George3d6 <george@cerebralab.com>
2019-02-13 09:46:07 +0000

    trying to integrate the probabilist validator, issues with sampler

--- mindsdb/libs/phases/model_analyzer/model_analyzer.py
+++ mindsdb/libs/phases/model_analyzer/model_analyzer.py
@@ -3,8 +3,7 @@ from mindsdb.libs.constants.mindsdb import *
 from mindsdb.libs.phases.base_module import BaseModule
 from mindsdb.libs.data_types.sampler import Sampler
 from mindsdb.libs.ml_models.pytorch.libs import base_model;
-
-import importlib
+from mindsdb.libs.ml_models.probabilistic_validator import ProbabilisticValidator
 
 
 class ModelAnalyzer(BaseModule):
@@ -14,15 +13,29 @@ class ModelAnalyzer(BaseModule):
     def run(self):
         #for group in self.transaction.model_data.validation_set:
         #columns = self.transaction.model_data.validation_set[group]
-        validation_samper = Sampler(self.transaction.model_data.train_set,
-        metadata_as_stored=self.transaction.persistent_model_metadata, sampler_mode=SAMPLER_MODES.LEARN)
+        validation_sampler = Sampler(self.transaction.model_data.validation_set,
+        metadata_as_stored=self.transaction.persistent_model_metadata, sampler_mode=SAMPLER_MODES.TRAIN)
 
         '''
         @ <--- field ids is not yet set at this point
-        bm = base_model.BaseModel(validation_samper.getSampleBatch())
+        bm = base_model.BaseModel(validation_sampler.getSampleBatch())
         self.data_model_object = bm.load_from_disk(file_ids=self.transaction.persistent_ml_model_info.fs_file_ids)
         '''
 
-        for batch in validation_samper:
-            ret = self.transaction.data_model_object.forward(batch.getInput(flatten=False))
-            print(ret)
+        probabilistic_validator = ProbabilisticValidator()
+
+        for batch in validation_sampler:
+            input = batch.getInput(flatten=False)
+
+            real_values = []
+            for col in self.transaction.metadata.model_predict_columns:
+                print(input.keys())
+                real_values.append(input[col])
+
+            features = input.drop(self.transaction.metadata.model_predict_columns, axis=1)
+
+            results = self.transaction.data_model_object.forward(features)
+            for i in range(results):
+                print(i)
+                result = results[i]
+                features = fe

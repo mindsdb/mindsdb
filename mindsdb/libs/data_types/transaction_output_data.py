fba8e7041cd1b10015e1b6ce57c7221d38ab2c5a
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-02-28 11:04:00 -0500

    update to output data object to be an iterator

--- mindsdb/libs/data_types/transaction_output_data.py
+++ mindsdb/libs/data_types/transaction_output_data.py
@@ -1,69 +1,30 @@
 from mindsdb.libs.constants.mindsdb import *
 
 from mindsdb.libs.data_types.mindsdb_logger import log
-
+from mindsdb.libs.data_types.transaction_output_row import TransactionOutputRow
 class TransactionOutputData():
 
-    def __init__(self, predicted_columns=[], columns_map = {}):
-        self.data_array = []
-        self.columns = []
-        self.predicted_columns = predicted_columns
-        self.columns_map = columns_map
-
-    def _getOrigColum(self, col):
-
-        for orig_col in self.columns_map:
-            if self.columns_map[orig_col] == col:
-                return orig_col
-
-        return col
-
-    @property
-    def predicted_values(self, as_list=False, add_header = False):
-        """
-        Get an array of dictionaries (unless as_list=True) for predicted values
-        :return: predicted_values
-        """
-        ret = []
-
-        # foreach row in the result extract only the predicted values
-        for row in self.data_array:
-
-            # prepare the result, either a dict or a list
-            if as_list:
-                ret_row = []
-            else:
-                ret_row = {}
-
-            # append predicted values
-            for col in self.predicted_columns:
-                col_index = self.columns.index(col)
-                if as_list:
-                    ret_row += [row[col_index]]
-                else:
-                    ret_row[self._getOrigColum(col)] = row[col_index]
+    def __init__(self, transaction = None, data = {}, evaluations = {}, ):
+        self.data = data
+        self.columns = None
+        self.evaluations = evaluations
+        self.transaction = transaction
 
 
-            # append confidence
-            col_index = self.columns.index(KEY_CONFIDENCE)
-            if as_list: # add confidence if its a dictionary
-                ret_row += row[col_index]
-            else:
-                if len(row) < col_index:
-                    log.warning('Output is smaller than expected, see transaction_output_data.py')
-                    ret_row[KEY_CONFIDENCE] = 0
-                else:
-                    try:
-                        ret_row[KEY_CONFIDENCE] = row[col_index]
-                    except:
-                        ret_row[KEY_CONFIDENCE] = 0
 
-            # append row to result
-            ret += [ret_row]
+    def __iter__(self):
+        self.columns = self.transaction.persistent_model_metadata.columns
+        first_col = self.columns[0]
 
-        # if add_header and as_list True, add the header to the result
-        if as_list and add_header:
-            header = [self._getOrigColum(col) for col in self.predicted_columns] + [KEY_CONFIDENCE]
-            ret = [header] + ret
+        for i, cell in enumerate(self.data[first_col]):
 
-        return ret
+            yield TransactionOutputRow(self, i)
+
+    def __getitem__(self, item):
+
+        return TransactionOutputRow(self, item)
+
+
+    def __str__(self):
+
+        return str(self.data)

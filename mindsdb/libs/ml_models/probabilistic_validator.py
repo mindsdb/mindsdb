9e57e658ac7508e638193e03d9879aed1c5013d7
wangshub <rocksong.hit@gmail.com>
2019-03-01 21:55:44 +0800

    fix: float(None) error

--- mindsdb/libs/ml_models/probabilistic_validator.py
+++ mindsdb/libs/ml_models/probabilistic_validator.py
@@ -92,6 +92,7 @@ class ProbabilisticValidator():
         :param predicted_value: The predicted value/label
         :param histogram: The histogram for the predicted column, which allows us to bucketize the `predicted_value` and `real_value`
         """
+        predicted_value = 0 if predicted_value is None else predicted_value
         predicted_value = predicted_value if self.data_type != DATA_TYPES.NUMERIC else float(predicted_value)
         predicted_value_b = self._get_value_bucket(predicted_value)
         real_value = real_value if self.data_type != DATA_TYPES.NUMERIC else float(real_value)

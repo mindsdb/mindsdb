66656f4b957f0a9778d10a5d47ee4115be868bea
George3d6 <george@cerebralab.com>
2019-03-31 13:11:49 +0000

    finished hypthesis executor

--- mindsdb/libs/model_examination/hypothesis_executor.py
+++ mindsdb/libs/model_examination/hypothesis_executor.py
@@ -9,7 +9,8 @@ class HypothesisExecutor():
     """
 
     def __init__(self):
-        pass
+        self.columnless_predictions = {}
+        self.normal_predictions = None
 
     def pickle(self):
         """
@@ -40,9 +41,42 @@ class HypothesisExecutor():
         score = score/len(input_columns)
         return score
 
-    def run(self, mode, output_columns, input_columns, full_dataset, stats):
-        predictions = self.transaction.model_backend.predict('validate', ignore_columns)
-        normal_accuracy = evaluate_accuracy(predictions, full_dataset, stats, input_columns)
+    def get_column_importance(self, model, output_columns, input_columns, full_dataset, stats):
+        self.normal_predictions = self.transaction.model_backend.predict('validate', ignore_columns)
+        normal_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, input_columns)
 
-        for output_column in output_columns:
-            # See what happens with the accuracy of the outputs
+        column_importance_dict = {}
+
+        for input_column in input_columns:
+            # See what happens with the accuracy of the outputs if only this column is present
+            ignore_columns = [col if col != input_column for col in input_columns]
+            col_only_predictions = model.predict('validate', ignore_columns)
+            col_only_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, input_columns)
+            col_only_normalized_accuracy = col_only_accuracy/normal_accuracy
+
+            # See what happens with the accuracy if all columns but this one are present
+            ignore_columns = [input_column]
+            col_missing_predictions = model.predict('validate', ignore_columns)
+
+            self.columnless_predictions[input_column] = col_missing_predictions
+
+            col_missing_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, input_columns)
+            col_missing_reverse_accuracy = (normal_accuracy - col_missing_accuracy)/normal_accuracy
+
+            column_importance = (col_only_normalized_accuracy + col_missing_reverse_accuracy)/2
+            column_importance_dict[input_column] = column_importance
+
+        return column_importance_dict
+
+    def get_column_influence(self):
+        pass
+
+
+
+
+
+
+
+
+
+#

e6b78c2248f4d17ad78dd31694ab016b6e8f731c
George3d6 <george@cerebralab.com>
2019-04-02 21:12:40 +0000

    a

--- integration_testing/run_a_file.py
+++ integration_testing/run_a_file.py
@@ -1,16 +1,18 @@
 from mindsdb import Predictor
 
 
-mdb = Predictor(name='titanic_model')
+mdb = Predictor(name='co_pollutant')
 
-#mdb.learn(from_data="integration_testing/train.csv", to_predict=['Survived','Pclass'])
+mdb.learn(from_data="AirQualityUCI.csv", to_predict='CO_GT')
 
 print('------------------------------------------------------------Done training------------------------------------------------------------')
 
-#mdb = Predictor(name='titanic_model')
-predicted = mdb.predict(when_data="integration_testing/train.csv")
+predicted = mdb.predict(when_data={
+    'Date':'11/03/2020',
+    'Time':'18.00.00',
+    'NMHC_GT': 1360.0,
+    'AH': 0.655
+})
 print('------------------------------------------------------------Preidiction output------------------------------------------------------------')
 for val in predicted:
     print(val)
-exit()
-print(predicted.predicted_values)

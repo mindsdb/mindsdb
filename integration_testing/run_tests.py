55e1818cb564e7bde8894770edb3cca3fdfbdf5e
George3d6 <george@cerebralab.com>
2019-04-13 20:04:37 +0000

    removed useless imports into __init__

--- integration_testing/run_tests.py
+++ integration_testing/run_tests.py
@@ -370,7 +370,7 @@ def test_multilabel_prediction():
 
 
 setup_testing_logger()
-test_one_label_prediction_wo_strings()
-test_timeseries()
-test_multilabel_prediction()
+#test_one_label_prediction_wo_strings()
+#test_timeseries()
+#test_multilabel_prediction()
 test_one_label_prediction()

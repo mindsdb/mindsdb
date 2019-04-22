df923cb20f6cb0375954e45dc96aa1fd01f16a03
George3d6 <george@cerebralab.com>
2019-04-22 08:29:43 +0000

    fixed NLP example

--- docs/examples/nlp/predict.py
+++ docs/examples/nlp/predict.py
@@ -4,7 +4,6 @@ mdb = Predictor(name='real_estate_desc')
 
 # Here we use the model to make predictions (NOTE: You need to run train.py first)
 result = mdb.predict(
-    predict='number_of_rooms',,
     when={
         "description": """A true gem
  rooms: 2

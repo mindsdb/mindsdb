ab0f0550ac7a5c01f6029748ec5549492d1e5bdf
George3d6 <george@cerebralab.com>
2019-04-22 20:12:50 +0000

    added buckding for low accuracy columns

--- mindsdb/libs/model_examination/column_evaluator.py
+++ mindsdb/libs/model_examination/column_evaluator.py
@@ -1,5 +1,5 @@
-from mindsdb.libs.helpers.general_helpers import evaluate_accuracy
-
+from mindsdb.libs.helpers.general_helpers import evaluate_accuracy, get_value_bucket
+from mindsdb.libs.phases.stats_generator import StatsGenerator
 
 class ColumnEvaluator():
     """
@@ -18,12 +18,29 @@ class ColumnEvaluator():
         normal_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, output_columns)
 
         column_importance_dict = {}
+        col_buckets_stats = {}
 
         for input_column in input_columns:
             # See what happens with the accuracy of the outputs if only this column is present
             ignore_columns = [col for col in input_columns if col != input_column ]
             col_only_predictions = model.predict('validate', ignore_columns)
             col_only_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, output_columns)
+
+            if col_only_accuracy > normal_accuracy*0.75:
+                split_data = {}
+                #columns = [[col, col_ind] for col_ind, col in enumerate(self.transaction.lmd['columns'])]
+                for value in full_dataset[input_column]:
+
+                    if 'percentage_buckets' in stats[input_column]:
+                        bucket = stats[input_column]['percentage_buckets']
+                    else:
+                        bucket = None
+
+                    vb = get_value_bucket(value, bucket, stats[input_column])
+                    split_data[vb] =
+
+                print(f'\n\n\n\n--------------------!!!!!!!!!!!!    {input_column}   !!!!!!!!!!!!--------------------\n\n\n\n')
+
             col_only_normalized_accuracy = col_only_accuracy/normal_accuracy
 
             # See what happens with the accuracy if all columns but this one are present
@@ -33,6 +50,10 @@ class ColumnEvaluator():
             self.columnless_predictions[input_column] = col_missing_predictions
 
             col_missing_accuracy = evaluate_accuracy(self.normal_predictions, full_dataset, stats, output_columns)
+
+            if col_missing_accuracy > normal_accuracy*0.75:
+                print(f'\n\n\n\n--------------------!!!!!!!!!!!!    {input_column}   !!!!!!!!!!!!--------------------\n\n\n\n')
+
             col_missing_reverse_accuracy = (normal_accuracy - col_missing_accuracy)/normal_accuracy
 
             column_importance = (col_only_normalized_accuracy + col_missing_reverse_accuracy)/2

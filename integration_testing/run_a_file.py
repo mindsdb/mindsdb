946d7a697b1d719af21c7e174e9179fd4aece024
George3d6 <george@cerebralab.com>
2019-05-15 17:13:37 +0000

    corrected name

--- integration_testing/run_a_file.py
+++ integration_testing/run_a_file.py
@@ -5,7 +5,7 @@ mdb = Predictor(name='suicide_model')
 mdb.learn(from_data="integration_testing/suicide.csv", to_predict='suicides_no')
 
 # use the model to make predictions
-result = Predictor(name='suicide_rates').predict(when={'country':'Greece','year':1981,'sex':'male','age':'35-54','population':300000})
+result = Predictor(name='suicide_model').predict(when={'country':'Greece','year':1981,'sex':'male','age':'35-54','population':300000})
 
 # you can now print the results
 print(result)

2a5df8c3f08f96a006048255eabd410d5795d71e
George3d6 <george@cerebralab.com>
2019-05-19 18:14:07 +0000

    added use gpu flag

--- mindsdb/libs/backends/ludwig.py
+++ mindsdb/libs/backends/ludwig.py
@@ -379,6 +379,8 @@ class LudwigBackend():
         return model_dir
 
     def get_useable_gpus(self):
+        if self.lmd['use_gpu'] == False:
+            return []
         local_device_protos = device_lib.list_local_devices()
         gpus = [x for x in local_device_protos if x.device_type == 'GPU']
         #bus_ids = [x.locality.bus_id for x in gpus]

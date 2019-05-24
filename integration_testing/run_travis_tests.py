075d8259ebf88c65b69bff882f75ec711d97c45f
George3d6 <george@cerebralab.com>
2019-05-24 07:58:40 +0000

    changed travis test

--- integration_testing/run_travis_tests.py
+++ integration_testing/run_travis_tests.py
@@ -10,6 +10,24 @@ import time
 import mindsdb
 from mindsdb import CONST
 
+#@TODO: Currently we use this isntead of randomly generated data since randomly generated data is not reliable enough
+
+# We tell mindsDB what we want to learn and from what data
+mdb = Predictor(name='home_rentals_price')
+
+mdb.learn(
+    to_predict='rental_price', # the column we want to learn to predict given all the data in the file
+    from_data="https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv"
+# the path to the file where we can learn from, (note: can be url)
+)
+
+prediction = mdb.predict(when={'sqft':300})
+print(prediction[0])
+amd = mdb.get_model_data('home_rentals_price')
+
+
+
+'''
 types_that_work = ['int','float','date','datetime','timestamp','ascii']
 
 logger = None
@@ -124,3 +142,4 @@ def run_tests():
 
 setup_testing_logger()
 run_tests()
+'''
--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from mindsdb import Predictor
 
 # We tell mindsDB what we want to learn and from what data
 mdb = Predictor(name='home_rentals_price')
-'''
+
 mdb.learn(
     to_predict='rental_price', # the column we want to learn to predict given all the data in the file
     from_data="https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv"
 # the path to the file where we can learn from, (note: can be url)
 )
-'''
+
+prediction = mdb.predict(when={'sqft':300})
+print(prediction[0])
 amd = mdb.get_model_data('home_rentals_price')
-print(amd)

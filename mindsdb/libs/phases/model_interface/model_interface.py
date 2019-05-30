e8d46362950b1c063455cffef86ff39e76b08f09
George3d6 <george@cerebralab.com>
2019-05-30 18:56:12 +0000

    finished model interface phase

--- mindsdb/libs/phases/model_interface/model_interface.py
+++ mindsdb/libs/phases/model_interface/model_interface.py
@@ -1,14 +1,16 @@
 from mindsdb.libs.backends.ludwig import LudwigBackend
 from mindsdb.libs.phases.base_module import BaseModule
 
+import datetime
+
 
 class ModelInterface(BaseModule):
 
     def run(self):
-        if self.lmd['model_backend'] == 'ludwig':
-            self.lmd['is_active'] = True
-            self.model_backend = LudwigBackend(self)
-            self.model_backend.train()
-            self.lmd['is_active'] = False
+        if self.transaction.lmd['model_backend'] == 'ludwig':
+            self.transaction.lmd['is_active'] = True
+            self.transaction.model_backend = LudwigBackend(self.transaction)
+            self.transaction.model_backend.train()
+            self.transaction.lmd['is_active'] = False
 
-        self.lmd['train_end_at'] = str(datetime.datetime.now())
+        self.transaction.lmd['train_end_at'] = str(datetime.datetime.now())

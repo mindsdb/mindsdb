47c42007735f0af968718dc7a6589c00d8f136c9
George3d6 <george@cerebralab.com>
2019-06-07 08:10:23 +0000

    added basic lightwood interface

--- mindsdb/libs/backends/lightwood.py
+++ mindsdb/libs/backends/lightwood.py
@@ -5,4 +5,8 @@ class LightwoodBackend():
     def __init__(self, transaction):
         self.transaction = transaction
 
-    
+    def train(self):
+        pass
+
+    def predict(self, mode='predict', ignore_columns=[]):
+        pass

22ed8027d35a63aa52964350004b52d38dab7cf5
George3d6 <george@cerebralab.com>
2019-06-07 14:32:09 +0000

    format change to transactio_data

--- mindsdb/libs/data_types/transaction_data.py
+++ mindsdb/libs/data_types/transaction_data.py
@@ -10,7 +10,6 @@ class TransactionData():
         self.train_indexes = {}
         self.test_indexes = {}
         self.validation_indexes = {}
-
         self.all_indexes = {}
 
         self.columns = []

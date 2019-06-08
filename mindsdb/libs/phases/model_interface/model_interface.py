d515614e4b1f7a5ff4f02618fc917102581b660e
George3d6 <george@cerebralab.com>
2019-06-08 08:08:26 +0000

    simplified model interface

--- mindsdb/libs/phases/model_interface/model_interface.py
+++ mindsdb/libs/phases/model_interface/model_interface.py
@@ -12,22 +12,12 @@ class ModelInterface(BaseModule):
 
     def run(self, mode='train'):
         if self.transaction.lmd['model_backend'] == 'ludwig':
-            if mode == 'train':
-                self.transaction.lmd['is_active'] = True
-                self.transaction.model_backend = LudwigBackend(self.transaction)
-                self.transaction.model_backend.train()
-                self.transaction.lmd['is_active'] = False
-                self.transaction.lmd['train_end_at'] = str(datetime.datetime.now())
-            elif mode == 'predict':
-                self.transaction.model_backend = LudwigBackend(self.transaction)
-                self.transaction.hmd['predictions'] = self.transaction.model_backend.predict()
-
+            self.transaction.model_backend = LudwigBackend(self.transaction)
         if self.transaction.lmd['model_backend'] == 'lightwood':
-            if mode == 'train':
-                self.transaction.lmd['is_active'] = True
-                self.transaction.model_backend = LightwoodBackend(self.transaction)
-                self.transaction.model_backend.train()
-                self.transaction.lmd['is_active'] = False
-                self.transaction.lmd['train_end_at'] = str(datetime.datetime.now())
-            elif mode == 'predict':
-                raise Exception('Predict not implemented for lightwood')
+            self.transaction.model_backend = LightwoodBackend(self.transaction)
+
+        if mode == 'train':
+            self.transaction.model_backend.train()
+            self.transaction.lmd['train_end_at'] = str(datetime.datetime.now())
+        elif mode == 'predict':
+            self.transaction.hmd['predictions'] = self.transaction.model_backend.predict()

241ab2818eab616f40f120feec946ad593caabe5
George3d6 <george@cerebralab.com>
2019-06-10 18:27:48 +0000

    corrected name

--- mindsdb/libs/backends/lightwood.py
+++ mindsdb/libs/backends/lightwood.py
@@ -30,9 +30,9 @@ class LightwoodBackend():
             if data_type in (DATA_TYPES.NUMERIC):
                 lightwood_data_type = 'numeric'
                 try:
-                    self.transaction.data_frame.data_frame[col_name] = self.transaction.data_frame.train_df[col_name].apply(int)
+                    self.transaction.input_data.data_frame[col_name] = self.transaction.input_data.train_df[col_name].apply(int)
                 except:
-                    self.transaction.data_frame.data_frame[col_name].apply(lambda x: float(x.replace(',','.')))
+                    self.transaction.input_data.data_frame[col_name].apply(lambda x: float(x.replace(',','.')))
 
             elif data_type in (DATA_TYPES.CATEGORICAL):
                 lightwood_data_type = 'categorical'

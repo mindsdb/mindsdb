e40c7611b80b42222fc623eda89bf242a2bbf14b
George3d6 <george@cerebralab.com>
2019-06-12 13:58:53 +0000

    Changed the ways scores are compared to give warnings in the StatsGenerator

--- mindsdb/libs/phases/stats_generator/stats_generator.py
+++ mindsdb/libs/phases/stats_generator/stats_generator.py
@@ -707,7 +707,7 @@ class StatsGenerator(BaseModule):
             if col_stats['quality_score'] < 6:
                 # Some scores are not that useful on their own, so we should only warn users about them if overall quality is bad.
                 self.log.warning('Column "{}" is considered of low quality, the scores that influenced this decission will be listed bellow')
-                if 'duplicates_score' in col_stats and col_stats['duplicates_score'] > 0.5:
+                if 'duplicates_score' in col_stats and col_stats['duplicates_score'] < 6:
                     duplicates_percentage = col_stats['duplicates_percentage']
                     w = f'{duplicates_percentage}% of the values in column {col_name} seem to be repeated, this might indicate your data is of poor quality.'
                     self.log.warning(w)
@@ -718,21 +718,21 @@ class StatsGenerator(BaseModule):
                 col_stats['duplicates_score_warning'] = None
 
             #Compound scores
-            if col_stats['consistency_score'] > 0.25:
+            if col_stats['consistency_score'] < 3:
                 w = f'The values in column {col_name} rate poorly in terms of consistency. This means the data has too many empty values, values with a hard to determine type and duplicate values. Please see the detailed logs bellow for more info'
                 self.log.warning(w)
                 col_stats['consistency_score_warning'] = w
             else:
                 col_stats['consistency_score_warning'] = None
 
-            if col_stats['redundancy_score'] > 0.45:
+            if col_stats['redundancy_score'] < 5:
                 w = f'The data in the column {col_name} is likely somewhat redundant, any insight it can give us can already by deduced from your other columns. Please see the detailed logs bellow for more info'
                 self.log.warning(w)
                 col_stats['redundancy_score_warning'] = w
             else:
                 col_stats['redundancy_score_warning'] = None
 
-            if col_stats['variability_score'] > 0.5:
+            if col_stats['variability_score'] < 6:
                 w = f'The data in the column {col_name} seems to have too contain too much noise/randomness based on the value variability. That is too say, the data is too unevenly distributed and has too many outliers. Please see the detailed logs bellow for more info.'
                 self.log.warning(w)
                 col_stats['variability_score_warning'] = w
@@ -740,7 +740,7 @@ class StatsGenerator(BaseModule):
                 col_stats['variability_score_warning'] = None
 
             # Some scores are meaningful on their own, and the user should be warnned if they fall bellow a certain threshold
-            if col_stats['empty_cells_score'] > 0.2:
+            if col_stats['empty_cells_score'] < 3:
                 empty_cells_percentage = col_stats['empty_percentage']
                 w = f'{empty_cells_percentage}% of the values in column {col_name} are empty, this might indicate your data is of poor quality.'
                 self.log.warning(w)
@@ -748,7 +748,7 @@ class StatsGenerator(BaseModule):
             else:
                 col_stats['empty_cells_score_warning'] = None
 
-            if col_stats['data_type_distribution_score'] > 0.2:
+            if col_stats['data_type_distribution_score'] < 3:
                 #self.log.infoChart(stats[col_name]['data_type_dist'], type='list', uid='Dubious Data Type Distribution for column "{}"'.format(col_name))
                 percentage_of_data_not_of_principal_type = col_stats['data_type_distribution_score'] * 100
                 principal_data_type = col_stats['data_type']
@@ -758,7 +758,7 @@ class StatsGenerator(BaseModule):
             else:
                 col_stats['data_type_distribution_score_warning'] = None
 
-            if 'z_test_based_outlier_score' in col_stats and col_stats['z_test_based_outlier_score'] > 0.3:
+            if 'z_test_based_outlier_score' in col_stats and col_stats['z_test_based_outlier_score'] < 4:
                 percentage_of_outliers = col_stats['z_test_based_outlier_score']*100
                 w = f"""Column {col_name} has a very high amount of outliers, {percentage_of_outliers}% of your data is more than 3 standard deviations away from the mean, this means there might
                 be too much randomness in this column for us to make an accurate prediction based on it."""
@@ -767,7 +767,7 @@ class StatsGenerator(BaseModule):
             else:
                 col_stats['z_test_based_outlier_score_warning'] = None
 
-            if 'lof_based_outlier_score' in col_stats and col_stats['lof_based_outlier_score'] > 0.3:
+            if 'lof_based_outlier_score' in col_stats and col_stats['lof_based_outlier_score'] < 4:
                 percentage_of_outliers = col_stats['percentage_of_log_based_outliers']
                 w = f"""Column {col_name} has a very high amount of outliers, {percentage_of_outliers}% of your data doesn't fit closely in any cluster using the KNN algorithm (20n) to cluster your data, this means there might
                 be too much randomness in this column for us to make an accurate prediction based on it."""
@@ -776,7 +776,7 @@ class StatsGenerator(BaseModule):
             else:
                 col_stats['lof_based_outlier_score_warning'] = None
 
-            if col_stats['value_distribution_score'] > 0.8:
+            if col_stats['value_distribution_score'] < 9:
                 max_probability_key = col_stats['max_probability_key']
                 w = f"""Column {col_name} is very biased towards the value {max_probability_key}, please make sure that the data in this column is correct !"""
                 self.log.warning(w)
@@ -784,7 +784,7 @@ class StatsGenerator(BaseModule):
             else:
                 col_stats['value_distribution_score_warning'] = None
 
-            if col_stats['similarity_score'] > 0.5:
+            if col_stats['similarity_score'] < 6:
                 similar_percentage = col_stats['similarity_score'] * 100
                 similar_col_name = col_stats['most_similar_column_name']
                 w = f'Column {col_name} and {similar_col_name} are {similar_percentage}% the same, please make sure these represent two distinct features of your data !'

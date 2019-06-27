d78c13f068d19e84c3175327bbdd4f91fdb76a5a
George3d6 <george@cerebralab.com>
2019-06-27 16:24:55 +0000

    feeding input data to _aply_to_all_data

--- mindsdb/libs/phases/data_transformer/data_transformer.py
+++ mindsdb/libs/phases/data_transformer/data_transformer.py
@@ -48,7 +48,8 @@ class DataTransformer(BaseModule):
         # @TODO Decide if we ever need/want the milliseconds
         return dt.strftime('%y-%m-%d %H:%M:%S')
 
-    def _aply_to_all_data(self, column, func):
+    @staticmethod
+    def _aply_to_all_data(input_data, column, func):
         input_data.data_frame[column] = input_data.data_frame[column].apply(func)
         input_data.train_df[column] = input_data.train_df[column].apply(func)
         input_data.test_df[column] = input_data.test_df[column].apply(func)
@@ -63,17 +64,17 @@ class DataTransformer(BaseModule):
             data_stype = self.transaction.lmd['column_stats'][column]['data_subtype']
 
             if data_type == DATA_TYPES.NUMERIC:
-                self._aply_to_all_data(column, clean_float)
+                self._aply_to_all_data(input_data, column, clean_float)
 
                 if data_stype == DATA_SUBTYPES.INT:
-                    self._aply_to_all_data(column, DataTransformer._try_round)
+                    self._aply_to_all_data(input_data, column, DataTransformer._try_round)
 
             if data_type == DATA_TYPES.DATE:
                 if data_stype == DATA_SUBTYPES.DATE:
-                    self._aply_to_all_data(column, self._standardize_date)
+                    self._aply_to_all_data(input_data, column, self._standardize_date)
 
                 elif data_subtype == DATA_SUBTYPES.TIMESTAMP:
-                    self._aply_to_all_data(column, self._standardize_datetime)
+                    self._aply_to_all_data(input_data, column, self._standardize_datetime)
 
         # Un-bias dataset for training
         for colum in self.transaction.lmd['predict_columns']:

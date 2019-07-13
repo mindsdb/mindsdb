762d468cda2a5552e5031ff3216a80e5706cffd8
George3d6 <george@cerebralab.com>
2019-07-13 06:58:14 +0000

    removed all useless and outdate files and tests from integration testing

--- tests/integration_tests/ins_train_indep_test/main.py
+++
@@ -1,7 +0,0 @@
-from mindsdb import Predictor
-
-mindsDb = Predictor("InsuranceModel")
-mindsDb.learn(
-    from_data="integration_testing/ins_train_indep_test/insu_train_indep_dep.csv",
-    to_predict='PolicyStatus'
-)

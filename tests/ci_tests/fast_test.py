ae130c1e918d6358dcf16b280452972145bc81be
George3d6 <george@cerebralab.com>
2019-07-13 07:17:58 +0000

    finished the full test suite, added debugging printing

--- tests/ci_tests/fast_test.py
+++ tests/ci_tests/fast_test.py
@@ -1,4 +1,6 @@
 from tests import basic_test
 
 
+# Test with a few basic options
 basic_test(backend='ludwig',use_gpu=False,ignore_columns=[])
+print('\n\n=============[Success]==============\nFinished running quick test !\n=============[Success]==============\n\n')

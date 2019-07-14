4cb8ab15acc31b1f078958a459428c5cc2f9ea78
George3d6 <george@cerebralab.com>
2019-07-14 09:12:24 +0000

    fixed windows dependency

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if os == 'Windows':
 
     requirements = remove_requirement(requirements, 'lightwood')
     requirements.append('lightwood == 0.7.1')
-    dependency_links.append('https://github.com/mindsdb/lightwood.git@ci_testing#egg=lightwood-0.7.1')
+    dependency_links.append('https://github.com/mindsdb/lightwood/tarball/ci_testing#egg=lightwood-0.7.1')
 
 setuptools.setup(
     name=about['__title__'],

a53e9cdedfe5219e29e9b7183a2ef18eaf7a457c
George3d6 <george@cerebralab.com>
2019-07-15 06:34:05 +0000

    changed order for tensorflow estimator dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ if os == 'Linux':
 # OSX specific requirements
 if os == 'Darwin':
     requirements = requirements
-    requirements.append('tensorflow-estimator >= 1.13.0')
 
 # Windows specific requirements
 if os == 'Windows':

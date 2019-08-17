0717a7c0147b36cf834bccd77f1df58ead957737
George3d6 <george@cerebralab.com>
2019-08-17 02:08:28 +0000

    better explaination

--- mindsdb/libs/data_types/transaction_output_row.py
+++ mindsdb/libs/data_types/transaction_output_row.py
@@ -22,7 +22,6 @@ class TransactionOutputRow:
             answers[pred_col] = []
 
             prediction_row = {col: self.data[col][self.row_index] for col in list(self.data.keys())}
-            explaination = explain_prediction(self.transaction_output.transaction.lmd, prediction_row)
 
             evaluation = self.evaluations[pred_col][self.row_index]
             clusters = evaluation.explain(self.transaction_output.transaction.lmd['column_stats'][pred_col])
@@ -53,6 +52,8 @@ class TransactionOutputRow:
                         range_end_start = round(range_end_start,1)
                     elif range_end_start > pow(10,-3):
                         range_end_start = round(range_end_start,6)
+
+                    explaination = explain_prediction(self.transaction_output.transaction.lmd, prediction_row, cluster['confidence'], pred_col)
 
                     answers[pred_col].append({
                         'value': predicted_value,

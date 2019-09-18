9d1f6532707a386d577676775efc0f3d917af6ed
George3d6 <george@cerebralab.com>
2019-09-18 09:38:31 +0000

    Added test for custom model backend

---
+++ tests/functional_testing/custom_model.py
@@ -0,0 +1,22 @@
+from mindsdb import Predictor
+import pandas as pd
+
+
+class CustomDTModel():
+    def __init__(self, transaction):
+        self.transaction = transaction
+        self.predict_columns = self.transaction.lmd['predict_columns']
+        self.train_df = self.transaction.input_data.train_df
+        self.test_dt = train_df = self.transaction.input_data.test_df
+
+    def train(self):
+        model.fit(train_df, train_df[predict_columns])
+        pass
+
+    def predict(self, mode='predict', ignore_columns=[]):
+        pd.dataFrame()
+        pass
+
+
+predictor = Predictor(name='custom_model_test_predictor')
+predictor.learn(to_predict='rental_price',from_data="https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv",backend=CustomDTModel)

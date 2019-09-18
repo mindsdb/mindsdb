9b035cff88fd2ca6f90879508a543465a0f25831
George3d6 <george@cerebralab.com>
2019-09-18 09:57:30 +0000

    added label encoders to the test DT model

--- tests/functional_testing/custom_model.py
+++ tests/functional_testing/custom_model.py
@@ -1,11 +1,13 @@
 from mindsdb import Predictor
 import pandas as pd
 from sklearn import tree
+from sklearn import preprocessing
 
 
 class CustomDTModel():
     def __init__(self):
         self.clf = tree.DecisionTreeClassifier()
+        le = preprocessing.LabelEncoder()
 
     def set_transaction(self, transaction):
         self.transaction = transaction
@@ -14,8 +16,22 @@ class CustomDTModel():
         self.train_df = self.transaction.input_data.train_df
         self.test_dt = train_df = self.transaction.input_data.test_df
 
+
     def train(self):
-        self.clf.fit(self.train_df[self.input_columns], self.train_df[self.output_columns])
+        self.le_arr = {}
+        for col in [*self.output_columns, *self.input_columns]:
+            self.le_arr[col] = preprocessing.LabelEncoder()
+            self.le_arr[col].fit(self.transaction.input_data.data_frame[col])
+
+        X = []
+        for col in self.input_columns:
+            X.append(self.le_arr[col].transform(self.transaction.input_data.train_df[col]))
+
+        Y = []
+        for col in self.output_columns:
+            Y.append(self.le_arr[col].transform(self.transaction.input_data.train_df[col]))
+
+        self.clf.fit(X, Y)
 
     def predict(self, mode='predict', ignore_columns=[]):
         pd.dataFrame()

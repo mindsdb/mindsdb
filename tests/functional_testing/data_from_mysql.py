d695ee0e22af0d19d40aabda8c8fd020b84f464e
George3d6 <george@cerebralab.com>
2019-10-04 12:52:26 +0000

    added full testing for mysql data source

--- tests/functional_testing/data_from_mysql.py
+++ tests/functional_testing/data_from_mysql.py
@@ -10,11 +10,12 @@ cur = con.cursor()
 
 cur.execute('DROP TABLE IF EXISTS test_mindsdb')
 cur.execute('CREATE TABLE test_mindsdb(col_1 Text, col_2 BIGINT, col_3 BOOL)')
-for i in range(0,200):
+for i in range(0,5000):
     cur.execute(f'INSERT INTO test_mindsdb VALUES ("This is tring number {i}", {i}, {i % 2 == 0})')
 con.commit()
 con.close()
 
 mdb = Predictor(name='analyse_dataset_test_predictor')
 mysql_ds = MySqlDS(query="SELECT * FROM test_mindsdb")
+assert(len(mysql_ds._df) == 5000)
 mdb.analyse_dataset(from_data=mysql_ds)

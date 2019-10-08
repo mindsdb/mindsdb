9981a167b4b27f2631c1a764242751cc691e9b31
George3d6 <george@cerebralab.com>
2019-10-08 08:40:34 +0000

    made doc build conditional

--- test.py
+++ test.py
@@ -4,17 +4,16 @@ import sys
 
 mdb = Predictor(name='sensor123')
 
-mdb.learn(to_predict='output',from_data="https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/sensor_data.csv",use_gpu=False,stop_training_in_x_seconds=40)
+mdb.learn(to_predict='rental_price',from_data="https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/home_rentals.csv",use_gpu=True,stop_training_in_x_seconds=15)
 
-p_arr = mdb.predict(when_data='https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/sensor_data.csv')
+p_arr = mdb.predict(when_data='https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/home_rentals.csv')
 
-pdct = mdb.predict(when={'sensor 1': 0.5, 'sensor 2': 2, 'sensor 3': 0, 'sensor4': 5})
-print(pdct)
 
 for p in p_arr:
     exp_s = p.epitomize()
-    exp = p.explain()
+    #exp = p.explain()
 
-    if len(exp['output']) > 0:
-        print(exp)
-        print(exp_s)
+    #print(exp)
+    print(exp_s)
+
+print(mdb.get_model_data('sensor123'))

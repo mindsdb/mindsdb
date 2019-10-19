3b168cf0f13a1da8b48824efba1161bf96be713d
George3d6 <george@cerebralab.com>
2019-10-19 22:26:19 +0000

    everything finally  works !!!

--- tests/accuracy_benchmarking/benchmark.py
+++ tests/accuracy_benchmarking/benchmark.py
@@ -37,10 +37,10 @@ def setup_logger():
     return logger
 
 
-def main():
+def run_benchmarks():
     logger = setup_logger()
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], 'rb') as fp:
+    if len(sys.argv) > 2:
+        with open(sys.argv[2], 'rb') as fp:
             cfg = json.load(fp)
     else:
         with open('config.json', 'rb') as fp:
@@ -74,8 +74,8 @@ def main():
     except:
         pass
 
-    TESTS = ['default_of_credit', 'cancer50', 'pulsar_stars', 'imdb_movie_review'] # cifar_100
-
+    #TESTS = ['default_of_credit', 'cancer50', 'pulsar_stars', 'imdb_movie_review'] # cifar_100
+    TESTS = ['default_of_credit']
     test_data_arr = []
     for test_name in TESTS:
         '''
@@ -131,4 +131,64 @@ def main():
     con.commit()
     con.close()
 
-main()
+
+def compare():
+    logger = setup_logger()
+    if len(sys.argv) > 2:
+        with open(sys.argv[2], 'rb') as fp:
+            cfg = json.load(fp)
+    else:
+        with open('config.json', 'rb') as fp:
+            cfg = json.load(fp)
+
+    con = MySQLdb.connect(cfg['mysql']['host'], cfg['mysql']['user'], cfg['mysql']['password'], cfg['mysql']['database'])
+    cur = con.cursor()
+
+    if len(sys.argv) > 3:
+        batch_id = sys.argv[3]
+    # Go with the latest batch by default
+    else:
+        cur.execute('SELECT batch_id FROM mindsdb_accuracy.tests WHERE batch_started=(SELECT max(batch_started) FROM mindsdb_accuracy.tests)')
+        batch_id = cur.fetchall()[0][0]
+
+    cur.execute('SELECT test_name, accuracy_function, accuracy, runtime, mindsdb_version, lightwood_version, ludwig_version, backend FROM mindsdb_accuracy.tests WHERE batch_id=%s', [batch_id])
+    batch_tests_arr = cur.fetchall()
+
+    for entity in batch_tests_arr:
+        test_name = entity[0]
+        accuracy_function = entity[1]
+        accuracy = entity[2]
+        runtime = entity[3]
+
+        mindsdb_version = entity[4]
+        lightwood_version = entity[5]
+        ludwig_version = entity[6]
+        backend = entity[7]
+
+        logger.info(f'\nRunning checks for test {test_name} in batch {batch_id}, the test took {runtime} seconds and had an accuracy of {accuracy} using {accuracy_function} and training on backend {backend}\nSystem info when ran: (mindsdb_version: {mindsdb_version}, lightwood_version: {lightwood_version}, ludwig_version: {ludwig_version})\n')
+
+        cur.execute('SELECT accuracy, batch_id, batch_started, runtime, mindsdb_version, lightwood_version, ludwig_version, backend FROM mindsdb_accuracy.tests WHERE test_name=%s AND accuracy_function=%s AND accuracy > %s', [test_name,accuracy_function,accuracy])
+        better_tests = cur.fetchall()
+
+        for row in better_tests:
+            old_accuracy = row[0]
+            old_batch_id = row[1]
+            old_batch_started = row[2]
+            old_runtime = row[3]
+
+            old_mindsdb_version = entity[4]
+            old_lightwood_version = entity[5]
+            old_ludwig_version = entity[6]
+            old_backend = entity[7]
+
+            logger.warning(f'There was a previous test for {test_name} which yielded better results than the current one. The results were:  \n * Accuracy: {old_accuracy} \n * Batch id: {old_batch_id} \n * Batch started: {old_batch_started} \n * Runtime: {old_runtime} \n * Backend: {old_backend} \n * System info when ran: (mindsdb_version: {old_mindsdb_version}, lightwood_version: {old_lightwood_version}, ludwig_version: {old_ludwig_version})')
+
+    con.commit()
+    con.close()
+
+
+if sys.argv[1] == 'bench':
+    run_benchmarks()
+
+if sys.argv[1] == 'compare':
+    compare()

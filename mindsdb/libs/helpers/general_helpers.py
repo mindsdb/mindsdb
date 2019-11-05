115f5fcd522485af9ea32a97db1f0b496db342dd
George3d6 <george@cerebralab.com>
2019-11-05 13:51:20 +0000

    Removed unused helpers

--- mindsdb/libs/helpers/general_helpers.py
+++ mindsdb/libs/helpers/general_helpers.py
@@ -20,13 +20,6 @@ import imagehash
 from PIL import Image
 
 
-def get_key_for_val(key, dict_map):
-    for orig_col in dict_map:
-        if dict_map[orig_col] == key:
-            return orig_col
-
-    return key
-
 def convert_cammelcase_to_snake_string(cammel_string):
     """
     Converts snake string to cammelcase
@@ -38,48 +31,6 @@ def convert_cammelcase_to_snake_string(cammel_string):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', cammel_string)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-
-def get_label_index_for_value(value, labels):
-    """
-    This will define what index interval does a value belong to given a list of labels
-
-    :param value: value to evaluate
-    :param labels: the labels to compare against
-    :return: an index value
-    """
-    if  value is None:
-        return 0 # Todo: Add another index for None values
-
-    if type(value) == type(''):
-        try:
-            index = labels.index(value)
-        except:
-            index = 0
-    else:
-        for index, compare_to in enumerate(labels):
-            if value < compare_to:
-                index = index
-                break
-
-    return index
-
-def convert_snake_to_cammelcase_string(snake_str, first_lower = False):
-    """
-    Converts snake to cammelcase
-
-    :param snake_str: as described
-    :param first_lower: if you want to start the string with lowercase as_said -> asSaid
-    :return: cammelcase string
-    """
-
-    components = snake_str.split('_')
-
-    if first_lower == True:
-        # We capitalize the first letter of each component except the first one
-        # with the 'title' method and join them together.
-        return components[0] + ''.join(x.title() for x in components[1:])
-    else:
-        return ''.join(x.title() for x in components)
 
 def pickle_obj(object_to_pickle):
     """

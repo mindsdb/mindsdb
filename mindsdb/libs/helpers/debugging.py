b0f88c52ab208bbd342e2ff34f4e056dda7972f3
George3d6 <george@cerebralab.com>
2019-11-20 13:03:05 +0000

    added useful function for unwinding dictionaries

---
+++ mindsdb/libs/helpers/debugging.py
@@ -0,0 +1,17 @@
+def print_key_and_type(d, nl=''):
+    for k in d:
+        print(nl + str(k))
+    if type(d[k]) == dict:
+        print_key_and_type(d[k],nl + '  ')
+    if type(d[k]) == list:
+        try:
+            if type(d[k][0]) == dict:
+                print('[\n')
+                print_key_and_type(d[k][0],nl + '  ')
+                print('\n]')
+            else:
+                print(nl + '[' + str(type(d[k][0])) + ']')
+        except:
+            print('Empty list for key: ' + k)
+    else:
+        print(nl + str(type(d[k])))

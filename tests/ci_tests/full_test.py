58d5a31465ad5fc94c3bbf255acf4c5a684591df
George3d6 <george@cerebralab.com>
2019-11-26 19:40:17 +0000

    Reverted tests back to non-unit-test version

--- tests/ci_tests/full_test.py
+++ tests/ci_tests/full_test.py
@@ -2,18 +2,20 @@ from tests import basic_test
 import torch
 import mindsdb
 
-use_gpu_settings = []
-if torch.cuda.is_available():
-    use_gpu_settings.append(True)
 
-use_gpu_settings.append(False)
+if __name__ == "__main__":
+    use_gpu_settings = []
+    if torch.cuda.is_available():
+        use_gpu_settings.append(True)
 
-# Cycle through a few options:
-for backend in ['lightwood','ludwig']:
-    for use_gpu in use_gpu_settings:
-        print(f'use_gpu is set to {use_gpu}, backend is set to {backend}')
-        basic_test(backend=backend,use_gpu=use_gpu,ignore_columns=[], IS_CI_TEST=True)
+    use_gpu_settings.append(False)
 
-# Try ignoring some columns and running only the stats generator
-basic_test(backend='lightwood',use_gpu=use_gpu_settings[0],ignore_columns=['days_on_market','number_of_bathrooms'],run_extra=True, IS_CI_TEST=True)
-print('\n\n=============[Success]==============\n     Finished running full test suite !\n=============[Success]==============\n\n')
+    # Cycle through a few options:
+    for backend in ['lightwood','ludwig']:
+        for use_gpu in use_gpu_settings:
+            print(f'use_gpu is set to {use_gpu}, backend is set to {backend}')
+            basic_test(backend=backend,use_gpu=use_gpu,ignore_columns=[], IS_CI_TEST=True)
+
+    # Try ignoring some columns and running only the stats generator
+    basic_test(backend='lightwood',use_gpu=use_gpu_settings[0],ignore_columns=['days_on_market','number_of_bathrooms'],run_extra=True, IS_CI_TEST=True)
+    print('\n\n=============[Success]==============\n     Finished running full test suite !\n=============[Success]==============\n\n')

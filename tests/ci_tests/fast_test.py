22ea88de9d5655dd8fa8915dc03b72203b6f6c76
George3d6 <george@cerebralab.com>
2019-11-26 17:46:49 +0000

    updated window test

--- tests/ci_tests/fast_test.py
+++ tests/ci_tests/fast_test.py
@@ -2,5 +2,7 @@ from tests import basic_test
 import torch
 
 # Test with a few basic options
-basic_test(backend='lightwood',use_gpu=torch.cuda.is_available(),ignore_columns=[], IS_CI_TEST=True)
-print('\n\n=============[Success]==============\n     Finished running quick test !\n=============[Success]==============\n\n')
+
+if __name__ == "__main__":
+    basic_test(backend='lightwood',use_gpu=torch.cuda.is_available(),ignore_columns=[], IS_CI_TEST=True)
+    print('\n\n=============[Success]==============\n     Finished running quick test !\n=============[Success]==============\n\n')

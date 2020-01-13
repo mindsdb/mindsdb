0629480b4d7d15268d55a6e15a3110b9a2c3de46
George3d6 <george@cerebralab.com>
2020-01-13 02:48:26 +0200

    simplified logic and made logging more user friendly

--- mindsdb/config/helpers.py
+++ mindsdb/config/helpers.py
@@ -15,24 +15,30 @@ def if_env_else(env_var, else_value):
 
 
 def create_directory(path):
-    if not os.path.exists(path):
-        try:
-            print(f'{path} does not exist, creating it now')
-            path = Path(path)
-            path.mkdir(mode=0o777, exist_ok=True, parents=True)
-        except:
-            print(traceback.format_exc())
-            print(f'MindsDB storage directory: {path} does not exist and could not be created')
+    print(f'{path} does not exist, creating it now')
+    path = Path(path)
+    path.mkdir(mode=0o777, exist_ok=True, parents=True)
+
 
 def get_and_create_default_storage_path():
+    print( os.path.abspath(inspect.getfile(inspect.currentframe())) )
     mindsdb_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), '/../../'))
     version_path = __version__.replace('.', '_')
     path = os.path.abspath(f'{mindsdb_path}/mindsdb_storage/{version_path}')
 
-    create_directory(path)
-    if not os.access(path, os.W_OK):
+
+    try:
+        create_directory(path)
+        correct_permissions = os.access(path, os.W_OK)
+    except:
+        correct_permissions = False
+
+    if not correct_permissions:
         home = os.path.expanduser('~')
         path = os.path.join(home, '.mindsdb_storage')
-        create_directory(path)
+        try:
+            create_directory(path)
+        except:
+            print(f'MindsDB storage directory: {path} does not exist and could not be created')
 
     return path

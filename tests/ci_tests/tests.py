36f9a1f4cf118c1ebe24205a8ce7d6d37002391d
George3d6 <george@cerebralab.com>
2020-01-15 01:29:36 +0200

    silicing error during validation

--- tests/ci_tests/tests.py
+++ tests/ci_tests/tests.py
@@ -66,7 +66,7 @@ def basic_test(backend='lightwood',use_gpu=True,ignore_columns=[], run_extra=Fal
     # Create & Learn
     to_predict = 'rental_price'
     mdb = mindsdb.Predictor(name='home_rentals_price')
-    mdb.learn(to_predict=to_predict,from_data="https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv",backend=backend, stop_training_in_x_seconds=20,use_gpu=use_gpu)
+    mdb.learn(to_predict=to_predict,from_data="https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv",backend=backend, stop_training_in_x_seconds=15,use_gpu=use_gpu)
 
     # Reload & Predict
     model_name = 'home_rentals_price'

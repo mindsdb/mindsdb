03d2fa911594a4aa310e6400aefa3e3c0c5aa3ca
George3d6 <george@cerebralab.com>
2020-01-28 23:55:52 +0200

    removed publishing to pip from install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 rm -rf build
 rm -rf dist
 rm -rf mindsdb.egg-info
-# python3 setup.py --help-commands
 
 echo "mode (prod/dev)?"
 
@@ -12,19 +11,7 @@ if [ "$mode" = "prod" ]; then
     python3 setup.py develop --uninstall
     python3 setup.py clean
     python3 setup.py build
-    #python3 setup.py install
-    #python3 setup.py bdist_egg -p win32
     python3 setup.py sdist
-
-    echo "Do you want to publish this version (yes/no)?"
-
-    read publish
-
-    if [ "$publish" = "yes" ]; then
-        echo "Publishing mindsdb to Pypi"
-        python3 -m twine upload dist/*
-    fi
-
 
 fi
 

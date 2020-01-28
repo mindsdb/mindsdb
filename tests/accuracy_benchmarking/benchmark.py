f5cf5b4d5d2a92c298ebd428541a90b12ea13949
George3d6 <george@cerebralab.com>
2020-01-28 20:12:28 +0200

    stashing changes

--- tests/accuracy_benchmarking/benchmark.py
+++ tests/accuracy_benchmarking/benchmark.py
@@ -48,6 +48,8 @@ def run_benchmarks():
     TESTS = ['default_of_credit', 'cancer50', 'pulsar_stars', 'cifar_100', 'imdb_movie_review']
     #TESTS = ['default_of_credit', 'cancer50', 'pulsar_stars']
 
+    os.system('git clone https://github.com/mindsdb/mindsdb-examples tmp_downloads')
+
     test_data_arr = []
     for test_name in TESTS:
         '''
@@ -60,9 +62,7 @@ def run_benchmarks():
         '''
         logger.debug(f'\n\n=================================\nRunning test: {test_name}\n=================================\n\n')
 
-        os.chdir(f'tmp_downloads')
-        os.system('git clone https://github.com/mindsdb/mindsdb-examples')
-        os.chdir(f'mindsdb-examples/{test_name}')
+        os.chdir(f'tmp_downloads/{test_name}')
 
         run_test = importlib.import_module(f'tmp_downloads.{test_name}.mindsdb_acc').run
 

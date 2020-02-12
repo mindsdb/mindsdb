87f7b8d57581b3624b0fff9e3c1952f8f2902780
George3d6 <george@cerebralab.com>
2020-02-12 14:38:00 +0200

    updated tests

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import time
 
 mdb = Predictor(name='test_predictor')
 #'rental_price',
-#mdb.learn(to_predict=['neighborhood'],from_data="https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/home_rentals.csv",use_gpu=False,stop_training_in_x_seconds=3000, backend='lightwood', unstable_parameters_dict={'use_selfaware_model':True})
+mdb.learn(to_predict=['neighborhood'],from_data="https://mindsdb-example-data.s3.eu-west-2.amazonaws.com/home_rentals.csv",use_gpu=False,stop_training_in_x_seconds=3000, backend='lightwood', unstable_parameters_dict={'use_selfaware_model':True})
 
 p = mdb.predict(when={'number_of_rooms': 3, 'number_of_bathrooms': 2, 'neighborhood': 'south_side', 'sqft':2411}, run_confidence_variation_analysis=True, use_gpu=True)
 e = p[0].explanation

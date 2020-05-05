87b77d5ed8cca4cf92cb322d9b6221abfd22816f
George3d6 <george@cerebralab.com>
2020-05-05 23:07:36 +0300

    fixes to remote test running

--- tests/accuracy_benchmarking/run_remote.sh
+++ tests/accuracy_benchmarking/run_remote.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:/var/benchmarks/mindsdb:/var/benchmarks/lightwood
-cd mindsdb-examples && git pull && cd ../ &&
+cd /var/benchmarks/mindsdb-examples && git pull && cd ../ &&
 cd /var/benchmarks/lightwood && git pull --all && git checkout $1 && git pull origin $1 && pip3 install -r requirements.txt &&
 cd /var/benchmarks/mindsdb && git pull --all && git checkout $2 && git pull origin $2 && pip3 install -r requirements.txt &&
 cd /var/benchmarks/mindsdb/tests/accuracy_benchmarking &&

a399de0e550dc50781b1403ff703fb30087477b9
George3d6 <george@cerebralab.com>
2020-05-06 18:46:10 +0300

    fix to launch client

--- tests/accuracy_benchmarking/launch_client.py
+++ tests/accuracy_benchmarking/launch_client.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 
 instance_id = 'i-0d6aa6ae788b28ccb'
 
-output = Popen(['aws', 'ec2', cmd, '--instance-ids' ,instance_id],stdout=PIPE)
+output = Popen(['aws', 'ec2', 'start-instances', '--instance-ids' ,instance_id],stdout=PIPE)
 response = output.communicate()
 
 print('Booted up instance, waiting 2 minutes for it to start')

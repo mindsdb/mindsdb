69b32b4c1be38996b8b25f17300356bfac57fc8d
Max Stepanov <stpmax@yandex.ru>
2020-06-29 13:17:52 +0000

    tests for mariadb integration

--- tests/integration_tests/test_mariadb.py
+++ tests/integration_tests/test_mariadb.py
@@ -5,15 +5,17 @@ import csv
 import time
 import inspect
 import subprocess
+import pathlib
+import psutil
+import atexit
 
-import MySQLdb
+import mysql.connector
 
 from mindsdb.interfaces.native.mindsdb import MindsdbNative
 from mindsdb.utilities.config import Config
+from mindsdb.interfaces.mariadb.mariadb import Mariadb
 
-
-TEST_CONFIG = '/home/maxs/dev/mdb/venv/sources/mindsdb/test_config.json'
-#TEST_CONFIG = '/home/george/mindsdb/etc/config.json'
+TEST_CONFIG = '/path_to/config.json'
 
 test_csv = 'tests/home_rentals.csv'
 test_data_table = 'home_rentals'
@@ -21,8 +23,8 @@ test_predictor_name = 'test_predictor'
 
 config = Config(TEST_CONFIG)
 
-def query(q):
-    con = MySQLdb.connect(
+def query(q, as_dict=False):
+    con = mysql.connector.connect(
         host=config['integrations']['default_mariadb']['host'],
         port=config['integrations']['default_mariadb']['port'],
         user=config['integrations']['default_mariadb']['user'],
@@ -30,14 +32,18 @@ def query(q):
         db='mindsdb'
     )
 
-    cur = con.cursor()
+    cur = con.cursor(dictionary=as_dict)
     cur.execute(q)
-    resp = cur.fetchall()
+    res = True
+    try:
+        res = cur.fetchall()
+    except:
+        pass
     con.commit()
     con.close()
-    return resp
+    return res
 
-class ClickhouseTest(unittest.TestCase):
+class MariaDBTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.mdb = MindsdbNative(config)
@@ -93,18 +99,161 @@ class ClickhouseTest(unittest.TestCase):
                         )''')
                     i += 1
 
-    def test_1_pass(self):
-        print(f'Executing {inspect.stack()[0].function}')
-        self.assertTrue(True)
+    def test_1_initial_state(self):
+        print(f'\nExecuting {inspect.stack()[0].function}')
+        print('Check all testing objects not exists')
+        
+        print(f'Predictor {test_predictor_name} not exists')
+        models = [x['name'] for x in self.mdb.get_models()]
+        self.assertTrue(test_predictor_name not in models)
+
+        print(f'Test datasource exists')
+        test_tables = query('show tables from test')
+        test_tables = [x[0] for x in test_tables]
+        self.assertTrue(test_data_table in test_tables)
+
+        print(f'Test predictor table not exists')
+        mindsdb_tables = query('show tables from mindsdb')
+        mindsdb_tables = [x[0] for x in mindsdb_tables]
+        self.assertTrue(test_predictor_name not in mindsdb_tables)
+
+        print(f'mindsdb.predictors table exists')
+        self.assertTrue('predictors' in mindsdb_tables)
+
+        print(f'mindsdb.commands table exists')
+        self.assertTrue('commands' in mindsdb_tables)
+
+
+    def test_2_insert_predictor(self):
+        print(f'\nExecuting {inspect.stack()[0].function}')
+        query(f"""
+            insert into mindsdb.predictors (name, predict_cols, select_data_query, training_options) values
+            (
+                '{test_predictor_name}',
+                'rental_price, location',
+                'select * from test.{test_data_table} limit 100',
+                '{{"join_learn_process": true, "stop_training_in_x_seconds": 3}}'
+            );
+        """)
+
+        print(f'predictor record in mindsdb.predictors')
+        res = query(f"select status from mindsdb.predictors where name = '{test_predictor_name}'", as_dict=True)
+        self.assertTrue(len(res) == 1)
+        self.assertTrue(res[0]['status'] == 'complete')
+
+        print(f'predictor table in mindsdb db')
+        mindsdb_tables = query('show tables from mindsdb')
+        mindsdb_tables = [x[0] for x in mindsdb_tables]
+        self.assertTrue(test_predictor_name in mindsdb_tables)
+
+    def test_3_query_predictor(self):
+        print(f'\nExecuting {inspect.stack()[0].function}')
+        res = query(f"""
+            select
+                rental_price, location, sqft, $rental_price_confidence, number_of_rooms
+            from
+                mindsdb.{test_predictor_name} where sqft=1000;
+        """, as_dict=True)
+
+        print('check result')
+        self.assertTrue(len(res) == 1)
+
+        res = res[0]
+
+        self.assertTrue(res['rental_price'] is not None and res['rental_price'] != 'None')
+        self.assertTrue(res['location'] is not None and res['location'] != 'None')
+        self.assertTrue(res['sqft'] == 1000)
+        self.assertIsInstance(res['$rental_price_confidence'], float)
+        self.assertTrue(res['number_of_rooms'] == 'None' or res['number_of_rooms'] is None)
+
+    def test_4_range_query(self):
+        print(f'\nExecuting {inspect.stack()[0].function}')
+
+        results = query(f"""
+            select
+                rental_price, location, sqft, $rental_price_confidence, number_of_rooms
+            from
+                mindsdb.{test_predictor_name} where $select_data_query='select * from test.{test_data_table} limit 3';
+        """, as_dict=True)
+
+        print('check result')
+        self.assertTrue(len(results) == 3)
+        for res in results:
+            self.assertTrue(res['rental_price'] is not None and res['rental_price'] != 'None')
+            self.assertTrue(res['location'] is not None and res['location'] != 'None')
+            self.assertIsInstance(res['$rental_price_confidence'], float)
+
+    def test_5_delete_predictor(self):
+        print(f'\nExecuting {inspect.stack()[0].function}')
+
+        query(f"""
+            insert into mindsdb.commands values ('delete predictor {test_predictor_name}');
+        """)
+
+        print(f'Predictor {test_predictor_name} not exists')
+        models = [x['name'] for x in self.mdb.get_models()]
+        self.assertTrue(test_predictor_name not in models)
+
+        print(f'Test predictor table not exists')
+        mindsdb_tables = query('show tables from mindsdb')
+        mindsdb_tables = [x[0] for x in mindsdb_tables]
+        self.assertTrue(test_predictor_name not in mindsdb_tables)
+
+
+def wait_mysql(timeout):
+    global config
+    m = Mariadb(config)
+
+    start_time = time.time()
+
+    connected = m.check_connection()
+    while connected is False and (time.time() - start_time) < timeout:
+        time.sleep(2)
+        connected = m.check_connection()
+
+    return connected
+
+def is_port_in_use(port_num):
+    portsinuse = []
+    conns = psutil.net_connections()
+    portsinuse = [x.laddr[1] for x in conns if x.status == 'LISTEN']
+    portsinuse.sort()
+    return int(port_num) in portsinuse
+
+def wait_port(port_num, timeout):
+    start_time = time.time()
+
+    in_use = is_port_in_use(port_num)
+    while in_use is False and (time.time() - start_time) < timeout:
+        time.sleep(2)
+        in_use = is_port_in_use(port_num)
+
+    return in_use
+
 
 if __name__ == "__main__":
-    sp = subprocess.Popen(['python3', '-m', 'mindsdb', '--api', 'mysql', '--config', TEST_CONFIG])
+    maria_sp = subprocess.Popen(
+        ['./cli.sh', 'mariadb'],
+        cwd=pathlib.Path(__file__).parent.absolute().joinpath('../docker/').resolve(),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    atexit.register(maria_sp.terminate)
+    maria_ready = wait_mysql(15)
+    sp = subprocess.Popen(
+        ['python3', '-m', 'mindsdb', '--api', 'mysql', '--config', TEST_CONFIG],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    atexit.register(sp.terminate)
+    port_num = config['api']['mysql']['port']
+    api_ready = wait_port(port_num, 15)
     try:
-        time.sleep(12)
+        if maria_ready is False or api_ready is False:
+            raise Exception()
         unittest.main()
         print('Tests passed !')
-    except:
+    except Exception as e:
         print('Tests Failed !')
-    finally:
-        sp.terminate()
+        print(e)
     print('done')

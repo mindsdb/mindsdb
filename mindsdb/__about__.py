43516dea6b7e898e4ca24ae109b67a9ca964e2cd
George3d6 <george@cerebralab.com>
2020-06-29 19:39:30 +0300

    various fixes

--- mindsdb/__about__.py
+++ mindsdb/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'MindsDB'
 __package_name__ = 'mindsdb'
-__version__ = '1.99.7'
+__version__ = '1.99.8'
 __description__ = "MindsDB server, provides server capabilities to mindsdb native python library"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
--- tests/integration_tests/test_mariadb.py
+++ tests/integration_tests/test_mariadb.py
@@ -12,12 +12,13 @@ from mindsdb.interfaces.native.mindsdb import MindsdbNative
 from mindsdb.utilities.config import Config
 
 
-TEST_CONFIG = '/home/maxs/dev/mdb/venv/sources/mindsdb/test_config.json'
-#TEST_CONFIG = '/home/george/mindsdb/etc/config.json'
+#TEST_CONFIG = '/home/maxs/dev/mdb/venv/sources/mindsdb/test_config.json'
+TEST_CONFIG = '/home/george/mindsdb/etc/config.json'
 
-test_csv = 'tests/home_rentals.csv'
-test_data_table = 'home_rentals'
+test_csv = 'tmp.csv'
+test_data_table = 'churn'
 test_predictor_name = 'test_predictor'
+
 
 config = Config(TEST_CONFIG)
 
@@ -37,74 +38,80 @@ def query(q):
     con.close()
     return resp
 
-class ClickhouseTest(unittest.TestCase):
+class MariadbInsert(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.mdb = MindsdbNative(config)
 
-        if os.path.isfile(test_csv) is False:
-            r = requests.get("https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv")
-            with open(test_csv, 'wb') as f:
-                f.write(r.content)
+        for mode in ['train','test']:
+            os.system(f'rm {test_csv}')
+            cls.mdb = MindsdbNative(config)
 
-        models = cls.mdb.get_models()
-        models = [x['name'] for x in models]
-        if test_predictor_name in models:
-            cls.mdb.delete_model(test_predictor_name)
+            if os.path.isfile(test_csv) is False:
+                r = requests.get(f"https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/benchmarks/churn/dataset/{mode}.csv")
+                with open(test_csv, 'wb') as f:
+                    f.write(r.content)
 
-        query('create database if not exists test')
-        test_tables = query('show tables from test')
-        test_tables = [x[0] for x in test_tables]
-        if test_data_table not in test_tables:
-            query(f'''
-                CREATE TABLE test.{test_data_table} (
-                    number_of_rooms int,
-                    number_of_bathrooms int,
-                    sqft int,
-                    location varchar(100),
-                    days_on_market int,
-                    initial_price int,
-                    neighborhood varchar(100),
-                    rental_price int
-                )
-            ''')
-            with open(test_csv) as f:
-                csvf = csv.reader(f)
-                i = 0
-                for row in csvf:
-                    if i > 0:
-                        number_of_rooms = int(row[0])
-                        number_of_bathrooms = int(row[1])
-                        sqft = int(float(row[2].replace(',','.')))
-                        location = str(row[3])
-                        days_on_market = int(row[4])
-                        initial_price = int(row[5])
-                        neighborhood = str(row[6])
-                        rental_price = int(float(row[7]))
-                        query(f'''INSERT INTO test.{test_data_table} VALUES (
-                            {number_of_rooms},
-                            {number_of_bathrooms},
-                            {sqft},
-                            '{location}',
-                            {days_on_market},
-                            {initial_price},
-                            '{neighborhood}',
-                            {rental_price}
-                        )''')
-                    i += 1
+            models = cls.mdb.get_models()
+            models = [x['name'] for x in models]
+            if test_predictor_name in models:
+                cls.mdb.delete_model(test_predictor_name)
+
+            query('create database if not exists test')
+            test_tables = query('show tables from test')
+            test_tables = [x[0] for x in test_tables]
+            if test_data_table not in test_tables:
+                query(f'DROP TABLE IF EXISTS data.{test_data_table}_{mode}')
+                query(f'''
+                    CREATE TABLE data.{test_data_table}_{mode} (
+                        CreditScore int,
+                        Geography varchar(300),
+                        Gender varchar(300),
+                        Age int,
+                        Tenure int,
+                        Balance float,
+                        NumOfProducts int,
+                        HasCrCard int,
+                        IsActiveMember int,
+                        EstimatedSalary float,
+                        Exited int
+                    )
+                ''')
+                with open(test_csv) as f:
+                    csvf = csv.reader(f)
+                    i = 0
+                    for row in csvf:
+                        if i > 0:
+                            CreditScore = int(row[0])
+                            Geography = str(row[1])
+                            Gender = str(row[2])
+                            Age = int(row[3])
+                            Tenure = int(row[4])
+                            Balance = float(row[5])
+                            NumOfProducts = int(row[6])
+                            HasCrCard = int(row[7])
+                            IsActiveMember = int(row[8])
+                            EstimatedSalary = float(row[9])
+                            Exited = int(row[10])
+
+                            query(f'''INSERT INTO data.{test_data_table}_{mode} VALUES (
+                                {CreditScore},
+                                '{Geography}',
+                                '{Gender}',
+                                {Age},
+                                {Tenure},
+                                {Balance},
+                                {NumOfProducts},
+                                {HasCrCard},
+                                {IsActiveMember},
+                                {EstimatedSalary},
+                                {Exited}
+                            )''')
+                        i += 1
+        os.system(f'rm {test_csv}')
 
     def test_1_pass(self):
         print(f'Executing {inspect.stack()[0].function}')
         self.assertTrue(True)
 
 if __name__ == "__main__":
-    sp = subprocess.Popen(['python3', '-m', 'mindsdb', '--api', 'mysql', '--config', TEST_CONFIG])
-    try:
-        time.sleep(12)
-        unittest.main()
-        print('Tests passed !')
-    except:
-        print('Tests Failed !')
-    finally:
-        sp.terminate()
-    print('done')
+    unittest.main()

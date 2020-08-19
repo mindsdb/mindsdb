70dde404490ce35091148f7af5f2627aa0cd677f
Max Stepanov <stpmax@yandex.ru>
2020-08-19 10:56:58 +0000

    fix different paths in mindsb and mindsdb_native

--- mindsdb/__init__.py
+++ mindsdb/__init__.py
@@ -1,18 +1,34 @@
 import os
-import json
 
-from mindsdb.utilities.fs import get_or_create_dir_struct
+from mindsdb.utilities.fs import get_or_create_dir_struct, create_directory
 from mindsdb.utilities.wizards import cli_config
+from mindsdb.utilities.config import Config
+from mindsdb.utilities.functions import args_parse
 
 config_dir, predictor_dir, datasource_dir = get_or_create_dir_struct()
 
-config_path = os.path.join(config_dir,'config.json')
+config_path = os.path.join(config_dir, 'config.json')
 if not os.path.exists(config_path):
-    _ = cli_config(None,None,predictor_dir,datasource_dir,config_dir,use_default=True)
+    _ = cli_config(None, None, predictor_dir, datasource_dir, config_dir, use_default=True)
 
-with open(config_path, 'r') as fp:
-    os.environ['MINDSDB_STORAGE_PATH'] = json.load(fp)['interface']['mindsdb_native']['storage_dir']
+args = args_parse()
+if args.config is not None:
+    config_path = args.config
 
+config = Config(config_path)
+
+try:
+    datasource_dir = config['interface']['datastore']['storage_dir']
+except KeyError:
+    pass
+try:
+    predictor_dir = config['interface']['mindsdb_native']['storage_dir']
+except KeyError:
+    pass
+
+map(create_directory, [datasource_dir, predictor_dir])
+
+os.environ['MINDSDB_STORAGE_PATH'] = datasource_dir
 
 from mindsdb_native import *
 # Figure out how to add this as a module

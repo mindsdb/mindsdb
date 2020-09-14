7e939de5d57a0673d3571b07dfd9451a91792ccd
George3d6 <george@cerebralab.com>
2020-09-14 14:11:43 +0300

    sourcing paths from config instead of dir creation function, added register/unregister to model renaming

--- mindsdb/interfaces/custom/custom_models.py
+++ mindsdb/interfaces/custom/custom_models.py
@@ -9,14 +9,12 @@ import pandas as pd
 
 from mindsdb.interfaces.database.database import DatabaseWrapper
 from mindsdb.interfaces.native.mindsdb import MindsdbNative
-from mindsdb.utilities.fs import get_or_create_dir_struct
 
 class CustomModels():
     def __init__(self, config):
         self.config = config
         self.dbw = DatabaseWrapper(self.config)
-        _, self.storage_dir = get_or_create_dir_struct()
-        self.storage_dir = os.path.join(self.storage_dir,'misc')
+        self.storage_dir = os.path.join(config['storage_dir'], 'misc')
         os.makedirs(self.storage_dir, exist_ok=True)
         self.model_cache = {}
         self.mindsdb_native = MindsdbNative(self.config)
@@ -134,8 +132,10 @@ class CustomModels():
         self.dbw.unregister_predictor(name)
 
     def rename_model(self, name, new_name):
+        self.dbw.unregister_predictor(name)
         shutil.move(self._dir(name), self._dir(new_name))
         shutil.move(os.path.join(self._dir(new_name) + f'{name}.py'), os.path.join(self._dir(new_name) ,f'{new_name}.py'))
+        self.dbw.register_predictors([self.get_model_data(new_name)], setup=False)
 
     def export_model(self, name):
         shutil.make_archive(base_name=name, format='zip', root_dir=self._dir(name))
@@ -150,7 +150,7 @@ class CustomModels():
             'name': name
             ,'data_analysis': model.column_type_map
             ,'predict': model.to_predict
-            ,'status': 'untrined'
+            ,'status': 'untrained'
             ,'is_custom': True
         })
 

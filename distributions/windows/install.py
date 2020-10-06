eab412bbf021f59d9278c9a3d3ddcf5ac26edde8
maximlopin <wzzhz13@gmail.com>
2020-10-06 16:21:26 +0300

    install in program files, create shortcut

--- distributions/windows/install.py
+++ distributions/windows/install.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import zipfile
+import winshell
 import requests
+from pathlib import Path
 
 assert os.name == 'nt'
 
@@ -10,7 +12,7 @@ GET_PIP_URL = 'https://bootstrap.pypa.io/get-pip.py'
 VC_REDIST_URL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
 
 if len(sys.argv) < 2:
-    INSTALL_DIR = os.path.join(os.path.abspath('mindsdb'))
+    INSTALL_DIR = 'C:/Program Files/MindsDB'
 else:
     INSTALL_DIR = os.path.join(os.path.abspath(sys.argv[1]), 'mindsdb')
     
@@ -37,9 +39,10 @@ def download_file(url):
                 f.write(chunk)
     return filename
 
+
 print('installing vc_redist')
 vc_redist_filename = download_file(VC_REDIST_URL)
-os.system('{} /install /quiet /norestart'.format(vc_redist_filename))
+os.system('"{}" /install /quiet /norestart'.format(vc_redist_filename))
 os.remove(vc_redist_filename)
 
 print('installing python')
@@ -59,16 +62,25 @@ with open(os.path.join(PYTHON_DIR, 'sitecustomize.py'), 'w') as f:
 PYTHON_EXE = os.path.join(PYTHON_DIR, 'python.exe')
 
 get_pip_filename = download_file(GET_PIP_URL)
-os.system('{} {} --no-warn-script-location'.format(PYTHON_EXE, get_pip_filename))
+os.system('"{}" {} --no-warn-script-location'.format(PYTHON_EXE, get_pip_filename))
 os.remove(get_pip_filename)
 
-os.system('{} -m pip install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html --no-warn-script-location'.format(PYTHON_EXE))
-os.system('{} -m pip install mindsdb --no-warn-script-location'.format(PYTHON_EXE))
+os.system('"{}" -m pip install torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html --no-warn-script-location'.format(PYTHON_EXE))
+os.system('"{}" -m pip install mindsdb --no-warn-script-location'.format(PYTHON_EXE))
 
 print('generating run_server.bat')
 with open(os.path.join(INSTALL_DIR, 'run_server.bat'), 'w') as f:
     lines = [
-        '{} -m pip install mindsdb --upgrade --no-warn-script-location'.format(PYTHON_EXE),
-        '{} -m mindsdb'.format(PYTHON_EXE)
+        '"{}" -m pip install mindsdb --upgrade --no-warn-script-location'.format(PYTHON_EXE),
+        '"{}" -m mindsdb'.format(PYTHON_EXE)
     ]
     f.write('\n'.join(lines))
+
+link_path = str(Path(winshell.desktop()) / 'MindsDB.lnk')
+
+# Create the shortcut on the desktop
+with winshell.shortcut(link_path) as link:
+    link.path = os.path.join(INSTALL_DIR, 'run_server.bat')
+    link.description = 'MindsDB'
+    # TODO
+    # link.icon = (@path@, 0)

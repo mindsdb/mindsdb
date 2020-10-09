1f2e0ba63f9e9777f3fe7c58c14de68a3085942d
maximlopin <wzzhz13@gmail.com>
2020-10-09 10:32:28 +0300

    deploy linux

---
+++ distributions/linux/rename_installer.py
@@ -0,0 +1,10 @@
+import os
+
+about = {}
+with open("mindsdb/__about__.py") as fp:
+    exec(fp.read(), about)
+
+filename = 'MindsDB-Server-{}-Setup.exe'.format(about['__version__'])
+os.system('cd distributions/linux && mkdir dist')
+os.system('cd distributions/linux && mv install.sh dist')
+os.system('cd distributions/linux/dist && mv install.sh {}'.format(filename))

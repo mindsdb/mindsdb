bc2d560916c2cd2210779962cf7577144a187937
Darknez07 <rishikkar42@gmail.com>
2020-10-10 17:12:21 +0530

    Added conda.recipe

---
+++ toconda.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+# This is for suggestions
+# conda activate base
+# conda install conda-build
+# conda install anaconda-client
+# conda config --set anaconda_upload yes
+# conda build conda.recipe
+# This the shell script to publish

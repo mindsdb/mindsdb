ab0c29dcde729f212e508de33365c2b6f5000cca
Max Stepanov <stpmax@yandex.ru>
2020-10-26 11:57:30 +0000

    check clickhouse test

--- tests/integration_tests/flows/common.py
+++ tests/integration_tests/flows/common.py
@@ -6,16 +6,27 @@ import requests
 import subprocess
 import atexit
 import os
+import socket
+from contextlib import closing
+import asyncio
 
-
+from mindsdb.utilities.fs import create_dirs_recursive
+from mindsdb.utilities.config import Config
 from mindsdb.interfaces.native.mindsdb import MindsdbNative
 from mindsdb.interfaces.datastore.datastore import DataStore
 from mindsdb.interfaces.database.database import DatabaseWrapper
-from mindsdb.utilities.ps import wait_port
+from mindsdb.utilities.ps import wait_port, is_port_in_use
 from mindsdb_native import CONFIG
 
+# USE_EXTERNAL_DB_SERVER = os.getenv('EXTERNAL_DB_SERVER')
+USE_EXTERNAL_DB_SERVER = True
+
+EXTERNAL_DB_CREDENTIALS = '~/.mindsdb_credentials.json'
+
+MINDSDB_DATABASE = f'mindsdb_{int(time()*1000)}' if USE_EXTERNAL_DB_SERVER else 'mindsdb'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
 TEST_CONFIG = dir_path + '/config/config.json'
 
 TESTS_ROOT = Path(__file__).parent.absolute().joinpath('../../').resolve()
@@ -24,9 +35,52 @@ START_TIMEOUT = 15
 
 OUTPUT = None  # [None|subprocess.DEVNULL]
 
+# import time
+# TEMP_DIR = Path(__file__).parent.absolute().joinpath(f'../../temp_{int(time.time()*1000)}/').resolve()
 TEMP_DIR = Path(__file__).parent.absolute().joinpath('../../temp/').resolve()
 TEMP_DIR.mkdir(parents=True, exist_ok=True)
 
+
+def prepare_config(config, enable_dbs=[], mindsdb_database='mindsdb', override_integration_config={}):
+    if isinstance(enable_dbs, list) is False:
+        enable_dbs = [enable_dbs]
+    for key in config._config['integrations'].keys():
+        config._config['integrations'][key]['enabled'] = key in enable_dbs
+
+    for integration in override_integration_config.keys():
+        config._config['integrations'][integration].update(override_integration_config[integration])
+
+    config['api']['mysql']['database'] = mindsdb_database
+    config['api']['mongodb']['database'] = mindsdb_database
+
+    storage_dir = TEMP_DIR.joinpath('storage')
+    config._config['storage_dir'] = str(storage_dir)
+
+    create_dirs_recursive(config.paths)
+
+    temp_config_path = str(TEMP_DIR.joinpath('config.json').resolve())
+    with open(temp_config_path, 'wt') as f:
+        json.dump(config._config, f, indent=4, sort_keys=True)
+
+    return temp_config_path
+
+
+if USE_EXTERNAL_DB_SERVER:
+    config = Config(TEST_CONFIG)
+    with open(EXTERNAL_DB_CREDENTIALS, 'rt') as f:
+        credentials = json.loads(f.read())
+    override = {}
+    for key, value in credentials.items():
+        override[f'default_{key}'] = value
+    TEST_CONFIG = prepare_config(config, override_integration_config=override)
+
+
+def is_port_opened(port, host='127.0.0.1'):
+    ''' try connect to host:port, to check it open or not
+    '''
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        is_open = sock.connect_ex((host, int(port))) == 0
+    return is_open
 
 def wait_api_ready(config, api='mysql'):
     port_num = config['api'][api]['port']
@@ -46,28 +100,6 @@ def wait_db(config, db_name):
         connected = m.check_connections()[db_name]
 
     return connected
-
-
-def prepare_config(config, dbs):
-    if isinstance(dbs, list) is False:
-        dbs = [dbs]
-    for key in config._config['integrations'].keys():
-        config._config['integrations'][key]['enabled'] = key in dbs
-
-    storage_dir = TEMP_DIR.joinpath('storage')
-    config._config['storage_dir'] = str(storage_dir)
-
-    paths = config.paths
-    for key in paths:
-        p = storage_dir.joinpath(key)
-        p.mkdir(mode=0o777, exist_ok=True, parents=True)
-        paths[key] = str(p)
-
-    temp_config_path = str(TEMP_DIR.joinpath('config.json').resolve())
-    with open(temp_config_path, 'wt') as f:
-        json.dump(config._config, f, indent=4, sort_keys=True)
-
-    return temp_config_path
 
 
 def is_container_run(name):
@@ -134,40 +166,53 @@ def stop_mindsdb(sp):
     sp.wait()
 
 
-def run_environment(db, config, run_apis='mysql'):
-    DEFAULT_DB = f'default_{db}'
+def run_environment(config, apis=['mysql'], run_docker_db=[], override_integration_config={}, mindsdb_database='mindsdb'):
+    ''' services = [mindsdb|]
+    '''
 
-    temp_config_path = prepare_config(config, DEFAULT_DB)
+    default_databases = [f'default_{db}' for db in run_docker_db]
+    temp_config_path = prepare_config(config, default_databases, mindsdb_database, override_integration_config)
 
-    if db in ['mssql', 'mongodb']:
-        db_ready = True
-    else:
+    db_ready = True
+    for db in run_docker_db:
         if is_container_run(f'{db}-test') is False:
             run_container(db)
-        db_ready = wait_db(config, DEFAULT_DB)
+        db_ready = db_ready and wait_db(config, f'default_{db}')
 
-    if isinstance(run_apis, list) is False:
-        run_apis = run_apis.split(',')
-    api_str = ','.join(run_apis)
-
-    if db_ready:
-        sp = subprocess.Popen(
-            ['python3', '-m', 'mindsdb', '--api', api_str, '--config', temp_config_path],
-            stdout=OUTPUT,
-            stderr=OUTPUT
-        )
-        atexit.register(stop_mindsdb, sp=sp)
-
-    api_ready = True
-    for api in run_apis:
-        apistr = 'mongodb' if api == 'mongodb' else api
-        api_ready = api_ready and wait_api_ready(config, apistr)
-        if api_ready is False:
-            break
-
-    if db_ready is False or api_ready is False:
-        print(f'Failed by timeout. {db} started={db_ready}, MindsDB started={api_ready}')
+    if db_ready is False:
+        print('Cant start databases.')
         raise Exception()
+
+    api_str = ','.join(apis)
+    sp = subprocess.Popen(
+        ['python3', '-m', 'mindsdb', '--api', api_str, '--config', temp_config_path],
+        stdout=OUTPUT,
+        stderr=OUTPUT
+    )
+    atexit.register(stop_mindsdb, sp=sp)
+
+    async def wait_port_async(port, timeout):
+        start_time = time.time()
+        started = is_port_in_use(port)
+        while (time.time() - start_time) < timeout and started is False:
+            await asyncio.sleep(1)
+            started = is_port_in_use(port)
+        return started
+
+    async def wait_apis_start(ports):
+        futures = [wait_port_async(port, 60) for port in ports]
+        success = True
+        for i, future in enumerate(asyncio.as_completed(futures)):
+            success = success and await future
+        return success
+
+    ports_to_wait = [config['api'][api]['port'] for api in apis]
+
+    ioloop = asyncio.get_event_loop()
+    success = ioloop.run_until_complete(wait_apis_start(ports_to_wait))
+    ioloop.close()
+    if not success:
+        raise Exception('Cant start mindsdb apis')
 
     CONFIG.MINDSDB_STORAGE_PATH = config.paths['predictors']
     mdb = MindsdbNative(config)

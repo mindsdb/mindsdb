9e74834fe81ff024ed0edf8e3097804250921da0
Max Stepanov <stpmax@yandex.ru>
2020-10-27 09:11:29 +0000

    move back removing by timeout, increase it to 3600s

--- mindsdb/api/http/namespaces/datasource.py
+++ mindsdb/api/http/namespaces/datasource.py
@@ -152,6 +152,8 @@ class Analyze(Resource):
         if name in ds_analysis:
             if ds_analysis[name] is None:
                 return {'status': 'analyzing'}, 200
+            elif (datetime.datetime.utcnow() - ds_analysis[name]['created_at']) > datetime.timedelta(seconds=3600):
+                del ds_analysis[name]
             else:
                 analysis = ds_analysis[name]['data']
                 return analysis, 200

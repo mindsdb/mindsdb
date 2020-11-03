1effad8bc42dd184c1c55a69a0cdee8dff6cbfb5
Max Stepanov <stpmax@yandex.ru>
2020-11-03 10:35:16 +0000

    mongos cluster fix

--- tests/docker/mongodb/glue/data/init_db_rs.js
+++ tests/docker/mongodb/glue/data/init_db_rs.js
@@ -2,3 +2,8 @@ rs.initiate({
     _id: 'replmain',
     members: [{_id: 0, host: '127.0.0.1:27001'}]
 });
+// we need create test_data db on exactly this shart.
+// instance will think it is 'master' in 2-3 seconds after rs.initiate
+sleep(5000);
+use test_data;
+db.placeholder.insert({x: 1});

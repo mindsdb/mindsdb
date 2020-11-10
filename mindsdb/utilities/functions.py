f686db7254cb527f643626f27b9e473765db97ab
George3d6 <george@cerebralab.com>
2020-11-10 20:08:15 +0200

    Fixed import as mindsdb in notebooks

--- mindsdb/utilities/functions.py
+++ mindsdb/utilities/functions.py
@@ -23,3 +23,16 @@ def cast_row_types(row, field_types):
         elif t == 'Date' and isinstance(row[key], (int, float)):
             timestamp = datetime.datetime.utcfromtimestamp(row[key])
             row[key] = timestamp.strftime('%Y-%m-%d')
+
+# From: https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+def is_notebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter

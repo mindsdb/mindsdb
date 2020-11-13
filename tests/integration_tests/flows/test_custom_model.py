ca7ea51bf6ca1f9f55a89c7c212478500511acc0
Max Stepanov <stpmax@yandex.ru>
2020-11-13 10:42:43 +0000

    custom model test

--- tests/integration_tests/flows/test_custom_model.py
+++ tests/integration_tests/flows/test_custom_model.py
@@ -39,17 +39,10 @@ CONDITION = {
 # ---
 
 root = 'http://127.0.0.1:47334/api'
-TEST_CSV = {
-    'name': 'home_rentals.csv',
-    'url': 'https://s3.eu-west-2.amazonaws.com/mindsdb-example-data/home_rentals.csv'
-}
-TEST_DATA_TABLE = 'home_rentals'
-TEST_PREDICTOR_NAME = 'test_predictor'
 
-EXTERNAL_DS_NAME = 'test_external'
-config = Config(TEST_CONFIG)
-PRED_NAME = 'this_is_my_custom_sklearnmodel'
-
+TEST_DATA_TABLE = TEST_DATASET
+TEST_PREDICTOR_NAME = f'{TEST_DATASET}_predictor'
+EXTERNAL_DS_NAME = f'{TEST_DATASET}_external'
 
 config = Config(TEST_CONFIG)
 
@@ -105,7 +98,6 @@ class CustomModelTest(unittest.TestCase):
 
             if TEST_DATA_TABLE not in test_tables:
                 test_csv_path = Path(DATASETS_PATH).joinpath(TEST_DATASET).joinpath('data.csv')
-                # test_csv_path = get_test_csv(TEST_CSV['name'], TEST_CSV['url'])
                 upload_csv(
                     query=query,
                     columns_map=DATASETS_COLUMN_TYPES[TEST_DATASET],
@@ -113,53 +105,6 @@ class CustomModelTest(unittest.TestCase):
                     table_name=TEST_DATA_TABLE,
                     csv_path=test_csv_path
                 )
-
-            # if TEST_DATA_TABLE not in test_tables:
-            #     print('creating test data table...')
-            #     query(f'''
-            #         CREATE TABLE test_data.{TEST_DATA_TABLE} (
-            #             number_of_rooms int,
-            #             number_of_bathrooms int,
-            #             sqft int,
-            #             location varchar(100),
-            #             days_on_market int,
-            #             initial_price int,
-            #             neighborhood varchar(100),
-            #             rental_price int
-            #         )
-            #     ''')
-
-            #     with open(test_csv_path) as f:
-            #         csvf = csv.reader(f)
-            #         i = 0
-            #         for row in csvf:
-            #             if i > 0:
-            #                 number_of_rooms = int(row[0])
-            #                 number_of_bathrooms = int(row[1])
-            #                 sqft = int(float(row[2].replace(',', '.')))
-            #                 location = str(row[3])
-            #                 days_on_market = int(row[4])
-            #                 initial_price = int(row[5])
-            #                 neighborhood = str(row[6])
-            #                 rental_price = int(float(row[7]))
-            #                 query(f'''INSERT INTO test_data.{TEST_DATA_TABLE} VALUES (
-            #                     {number_of_rooms},
-            #                     {number_of_bathrooms},
-            #                     {sqft},
-            #                     '{location}',
-            #                     {days_on_market},
-            #                     {initial_price},
-            #                     '{neighborhood}',
-            #                     {rental_price}
-            #                 )''')
-            #             i += 1
-            #     print('done')
-
-        # ds = datastore.get_datasource(EXTERNAL_DS_NAME)
-        # if ds is not None:
-        #     datastore.delete_datasource(EXTERNAL_DS_NAME)
-        # short_csv_file_path = get_test_csv(f'{EXTERNAL_DS_NAME}.csv', TEST_CSV['url'], lines_count=30, rewrite=True)
-        # datastore.save_datasource(EXTERNAL_DS_NAME, 'file', 'test.csv', short_csv_file_path)
 
         ds = datastore.get_datasource(EXTERNAL_DS_NAME)
         if ds is not None:
@@ -215,7 +160,7 @@ class Model(ModelInterface):
         shutil.make_archive(base_name='my_model', format='zip', root_dir=dir_name)
 
         # Upload the model (new endpoint)
-        res = requests.put(f'{root}/predictors/custom/{PRED_NAME}', files=dict(file=open('my_model.zip', 'rb')), json={
+        res = requests.put(f'{root}/predictors/custom/{TEST_PREDICTOR_NAME}', files=dict(file=open('my_model.zip', 'rb')), json={
             'trained_status': 'untrained'
         })
         print(res.status_code)
@@ -228,7 +173,7 @@ class Model(ModelInterface):
             'to_predict': 'rental_price',
             'kwargs': {}
         }
-        res = requests.post(f'{root}/predictors/{PRED_NAME}/learn', json=params)
+        res = requests.post(f'{root}/predictors/{TEST_PREDICTOR_NAME}/learn', json=params)
         print(res.status_code)
         print(res.text)
         assert res.status_code == 200
@@ -237,7 +182,7 @@ class Model(ModelInterface):
         params = {
             'when': {'initial_price': 5000}
         }
-        url = f'{root}/predictors/{PRED_NAME}/predict'
+        url = f'{root}/predictors/{TEST_PREDICTOR_NAME}/predict'
         res = requests.post(url, json=params)
         assert res.status_code == 200
         assert isinstance(res.json()[0]['rental_price']['predicted_value'], float)
@@ -246,7 +191,7 @@ class Model(ModelInterface):
         params = {
             'data_source_name': EXTERNAL_DS_NAME
         }
-        url = f'{root}/predictors/{PRED_NAME}/predict_datasource'
+        url = f'{root}/predictors/{TEST_PREDICTOR_NAME}/predict_datasource'
         res = requests.post(url, json=params)
 
         assert res.status_code == 200
@@ -258,7 +203,7 @@ class Model(ModelInterface):
         res = fetch(
             f"""
                 SELECT rental_price
-                FROM {MINDSDB_DATABASE}.{PRED_NAME}
+                FROM {MINDSDB_DATABASE}.{TEST_PREDICTOR_NAME}
                 WHERE external_datasource='{EXTERNAL_DS_NAME}'
             """,
             as_dict=True
@@ -270,11 +215,11 @@ class Model(ModelInterface):
     def test_3_retrain_model(self):
         query(f"""
             INSERT INTO {MINDSDB_DATABASE}.predictors (name, predict, select_data_query)
-            VALUES ('{PRED_NAME}', 'sqft', 'SELECT * FROM test_data.{TEST_DATA_TABLE}')
+            VALUES ('{TEST_PREDICTOR_NAME}', 'sqft', 'SELECT * FROM test_data.{TEST_DATA_TABLE} where sqft is not null')
         """)
 
     def test_4_predict_with_retrained_from_sql(self):
-        res = fetch(f"""SELECT sqft FROM {MINDSDB_DATABASE}.{PRED_NAME} WHERE initial_price=6000""", as_dict=True)
+        res = fetch(f"""SELECT sqft FROM {MINDSDB_DATABASE}.{TEST_PREDICTOR_NAME} WHERE initial_price=6000""", as_dict=True)
 
         self.assertTrue(len(res) > 0)
         print(res)
@@ -284,7 +229,7 @@ class Model(ModelInterface):
         res = fetch(
             f"""
                 SELECT sqft
-                FROM {MINDSDB_DATABASE}.{PRED_NAME}
+                FROM {MINDSDB_DATABASE}.{TEST_PREDICTOR_NAME}
                 WHERE select_data_query='SELECT * FROM test_data.{TEST_DATA_TABLE}'
             """,
             as_dict=True
@@ -298,7 +243,7 @@ class Model(ModelInterface):
         params = {
             'when': {'initial_price': 5000, 'rental_price': 4000}
         }
-        url = f'{root}/predictors/{PRED_NAME}/predict'
+        url = f'{root}/predictors/{TEST_PREDICTOR_NAME}/predict'
         res = requests.post(url, json=params)
         assert res.status_code == 200
         assert isinstance(res.json()[0]['sqft']['predicted_value'], float)
@@ -306,7 +251,7 @@ class Model(ModelInterface):
         params = {
             'data_source_name': EXTERNAL_DS_NAME
         }
-        url = f'{root}/predictors/{PRED_NAME}/predict_datasource'
+        url = f'{root}/predictors/{TEST_PREDICTOR_NAME}/predict_datasource'
         res = requests.post(url, json=params)
 
         assert res.status_code == 200
@@ -317,22 +262,22 @@ class Model(ModelInterface):
     def test_7_utils_from_http_api(self):
         res = requests.get(f'{root}/predictors')
         assert res.status_code == 200
-        assert PRED_NAME in [x['name'] for x in res.json()]
+        assert TEST_PREDICTOR_NAME in [x['name'] for x in res.json()]
         for ele in res.json():
             print(ele)
-            if ele['name'] == PRED_NAME:
+            if ele['name'] == TEST_PREDICTOR_NAME:
                 assert ele['is_custom'] is True
 
-        res = requests.get(f'{root}/predictors/{PRED_NAME}')
+        res = requests.get(f'{root}/predictors/{TEST_PREDICTOR_NAME}')
         assert res.status_code == 200
-        assert res.json()['name'] == PRED_NAME
+        assert res.json()['name'] == TEST_PREDICTOR_NAME
         assert res.json()['is_custom'] is True
 
     def test_8_delete_from_http_api(self):
-        res = requests.delete(f'{root}/predictors/{PRED_NAME}')
+        res = requests.delete(f'{root}/predictors/{TEST_PREDICTOR_NAME}')
         assert res.status_code == 200
         res = requests.get(f'{root}/predictors')
-        assert PRED_NAME not in [x['name'] for x in res.json()]
+        assert TEST_PREDICTOR_NAME not in [x['name'] for x in res.json()]
 
 
 if __name__ == "__main__":

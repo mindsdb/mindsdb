ba967c7a669a13be8c924bc039870d5090323cde
George3d6 <george@cerebralab.com>
2020-11-17 14:44:41 +0200

    added uuid endpoint

--- mindsdb/api/http/namespaces/util.py
+++ mindsdb/api/http/namespaces/util.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restx import Resource, abort
+from flask import current_app as ca
 
 from mindsdb.api.http.namespaces.configs.util import ns_conf
 from mindsdb import __about__
@@ -25,11 +26,20 @@ class Shutdown(Resource):
             return '', 200
         abort(403, "")
 
-                
+
 @ns_conf.route('/util/version')
 class Version(Resource):
     @ns_conf.doc('get_endpoint')
     def get(self):
         '''Check endpoint'''
         return {'mindsdb': "{__about__.__version__}"}
-    
+
+@ns_conf.route('/report_uuid')
+class ReportUUID(Resource):
+    @ns_conf.doc('get_report_uuid')
+    def get(self):
+        metamodel_name = '___monitroing_metamodel___'
+        predictor = ca.mindsdb_native.create(metamodel_name)
+        return {
+            'report_uuid': predictor.report_uuid
+        }

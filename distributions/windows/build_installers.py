a7a06342d3fa975cab5fefbdc6045da2c397dfe1
maximlopin <wzzhz13@gmail.com>
2020-12-10 14:16:40 +0300

    run as admin by default

--- distributions/windows/build_installers.py
+++ distributions/windows/build_installers.py
@@ -21,5 +21,5 @@ with open('distributions/windows/latest.py', 'w+') as f:
 with open('distributions/windows/fixed.py', 'w+') as f:
     f.write(install_py.replace('$name', FIXED_NAME).replace('$version', about['__version__']))
 
-os.system('cd distributions/windows && pyinstaller latest.py -F --onefile -n {}-Setup.exe'.format(LATEST_NAME))
-os.system('cd distributions/windows && pyinstaller fixed.py -F --onefile -n {}-Setup.exe'.format(FIXED_NAME))
+os.system('cd distributions/windows && pyinstaller latest.py -F --onefile --uac-admin -n {}-Setup.exe'.format(LATEST_NAME))
+os.system('cd distributions/windows && pyinstaller fixed.py -F --onefile --uac-admin -n {}-Setup.exe'.format(FIXED_NAME))

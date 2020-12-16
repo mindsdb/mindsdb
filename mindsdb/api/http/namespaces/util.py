3a66868cec4225259111ae2744279e35bfc3ab2b
Ilia Tsyplenkov <ilya@mindsdb.com>
2020-12-16 13:47:09 +0300

    enable/disable telemetry endpoint

--- mindsdb/api/http/namespaces/util.py
+++ mindsdb/api/http/namespaces/util.py
@@ -1,9 +1,12 @@
+import os
 from flask import request
 from flask_restx import Resource, abort
 from flask import current_app as ca
 
 from mindsdb.api.http.namespaces.configs.util import ns_conf
 from mindsdb import __about__
+
+TELEMETRY_FILE = 'telemetry.lock'
 
 @ns_conf.route('/ping')
 class Ping(Resource):
@@ -21,3 +24,34 @@ class ReportUUID(Resource):
         return {
             'report_uuid': predictor.report_uuid
         }
+
+@ns_conf.route('/telemetry')
+class Telemetry(Resource):
+    @ns_conf.doc('get_telemetry_status')
+    def get(self):
+        status = "enabled" if is_telemetry_active() else "disabled"
+        return {"status": status}
+
+    @ns_conf.doc('set_telemetry')
+    def post(self):
+        data = request.json
+        action = data['action']
+        if str(action).lower() in ["true", "enable", "on"]:
+            enable_telemetry()
+        else:
+            disable_telemetry()
+
+
+def disable_telemetry():
+    path = os.path.join(ca.config_obj['storage_dir'], TELEMETRY_FILE)
+    if os.path.exists(path):
+        os.remove(path)
+
+def enable_telemetry():
+    path = os.path.join(ca.config_obj['storage_dir'], TELEMETRY_FILE)
+    with open(path, 'w') as _:
+        pass
+
+def is_telemetry_active():
+    path = os.path.join(ca.config_obj['storage_dir'], TELEMETRY_FILE)
+    return os.path.exists(path)

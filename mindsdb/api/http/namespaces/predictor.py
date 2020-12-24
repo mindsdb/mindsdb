627a8286df651f01d8b4145fc64a5672f7cc6cc7
Max Stepanov <stpmax@yandex.ru>
2020-12-24 13:00:51 +0000

    remove 'data_analisys' from 'get columns' route

--- mindsdb/api/http/namespaces/predictor.py
+++ mindsdb/api/http/namespaces/predictor.py
@@ -199,34 +199,44 @@ class PredictorLearn(Resource):
 
         return '', 200
 
+
 @ns_conf.route('/<name>/columns')
 @ns_conf.param('name', 'The predictor identifier')
 class PredictorColumns(Resource):
     @ns_conf.doc('get_predictor_columns')
     def get(self, name):
         '''List of predictors colums'''
+        columns = []
         try:
             if is_custom(name):
                 model = ca.custom_models.get_model_data(name)
+                for columns_name in model['data_analysis']:
+                    columns_type = model['data_analysis'][columns_name].get('typing', {'data_type': 'Numeric'}).get('data_type', 'Numeric')
+                    column = {
+                        'name': columns_name,
+                        'data_type': columns_type.lower(),
+                        'is_target_column': columns_name in model['predict']
+                    }
+                    if columns_type == 'Categorical':
+                        column['distribution'] = model['data_analysis'][columns_name]['histogram']['x']
+                    columns.append(column)
             else:
                 model = ca.mindsdb_native.get_model_data(name, native_view=True)
+                for columns_name in model['data_analysis_v2']['columns']:
+                    columns_type = model['data_analysis_v2'][columns_name].get('typing', {'data_type': 'Numeric'}).get('data_type', 'Numeric')
+                    column = {
+                        'name': columns_name,
+                        'data_type': columns_type.lower(),
+                        'is_target_column': columns_name in model['predict']
+                    }
+                    if columns_type == 'Categorical':
+                        column['distribution'] = model['data_analysis_v2'][columns_name]['histogram']['x']
+                    columns.append(column)
         except Exception:
             abort(404, 'Invalid predictor name')
 
-        columns = []
-        for array, is_target_array in [(model['data_analysis']['target_columns_metadata'], True),
-                                       (model['data_analysis']['input_columns_metadata'], False)]:
-            for col_data in array:
-                column = {
-                    'name': col_data['column_name'],
-                    'data_type': col_data['data_type'].lower(),
-                    'is_target_column': is_target_array
-                }
-                if column['data_type'] == 'categorical':
-                    column['distribution'] = col_data["data_distribution"]["data_histogram"]["x"]
-                columns.append(column)
-
         return columns, 200
+
 
 @ns_conf.route('/<name>/predict')
 @ns_conf.param('name', 'The predictor identifier')

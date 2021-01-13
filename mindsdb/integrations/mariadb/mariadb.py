71bae067aa4f9a630bd5f2c913fa5c053496f170
George3d6 <george@cerebralab.com>
2021-01-13 15:51:26 +0000

    rerun tests

--- mindsdb/integrations/mariadb/mariadb.py
+++ mindsdb/integrations/mariadb/mariadb.py
@@ -5,6 +5,7 @@ from mindsdb.integrations.base import Integration
 
 
 class Mariadb(Integration):
+    
     def _to_mariadb_table(self, stats, predicted_cols):
         subtype_map = {
             DATA_SUBTYPES.INT: 'int',

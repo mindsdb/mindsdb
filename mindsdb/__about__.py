34df480f6e2701608b9ebbc5cdb4f451d11141bd
George3d6 <george@cerebralab.com>
2021-01-26 15:27:40 +0000

    updated native req

--- mindsdb/__about__.py
+++ mindsdb/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'MindsDB'
 __package_name__ = 'mindsdb'
-__version__ = '2.19.2'
+__version__ = '2.19.3'
 __description__ = "MindsDB server, provides server capabilities to mindsdb native python library"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'

22999f587ce042a3eba5c663d0317d83fa1cb8c2
Max Stepanov <stpmax@yandex.ru>
2021-02-04 09:04:32 +0000

    fix for cheking connection to atlas data lake

--- mindsdb/integrations/mongodb/mongodb.py
+++ mindsdb/integrations/mongodb/mongodb.py
@@ -1,3 +1,6 @@
+import re
+
+import certifi
 from pymongo import MongoClient
 
 from mindsdb.integrations.base import Integration
@@ -19,12 +22,35 @@ class MongoDB(Integration):
     def check_connection(self):
         try:
             integration = self.config['integrations'][self.name]
+            host = integration['host']
+            user = integration['user']
+            password = integration['password']
+
+            kwargs = {}
+
+            if isinstance(user, str) and len(user) > 0:
+                kwargs['username'] = user
+
+            if isinstance(password, str) and len(password) > 0:
+                kwargs['password'] = password
+
+            if re.match(r'\/\?.*tls=true', host.lower()):
+                kwargs['tls'] = True
+
+            if re.match(r'\/\?.*tls=false', host.lower()):
+                kwargs['tls'] = False
+
+            if re.match(r'.*\.mongodb.net', host.lower()) and kwargs.get('tls', None) is None:
+                kwargs['tlsCAFile'] = certifi.where()
+                if kwargs.get('tls', None) is None:
+                    kwargs['tls'] = True
+
             server = MongoClient(
-                integration['host'],
+                host,
                 port=integration.get('port', 27017),
-                username=integration['user'],
-                password=integration['password'],
-                serverSelectionTimeoutMS=5000
+                serverSelectionTimeoutMS=5000,
+                tlsCAFile=certifi.where(),
+                **kwargs
             )
             server.server_info()
             connected = True

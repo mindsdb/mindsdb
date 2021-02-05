067904232ff708597c53891184f3281a1683a2b1
Max Stepanov <stpmax@yandex.ru>
2021-02-05 10:00:33 +0000

    allow any columns name for mongo api

--- mindsdb/api/mongo/responders/find.py
+++ mindsdb/api/mongo/responders/find.py
@@ -40,9 +40,7 @@ class Responce(Responder):
 
             for key in where_data:
                 if key not in columns:
-                    sub_key = key[:key.find('.')] if '.' in key else key
-                    if sub_key not in columns:
-                        raise Exception(f"Unknown column '{key}'. Only columns from this list can be used in query: {', '.join(columns)}")
+                    columns.append(key)
 
             prediction = mindsdb_env['mindsdb_native'].predict(name=table, when_data=where_data)
 

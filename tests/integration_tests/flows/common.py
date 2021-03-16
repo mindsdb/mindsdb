e83e828bde033a978b46438a58ba09bb9b972203
George3d6 <george@cerebralab.com>
2021-03-16 08:33:47 +0000

    properly closing rcp server

--- tests/integration_tests/flows/common.py
+++ tests/integration_tests/flows/common.py
@@ -209,7 +209,7 @@ def stop_mindsdb(sp=None):
     conns = net_connections()
     pids = [x.pid for x in conns
             if x.pid is not None and x.status in ['LISTEN', 'CLOSE_WAIT']
-            and x.laddr[1] in (47334, 47335, 47336)]
+            and x.laddr[1] in (47334, 47335, 47336, 17329)]
 
     for pid in pids:
         try:

3bfe04e4daf309be4e4c72ea6b1ec23e4dafc4f1
Ilia Tsyplenkov <ilya@mindsdb.com>
2021-03-16 14:22:56 +0300

    Initial working concept

--- mindsdb/api/http/namespaces/stream.py
+++ mindsdb/api/http/namespaces/stream.py
@@ -3,6 +3,7 @@ from flask_restx import Resource, abort
 from flask import current_app as ca
 from mindsdb.utilities.log import log
 from mindsdb.api.http.namespaces.configs.streams import ns_conf
+from mindsdb.streams.redis.redis_stream import RedisStream
 
 
 
@@ -18,15 +19,26 @@ class StreamList(Resource):
 class Stream(Resource):
     @ns_conf.doc("get_stream")
     def get(self, name):
+        log.error("in get")
         if name == "fake":
             abort(404, f"Can\'t find steam: {name}")
         return {"name": name, "status": "fake"}
 
     @ns_conf.doc("put_stream")
     def put(self, name):
-        params = request.json.get('params')
-        if not isinstance(params, dict):
-            abort(400, "type of 'params' must be dict")
-        if 'integration_name' not in params:
-            abort(400, "need to specify redis integration")
-        return params, 200
+        # params = request.json.get('params')
+        # if not isinstance(params, dict):
+        #     abort(400, "type of 'params' must be dict")
+        # if 'integration_name' not in params:
+        #     abort(400, "need to specify redis integration")
+        # return params, 200
+        log.error("in put")
+        integration_name = "redis_test"
+        redis_host = "127.0.0.1"
+        redis_port = 6379
+        db = 0
+        predictor = "metro_traffic"
+        input_stream = "predict_when"
+        stream = RedisStream(redis_host, redis_port, db, input_stream, predictor)
+        log.error("running redis stream")
+        stream.start()
--- mindsdb/streams/redis/redis_stream.py
+++ mindsdb/streams/redis/redis_stream.py
@@ -1,6 +1,10 @@
+import time
+import json
 from threading import Thread
 import walrus
-from flask import current_app as ca
+# from flask import current_app as ca
+from mindsdb.utilities.log import log
+from mindsdb_native import Predictor
 
 
 class RedisStream(Thread):
@@ -8,7 +12,7 @@ class RedisStream(Thread):
         self.host = host
         self.port = port
         self.db = database
-        self.predictor = predictor
+        self.predictor_name = predictor
         self.client = self._get_client()
         self.stream_in = self.client.Stream(input_stream)
         self.stream_out = self.client.Stream(predictor)
@@ -18,9 +22,53 @@ class RedisStream(Thread):
         return walrus.Database(host=self.host, port=self.port, db=self.db)
     
     def make_prediction(self):
+        predictor = Predictor(self.predictor_name)
         while True:
+            time.sleep(10)
             predict_info = self.stream_in.read()
+            log.error("got predict request(s): %s" % predict_info)
             for record in predict_info:
-                when_data = record[1]
-                result = ca.mindsdb_native.predict(self.predictor, when_data=when_data)
-                self.stream_out.add({'prediction': result})
+                # log.error("handling predict request: %s" % record)
+                raw_when_data = record[1]
+                log.error("raw when_data: %s" % raw_when_data)
+                when_data = self.decode(raw_when_data)
+                log.error("when_data: %s" % when_data)
+
+                # result = ca.mindsdb_native.predict(self.predictor_name, when_data=when_data)
+                result = predictor.predict(when_data=when_data)
+                log.error("when_data: %s\tprediction: %s" % (when_data, result))
+                # encoded_res = self.encode(result)
+                # log.error("encoded_res: %s" % encoded_res)
+                for res in result:
+                    log.error("explainded: %s" % res.explain())
+                    in_json = json.dumps(res.explain())
+                    log.error("json converted: %s" % in_json)
+                    self.stream_out.add({"prediction": in_json})
+                # self.stream_out.add(encoded_res)
+
+    def decode(self, redis_data):
+        decoded = {}
+        for k in redis_data:
+            decoded[k.decode('utf8')] = redis_data[k].decode('utf8')
+        return decoded
+
+    def encode(self, prediction_info):
+        encoded = {}
+        for k in prediction_info:
+            log.error("%s: %s" % (k, type(k)))
+            log.error(k.explain())
+
+        for k in prediction_info:
+            if isinstance(k, str):
+                if isinstance(prediction_info[k], (list, tuple)):
+                    if len(prediction_info[k]) == 1:
+                        if not isinstance(prediction_info[k][0], (list, tuple)):
+                            encoded[k] = prediction_info[k][0]
+                        else:
+                            encoded[k] = str(prediction_info[k][0])
+                    else:
+                        encoded[k] = str(prediction_info[k])
+                else:
+                    encoded[k] = prediction_info[k]
+
+        return encoded

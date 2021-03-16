a1f68dcea6e9a7a1e0b639ffc0480f52e3f1d257
George3d6 <george@cerebralab.com>
2021-03-16 19:50:08 +0000

    removed undeed native import

--- mindsdb/interfaces/ai_table/ai_table.py
+++ mindsdb/interfaces/ai_table/ai_table.py
@@ -7,7 +7,7 @@ import pickle
 import pandas as pd
 
 from mindsdb.interfaces.model.model_interface import ModelInterface as NativeInterface
-from mindsdb_native import FileDS, ClickhouseDS, MariaDS, MySqlDS, PostgresDS, MSSQLDS, MongoDS, SnowflakeDS
+from mindsdb_datasources import FileDS, ClickhouseDS, MariaDS, MySqlDS, PostgresDS, MSSQLDS, MongoDS, SnowflakeDS
 from mindsdb.utilities.config import Config
 from mindsdb.interfaces.storage.db import session, Datasource, AITable
 from mindsdb.interfaces.storage.fs import FsSotre

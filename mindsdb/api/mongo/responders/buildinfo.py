2637777ab12df65c4e6ffa2b0295ffd1163c52e3
Max Stepanov <stpmax@yandex.ru>
2021-04-07 16:41:08 +0000

    fix buildinfo responder

--- mindsdb/api/mongo/responders/buildinfo.py
+++ mindsdb/api/mongo/responders/buildinfo.py
@@ -5,9 +5,7 @@ from mindsdb.api.mongo.classes import Responder
 class Responce(Responder):
     def when(self, query):
         return (
-            ('buildinfo' in query or 'buildInfo' in query)
-            and '$db' in query
-            and (query['$db'] == 'test' or query['$db'] == 'admin')
+            'buildinfo' in query or 'buildInfo' in query
         )
 
     result = {

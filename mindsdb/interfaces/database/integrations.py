4077d795a8ddd5c5cb6561246ebdacec3f694dc3
George3d6 <george@cerebralab.com>
2021-04-23 23:10:10 +0100

    fixed get_db_integration when the integration doesnt exist

--- mindsdb/interfaces/database/integrations.py
+++ mindsdb/interfaces/database/integrations.py
@@ -27,6 +27,8 @@ def remove_db_integration(name, company_id):
 
 def get_db_integration(name, company_id):
     integration_record = session.query(Integration).filter_by(company_id=company_id, name=name).first()
+    if integration_record is None
+        return None
     return integration_record.data
 
 def get_db_integrations(company_id):

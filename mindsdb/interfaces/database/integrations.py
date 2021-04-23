918aa92a0e7b2289d693ff60a3986ee09eb8c626
George3d6 <george@cerebralab.com>
2021-04-23 23:00:54 +0100

    made the integration helper function budhist (removed mistakenly leftover self argument)

--- mindsdb/interfaces/database/integrations.py
+++ mindsdb/interfaces/database/integrations.py
@@ -1,7 +1,7 @@
 from mindsdb.interfaces.storage.db import session
 from mindsdb.interfaces.storage.db import Integration
 
-def add_db_integration(self, name, data, company_id):
+def add_db_integration(name, data, company_id):
     if 'database_name' not in data:
         data['database_name'] = name
     if 'publish' not in dict:
@@ -11,7 +11,7 @@ def add_db_integration(self, name, data, company_id):
     session.add(integration_record)
     session.commit()
 
-def modify_db_integration(self, name, data, company_id):
+def modify_db_integration(name, data, company_id):
     integration_record = session.query(Integration).filter_by(company_id=company_id, name=name).first()
     old_data = integration_record.data
     for k in old_data:
@@ -21,15 +21,15 @@ def modify_db_integration(self, name, data, company_id):
     integration_record.data = data
     session.commit()
 
-def remove_db_integration(self, name, company_id):
+def remove_db_integration(name, company_id):
     session.query(Integration).filter_by(company_id=company_id, name=name).delete()
     session.commit()
 
-def get_db_integration(self, name, company_id):
+def get_db_integration(name, company_id):
     integration_record = session.query(Integration).filter_by(company_id=company_id, name=name).first()
     return integration_record.data
 
-def get_db_integrations(self, company_id):
+def get_db_integrations(company_id):
     integration_records = session.query(Integration).filter_by(company_id=company_id).all()
     integration_dict = {}
     for record in integration_records:

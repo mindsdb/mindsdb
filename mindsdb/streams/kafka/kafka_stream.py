a5adc5884de278f6f7a4e26be091d320ce7f9886
maximlopin <wzzhz13@gmail.com>
2021-06-16 15:56:40 +0300

    add consumer_timeout_ms

--- mindsdb/streams/kafka/kafka_stream.py
+++ mindsdb/streams/kafka/kafka_stream.py
@@ -7,8 +7,8 @@ from mindsdb.streams.base.base_stream import BaseStream
 class KafkaStream(BaseStream):
     def __init__(self, topic, connection_info):
         self.topic = topic
-        self.consumer = kafka.KafkaConsumer(**connection_info)
         self.producer = kafka.KafkaProducer(**connection_info, acks='all')
+        self.consumer = kafka.KafkaConsumer(**connection_info, consumer_timeout_ms=0)
         self.consumer.subscribe(topics=[topic])
         self.position = 0
 

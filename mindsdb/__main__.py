842af3f832414792828d7f44ef422391380cb1ac
Max Stepanov <stpmax@yandex.ru>
2021-07-19 13:30:24 +0000

    remove integration if same exists in config

--- mindsdb/__main__.py
+++ mindsdb/__main__.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     print(f"Storage path:\n   {config['paths']['root']}")
 
     # @TODO Backwards compatibiltiy for tests, remove later
-    from mindsdb.interfaces.database.integrations import add_db_integration, get_db_integration
+    from mindsdb.interfaces.database.integrations import add_db_integration, get_db_integration, remove_db_integration
     dbw = DatabaseWrapper(COMPANY_ID)
     model_interface = ModelInterface()
     raw_model_data_arr = model_interface.get_models()
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     if not is_cloud:
         for integration_name in get_db_integrations(COMPANY_ID, sensitive_info=True):
             print(f"Setting up integration: {integration_name}")
-            if get_db_integration(integration_name, COMPANY_ID)['publish']:
+            if get_db_integration(integration_name, COMPANY_ID).get('publish', False):
                 # do setup and register only if it is 'publish' integration
                 dbw.setup_integration(integration_name)
                 dbw.register_predictors(model_data_arr, integration_name=integration_name)
@@ -90,12 +90,13 @@ if __name__ == '__main__':
     for integration_name in config.get('integrations', {}):
         try:
             it = get_db_integration(integration_name, None)
-            if it is None:      # register and setup it only if it doesn't conflict with records in db
-                print(f'Adding: {integration_name}')
-                add_db_integration(integration_name, config['integrations'][integration_name], None)            # Setup for user `None`, since we don't need this for cloud
-                if config['integrations'][integration_name].get('publish', False) and not is_cloud:
-                    dbw.setup_integration(integration_name)
-                    dbw.register_predictors(model_data_arr, integration_name=integration_name)
+            if it is not None:
+                remove_db_integration(integration_name)
+            print(f'Adding: {integration_name}')
+            add_db_integration(integration_name, config['integrations'][integration_name], None)            # Setup for user `None`, since we don't need this for cloud
+            if config['integrations'][integration_name].get('publish', False) and not is_cloud:
+                dbw.setup_integration(integration_name)
+                dbw.register_predictors(model_data_arr, integration_name=integration_name)
         except Exception as e:
             log.error(f'\n\nError: {e} adding database integration {integration_name}\n\n')
 

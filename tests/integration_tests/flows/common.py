de8a6e41282357c2e3e6791cd11ce39c128cd605
George3d6 <george@cerebralab.com>
2021-08-05 14:38:49 +0300

    added lw dep

--- tests/integration_tests/flows/common.py
+++ tests/integration_tests/flows/common.py
@@ -340,6 +340,7 @@ def check_prediction_values(row, to_predict):
         for field_name, field_type in to_predict.items():
             if field_type in [int, float]:
                 print(f'checking {field_name} is int or float')
+                print(row[field_name], type(row[field_name]))
                 assert isinstance(row[field_name], (int, float))
                 print('checking min bound')
                 assert isinstance(row[f'{field_name}_min'], (int, float))

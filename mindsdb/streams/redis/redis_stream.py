b597eb9c29d479b427cc6e3be3bec0ae31c3f26c
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-08-05 08:44:41 +0300

    Back compatibility for creating stream through redis API

--- mindsdb/streams/redis/redis_stream.py
+++ mindsdb/streams/redis/redis_stream.py
@@ -9,9 +9,20 @@ class RedisStream(BaseStream):
         self.client = walrus.Database(**connection_info)
         self.stream = self.client.Stream(stream)
 
+    @staticmethod
+    def _decode(redis_data):
+        decoded = {}
+        for k in redis_data:
+            decoded[k.decode('utf8')] = redis_data[k].decode('utf8')
+        return decoded
+
     def read(self):
         for k, when_data in self.stream.read():
-            yield json.loads(when_data[b''])
+            try:
+                res = json.loads(when_data[b''])
+            except KeyError:
+                res = self._decode(when_data)
+            yield res
             self.stream.delete(k)
 
     def write(self, dct):

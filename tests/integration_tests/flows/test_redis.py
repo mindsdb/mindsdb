ace2e6c672dc66fb9535d00b6d9c9889485a1ba3
Max Stepanov <stpmax@yandex.ru>
2021-08-13 12:20:33 +0000

    fix redis test to run it locally

--- tests/integration_tests/flows/test_redis.py
+++ tests/integration_tests/flows/test_redis.py
@@ -9,8 +9,6 @@ import pandas as pd
 
 from common import HTTP_API_ROOT, run_environment, EXTERNAL_DB_CREDENTIALS, USE_EXTERNAL_DB_SERVER
 
-from mindsdb.streams import RedisStream
-
 INTEGRATION_NAME = 'test_redis'
 redis_creds = {}
 if USE_EXTERNAL_DB_SERVER:
@@ -49,6 +47,7 @@ class RedisTest(unittest.TestCase):
 
     def test_length(self):
         print(f"\nExecuting {self._testMethodName}")
+        from mindsdb.streams import RedisStream
         stream = RedisStream(f'test_stream_length_{STREAM_SUFFIX}', CONNECTION_PARAMS)
 
         self.assertEqual(len(list(stream.read())), 0)
@@ -150,6 +149,7 @@ class RedisTest(unittest.TestCase):
 
     def test_3_making_stream_prediction(self):
         print(f"\nExecuting {self._testMethodName}")
+        from mindsdb.streams import RedisStream
         stream_in = RedisStream(STREAM_IN, CONNECTION_PARAMS)
         stream_out = RedisStream(STREAM_OUT, CONNECTION_PARAMS)
 
@@ -176,6 +176,7 @@ class RedisTest(unittest.TestCase):
 
     def test_5_making_ts_stream_prediction(self):
         print(f"\nExecuting {self._testMethodName}")
+        from mindsdb.streams import RedisStream
         stream_in = RedisStream(STREAM_IN_TS, CONNECTION_PARAMS)
         stream_out = RedisStream(STREAM_OUT_TS, CONNECTION_PARAMS)
 
@@ -184,9 +185,10 @@ class RedisTest(unittest.TestCase):
             time.sleep(5)
 
         self.assertEqual(len(list(stream_out.read())), 2)
-    '''
+    
     def test_6_create_stream_redis_native_api(self):
         print(f"\nExecuting {self._testMethodName}")
+        from mindsdb.streams import RedisStream
         control_stream = RedisStream(CONTROL_STREAM, CONNECTION_PARAMS)
         control_stream.write({
             'action': 'create',
@@ -210,6 +212,7 @@ class RedisTest(unittest.TestCase):
 
     def test_8_test_online_learning(self):
         print(f"\nExecuting {self._testMethodName}")
+        from mindsdb.streams import RedisStream
         control_stream = RedisStream(CONTROL_STREAM, CONNECTION_PARAMS)
         learning_stream = RedisStream(LEARNING_STREAM, CONNECTION_PARAMS)
 
@@ -224,7 +227,7 @@ class RedisTest(unittest.TestCase):
 
         for x in range(1, 101):
             learning_stream.write({'x1': x, 'x2': 2*x, 'y': 3*x})
-        '''
+
 
 if __name__ == "__main__":
     try:

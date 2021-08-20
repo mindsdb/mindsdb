8e6b66f4f8816cdbc50625abde97522a37774155
George3d6 <george@cerebralab.com>
2021-08-20 15:43:42 +0300

    giving predictor json ai object

--- mindsdb/interfaces/model/learn_process.py
+++ mindsdb/interfaces/model/learn_process.py
@@ -140,8 +140,9 @@ def run_update(name: str, company_id: int):
         if 'stop_training_in_x_seconds' in problem_definition:
             problem_definition['time_aim'] = problem_definition['stop_training_in_x_seconds']
 
-        predictor_record.json_ai = lightwood.json_ai_from_problem(df, problem_definition).to_dict()
-        predictor_record.code = lightwood.code_from_json_ai(predictor_record.json_ai)
+        json_ai = lightwood.json_ai_from_problem(df, problem_definition)
+        predictor_record.json_ai = json_ai.to_dict()
+        predictor_record.code = lightwood.code_from_json_ai(json_ai)
         session.commit()
         predictor: lightwood.PredictorInterface = lightwood.predictor_from_code(predictor_record.code)
         predictor.learn(df)

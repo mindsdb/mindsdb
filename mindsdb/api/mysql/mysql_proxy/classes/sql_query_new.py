c1a6f0360fa77aacca11b1625363f737fb084dbe
Max Stepanov <stpmax@yandex.ru>
2021-08-24 11:13:13 +0000

    prepare to new mindsdb_sql

--- mindsdb/api/mysql/mysql_proxy/classes/sql_query_new.py
+++ mindsdb/api/mysql/mysql_proxy/classes/sql_query_new.py
@@ -26,12 +26,15 @@ from mindsdb_sql.parser.ast import (
     UnaryOperation,
     BinaryOperation,
     OrderBy,
-    Star
+    Star,
+    Union,
+    Select
 )
 from mindsdb_sql.planner.steps import (
     FetchDataframeStep,
     ApplyPredictorStep,
     ApplyPredictorRowStep,
+    # MapReduceStep,
     JoinStep,
     UnionStep,
     ProjectStep,
@@ -90,7 +93,19 @@ class SQLQuery():
             name = name + ('.'.join(table_obj.alias.parts),)
             return name
 
-        def get_all_tables(from_stmt):
+        def get_all_tables(stmt):
+            if isinstance(stmt, Union):
+                left = get_all_tables(stmt.left)
+                right = get_all_tables(stmt.right)
+                return left + right
+
+            if isinstance(stmt, Select):
+                from_stmt = stmt.from_table
+            elif isinstance(stmt, (Identifier, Join)):
+                from_stmt = stmt
+            else:
+                raise Exception(f'Unknown type of identifier: {stmt}')
+
             result = []
             if isinstance(from_stmt, Identifier):
                 result.append(from_stmt.parts[-1])
@@ -106,33 +121,28 @@ class SQLQuery():
 
         mindsdb_datanode = self.datahub.get(self.database)
 
-        all_tables = get_all_tables(mindsdb_sql_struct.from_table)
+        all_tables = get_all_tables(mindsdb_sql_struct)
 
         models = mindsdb_datanode.model_interface.get_models()
         model_names = [m['name'] for m in models]
         predictor_metadata = {}
         potential_ts_predictor = False
         for model_name in (set(model_names) & set(all_tables)):
-            # predictors = db.session.query(db.Predictor).filter_by(company_id=self.session.company_id)
-            # for p in predictors:
-            #     if isinstance(p.data, dict) and p.data.get('status') == 'complete':
-            #         ts_settings = p.learn_args.get('learn_args', {}).get('timeseries_settings', {})
-
-            model_meta = mindsdb_datanode.model_interface.get_model_data(name=model_name)
-            self.model_types.update(model_meta.get('dtypes', {}))
-
-            ts_settings = model_meta.get('problem_definition', {}).get('timeseries_settings', {})
-            if ts_settings.get('is_timeseries') is True:
-                window = ts_settings.get('window')
-                order_by = ts_settings.get('order_by')[0]
-                group_by = ts_settings.get('group_by')[0]
-                potential_ts_predictor = True
-                predictor_metadata[model_meta['name']] = {
-                    'timeseries': True,
-                    'window': window,
-                    'order_by_column': order_by,
-                    'group_by_column': group_by
-                }
+            predictors = db.session.query(db.Predictor).filter_by(company_id=self.session.company_id)
+            for p in predictors:
+                if isinstance(p.data, dict) and p.data.get('status') == 'complete':
+                    ts_settings = p.learn_args.get('timeseries_settings', {})
+                    if ts_settings.get('is_timeseries') is True:
+                        window = ts_settings.get('window')
+                        order_by = ts_settings.get('order_by')[0]
+                        group_by = ts_settings.get('group_by')[0]
+                        potential_ts_predictor = True
+                        predictor_metadata[model_name] = {
+                            'timeseries': True,
+                            'window': window,
+                            'order_by_column': order_by,
+                            'group_by_column': group_by
+                        }
 
         if potential_ts_predictor is True:
             mindsdb_sql_struct.limit = None

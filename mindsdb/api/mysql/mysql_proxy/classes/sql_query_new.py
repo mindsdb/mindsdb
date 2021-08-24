e8d55cde30cbeaf590343358c58dc233b1cdf08f
Max Stepanov <stpmax@yandex.ru>
2021-08-24 09:46:34 +0000

    constants in queryes

--- mindsdb/api/mysql/mysql_proxy/classes/sql_query_new.py
+++ mindsdb/api/mysql/mysql_proxy/classes/sql_query_new.py
@@ -263,7 +263,16 @@ class SQLQuery():
                                     else:
                                         appropriate_table = table_name
                             if appropriate_table is None:
-                                raise Exception(f'Can not find approproate table for column {column_name}')
+                                # it is probably constaint
+                                column_name = column_name.strip("'")
+                                name_or_alias = column_name or column_alias
+                                column_alias = name_or_alias
+                                # appropriate_table = ''
+                                for row in step_data:
+                                    for table in row:
+                                        row[table][name_or_alias] = column_name
+                                appropriate_table = list(step_data[0].keys())[0]
+                                # raise Exception(f'Can not find approproate table for column {column_name}')
                             columns_list.append(appropriate_table + (column_name, column_alias))
                         elif len(column_name_parts) == 2:
                             table_name_or_alias = column_name_parts[0]

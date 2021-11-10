d9a24ce42bc9ef5f9dd0950080446ec91feba7c7
Max Stepanov <stpmax@yandex.ru>
2021-11-10 07:26:00 +0000

    del external_ds from mssql test

--- tests/integration_tests/flows/test_mssql.py
+++ tests/integration_tests/flows/test_mssql.py
@@ -2,13 +2,11 @@ import unittest
 import inspect
 from pathlib import Path
 import json
-import requests
 
 import pytds
 
 from common import (
     MINDSDB_DATABASE,
-    HTTP_API_ROOT,
     CONFIG_PATH,
     condition_dict_to_str,
     run_environment
@@ -29,7 +27,6 @@ CONDITION = {
 
 TEST_DATA_TABLE = TEST_DATASET
 TEST_PREDICTOR_NAME = f'{TEST_DATASET}_predictor'
-EXTERNAL_DS_NAME = f'{TEST_DATASET}_external'
 
 INTEGRATION_NAME = 'default_mssql'
 
@@ -98,28 +95,6 @@ class MSSQLTest(unittest.TestCase):
 
         self.assertTrue(len(predictors) == 0)
 
-    def test_2_put_external_ds(self):
-        print(f'\nExecuting {inspect.stack()[0].function}')
-        params = {
-            'name': EXTERNAL_DS_NAME,
-            'query': f'select * from test_data.{TEST_DATA_TABLE} order by sqft offset 0 rows fetch next 50 rows only',
-            'integration_id': INTEGRATION_NAME
-        }
-
-        url = f'{HTTP_API_ROOT}/datasources/{EXTERNAL_DS_NAME}'
-        res = requests.put(url, json=params)
-        self.assertTrue(res.status_code == 200)
-        ds_data = res.json()
-
-        self.assertTrue(ds_data['source_type'] == INTEGRATION_NAME)
-        self.assertTrue(ds_data['row_count'] == 50)
-
-        url = f'{HTTP_API_ROOT}/datasources'
-        res = requests.get(url)
-        self.assertTrue(res.status_code == 200)
-        ds_data = res.json()
-        self.assertTrue(len(ds_data) == 1)
-
     def test_3_insert_predictor(self):
         print(f'\nExecuting {inspect.stack()[0].function}')
         query(f"""
@@ -139,42 +114,6 @@ class MSSQLTest(unittest.TestCase):
         """, as_dict=True, fetch=True)
         self.assertTrue(len(res) == 1)
         self.assertTrue(res[0]['status'] == 'complete')
-
-    def test_4_externael_ds(self):
-        name = f'{TEST_PREDICTOR_NAME}_external'
-
-        query(f"""
-            exec ('
-                insert into predictors (name, predict, external_datasource, training_options)
-                values (
-                    ''{name}'',
-                    ''rental_price'',
-                    ''{EXTERNAL_DS_NAME}'',
-                    ''{{"join_learn_process": true, "stop_training_in_x_seconds": 3}}''
-                )') AT {MINDSDB_DATABASE};
-        """)
-
-        print('predictor record in mindsdb.predictors')
-        res = query(f"""
-            exec ('SELECT status FROM mindsdb.predictors where name = ''{name}''') AT {MINDSDB_DATABASE};
-        """, as_dict=True, fetch=True)
-        self.assertTrue(len(res) == 1)
-        self.assertTrue(res[0]['status'] == 'complete')
-
-        res = query(f"""
-            exec ('
-                select
-                    *
-                from
-                    mindsdb.{name}
-                where
-                    external_datasource=''{EXTERNAL_DS_NAME}''
-            ') AT {MINDSDB_DATABASE};
-        """, as_dict=True, fetch=True)
-
-        print('check result')
-        self.assertTrue(len(res) > 0)
-        self.assertTrue(res[0]['rental_price'] is not None and res[0]['rental_price'] != 'None')
 
     def test_5_query_predictor(self):
         print(f'\nExecuting {inspect.stack()[0].function}')

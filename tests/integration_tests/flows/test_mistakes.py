b45103062b53105df64f4509b1e937a920e33007
Max Stepanov <stpmax@yandex.ru>
2021-11-12 08:18:15 +0000

    fix tests

--- tests/integration_tests/flows/test_mistakes.py
+++ tests/integration_tests/flows/test_mistakes.py
@@ -132,7 +132,7 @@ class MistakesTest_1(unittest.TestCase):
             'data_source_name': 'wrong ds'
         }
         res = requests.put(f'{HTTP_API_ROOT}/predictors/{TEST_PREDICTOR}', json=data)
-        assert 'Can not find datasource' in res.json()['message']
+        assert 'DS not exists' in res.json()['title']
 
         check_predictor_not_exists(TEST_PREDICTOR)
 

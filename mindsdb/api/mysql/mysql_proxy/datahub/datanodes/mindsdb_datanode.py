d804471decf01bdee104e9974164a6af891f6b44
Ilia Tsyplenkov <ilia-tsyplenkov@users.noreply.github.com>
2022-01-14 14:31:44 +0300

    MySqlApi: ts prediciton error if nr_predicitons==1 (#1846)

--- mindsdb/api/mysql/mysql_proxy/datahub/datanodes/mindsdb_datanode.py
+++ mindsdb/api/mysql/mysql_proxy/datahub/datanodes/mindsdb_datanode.py
@@ -329,19 +329,18 @@ class MindsDBDataNode(DataNode):
                         date_values = [date_values]
 
                 for i in range(len(rows) - 1):
-                    rows[i][predict] = rows[i][predict][0]
-                    rows[i][order_by_column] = rows[i][order_by_column][0]
+                    if nr_predictions > 1:
+                        rows[i][predict] = rows[i][predict][0]
+                        rows[i][order_by_column] = rows[i][order_by_column][0]
                     for col in ('predicted_value', 'confidence', 'confidence_lower_bound', 'confidence_upper_bound'):
-                        explanations[i][predict][col] = explanations[i][predict][col][0]
+                        if nr_predictions > 1:
+                            explanations[i][predict][col] = explanations[i][predict][col][0]
 
                 last_row = rows.pop()
                 last_explanation = explanations.pop()
                 for i in range(nr_predictions):
                     new_row = copy.deepcopy(last_row)
-                    if nr_predictions == 1:
-                        new_row[predict] = new_row[predict]
-                        new_row[order_by_column] = new_row[order_by_column]
-                    else:
+                    if nr_predictions > 1:
                         new_row[predict] = new_row[predict][i]
                         new_row[order_by_column] = new_row[order_by_column][i]
                     if '__mindsdb_row_id' in new_row and (i > 0 or __mdb_make_predictions is False):
@@ -350,9 +349,7 @@ class MindsDBDataNode(DataNode):
 
                     new_explanation = copy.deepcopy(last_explanation)
                     for col in ('predicted_value', 'confidence', 'confidence_lower_bound', 'confidence_upper_bound'):
-                        if nr_predictions == 1:
-                            new_explanation[predict][col] = new_explanation[predict][col]
-                        else:
+                        if nr_predictions > 1:
                             new_explanation[predict][col] = new_explanation[predict][col][i]
                     if i != 0:
                         new_explanation[predict]['anomaly'] = None

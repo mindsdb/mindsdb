5882daacb751ba680cf32871a863a761ce1c57ba
Max Stepanov <stpmax@yandex.ru>
2022-03-17 07:44:46 +0000

    fix

--- mindsdb/api/http/start.py
+++ mindsdb/api/http/start.py
@@ -10,7 +10,7 @@ from flask import send_from_directory, request, current_app
 from flask_compress import Compress
 
 from mindsdb.api.http.namespaces.predictor import ns_conf as predictor_ns
-# from mindsdb.api.nlp.nlp import ns_conf as nlp_ns
+from mindsdb.api.nlp.nlp import ns_conf as nlp_ns
 from mindsdb.api.http.namespaces.datasource import ns_conf as datasource_ns
 from mindsdb.api.http.namespaces.util import ns_conf as utils_ns
 from mindsdb.api.http.namespaces.config import ns_conf as conf_ns
@@ -57,8 +57,8 @@ def start(verbose, no_studio, with_nlp):
     api.add_namespace(utils_ns)
     api.add_namespace(conf_ns)
     api.add_namespace(stream_ns)
-    # if with_nlp:
-    #     api.add_namespace(nlp_ns)
+    if with_nlp:
+        api.add_namespace(nlp_ns)
 
     @api.errorhandler(Exception)
     def handle_exception(e):

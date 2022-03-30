44d7ef0e08e5144870ad2831ce6e221f9044c47c
Max Stepanov <stpmax@yandex.ru>
2022-03-30 09:50:32 +0000

    'files' route

---
+++ mindsdb/api/http/namespaces/file.py
@@ -0,0 +1,112 @@
+import os
+import zipfile
+import tarfile
+
+from flask import request
+from flask_restx import Resource, abort
+import tempfile
+import multipart
+
+from mindsdb.utilities.log import log
+from mindsdb.api.http.utils import http_error
+from mindsdb.api.http.namespaces.configs.file import ns_conf
+
+
+@ns_conf.route('/')
+class FilesList(Resource):
+    @ns_conf.doc('get_files_list')
+    def get(self):
+        '''List all files'''
+        return request.default_store.get_files()
+
+
+@ns_conf.route('/<name>')
+@ns_conf.param('name', "MindsDB's name for file")
+class File(Resource):
+    def put(self, name: str):
+        '''add new file'''
+
+        data = {}
+
+        def on_field(field):
+            name = field.field_name.decode()
+            value = field.value.decode()
+            data[name] = value
+
+        file_object = None
+
+        def on_file(file):
+            nonlocal file_object
+            data['file'] = file.file_name.decode()
+            file_object = file.file_object
+
+        temp_dir_path = tempfile.mkdtemp(prefix='mindsdb_file_')
+
+        if request.headers['Content-Type'].startswith('multipart/form-data'):
+            parser = multipart.create_form_parser(
+                headers=request.headers,
+                on_field=on_field,
+                on_file=on_file,
+                config={
+                    'UPLOAD_DIR': temp_dir_path.encode(),    # bytes required
+                    'UPLOAD_KEEP_FILENAME': True,
+                    'UPLOAD_KEEP_EXTENSIONS': True,
+                    'MAX_MEMORY_FILE_SIZE': 0
+                }
+            )
+
+            while True:
+                chunk = request.stream.read(8192)
+                if not chunk:
+                    break
+                parser.write(chunk)
+            parser.finalize()
+            parser.close()
+
+            if file_object is not None and not file_object.closed:
+                file_object.close()
+        else:
+            data = request.json
+
+        mindsdb_file_name = data['name'] if 'name' in data else name
+        original_file_name = data.get('original_file_name')
+
+        file_path = os.path.join(temp_dir_path, data['file'])
+        lp = file_path.lower()
+        if lp.endswith(('.zip', '.tar.gz')):
+            if lp.endswith('.zip'):
+                with zipfile.ZipFile(file_path) as f:
+                    f.extractall(temp_dir_path)
+            elif lp.endswith('.tar.gz'):
+                with tarfile.open(file_path) as f:
+                    f.extractall(temp_dir_path)
+            os.remove(file_path)
+            files = os.listdir(temp_dir_path)
+            if len(files) != 1:
+                os.rmdir(temp_dir_path)
+                return http_error(400, 'Wrong content.', 'Archive must contain only one data file.')
+            file_path = os.path.join(temp_dir_path, files[0])
+            mindsdb_file_name = files[0]
+            if not os.path.isfile(file_path):
+                os.rmdir(temp_dir_path)
+                return http_error(400, 'Wrong content.', 'Archive must contain data file in root.')
+
+        request.default_store.save_file(mindsdb_file_name, file_path, file_name=original_file_name)
+
+        os.rmdir(temp_dir_path)
+
+        return '', 200
+
+    def delete(self, name: str):
+        '''delete file'''
+
+        try:
+            request.default_store.delete_file(name)
+        except Exception as e:
+            log.error(e)
+            return http_error(
+                400,
+                "Error deleting file",
+                f"There was an error while tring to delete file with name '{name}'"
+            )
+        return '', 200

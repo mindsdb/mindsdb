1d0f76a521a05503ed0043ff54773ef632ee89d4
ZoranPandovski <zoran.pandovski@gmail.com>
2022-04-21 15:27:32 +0200

    Add new tests

--- mindsdb/integrations/mysql_handler/tests/test_mysql_handler.py
+++ mindsdb/integrations/mysql_handler/tests/test_mysql_handler.py
@@ -2,7 +2,8 @@ import unittest
 import pandas as pd
 
 from mindsdb.utilities.config import Config
-from mindsdb.integrations.mysql_handler.mysql_handler.mysql_handler import MySQLHandler
+from mindsdb.integrations.mysql_handler.mysql_handler import MySQLHandler
+from mindsdb.api.mysql.mysql_proxy.mysql_proxy import RESPONSE_TYPE
 
 
 class MySQLHandlerTest(unittest.TestCase):
@@ -17,72 +18,37 @@ class MySQLHandlerTest(unittest.TestCase):
             "database": "test",
             "ssl": False
         }
-        cls.handler = MySQLHandler('test_handler', **cls.kwargs)
+        cls.handler = MySQLHandler('test_mysql_handler', **cls.kwargs)
 
     def test_0_connect(self):
-        self.handler.connect()
+        self.handler.check_status()
 
-    def test_1_check_status(self):
-        assert self.handler.check_status()
-
-    def test_2_native_query_show_dbs(self):
+    def test_1_native_query_show_dbs(self):
         dbs = self.handler.native_query("SHOW DATABASES;")
-        assert isinstance(dbs, list)
+        assert dbs['type'] is not RESPONSE_TYPE.ERROR
 
-    def test_3_get_tables(self):
+    
+    def test_2_get_tables(self):
         tbls = self.handler.get_tables()
-        assert isinstance(tbls, list)
+        assert tbls['type'] is not RESPONSE_TYPE.ERROR
 
-    def test_4_get_views(self):
+    def test_3_get_views(self):
         views = self.handler.get_views()
-        assert isinstance(views, list)
-
+        assert views['type'] is not RESPONSE_TYPE.ERROR
+    
     def test_5_drop_table(self):
-        try:
-            result = self.handler.native_query("DROP TABLE test_mdb")
-        except:
-            self.handler.native_query("CREATE TABLE test_mdb (test_col INT)")
-            self.test_5_drop_table_native()
+        res = self.handler.native_query("DROP TABLE IF EXISTS test_mdb")
+        assert res['type'] is not RESPONSE_TYPE.ERROR 
 
-    def test_6_create_table(self):
-        try:
-            self.handler.native_query("CREATE TABLE test_mdb (test_col INT)")
-        except Exception:
-            pass
+    def test_4_create_table(self):
+        res = self.handler.native_query("CREATE TABLE IF NOT EXISTS test_mdb (test_col INT)")
+        assert res['type'] is not RESPONSE_TYPE.ERROR 
 
-    def test_7_describe_table(self):
+    def test_6_describe_table(self):
         described = self.handler.describe_table("test_mdb")
-        assert isinstance(described, list)
+        assert described['type'] is RESPONSE_TYPE.TABLE
 
-    def test_8_select_query(self):
+    def test_7_select_query(self):
         query = "SELECT * FROM test_mdb WHERE 'id'='a'"
         result = self.handler.select_query(query)
-
-    def test_8_select_into(self):
-        try:
-            result = self.handler.native_query("DROP TABLE test_mdb2")
-        except:
-            pass
-        try:
-            self.handler.native_query("CREATE TABLE test_mdb2 (test_col INT)")
-        except Exception:
-            pass
-
-        result = self.handler.select_into('test_mdb2', pd.DataFrame.from_dict({'test_col': [1]}))
-
-        tbls = self.handler.get_tables()
-        assert 'test_mdb2' in [item['Tables_in_test'] for item in tbls]
-
-    def test_9_join(self):
-        self.handler.native_query("INSERT INTO test_mdb(test_col) VALUES (1)")
-        self.handler.native_query("INSERT INTO test_mdb2(test_col) VALUES (1)")
-
-        into_table = 'test_join_into_mysql'
-        query = f"SELECT test_col FROM test_mdb JOIN test_mdb2"
-        parsed = self.handler.parser(query, dialect=self.handler.dialect)
-        result = self.handler.join(parsed, self.handler, into=into_table)
-        assert len(result) > 0
-
-        q = f"SELECT * FROM {into_table}"
-        qp = self.handler.parser(q, dialect='mysql')
-        assert len(self.handler.select_query(query)) > 0
+        assert result['type'] is RESPONSE_TYPE.TABLE

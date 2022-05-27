c98373d75f364b750293e0efdc7a31de3fa93c45
Max Stepanov <stpmax@yandex.ru>
2022-05-27 13:39:15 +0000

    fix mariadb handler

--- mindsdb/integrations/handlers/mariadb_handler/mariadb_handler.py
+++ mindsdb/integrations/handlers/mariadb_handler/mariadb_handler.py
@@ -1,10 +1,11 @@
-from mindsdb.integrations.mysql_handler.mysql_handler import MySQLHandler
+from ..mysql_handler.mysql_handler import MySQLHandler
 
 
 class MariaDBHandler(MySQLHandler):
     """
-    This handler handles connection and execution of the MariaDB statements. 
+    This handler handles connection and execution of the MariaDB statements.
     """
+    type = 'mariadb'
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)

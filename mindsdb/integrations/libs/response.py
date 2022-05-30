555f0bc4176306e61eb9483f674b104691d399da
Max Stepanov <stpmax@yandex.ru>
2022-05-30 15:02:31 +0000

    hanlder response class

--- mindsdb/integrations/libs/response.py
+++ mindsdb/integrations/libs/response.py
@@ -1,16 +1,13 @@
-from typing import List, Dict
+from pandas import DataFrame
 
 from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
 
 
 class HandlerResponse:
-    def __init__(self, resp_type: RESPONSE_TYPE, columns: List[Dict] = None, data: List[Dict] = None,
-                 status: int = None, state_track: List[List] = None, error_code: int = None, error_message: str = None):
+    def __init__(self, resp_type: RESPONSE_TYPE, data_frame: DataFrame = None,
+                 error_code: int = 0, error_message: str = None):
         self.resp_type = resp_type
-        self.columns = columns
-        self.data = data
-        self.status = status
-        self.state_track = state_track
+        self.data_frame = data_frame
         self.error_code = error_code
         self.error_message = error_message
 

b6b42699b6224a2a5796b5df2b97604a35d8e7b8
andrew <elkin@raketa.im>
2022-06-17 13:07:07 +0300

    rework mongo server in progress

--- mindsdb/api/mongo/responders/find.py
+++ mindsdb/api/mongo/responders/find.py
@@ -2,16 +2,72 @@ from bson.int64 import Int64
 from collections import OrderedDict
 
 from lightwood.api import dtype
-
+from mindsdb_sql.parser.ast import *
 import mindsdb.api.mongo.functions as helpers
 from mindsdb.api.mongo.classes import Responder
 from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
-
+from mindsdb.integrations.handlers.mongodb_handler.utils.mongodb_ast import MongoToAst
+from mindsdb.api.mysql.mysql_proxy.classes.sql_query import SQLQuery
 
 class Responce(Responder):
     when = {'find': helpers.is_true}
 
     def result(self, query, request_env, mindsdb_env, session):
+
+        mongoToAst = MongoToAst()
+
+        if not query['singleBatch'] and 'collection' in query.get('filter'):
+            # convert to join
+
+            # upper query
+            ast_query = mongoToAst.find(
+                collection=query['find'],
+                projection=query.get('projection'),
+                sort=query.get('sort'),
+                limit=query.get('limit'),
+                skip=query.get('skip'),
+            )
+
+            # table_query
+            collection = query['filter']['collection']
+            filter = query['filter']['query']
+            table_select = mongoToAst.find(
+                collection=collection,
+                filter=filter,
+            )
+
+            # convert to join
+            predictor = ast_query.from_table
+
+            ast_join = Join(
+                left=table_select,
+                right=predictor,
+                join_type=None
+            )
+            ast_query.from_table = ast_join
+
+        else:
+            # is single table
+            ast_query = mongoToAst.find(
+                collection=query['find'],
+                filter=query.get('filter'),
+                projection=query.get('projection'),
+                sort=query.get('sort'),
+                limit=query.get('limit'),
+                skip=query.get('skip'),
+            )
+
+        query = SQLQuery(
+            ast_query,
+            session=session
+        )
+
+        result = query.fetch(
+            session.datahub
+        )
+
+        # TODO
+
         models = mindsdb_env['model_interface'].get_models()
         model_names = [x['name'] for x in models]
         table = query['find']

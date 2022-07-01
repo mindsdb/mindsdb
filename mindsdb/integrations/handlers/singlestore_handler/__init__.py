e921c55fdadbe460e8798922168f736e622120d4
ZoranPandovski <zoran.pandovski@gmail.com>
2022-07-01 16:07:02 +0200

    Add missing structure

---
+++ mindsdb/integrations/handlers/singlestore_handler/__init__.py
@@ -0,0 +1,21 @@
+from mindsdb.integrations.libs.const import HANDLER_TYPE
+
+try:
+    from .singlestore_handler import SingleStoreHandler as Handler
+    import_error = None
+except Exception as e:
+    Handler = None
+    import_error = e
+from .__about__ import __version__ as version, __description__ as description
+
+
+title = 'SingleStore'
+name = 'singlestore'
+version = 0.1
+type = HANDLER_TYPE.DATA
+icon_path = 'icon.svg'
+
+__all__ = [
+    'Handler', 'version', 'name', 'type', 'title', 'description',
+    'import_error', 'icon_path'
+]

d7b84a0df700ade635ce4aea3251802337c7eb17
Minura Punchihewa <minurap@jkintranet.com>
2022-07-11 00:36:45 +0530

    implemented the get_tables and get_columns methods

--- mindsdb/integrations/handlers/firebird_handler/firebird_handler.py
+++ mindsdb/integrations/handlers/firebird_handler/firebird_handler.py
@@ -158,3 +158,84 @@ class FirebirdHandler(DatabaseHandler):
         renderer = SqlalchemyRender('firebird')
         query_str = renderer.get_string(query, with_failback=True)
         return self.native_query(query_str)
+
+    def get_tables(self) -> StatusResponse:
+        """
+        Return list of entities that will be accessible as tables.
+        Returns:
+            HandlerResponse
+        """
+
+        query = """
+            SELECT RDB$RELATION_NAME 
+            FROM RDB$RELATIONS
+            WHERE (RDB$SYSTEM_FLAG <> 1 OR RDB$SYSTEM_FLAG IS NULL) AND RDB$VIEW_BLR IS NULL
+            ORDER BY RDB$RELATION_NAME;
+        """
+        result = self.native_query(query)
+        df = result.data_frame
+        df[df.columns[0]] = df[df.columns[0]].apply(lambda row: row.strip())
+        result.data_frame = df.rename(columns={df.columns[0]: 'table_name'})
+        return result
+
+    def get_columns(self, table_name: str) -> StatusResponse:
+        """
+        Returns a list of entity columns.
+        Args:
+            table_name (str): name of one of tables returned by self.get_tables()
+        Returns:
+            HandlerResponse
+        """
+
+        query = f"""
+            SELECT
+              RF.RDB$FIELD_NAME FIELD_NAME,
+              CASE F.RDB$FIELD_TYPE
+                WHEN 7 THEN
+                  CASE F.RDB$FIELD_SUB_TYPE
+                    WHEN 0 THEN 'SMALLINT'
+                    WHEN 1 THEN 'NUMERIC(' || F.RDB$FIELD_PRECISION || ', ' || (-F.RDB$FIELD_SCALE) || ')'
+                    WHEN 2 THEN 'DECIMAL'
+                  END
+                WHEN 8 THEN
+                  CASE F.RDB$FIELD_SUB_TYPE
+                    WHEN 0 THEN 'INTEGER'
+                    WHEN 1 THEN 'NUMERIC('  || F.RDB$FIELD_PRECISION || ', ' || (-F.RDB$FIELD_SCALE) || ')'
+                    WHEN 2 THEN 'DECIMAL'
+                  END
+                WHEN 9 THEN 'QUAD'
+                WHEN 10 THEN 'FLOAT'
+                WHEN 12 THEN 'DATE'
+                WHEN 13 THEN 'TIME'
+                WHEN 14 THEN 'CHAR(' || (TRUNC(F.RDB$FIELD_LENGTH / CH.RDB$BYTES_PER_CHARACTER)) || ') '
+                WHEN 16 THEN
+                  CASE F.RDB$FIELD_SUB_TYPE
+                    WHEN 0 THEN 'BIGINT'
+                    WHEN 1 THEN 'NUMERIC(' || F.RDB$FIELD_PRECISION || ', ' || (-F.RDB$FIELD_SCALE) || ')'
+                    WHEN 2 THEN 'DECIMAL'
+                  END
+                WHEN 27 THEN 'DOUBLE'
+                WHEN 35 THEN 'TIMESTAMP'
+                WHEN 37 THEN 'VARCHAR(' || (TRUNC(F.RDB$FIELD_LENGTH / CH.RDB$BYTES_PER_CHARACTER)) || ')'
+                WHEN 40 THEN 'CSTRING' || (TRUNC(F.RDB$FIELD_LENGTH / CH.RDB$BYTES_PER_CHARACTER)) || ')'
+                WHEN 45 THEN 'BLOB_ID'
+                WHEN 261 THEN 'BLOB SUB_TYPE ' || F.RDB$FIELD_SUB_TYPE
+                ELSE 'RDB$FIELD_TYPE: ' || F.RDB$FIELD_TYPE || '?'
+              END FIELD_TYPE,
+              IIF(COALESCE(RF.RDB$NULL_FLAG, 0) = 0, NULL, 'NOT NULL') FIELD_NULL,
+              CH.RDB$CHARACTER_SET_NAME FIELD_CHARSET,
+              DCO.RDB$COLLATION_NAME FIELD_COLLATION,
+              COALESCE(RF.RDB$DEFAULT_SOURCE, F.RDB$DEFAULT_SOURCE) FIELD_DEFAULT,
+              F.RDB$VALIDATION_SOURCE FIELD_CHECK,
+              RF.RDB$DESCRIPTION FIELD_DESCRIPTION
+            FROM RDB$RELATION_FIELDS RF
+            JOIN RDB$FIELDS F ON (F.RDB$FIELD_NAME = RF.RDB$FIELD_SOURCE)
+            LEFT OUTER JOIN RDB$CHARACTER_SETS CH ON (CH.RDB$CHARACTER_SET_ID = F.RDB$CHARACTER_SET_ID)
+            LEFT OUTER JOIN RDB$COLLATIONS DCO ON ((DCO.RDB$COLLATION_ID = F.RDB$COLLATION_ID) AND (DCO.RDB$CHARACTER_SET_ID = F.RDB$CHARACTER_SET_ID))
+            WHERE (RF.RDB$RELATION_NAME = '{table_name.upper()}') AND (COALESCE(RF.RDB$SYSTEM_FLAG, 0) = 0)
+            ORDER BY RF.RDB$FIELD_POSITION;
+        """
+        result = self.native_query(query)
+        df = result.data_frame
+        result.data_frame = df.rename(columns={'FIELD_NAME': 'column_name', 'FIELD_TYPE': 'data_type'})
+        return result

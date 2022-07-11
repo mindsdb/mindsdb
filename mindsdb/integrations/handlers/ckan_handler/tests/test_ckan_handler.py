118b079ddf5c4ecfaa13df6fed527b94cca07a17
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2022-07-11 18:06:37 +0200

    Fix test

--- mindsdb/integrations/handlers/ckan_handler/tests/test_ckan_handler.py
+++ mindsdb/integrations/handlers/ckan_handler/tests/test_ckan_handler.py
@@ -8,10 +8,14 @@ class CkanHandlerTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.handler = CkanHandler('test_ckan_handler')
+        cls.kwargs = {
+            "connection_data": {
+                "url": "http://demo.ckan.org/",
+                "api_key": "demo"}
+        }
 
     def test_connect(self):
-        kwargs = {"url": "https://demo.ckan.org/"}
-        self.handler.connect(**kwargs)
+        self.handler.connect()
 
     def test_query(self):
         self.handler.query("SELECT * from 'b53c9e72-6b59-4cda-8c0c-7d6a51dad12a'")

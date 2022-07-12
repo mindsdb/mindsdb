e809721e270ce5d69a7357c1bdc23d41dbd48f6f
Minura Punchihewa <minurap@jkintranet.com>
2022-07-12 23:16:24 +0530

    implemented the native_query and query methods

--- mindsdb/integrations/handlers/databricks_handler/databricks_handler.py
+++ mindsdb/integrations/handlers/databricks_handler/databricks_handler.py
@@ -103,3 +103,55 @@ class DatabricksHandler(DatabaseHandler):
                 self.is_connected = False
 
         return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """
+        Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
+        """
+
+        need_to_close = self.is_connected is False
+
+        connection = self.connect()
+        with connection.cursor() as cursor:
+            try:
+                result = cursor.fetchall()
+                if result:
+                    response = Response(
+                        RESPONSE_TYPE.TABLE,
+                        data_frame=pd.DataFrame(
+                            result,
+                            columns=[x[0] for x in cursor.description]
+                        )
+                    )
+
+                else:
+                    response = Response(RESPONSE_TYPE.OK)
+                    connection.commit()
+            except Exception as e:
+                log.error(f'Error running query: {query} on {self.connection_data["schema"]}!')
+                response = Response(
+                    RESPONSE_TYPE.ERROR,
+                    error_message=str(e)
+                )
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> StatusResponse:
+        """
+        Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
+        renderer = SqlalchemyRender('databricks')
+        query_str = renderer.get_string(query, with_failback=True)
+        return self.native_query(query_str)

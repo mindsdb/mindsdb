53db9daaa25455506769f204fae9fbeed2420966
Minura Punchihewa <minurap@jkintranet.com>
2022-07-18 19:46:30 +0530

    implemented the check_connection, native_query and query methods

--- mindsdb/integrations/handlers/redshift_handler/redshift_handler.py
+++ mindsdb/integrations/handlers/redshift_handler/redshift_handler.py
@@ -6,7 +6,7 @@ import redshift_connector
 
 from mindsdb_sql import parse_sql
 from mindsdb_sql.render.sqlalchemy_render import SqlalchemyRender
-from sqlalchemy_databricks import DatabricksDialect
+from sqlalchemy_redshift import RedshiftDialect
 from mindsdb.integrations.libs.base_handler import DatabaseHandler
 
 from mindsdb_sql.parser.ast.base import ASTNode
@@ -81,4 +81,79 @@ class RedshiftHandler(DatabaseHandler):
         self.is_connected = False
         return self.is_connected
 
+    def check_connection(self) -> StatusResponse:
+        """
+        Check connection to the handler.
+        Returns:
+            HandlerStatusResponse
+        """
 
+        response = StatusResponse(False)
+        need_to_close = self.is_connected is False
+
+        try:
+            self.connect()
+            response.success = True
+        except Exception as e:
+            log.error(f'Error connecting to Redshift {self.connection_data["database"]}, {e}!')
+            response.error_message = str(e)
+        finally:
+            if response.success is True and need_to_close:
+                self.disconnect()
+            if response.success is False and self.is_connected is True:
+                self.is_connected = False
+
+        return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """
+        Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
+        """
+
+        need_to_close = self.is_connected is False
+
+        connection = self.connect()
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                if result:
+                    response = Response(
+                        RESPONSE_TYPE.TABLE,
+                        data_frame=pd.DataFrame(
+                            result,
+                            columns=[x[0] for x in cursor.description]
+                        )
+                    )
+
+                else:
+                    response = Response(RESPONSE_TYPE.OK)
+                    connection.commit()
+            except Exception as e:
+                log.error(f'Error running query: {query} on {self.connection_data["database"]}!')
+                response = Response(
+                    RESPONSE_TYPE.ERROR,
+                    error_message=str(e)
+                )
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> StatusResponse:
+        """
+        Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
+        renderer = SqlalchemyRender(RedshiftDialect)
+        query_str = renderer.get_string(query, with_failback=True)
+        return self.native_query(query_str)

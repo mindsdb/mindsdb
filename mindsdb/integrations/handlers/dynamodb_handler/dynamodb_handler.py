3d7f9f546384bec33c7703a408a25c3f346c83ce
Minura Punchihewa <minurap@jkintranet.com>
2022-07-24 20:39:55 +0530

    added the connection_args and connection_args_example dicts

--- mindsdb/integrations/handlers/dynamodb_handler/dynamodb_handler.py
+++ mindsdb/integrations/handlers/dynamodb_handler/dynamodb_handler.py
@@ -175,3 +175,25 @@ class DyanmoDBHandler(DatabaseHandler):
         )
 
         return response
+
+
+connection_args = OrderedDict(
+    aws_access_key_id={
+        'type': ARG_TYPE.STR,
+        'description': 'The access key for the AWS account.'
+    },
+    aws_secret_access_key={
+        'type': ARG_TYPE.STR,
+        'description': 'The secret key for the AWS account.'
+    },
+    region_name={
+        'type': ARG_TYPE.STR,
+        'description': 'The AWS region where the DynamoDB tables are created.'
+    }
+)
+
+connection_args_example = OrderedDict(
+    aws_access_key_id='PCAQ2LJDOSWLNSQKOCPW',
+    aws_secret_access_key='U/VjewPlNopsDmmwItl34r2neyC6WhZpUiip57i',
+    region_name='us-east-1'
+)

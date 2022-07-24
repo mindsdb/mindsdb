19bc07bcdf38cdd71744eda41c8b42fe1a859ca8
Minura Punchihewa <minurap@jkintranet.com>
2022-07-24 20:51:50 +0530

    updated __init__.py, __about__.py, icon.png, requirements.txt

--- mindsdb/integrations/handlers/dynamodb_handler/__about__.py
+++ mindsdb/integrations/handlers/dynamodb_handler/__about__.py
@@ -1,7 +1,7 @@
-__title__ = 'MindsDB Firebird handler'
-__package_name__ = 'mindsdb_firebird_handler'
+__title__ = 'MindsDB DynamoDB handler'
+__package_name__ = 'mindsdb_dynamodb_handler'
 __version__ = '0.0.1'
-__description__ = "MindsDB handler for Firebird"
+__description__ = "MindsDB handler for DynamoDB"
 __author__ = 'Minura Punchihewa'
 __github__ = 'https://github.com/mindsdb/mindsdb'
 __pypi__ = 'https://pypi.org/project/mindsdb/'

336850a72c5d8ca1a760a81b6c61ec199bfbdf20
parthiv11 <parthivmakwana11@gmail.com>
2022-07-31 04:34:30 +0530

    ADDED DocuMENtation ;>

--- mindsdb/integrations/handlers/db2_handler/db2_handler.py
+++ mindsdb/integrations/handlers/db2_handler/db2_handler.py
@@ -28,7 +28,12 @@ class DB2Handler(DatabaseHandler):
     name= 'DB2'
 
     def __init__(self, name: str, connection_data: Optional[dict], **kwargs):
-
+        """ Initialize the handler
+        Args:
+            name (str): name of particular handler instance
+            connection_data (dict): parameters for connecting to the database
+            **kwargs: arbitrary keyword arguments.
+        """
         super().__init__(name)
         
         self.kwargs = kwargs
@@ -57,6 +62,12 @@ class DB2Handler(DatabaseHandler):
           
     
     def connect(self):
+        """ Set up any connections required by the handler
+        Should return output of check_connection() method after attempting
+        connection. Should switch self.is_connected.
+        Returns:
+            Connection Object
+        """
         if self.is_connected is True:
             return self.connection
 
@@ -72,6 +83,9 @@ class DB2Handler(DatabaseHandler):
 
 
     def disconnect(self):
+        """ Close any existing connections
+        Should switch self.is_connected.
+        """
         if self.is_connected is False:
             return
         try:
@@ -83,6 +97,10 @@ class DB2Handler(DatabaseHandler):
 
 
     def check_connection(self) -> StatusResponse:
+        """ Check connection to the handler
+        Returns:
+            HandlerStatusResponse
+        """
         responseCode = StatusResponse(False)
         need_to_close = self.is_connected is False
 
@@ -102,6 +120,13 @@ class DB2Handler(DatabaseHandler):
 
 
     def native_query(self, query: str) -> StatusResponse:
+        """Receive raw query and act upon it somehow.
+        Args:
+            query (Any): query in native format (str for sql databases,
+                dict for mongo, etc)
+        Returns:
+            HandlerResponse
+        """
         need_to_close = self.is_connected is False
         query=query.upper()
         conn = self.connect()
@@ -136,6 +161,13 @@ class DB2Handler(DatabaseHandler):
 
     
     def query(self, query: ASTNode) -> StatusResponse:
+        """Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
         
 
 
@@ -145,6 +177,13 @@ class DB2Handler(DatabaseHandler):
 
 
     def get_tables(self) -> StatusResponse:
+        """ Return list of entities
+        Return list of entities that will be accesible as tables.
+        Returns:
+            HandlerResponse: shoud have same columns as information_schema.tables
+                (https://dev.mysql.com/doc/refman/8.0/en/information-schema-tables-table.html)
+                Column 'TABLE_NAME' is mandatory, other is optional.
+        """
         self.connect()
 
 
@@ -175,6 +214,16 @@ class DB2Handler(DatabaseHandler):
 
     
     def get_columns(self, table_name: str) -> StatusResponse:
+        """ Returns a list of entity columns
+        Args:
+            table_name (str): name of one of tables returned by self.get_tables()
+        Returns:
+            HandlerResponse: shoud have same columns as information_schema.columns
+                (https://dev.mysql.com/doc/refman/8.0/en/information-schema-columns-table.html)
+                Column 'COLUMN_NAME' is mandatory, other is optional. Hightly
+                recomended to define also 'DATA_TYPE': it should be one of
+                python data types (by default it str).
+        """
         
         self.connect()
 
@@ -248,3 +297,4 @@ connection_args_example = OrderedDict(
     schemaName="db2admin",
     database="BOOKS",
 )
+

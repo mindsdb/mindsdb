c38130fdc60691f7311a7082cac67fd573e4bf89
Minura Punchihewa <minurap@jkintranet.com>
2022-08-01 22:19:07 +0530

    addded tests

--- mindsdb/integrations/handlers/access_handler/tests/test_access_handler.py
+++ mindsdb/integrations/handlers/access_handler/tests/test_access_handler.py
@@ -1,21 +1,21 @@
 import unittest
-from mindsdb.integrations.handlers.sqlite_handler.sqlite_handler import SQLiteHandler
+from mindsdb.integrations.handlers.access_handler.access_handler import AccessHandler
 from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
 
 
-class SQLiteHandlerTest(unittest.TestCase):
+class AccessHandlerTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.kwargs = {
-            "db_file": "chinook.db",
+            "db_file": 'C:\\Users\\minurap\\Documents\\Learning\\Microsoft Access\\example_db.accdb',
         }
-        cls.handler = SQLiteHandler('test_sqlite_handler', cls.kwargs)
+        cls.handler = AccessHandler('test_access_handler', cls.kwargs)
 
     def test_0_check_connection(self):
         assert self.handler.check_connection()
 
     def test_1_native_query_select(self):
-        query = "SELECT * FROM customers"
+        query = "SELECT * FROM example_tbl"
         result = self.handler.native_query(query)
         assert result.type is RESPONSE_TYPE.TABLE
 
@@ -23,8 +23,8 @@ class SQLiteHandlerTest(unittest.TestCase):
         tables = self.handler.get_tables()
         assert tables.type is not RESPONSE_TYPE.ERROR
 
-    def test_4_get_columns(self):
-        columns = self.handler.get_columns('customers')
+    def test_3_get_columns(self):
+        columns = self.handler.get_columns('example_tbl')
         assert columns.type is not RESPONSE_TYPE.ERROR
 
 

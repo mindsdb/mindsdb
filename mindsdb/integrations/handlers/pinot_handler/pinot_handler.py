80edba0eff4c7b50d2cacda94a8ca2089fa8c430
Minura Punchihewa <minura.jkh@keells.com>
2022-08-15 00:13:45 +0530

    implemented the connection_args dicts and made the scheme parameter optional

--- mindsdb/integrations/handlers/pinot_handler/pinot_handler.py
+++ mindsdb/integrations/handlers/pinot_handler/pinot_handler.py
@@ -47,6 +47,9 @@ class PinotHandler(DatabaseHandler):
             if parameter not in connection_data:
                 connection_data[parameter] = None
 
+        if 'scheme' not in connection_data:
+            connection_data['scheme'] = 'http'
+
         self.connection_data = connection_data
         self.kwargs = kwargs
 
@@ -222,9 +225,45 @@ class PinotHandler(DatabaseHandler):
 
 
 connection_args = OrderedDict(
-
+    host={
+        'type': ARG_TYPE.STR,
+        'description': 'The host name or IP address of the Apache Pinot cluster.'
+    },
+    broker_port={
+        'type': ARG_TYPE.INT,
+        'description': 'The port that the Broker of the Apache Pinot cluster is running on.'
+    },
+    controller_port={
+        'type': ARG_TYPE.INT,
+        'description': 'The port that the Controller of the Apache Pinot cluster is running on.'
+    },
+    path={
+        'type': ARG_TYPE.STR,
+        'description': 'The query path.'
+    },
+    scheme={
+        'type': ARG_TYPE.STR,
+        'description': 'The URI schema. This parameter is optional and the default will be https.'
+    },
+    username={
+        'type': ARG_TYPE.STR,
+        'description': 'The user name used to authenticate with the Apache Pinot cluster. This parameter is optional.'
+    },
+    password={
+        'type': ARG_TYPE.STR,
+        'description': 'The password used to authenticate with the Apache Pinot cluster. This parameter is optional.'
+    },
+    verify_ssl={
+        'type': ARG_TYPE.STR,
+        'description': 'The flag for whether SSL certificates should be verified or not. This parameter is optional and '
+                       'if specified, it should be either True or False'
+    },
 )
 
 connection_args_example = OrderedDict(
-
+    host='localhost',
+    broker_port=8000,
+    controller_port=9000,
+    path='/query/sql',
+    scheme='http'
 )

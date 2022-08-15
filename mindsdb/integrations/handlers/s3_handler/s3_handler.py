4c20820d35782ed27f41e964ad2a429420b0eb67
Minura Punchihewa <minura.jkh@keells.com>
2022-08-15 22:57:49 +0530

    implemented the connection_args and connection_args_example dicts

--- mindsdb/integrations/handlers/s3_handler/s3_handler.py
+++ mindsdb/integrations/handlers/s3_handler/s3_handler.py
@@ -182,7 +182,7 @@ class S3Handler(DatabaseHandler):
 
         return response
 
-    def get_columns(self, table_name: str = None) -> StatusResponse:
+    def get_columns(self) -> StatusResponse:
         """
         Returns a list of entity columns.
         Args:
@@ -208,11 +208,39 @@ class S3Handler(DatabaseHandler):
 
 
 connection_args = OrderedDict(
-
+    aws_access_key_id={
+        'type': ARG_TYPE.STR,
+        'description': 'The access key for the AWS account.'
+    },
+    aws_secret_access_key={
+        'type': ARG_TYPE.STR,
+        'description': 'The secret key for the AWS account.'
+    },
+    region_name={
+        'type': ARG_TYPE.STR,
+        'description': 'The AWS region where the S3 bucket is located.'
+    },
+    bucket={
+        'type': ARG_TYPE.STR,
+        'description': 'The name of the S3 bucket.'
+    },
+    key={
+        'type': ARG_TYPE.STR,
+        'description': 'The key of the object to be queried.'
+    },
+    input_serialization={
+        'type': ARG_TYPE.STR,
+        'description': 'The format of the data in the object that is to be queried.'
+    }
 )
 
 connection_args_example = OrderedDict(
-
+    aws_access_key_id='PCAQ2LJDOSWLNSQKOCPW',
+    aws_secret_access_key='U/VjewPlNopsDmmwItl34r2neyC6WhZpUiip57i',
+    region_name='us-east-1',
+    bucket='mindsdb-bucket',
+    key='iris.csv',
+    input_serialization="{'CSV': {'FileHeaderInfo': 'NONE'}}",
 )
 
 

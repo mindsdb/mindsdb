cc40e400d4b68b4fca102ed998e1142da127fff7
Minura Punchihewa <minura.jkh@keells.com>
2022-08-17 20:17:23 +0530

    implemented the query() and native_query() methods

--- mindsdb/integrations/handlers/airtable_handler/airtable_handler.py
+++ mindsdb/integrations/handlers/airtable_handler/airtable_handler.py
@@ -121,3 +121,55 @@ class AirtableHandler(DatabaseHandler):
                 self.is_connected = False
 
         return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """
+        Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
+        """
+
+        need_to_close = self.is_connected is False
+
+        connection = self.connect()
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                if result:
+                    response = Response(
+                        RESPONSE_TYPE.TABLE,
+                        data_frame=pd.DataFrame(
+                            result,
+                            columns=[x[0] for x in cursor.description]
+                        )
+                    )
+
+                else:
+                    response = Response(RESPONSE_TYPE.OK)
+                    connection.commit()
+            except Exception as e:
+                log.error(f'Error running query: {query} on table {self.connection_data["table_name"]} in base {self.connection_data["base_id"]}!')
+                response = Response(
+                    RESPONSE_TYPE.ERROR,
+                    error_message=str(e)
+                )
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> StatusResponse:
+        """
+        Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
+
+        return self.native_query(query.to_string())

a4750f37f28b616ea9c85bdd0f98691f2d094b7a
Minura Punchihewa <minura.jkh@keells.com>
2022-08-19 23:18:44 +0530

    set up the handler class for Elasticsearch and implemented the connect() method

---
+++ mindsdb/integrations/handlers/elasticsearch_handler/elasticsearch_handler.py
@@ -0,0 +1,65 @@
+from typing import Optional
+from collections import OrderedDict
+
+import pandas as pd
+from elasticsearch import Elasticsearch
+
+from mindsdb_sql import parse_sql
+from mindsdb.integrations.libs.base_handler import DatabaseHandler
+
+from mindsdb_sql.parser.ast.base import ASTNode
+
+from mindsdb.utilities.log import log
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+    HandlerResponse as Response,
+    RESPONSE_TYPE
+)
+from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_TYPE
+
+
+class ElasticsearchHandler(DatabaseHandler):
+    """
+    This handler handles connection and execution of the Airtable statements.
+    """
+
+    name = 'elasticsearch'
+
+    def __init__(self, name: str, connection_data: Optional[dict], **kwargs):
+        """
+        Initialize the handler.
+        Args:
+            name (str): name of particular handler instance
+            connection_data (dict): parameters for connecting to the database
+            **kwargs: arbitrary keyword arguments.
+        """
+        super().__init__(name)
+        self.parser = parse_sql
+        self.dialect = 'elasticsearch'
+        self.connection_data = connection_data
+        self.kwargs = kwargs
+
+        self.connection = None
+        self.is_connected = False
+
+    def __del__(self):
+        if self.is_connected is True:
+            self.disconnect()
+
+    def connect(self) -> StatusResponse:
+        """
+        Set up the connection required by the handler.
+        Returns:
+            HandlerStatusResponse
+        """
+
+        if self.is_connected is True:
+            return self.connection
+
+        self.connection = Elasticsearch(
+            hosts=self.connection_data['hosts'].split(','),
+            basic_auth=(self.connection_data['username'], self.connection_data['password'])
+        )
+        self.is_connected = True
+
+        return self.connection

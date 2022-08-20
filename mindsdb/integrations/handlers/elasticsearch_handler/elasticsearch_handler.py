e8b1efdd3dff63080a6fefe988c4983a33f641c7
Minura Punchihewa <minura.jkh@keells.com>
2022-08-20 21:56:40 +0530

    implemented the disconnect(), check_connection(), native_query() and query() methods

--- mindsdb/integrations/handlers/elasticsearch_handler/elasticsearch_handler.py
+++ mindsdb/integrations/handlers/elasticsearch_handler/elasticsearch_handler.py
@@ -5,6 +5,8 @@ import pandas as pd
 from elasticsearch import Elasticsearch
 
 from mindsdb_sql import parse_sql
+from mindsdb_sql.render.sqlalchemy_render import SqlalchemyRender
+from es.elastic.sqlalchemy import ESDialect
 from mindsdb.integrations.libs.base_handler import DatabaseHandler
 
 from mindsdb_sql.parser.ast.base import ASTNode
@@ -64,3 +66,102 @@ class ElasticsearchHandler(DatabaseHandler):
         self.is_connected = True
 
         return self.connection
+
+    def disconnect(self):
+        """
+        Close any existing connections.
+        """
+
+        if self.is_connected is False:
+            return
+
+        self.connection.close()
+        self.is_connected = False
+        return self.is_connected
+
+    def check_connection(self) -> StatusResponse:
+        """
+        Check connection to the handler.
+        Returns:
+            HandlerStatusResponse
+        """
+
+        response = StatusResponse(False)
+        need_to_close = self.is_connected is False
+
+        try:
+            self.connect()
+            response.success = True
+        except Exception as e:
+            log.error(f'Error connecting to Elasticsearch {self.connection_data["hosts"]}, {e}!')
+            response.error_message = str(e)
+        finally:
+            if response.success is True and need_to_close:
+                self.disconnect()
+            if response.success is False and self.is_connected is True:
+                self.is_connected = False
+
+        return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """
+        Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
+        """
+
+        need_to_close = self.is_connected is False
+
+        connection = self.connect()
+
+        try:
+            response = connection.sql.query(body={'query': query})
+            records = response['rows']
+            columns = response['columns']
+
+            new_records = True
+            while new_records:
+                try:
+                    if response['cursor']:
+                        response = connection.sql.query(body={'query': query, 'cursor': response['cursor']})
+
+                        new_records = response['rows']
+                        records = records + new_records
+                except Exception as e:
+                    new_records = False
+
+            if records:
+                response = Response(
+                    RESPONSE_TYPE.TABLE,
+                    data_frame=pd.DataFrame(
+                        records,
+                        columns=[column['name'] for column in columns]
+                    )
+                )
+        except Exception as e:
+            log.error(f'Error running query: {query} on {self.connection_data["database"]}!')
+            response = Response(
+                RESPONSE_TYPE.ERROR,
+                error_message=str(e)
+            )
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> StatusResponse:
+        """
+        Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
+
+        renderer = SqlalchemyRender(ESDialect)
+        query_str = renderer.get_string(query, with_failback=True)
+        return self.native_query(query_str)

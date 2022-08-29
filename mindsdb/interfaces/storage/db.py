81eb5fcd12dcba5edf650c28444cee947170f8d5
Max Stepanov <stpmax@yandex.ru>
2022-08-29 11:25:36 +0000

    migration

--- mindsdb/interfaces/storage/db.py
+++ mindsdb/interfaces/storage/db.py
@@ -3,11 +3,10 @@ import json
 import datetime
 
 import numpy as np
-from sqlalchemy import create_engine, orm, types, UniqueConstraint
+from sqlalchemy import create_engine, types, UniqueConstraint
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, Index
-from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.schema import ForeignKey
 
 
@@ -70,7 +69,9 @@ class Semaphor(Base):
     entity_id = Column('entity_id', Integer)
     action = Column(String)
     company_id = Column(Integer)
-    uniq_const = UniqueConstraint('entity_type', 'entity_id')
+    __table_args__ = (
+        UniqueConstraint('entity_type', 'entity_id', name='uniq_const'),
+    )
 
 
 class Predictor(Base):
@@ -101,7 +102,6 @@ class Predictor(Base):
     code = Column(String, nullable=True)
     lightwood_version = Column(String, nullable=True)
     dtype_dict = Column(Json, nullable=True)
-    uniq_const = UniqueConstraint('name', 'company_id', name='unique_predictor_name_company_id')
 
 
 class Log(Base):
@@ -125,7 +125,9 @@ class Integration(Base):
     engine = Column(String, nullable=False)
     data = Column(Json)
     company_id = Column(Integer)
-    uniq_const = UniqueConstraint('name', 'company_id', name='unique_integration_name_company_id')
+    __table_args__ = (
+        UniqueConstraint('name', 'company_id', name='unique_integration_name_company_id'),
+    )
 
 
 class Stream(Base):
@@ -157,7 +159,9 @@ class File(Base):
     columns = Column(Json, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
-    uniq_const = UniqueConstraint('name', 'company_id', name='unique_file_name_company_id')
+    __table_args__ = (
+        UniqueConstraint('name', 'company_id', name='unique_file_name_company_id'),
+    )
 
 
 class View(Base):
@@ -166,7 +170,9 @@ class View(Base):
     name = Column(String, nullable=False)
     company_id = Column(Integer)
     query = Column(String, nullable=False)
-    uniq_const = UniqueConstraint('name', 'company_id', name='unique_view_name_company_id')
+    __table_args__ = (
+        UniqueConstraint('name', 'company_id', name='unique_view_name_company_id'),
+    )
 
 
 # DDL is changing through migrations

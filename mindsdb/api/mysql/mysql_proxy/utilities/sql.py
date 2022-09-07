5ae6dcf4805097c271e9e12839e0a397d4c8bf7b
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2022-09-07 17:43:04 -0400

    check for identifier instance

--- mindsdb/api/mysql/mysql_proxy/utilities/sql.py
+++ mindsdb/api/mysql/mysql_proxy/utilities/sql.py
@@ -78,7 +78,10 @@ def query_df(df, query, session=None):
     result_df = con.execute(query_str).fetchdf()
     for col in query_ast.targets:
         if isinstance(col, Identifier):
-            col_alias = col.alias.parts[-1] if hasattr(col, 'alias') else col.parts[-1]
+            if hasattr(col, 'alias'):
+                col_alias = col.alias.parts[-1]
+            else:
+                col_alias = col.parts[-1]
             if hasattr(col, 'type_name') and col.type_name in TYPECAST_MAP.keys():
                 result_df[col_alias] = result_df[col_alias].apply(lambda x: TYPECAST_MAP[col.type_name](x))
     result_df = result_df.replace({np.nan: None}).dropna(how='all')

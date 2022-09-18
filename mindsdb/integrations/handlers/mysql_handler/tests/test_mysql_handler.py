ed72ed654b77cc7b51478044e91387d57051ee9b
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-09-18 16:40:05 +0300

    full working scope of initial tests with new test environment

--- mindsdb/integrations/handlers/mysql_handler/tests/test_mysql_handler.py
+++ mindsdb/integrations/handlers/mysql_handler/tests/test_mysql_handler.py
@@ -73,30 +73,55 @@ class MySQLHandlerTest(unittest.TestCase):
         expected_db = self.kwargs["connection_data"]["database"]
         assert expected_db in dbs, f"expected to have {expected_db} db in response: {dbs}"
 
-    # def test_2_get_tables(self):
-    #     tbls = self.handler.get_tables()
-    #     assert tbls['type'] is not RESPONSE_TYPE.ERROR
+    def test_2_get_tables(self):
+        tables = self.get_table_names()
+        assert "rentals" in tables, f"expected to have 'rentals' table in the db but got: {tables}"
 
-    # def test_3_get_views(self):
-    #     views = self.handler.get_views()
-    #     assert views['type'] is not RESPONSE_TYPE.ERROR
+    def test_3_describe_table(self):
+        described = self.handler.get_columns("rentals")
+        describe_data = described.data_frame
+        self.check_valid_response(described)
+        got_columns = list(describe_data.iloc[:, 0])
+        want_columns = ["number_of_rooms", "number_of_bathrooms",
+                        "sqft", "location", "days_on_market",
+                        "initial_price", "neighborhood", "rental_price"]
+        assert got_columns == want_columns, f"expected to have next columns in rentals table:\n{want_columns}\nbut got:\n{got_columns}"
 
-    # def test_5_drop_table(self):
-    #     res = self.handler.native_query("DROP TABLE IF EXISTS test_mdb")
-    #     assert res['type'] is not RESPONSE_TYPE.ERROR 
+    def test_4_create_table(self):
+        new_table = "test_mdb"
+        res = self.handler.native_query(f"CREATE TABLE IF NOT EXISTS {new_table} (test_col INT)")
+        self.check_valid_response(res)
+        tables = self.get_table_names()
+        assert new_table in tables, f"expected to have {new_table} in database, but got: {tables}"
 
-    # def test_4_create_table(self):
-    #     res = self.handler.native_query("CREATE TABLE IF NOT EXISTS test_mdb (test_col INT)")
-    #     assert res['type'] is not RESPONSE_TYPE.ERROR 
+    def test_5_drop_table(self):
+        drop_table = "test_md"
+        res = self.handler.native_query(f"DROP TABLE IF EXISTS {drop_table}")
+        self.check_valid_response(res)
+        tables = self.get_table_names()
+        assert drop_table not in tables
 
-    # def test_6_describe_table(self):
-    #     described = self.handler.get_columns("test_mdb")
-    #     assert described['type'] is RESPONSE_TYPE.TABLE
+    def test_6_select_query(self):
+        limit = 5
+        query = f"SELECT * FROM rentals WHERE number_of_rooms = 2 LIMIT {limit}"
+        res = self.handler.query(query)
+        self.check_valid_response(res)
+        got_rows = res.data_frame.shape[0]
+        want_rows = limit
+        assert got_rows == want_rows, f"expected to have {want_rows} rows in response but got: {got_rows}"
 
-    # def test_7_select_query(self):
-    #     query = "SELECT * FROM test_mdb WHERE 'id'='a'"
-    #     result = self.handler.query(query)
-    #     assert result['type'] is RESPONSE_TYPE.TABLE
+    def check_valid_response(self, res):
+        if res.resp_type == RESPONSE_TYPE.TABLE:
+            assert res.data_frame is not None, "expected to have some data, but got None"
+        assert res.error_code == 0, f"expected to have zero error_code, but got {df.error_code}"
+        assert res.error_message is None, f"expected to have None in error message, but got {df.error_message}"
+
+    def get_table_names(self):
+        res = self.handler.get_tables()
+        tables = res.data_frame
+        assert tables is not None, "expected to have some tables in the db, but got None"
+        assert 'table_name' in tables, f"expected to get 'table_name' column in the response:\n{tables}"
+        return list(tables['table_name'])
 
 
 if __name__ == "__main__":

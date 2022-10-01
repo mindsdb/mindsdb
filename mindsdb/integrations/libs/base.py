ea0e20f3de1a3ae297dd3c71f782678230ffb2d1
Patricio Cerda Mardini <pcerdam@live.com>
2022-10-01 18:30:52 +0300

    modified some signatures

--- mindsdb/integrations/libs/base.py
+++ mindsdb/integrations/libs/base.py
@@ -122,19 +122,40 @@ class BaseMLEngine:
     """
     Base class for integration engine to connect with other Machine Learning libraries/frameworks.
 
-    An instance of this class will be generated, used, and destroyed for each interaction with the underlying framework.
+    This class will be instanced when interacting with the underlying framework.
     """
 
     def __init__(self, model_storage, engine_storage) -> None:
         """
-        Initialize any objects, fields or parameters required by the ML engine.
+        Warning: This method should not be overridden.
 
-        At least, two storage objects should be available:
-            - model_storage: stores models in the file system (path specified in MindsDB config).
-            - engine_storage: stores model-related metadata in an internal MindsDB database.
+        Initialize storage objects required by the ML engine.
+
+        - engine_storage: persists engine-related internals.
+        - model_storage: stores artifacts for any given model.
         """
         self.model_storage = model_storage
         self.engine_storage = engine_storage
+
+    def create(self, target: str, df: Optional = Union[None, pd.DataFrame], args: Optional = dict) -> None:
+        """
+        Saves a model inside the engine registry for later usage.
+
+        Normally, an input dataframe is required to train the model.
+        However, some integrations may merely require registering the model instead of training, in which case `df` can be omitted.
+
+        Any other arguments required to register the model can be passed in an `args` dictionary.
+        """
+        raise NotImplementedError
+
+    def predict(self, df: pd.DataFrame, args: Optional = dict) -> pd.DataFrame:
+        """
+        Calls a model with some input dataframe `df`, and optionally some arguments `args` that may modify the model behavior.
+
+        The expected output is a dataframe with the predicted values in the target-named column.
+        Additional columns can be present, and will be considered row-wise explanations if their names finish with `_explain`.
+        """
+        raise NotImplementedError
 
     def create_engine(self, connection_args: dict):
         """
@@ -144,33 +165,13 @@ class BaseMLEngine:
         """
         raise NotImplementedError
 
-    def create(self, target: str, df: Optional = Union[None, pd.DataFrame], args: Optional = dict) -> None:
-        """
-        Registers a model inside the engine registry for later usage.
-
-        Normally, an input dataframe is required to train the model.
-        However, some integrations may merely require registering the model instead of training, in which case `df` can be omitted.
-
-        Any other arguments required to register the model can be passed in an `args` dictionary.
-        """
-        raise NotImplementedError
-
     def update(self, df: Optional = Union[None, pd.DataFrame]) -> None:
         """
         Optional.
 
-        Used to update/fine-tune/adjust a pre-existing model without resetting the internal state (e.g. weights).
+        Used to update/fine-tune/adjust a pre-existing model without resetting its internal state (e.g. weights).
 
-        Its availability will depend on underlying integration support, as not all ML models can be partially updated.
-        """
-        raise NotImplementedError
-
-    def predict(self, df: pd.DataFrame, args: Optional = dict) -> pd.DataFrame:
-        """
-        Calls a model with some input dataframe `df`, and optionally some arguments `args`.
-
-        The expected output is a dataframe with the predicted values in the target-named column.
-        Additional columns can be present, and will be considered row-wise explanations if their names finish with `_explain`.
+        Availability will depend on underlying integration support, as not all ML models can be partially updated.
         """
         raise NotImplementedError
 

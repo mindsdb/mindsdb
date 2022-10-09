0dea0e8e4e2d68f3e367a960b71f4032430f3a58
Minura Punchihewa <minura.jkh@keells.com>
2022-10-09 15:21:57 +0530

    updated the correct imports and fixed the __init__ method

--- mindsdb/integrations/handlers/databend_handler/databend_handler.py
+++ mindsdb/integrations/handlers/databend_handler/databend_handler.py
@@ -1,17 +1,17 @@
-from ..clickhouse_handler import Handler as ClickhouseHandler
+from ..clickhouse_handler.clickhouse_handler import ClickHouseHandler
 
 from collections import OrderedDict
-from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_TYPE
+from typing import Optional
 
 
-class DatabendHandler(ClickhouseHandler):
+class DatabendHandler(ClickHouseHandler):
     """
     This handler handles connection and execution of the Databend statements.
     """
     name = 'databend'
 
-    def __init__(self, name, **kwargs):
-        super().__init__(name, **kwargs)
+    def __init__(self, name: str, connection_data: Optional[dict], **kwargs):
+        super().__init__(name, connection_data, **kwargs)
 
 
 connection_args_example = OrderedDict(

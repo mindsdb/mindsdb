1cf1330be99f22d8f96d11b919636e377159c13a
Minura Punchihewa <minura.jkh@keells.com>
2022-10-09 14:46:29 +0530

    added the connection_args_example dict to the handler

--- mindsdb/integrations/handlers/databend_handler/databend_handler.py
+++ mindsdb/integrations/handlers/databend_handler/databend_handler.py
@@ -1,5 +1,8 @@
 from ..clickhouse_handler import Handler as ClickhouseHandler
 
+from collections import OrderedDict
+from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_TYPE
+
 
 class DatabendHandler(ClickhouseHandler):
     """
@@ -9,3 +12,13 @@ class DatabendHandler(ClickhouseHandler):
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+
+
+connection_args_example = OrderedDict(
+    protocol='https',
+    host='some-url.aws-us-east-2.default.databend.com',
+    port=443,
+    user='root',
+    password='password',
+    database='test_db'
+)

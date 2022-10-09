af36f07bab5765c381383bc944be8636f71d586e
Minura Punchihewa <minura.jkh@keells.com>
2022-10-09 12:53:56 +0530

    added the query and native_query methods

--- mindsdb/integrations/handlers/sheets_handler/sheets_handler.py
+++ mindsdb/integrations/handlers/sheets_handler/sheets_handler.py
@@ -56,6 +56,12 @@ class SheetsHandler(DatabaseHandler):
         if self.is_connected is True:
             return self.connection
 
+        self.connection = None
+
+        self.is_connected = True
+
+        return self.connection
+
     def disconnect(self):
         """ Close any existing connections
         Should switch self.is_connected.
@@ -86,3 +92,54 @@ class SheetsHandler(DatabaseHandler):
                 self.is_connected = False
 
         return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """
+        Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
+        """
+
+        need_to_close = self.is_connected is False
+
+        connection = self.connect()
+        cursor = connection.cursor()
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            if result:
+                response = Response(
+                    RESPONSE_TYPE.TABLE,
+                    data_frame=pd.DataFrame(
+                        result,
+                        columns=[x[0] for x in cursor.description]
+                    )
+                )
+
+            else:
+                response = Response(RESPONSE_TYPE.OK)
+        except Exception as e:
+            log.error(f'Error running query: {query} on the Google Sheet with ID {self.connection_data["base_id"]}!')
+            response = Response(
+                RESPONSE_TYPE.ERROR,
+                error_message=str(e)
+            )
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> StatusResponse:
+        """
+        Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind
+                of query: SELECT, INTSERT, DELETE, etc
+        Returns:
+            HandlerResponse
+        """
+
+        return self.native_query(query.to_string())

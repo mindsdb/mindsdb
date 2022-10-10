af0e1092416439b1eb33b62ef78eef000ecdc586
Minura Punchihewa <minura.jkh@keells.com>
2022-10-10 07:21:10 +0530

    added the connection_args and connection_args_example dicts

--- mindsdb/integrations/handlers/sheets_handler/sheets_handler.py
+++ mindsdb/integrations/handlers/sheets_handler/sheets_handler.py
@@ -202,3 +202,19 @@ class SheetsHandler(DatabaseHandler):
         )
 
         return response
+
+connection_args = OrderedDict(
+    spreadsheet_id={
+        'type': ARG_TYPE.STR,
+        'description': 'The unique ID of the Google Sheet.'
+    },
+    sheet_name={
+        'type': ARG_TYPE.STR,
+        'description': 'The name of the sheet within the Google Sheet.'
+    }
+)
+
+connection_args_example = OrderedDict(
+    spreadsheet_id='12wgS-1KJ9ymUM-6VYzQ0nJYGitONxay7cMKLnEE2_d0',
+    sheet_name='iris'
+)

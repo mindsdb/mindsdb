28859eefa9dd4f7a0434f5abeb7977ad94f9c712
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-10-17 17:19:11 +0300

    change ML service to new MLHandler design

--- mindsdb/integrations/handlers_wrapper/ml_handler_wrapper.py
+++ mindsdb/integrations/handlers_wrapper/ml_handler_wrapper.py
@@ -1,6 +1,7 @@
 import traceback
 import pickle
 import json
+from pandas import read_json
 from flask import Flask, request
 
 from mindsdb.integrations.libs.response import (
@@ -8,11 +9,10 @@ from mindsdb.integrations.libs.response import (
     HandlerResponse as Response,
     RESPONSE_TYPE
 )
+from mindsdb.integrations.libs.const import PREDICTOR_STATUS
 
 from mindsdb.utilities.config import Config
-from mindsdb.interfaces.storage.fs import FsStore
-from mindsdb.interfaces.model.model_controller import ModelController
-from mindsdb.utilities.with_kwargs_wrapper import WithKWArgsWrapper
+from mindsdb.interfaces.storage.fs import ModelStorage, HandlerStorage
 
 from mindsdb.integrations.libs.handler_helpers import define_ml_handler
 from mindsdb.utilities.log import log
@@ -30,7 +30,7 @@ class BaseMLWrapper:
         try:
             context = request.json.get("context")
         except Exception as e:
-            logger.error("error getting request context: %s", e)
+            log.error("error getting request context: %s", e)
             context = {}
         ctx_str = json.dumps(context, sort_keys=True)
         return {"hash": hash(ctx_str), "context": context}
@@ -43,124 +43,101 @@ class BaseMLWrapper:
 
 
 class MLHandlerWrapper(BaseMLWrapper):
-    def __init__(self,  name, _type):
+    def __init__(self,  name):
         super().__init__(name)
-        handler_class = define_ml_handler(_type)
-        if handler_class is None:
-            raise Exception(f"ML of '{_type}' type is not supported")
-        self.handler = handler_class(
-                name,
-                fs_store=WithKWArgsWrapper(FsStore(), company_id=None),
-                model_controller=WithKWArgsWrapper(ModelController(), company_id=None),
-        )
 
         # CONVERT METHODS TO FLASK API ENDPOINTS
-        connect_route = self.app.route("/connect", methods = ["GET", ])
-        self.connect = connect_route(self.connect)
-
         check_connect_route = self.app.route("/check_connection", methods = ["GET", ])
         self.check_connection = check_connect_route(self.check_connection)
 
-        get_tables_route = self.app.route("/get_tables", methods = ["GET", ])
-        self.get_tables = get_tables_route(self.get_tables)
+        predict_route = self.app.route("/predict", methods = ["GET", ])
+        self.predict = predict_route(self.predict)
 
-        native_query_route = self.app.route("/native_query", methods = ["POST", "PUT"])
-        self.native_query = native_query_route(self.native_query)
+        query_create = self.app.route("/create", methods = ["POST", ])
+        self.create = query_create(self.create)
 
-        query_route = self.app.route("/query", methods = ["POST", "PUT"])
-        self.query = query_route(self.query)
+    def _get_handler_general_data(self) -> dict:
+        return {"company_id": request.json.get("company_id", None),
+                "predictor_id": request.json.get("predictor_id", None),
+                "type": request.json.get("type")}
 
-        query_route = self.app.route("/predict", methods = ["GET", ])
-        self.query = query_route(self.query)
+    def _get_handler_class(self):
+        common_data = self._get_handler_general_data()
+        handler_class = define_ml_handler(common_data["type"])
+        return handler_class
+
+    def _get_create_data(self) -> dict:
+        data = {"target": request.json.get("target"),
+                "args": request.json.get("args", None)}
+        df = None
+        try:
+            df = request.json.get("df", None)
+            if df is not None:
+                df = read_json(df, orient="split")
+        except Exception:
+            log.error("%s: error getting training dataframe from request", self.__class__.__name__)
+        data["df"] = df
+
+        return data
+
+    def _get_predict_data(self):
+        df = None
+        try:
+            df = request.get_data()
+            df = json.loads(df)
+            df = read_json(df, orient="split")
+        except Exception:
+            log.error("%s: error getting predict dataframe from request", self.__class__.__name__)
+        return {"df": df}
+
+    def _get_handler(self):
+        # seems that integration_id is senseless for models
+        integration_id = 0
+        handler_class = self._get_handler_class()
+        common_args = self._get_handler_general_data()
+        company_id = common_args["company_id"]
+        predictor_id = common_args["predictor_id"]
+        engineStorage = HandlerStorage(company_id, integration_id)
+        modelStorage = ModelStorage(company_id, predictor_id)
+        handler = handler_class(engine_storage=engineStorage,
+                                model_storage=modelStorage)
+        return handler
 
     def check_connection(self):
         log.info("%s: calling 'check_connection'", self.__class__.__name__)
+        result = StatusResponse(success=True,
+                                error_message=None)
+        return result.to_json(), 200
+
+
+    def create(self):
         try:
-           result = self.handler.check_connection()
-           return result.to_json(), 200
+            handler = self._get_handler()
+            create_kwargs = self._get_create_data()
+            handler.create(**create_kwargs)
+            status = StatusResponse(success=True)
+            return status.to_json(), 200
         except Exception:
             msg = traceback.format_exc()
-            log.error("%s: in 'check_connection' - %s", self.__class__.__name__, msg)
-            result = StatusResponse(success=False,
-                                    error_message=msg)
-            return result.to_json(), 500
-
-    def connect(self):
-        return self.check_connection()
-
-    def get_tables(self):
-        log.info("%s: calling 'get_tables'", self.__class__.__name__)
-        try:
-           result = self.handler.get_tables()
-           return result.to_json(), 200
-        except Exception:
-            msg = traceback.format_exc()
-            log.error("%s: in 'get_tables' - %s", self.__class__.__name__, msg)
-            result = Response(resp_type=RESPONSE_TYPE.ERROR,
-                              error_code=1,
-                              error_message=msg)
-            return result.to_json(), 500
-
-    def get_columns(self):
-        table = request.args.get("table")
-        log.info("%s: calling 'get_columns of %s table'", self.__class__.__name__, table)
-        try:
-           result = self.handler.get_columns(table)
-           return result.to_json(), 200
-        except Exception:
-            msg = traceback.format_exc()
-            log.error("%s: in 'get_columns' - %s", self.__class__.__name__, msg)
-            result = Response(resp_type=RESPONSE_TYPE.ERROR,
-                              error_code=1,
-                              error_message=msg)
-            return result.to_json(), 500
-
-    def native_query(self):
-        query = request.json.get("query")
-        log.info("%s: calling 'native_query' with query string - %s", self.__class__.__name__, query)
-        try:
-            result = self.handler.native_query(query)
-            return result.to_json(), 200
-        except Exception:
-            msg = traceback.format_exc()
-            log.error("%s: in 'native_query' - %s", self.__class__.__name__, msg)
-            result = Response(resp_type=RESPONSE_TYPE.ERROR,
-                              error_code=1,
-                              error_message=msg)
-            return result.to_json(), 500
-
-    def query(self):
-        s_query = request.get_data()
-        log.info("%s: calling 'query' with query object - %s", self.__class__.__name__, s_query)
-        try:
-            query = pickle.loads(s_query)
-            result = handler.query(query)
-            return result.to_json(), 200
-        except Exception:
-            msg = traceback.format_exc()
-            log.error("%s: in 'native_query' - %s", self.__class__.__name__, msg)
-            result = Response(resp_type=RESPONSE_TYPE.ERROR,
-                              error_code=1,
-                              error_message=msg)
-            return result.to_json(), 500
+            log.error("%s error in calling 'create': %s", self.__class__.__name__, msg)
+            status = StatusResponse(success=False, error_message=msg)
+            return status.to_json(), 500
 
     def predict(self):
-        model_name = request.json.get("model_name")
-        data = request.json.get("data")
-        pred_format = request.json.get("pred_format")
-
-        log.info("%s: calling 'predict':\nmodel - %s\ndata - %s, format - %s",
-                 self.__class__.__name__,
-                 model_name,
-                 data,
-                 pred_format)
         try:
-            result = handler.predict(model_name, data, pred_format)
+            handler = self._get_handler()
+            predict_kwargs = self._get_predict_data()
+            predictions = handler.predict(**predict_kwargs)
+            result = Response(resp_type=RESPONSE_TYPE.OK,
+                              data_frame=predictions
+                              error_code=1,
+                              error_message=msg)
             return result.to_json(), 200
-        except Exception as e:
+        except Exception:
             msg = traceback.format_exc()
-            log.error("%s: in 'predict' - %s", self.__class__.__name__, msg)
+            log.error("%s error in calling 'predict': %s", self.__class__.__name__, msg)
             result = Response(resp_type=RESPONSE_TYPE.ERROR,
                               error_code=1,
                               error_message=msg)
             return result.to_json(), 500
+
--- mindsdb/integrations/libs/handler_helpers.py
+++ mindsdb/integrations/libs/handler_helpers.py
@@ -1,7 +1,6 @@
 from mindsdb.integrations.handlers.mysql_handler.mysql_handler import MySQLHandler
 from mindsdb.integrations.handlers.mariadb_handler.mariadb_handler import MariaDBHandler
 from mindsdb.integrations.handlers.postgres_handler.postgres_handler import PostgresHandler
-# from mindsdb.integrations.handlers.lightwood_handler.lightwood_handler.lightwood_handler import LightwoodHandler
 
 
 def define_ml_handler(_type):
@@ -10,6 +9,12 @@ def define_ml_handler(_type):
         try:
             from mindsdb.integrations.handlers.lightwood_handler.lightwood_handler.lightwood_handler import LightwoodHandler
             return LightwoodHandler
+        except ImportError:
+            pass
+    elif _type == 'huggingface':
+        try:
+            from mindsdb.integrations.handlers.huggingface_handler.huggingface_handler import HuggingFaceHandler
+            return HuggingFaceHandler
         except ImportError:
             pass
     return None

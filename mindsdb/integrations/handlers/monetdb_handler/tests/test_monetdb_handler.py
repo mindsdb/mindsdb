280d62e827bfdc5291f1fe6a343d9a5b91c7c436
Minura Punchihewa <minura.jkh@keells.com>
2022-10-18 10:34:58 +0530

    cleaned up whitespace and indentation in test_monetdb_handler

--- mindsdb/integrations/handlers/monetdb_handler/tests/test_monetdb_handler.py
+++ mindsdb/integrations/handlers/monetdb_handler/tests/test_monetdb_handler.py
@@ -17,14 +17,11 @@ class MonetDBHandlerTest(unittest.TestCase):
         cls.handler = MonetDBHandler('test_monet_handler', cls.kwargs)
 
     def test_0_connect(self):
-         self.handler.connect()
-
-    
+        self.handler.connect()
 
     def test_1_drop_table(self):
         res = self.handler.query("DROP TABLE IF EXISTS PREM;")
         assert res.type is  not RESPONSE_TYPE.ERROR 
-
 
     def test_2_create_table(self):
         res = self.handler.query("CREATE TABLE IF NOT EXISTS PREM (Premi varchar(50));")
@@ -33,20 +30,19 @@ class MonetDBHandlerTest(unittest.TestCase):
     def test_3_insert_table(self):
         res = self.handler.query("INSERT INTO PREM VALUES('Radha <3 Krishna');")
         assert res.type is not  RESPONSE_TYPE.ERROR 
-    
 
     def test_4_get_tables(self):
         tables = self.handler.get_tables()
         assert tables.type is not  RESPONSE_TYPE.ERROR
 
- 
     def test_5_select_query(self):
         query = "SELECT * FROM PREM;"
         result = self.handler.native_query(query)
         assert result.type is RESPONSE_TYPE.TABLE or RESPONSE_TYPE.OK
 
     def test_6_check_connection(self):
-         self.handler.check_connection()
+        self.handler.check_connection()
+
 
 if __name__ == '__main__':
     unittest.main()

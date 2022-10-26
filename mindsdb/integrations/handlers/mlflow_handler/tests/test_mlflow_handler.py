488a70cdc22934dc08f322f9cb63a7da76bff279
Patricio Cerda Mardini <pcerdam@live.com>
2022-10-27 00:12:47 +0300

    fix mlflow test

--- mindsdb/integrations/handlers/mlflow_handler/tests/test_mlflow_handler.py
+++ mindsdb/integrations/handlers/mlflow_handler/tests/test_mlflow_handler.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from mindsdb.utilities.config import Config
 from mindsdb.interfaces.storage.fs import ModelStorage, HandlerStorage
-from mindsdb.integrations.handlers.mlflow_handler.mlflow_handler.mlflow_handler import MLflowHandler
+from mindsdb.integrations.handlers.mlflow_handler.mlflow_handler import MLflowHandler
 from mindsdb.integrations.handlers.mysql_handler.mysql_handler import MySQLHandler
 
 
@@ -11,7 +11,7 @@ class MLflowHandlerTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.registered_model_name = 'nlp_kaggle3'  # already saved to mlflow local instance
+        cls.registered_model_name = 'nlp_kaggle'  # already saved to mlflow local instance
         cls.handler = MLflowHandler(ModelStorage(None, None), HandlerStorage(None, None))
         cls.config = Config()
 
@@ -27,23 +27,18 @@ class MLflowHandlerTest(unittest.TestCase):
         cls.data_table_name = 'train_escaped_csv'  # 'tweet_sentiment_train'
         cls.data_handler = MySQLHandler(cls.sql_handler_name, **kwargs)
 
-    def __random(self):
-        self.handler.connect(
-            mlflow_server_url='http://127.0.0.1:5001',  # for this test, serve at 5001 and served model at 5000
-            model_registry_path='sqlite:///../../../../../temp/experiments/BYOM/mlflow.db',
-            config={
-                'path': self.config['paths']['root'],
-                'name': 'test_name'
-            }
-        )
+    def test_1_create_predictor(self):
+        args = {
+            "model_name": self.registered_model_name,
+            "mlflow_server_url": "http://0.0.0.0:5001/",
+            "mlflow_server_path": "sqlite:////path/to/mlflow.db",
+            'predict_url': "'http://localhost:5000/invocations'"
+        }
+        target_col = 'target'
+        self.handler.create(target_col, args=args)
 
-    def test_3_create_predictor(self):
-        self.handler.create(self.registered_model_name,
-                            'target',
-                            args={'predict_url': "'http://localhost:5000/invocations'"})
+    def test_2_predict(self):
+        self.handler.predict(pd.DataFrame.from_dict({'text': ['This is nice.']}))
 
-    def test_5_describe_table(self):
-        df = self.handler.describe_table(self.registered_model_name)
-
-    def test_6_predict(self):
-        df = self.handler.predict(pd.DataFrame.from_dict({'text': ['This is nice.']}))
+    def test_3_describe_table(self):
+        self.handler.describe(self.registered_model_name)

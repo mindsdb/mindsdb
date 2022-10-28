eea22ab70a289510f25a5a463748ece444624f45
Minura Punchihewa <minurapunchihewa17@gmail.com>
2022-10-28 18:54:47 +0530

    used model_storage instead of engine_storae

--- mindsdb/integrations/handlers/auto_sklearn_handler/auto_sklearn_handler.py
+++ mindsdb/integrations/handlers/auto_sklearn_handler/auto_sklearn_handler.py
@@ -1,6 +1,6 @@
 from typing import Optional, Dict
 
-import pickle
+import dill
 import pandas as pd
 import sklearn
 import autosklearn.regression
@@ -12,8 +12,8 @@ class AutoSklearnHandler(BaseMLEngine):
     name = 'auto_sklearn'
 
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None):
-        y = df.pop(target)
         X = df.copy()
+        y = X.pop(target)
 
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
             X,
@@ -29,21 +29,17 @@ class AutoSklearnHandler(BaseMLEngine):
 
         regressor.fit(X_train, y_train)
 
-        model_storage_path = self.engine_storage.folder_get('auto_sklearn_model')
+        self.model_storage.file_set('model', dill.dumps(regressor))
 
-        with open(f"{model_storage_path}/regression_model.pkl", 'wb') as f:
-            pickle.dump(regressor, f)
-
-        args['model_storage_path'] = model_storage_path
-        self.model_storage.json_set('args', args)
+        # args['model_storage_path'] = model_storage_path
+        # self.model_storage.json_set('args', args)
 
         self.engine_storage.folder_sync('auto_sklearn_model')
 
     def predict(self, df: pd.DataFrame, args: Optional[Dict] = None):
-        args = self.model_storage.json_get('args')
-        model_storage_path = args['model_storage_path']
+        # args = self.model_storage.json_get('args')
+        # model_storage_path = args['model_storage_path']
 
-        with open(f"{model_storage_path}/regression_model.pkl", 'rb') as f:
-            regressor = pickle.load(f)
+        regressor = dill.loads(self.model_storage.file_get('model'))
 
         return regressor.predict(df)

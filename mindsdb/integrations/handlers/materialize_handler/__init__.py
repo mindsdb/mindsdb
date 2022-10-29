dba3310c24e2eaa74babba385e35d5f242914a3b
parthiv11 <parthivmakwana11@gmail.com>
2022-10-29 16:06:19 +0530

    Updated init

--- mindsdb/integrations/handlers/materialize_handler/__init__.py
+++ mindsdb/integrations/handlers/materialize_handler/__init__.py
@@ -12,7 +12,7 @@ from .__about__ import __version__ as version, __description__ as description
 title = 'Materialize'
 name = 'materialize'
 type = HANDLER_TYPE.DATA
-icon_path = 'icon.svg'
+icon_path = 'icon.png'
 
 __all__ = [
     'Handler', 'version', 'name', 'type', 'title', 'description',

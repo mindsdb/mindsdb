994b1af0a4dd7efa1766840c98b5358db6a7af76
Alissa <alissatroianonyc@gmail.com>
2022-10-29 16:21:46 -0400

    Make some changes to sf handler and test

--- mindsdb/integrations/handlers/salesforce_handler/salesforce_handler.py
+++ mindsdb/integrations/handlers/salesforce_handler/salesforce_handler.py
@@ -1,5 +1,5 @@
+# Set up Python package and API access
 from simple_salesforce import Salesforce
-
 import requests
 import pandas as pd
 from io import StringIO
@@ -19,191 +19,153 @@ from mindsdb.integrations.libs.response import (
     HandlerResponse as Response,
     RESPONSE_TYPE
 )
+from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_TYPE
 
 class SalesforceHandler(DatabaseHandler):
     """
-    This handler handles connection and execution of Salesforce statements.
+     This handler handles connection and execution of Salesforce statements.
     """
 
     name = "salesforce"
 
-    def __init__(self, name= str, connection_data: Optional[dict], **kwargs):
+    def __init__(self, name: str, connection_data: Optional[dict], **kwargs):
+        """
+        Initialize the handler
+        Args:
+            name (str): name of particular handler instance
+                        connection_data (dict): parameters for connecting to the database
+            **kwargs: arbitrary keyword arguments.
+        """
         super().__init__(name)
         self.parser = parse_sql
         self.dialect = 'salesforce'
         self.connection_data = connection_data
-        self kwargs = kwargs
+        self.kwargs = kwargs
 
         self.connection = None
         self.is_connected = False
 
     def __del__(self):
-        if self.is_connected is True
+        if self.is_connected is True:
             self.disconnect()
 
     def connect(self) -> StatusResponse:
         """
         Set up the connection required by the handler.
         Returns:
-                HandlerStatusResponse
+            HandlerStatusResponse
         """
-
         if self.is_connected is True:
-            return self.connection
+            return StatusResponse(True)
 
-        self.connection = sf.client(
-            'salesforce',
-            sf_username=self.connection_data['user'],
-            sf_password=self.connection_data['password'],
-            sf_security_token=self.connection_data['security_token']
+        self.connection = sf.connect (
+            sf_instance = 'https://na1.salesforce.com',
+            reportId = '00O1t00000CJ1YpEAL',
+            export = '?isdtp=p1&export=1&enc=UTF-8&xf=csv',
+            sfUrl = sf_instance + reportId + export,
+            response = requests.get(sfUrl, headers=headers)
+            download = response.content.decode('utf-8'),
+            df = pd.read_csv(StringIO(download))
         )
+    
         self.is_connected = True
+        return StatusResponse(True)
 
-        return self.connection
-
-    def disconnect(self) -> StatusResponse:
-         """ 
-         Close any existing connections
-        Should switch self.is_connected.
-        """
-        self.connection = None
+     def disconnect(self):
+        if self.is_connected is False:
+            return
+        self.connection.close()
         self.is_connected = False
-
-        return self.connection
 
     def check_connection(self) -> StatusResponse:
         """
-        Check if the connection is active.
-        Returns:
-            HandlerStatusResponse
+        Check the Salesforce connection
         """
         response = StatusResponse(False)
-        need_to_close = False
-
+        need_to_close = self.is_connected is False
+        if need_to_close is True:
+            self.connect()
         try:
-            connection = self.connection
-            if connection is None:
-                connection = self.connect()
-                need_to_close = True
-
-            response.success = True
+            self.connection.query("SELECT Id FROM Account LIMIT 1")
+            response = StatusResponse(True)
         except Exception as e:
-            response.error = str(e)
-        finally:
-            if need_to_close is True:
-                self.disconnect()
-
+            response = StatusResponse(False, error=str(e))
+        if need_to_close is True:
+            self.disconnect()
         return response
-    
+
     def native_query(self, query: str) -> Response:
         """
-        Receive SQL query and run it
-        :param query: The SQL query to run in the database
-        :return: returns the records from the dataset
+        Receive SOQL query from Salesforce and return the result as a pandas dataframe
+        Args: 
+            query (str): SOQL query
+        Returns:
+            HandlerResponse
         """
         need_to_close = self.is_connected is False
+        connection = self.connection
 
-        connection = self.connect()
-        
-        with connection.cursor() as cursor:
-            try:
-                cursor.execute(query)
-                if ExecStatus(cursor.result.StatusResponse) == ExecStatus.COMMAND_OK:
-                    response = Response(RESPONSE_TYPE.SUCCESS)
-                    response.data = cursor.fetchall()
-                else:
-                    result = cursor.fetchall()
-                    response = Response(RESPONSE_TYPE.TABLE)
-                    response.data = pd.DataFrame(result)
-            except Exception as e:
-                response = Response(RESPONSE_TYPE.ERROR)
-                response.error = str(e)
-            finally:
-                if need_to_close is True:
-                    self.disconnect()
+        if need_to_close is True:
+            self.connect()
+            connection = self.connection
+
+        try:
+            result = connection.select_object_content(
+                sf_instance=self.connection_data['sf_instance'],
+                reportId=self.connection_data['reportId'],
+                export = '?isdtp=p1&export=1&enc=UTF-8&xf=csv',
+                sfUrl = sf_instance + reportId + export,
+                response = requests.get(sfUrl, headers=headers)
+                download = response.content.decode('utf-8'),
+                df = pd.read_csv(StringIO(download))
+            )
+            response = Response(RESPONSE_TYPE.SUCCESS, result)
+        except Exception as e:
+            response = Response(RESPONSE_TYPE.ERROR, str(e))
+
+        if need_to_close is True:
+            self.disconnect()
 
         return response
 
-    def get_connection_params(self) -> OrderedDict:
-        """
-        Returns the connection parameters required by the handler.
-        Returns:
-            OrderedDict
-        """
-        return OrderedDict([
-            ('user', ARG_TYPE.STRING),
-            ('password', ARG_TYPE.STRING),
-            ('security_token', ARG_TYPE.STRING)
-        ])
 
     def query(self, query: ASTNode) -> Response:
         """
-        Retrieve the data from the SQL query
-        :param query: The SQL query to run in the database
-        :return: returns the records from the dataset
+        Receive a query from MindsDB and return the result as a pandas dataframe
+        Args:
+            query (ASTNode): MindsDB query
+        Returns:
+            HandlerResponse
         """
-        query_str = self.renderer.render(query)
+        renderer = SqlalchemyRender(Salesforce)
+        query_str = renderer.get_string(query, with_failback=True)
         return self.native_query(query_str)
-        
+
     def get_tables(self) -> Response:
         """
-        Retrieve the tables from the database
-        :return: returns the tables from the database
+        Get a list of tables in the database
         """
-        query = 
-        """
+        query = """
             SELECT
                 table_schema,
-                table_name
+                table_name,
                 table_type
             FROM
                 information_schema.tables
             WHERE
-                table_schema NOT IN ('pg_catalog', 'information_schema')
-                and table_type = ('BASE TABLE', 'VIEW')
+                table_schema NOT IN ('information_schema', 'pg_catalog')
+                and table_type in ('BASE TABLE', 'VIEW')
         """
-
         return self.native_query(query)
 
-    def get_columns(self, table: str) -> Response:
+    def get_columns(self, table_name):
+        query = f"""
+            SELECT
+                column_name as "Field",
+                data_type as "Type"
+            FROM
+                information_schema.columns
+            WHERE
+                table_name = '{table_name}'
         """
-        Retrieve the columns from the database
-        :return: returns the columns from the database
-        """
-        query = f'SELECT * FROM {table}'
         return self.native_query(query)
-
-    def get_sample(self, table: str, limit: int = 100) -> Response:
-        """
-        Retrieve the sample from the database
-        :return: returns the sample from the database
-        """
-        query = f'SELECT * FROM {table} LIMIT {limit}'
-        return self.native_query(query)
-
-    connection_args = OrderedDict(
-        protocol={
-            'type': ARG_TYPE.STRING,
-            'description': 'The protocol to use for the connection'
-        },
-        user= {
-            'type': ARG_TYPE.STRING,
-            'description': 'The username to use for the connection'
-        },
-        password={
-            'type': ARG_TYPE.STRING,
-            'description': 'The password to use for the connection'
-        },
-        security_token= {
-            'type': ARG_TYPE.STRING,
-            'description': 'The security token to use for the connection'
-        }
-        host= {
-            'type': ARG_TYPE.STRING,
-            'description': 'The host to use for the connection'
-        },
-        port= {
-            'type': ARG_TYPE.STRING,
-            'description': 'The port to use for the connection'
-        }
-    )

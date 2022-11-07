5f0e8ffc4682bb8bee4826e2a014d4819b22b8c0
andrew <elkin@raketa.im>
2022-11-07 16:46:21 +0300

    fix migrations

--- mindsdb/migrations/env.py
+++ mindsdb/migrations/env.py
@@ -22,9 +22,16 @@ fileConfig(config.config_file_name)
 import os
 
 
-# init database
 
+from mindsdb.utilities.config import Config
 from mindsdb.interfaces.storage import db
+
+# initialize config
+if not 'MINDSDB_CONFIG_PATH' in os.environ:
+    os.environ['MINDSDB_CONFIG_PATH'] = 'absent'
+Config()
+
+# init database
 db.init()
 
 target_metadata = db.Base.metadata

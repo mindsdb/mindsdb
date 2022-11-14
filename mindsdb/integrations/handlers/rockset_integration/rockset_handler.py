5a16640e3042daa502940b6672ee0ec312854ab2
Alissa <alissatroianonyc@gmail.com>
2022-11-14 11:26:01 -0500

    Add the init function to rockset_handler.py

--- mindsdb/integrations/handlers/rockset_integration/rockset_handler.py
+++ mindsdb/integrations/handlers/rockset_integration/rockset_handler.py
@@ -5,3 +5,7 @@ class RocksetIntegration(MySQLHandler):
     This handler handles connection and execution of the Rockset integration
     """
     name = 'rockset'
+
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+

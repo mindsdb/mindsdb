8bad50125306616868ac1dd3ccd30faca137407f
Parthiv Makwana <75653580+parthiv11@users.noreply.github.com>
2022-11-15 18:37:35 +0530

    🌷🌷Added Vitess Integration (#3789)
    * Added Handler
    
    * Added Magic Methods
    
    * Added Readme
    
    * Added Reqiurements
    
    * Passed Tests
    
    * Added Manual_QA
    
    * Added Icon
    
    * Improve docs

---
+++ mindsdb/integrations/handlers/vitess_handler/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'MindsDB Vitess handler'
+__package_name__ = 'mindsdb_vitess_handler'
+__version__ = '0.0.1'
+__description__ = "MindsDB handler for Vitess"
+__author__ = 'Parthiv Makwana'
+__github__ = 'https://github.com/mindsdb/mindsdb'
+__pypi__ = 'https://pypi.org/project/mindsdb/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2022- mindsdb'

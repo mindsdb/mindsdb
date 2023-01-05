df6d04b12809a997417913cd91f6d5e36d14b5f6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 20:10:29 +0000

    Adding tarfile member sanitization to extractall()

--- mindsdb/api/http/namespaces/file.py
+++ mindsdb/api/http/namespaces/file.py
@@ -134,7 +134,26 @@ class File(Resource):
                     f.extractall(temp_dir_path)
             elif lp.endswith('.tar.gz'):
                 with tarfile.open(file_path) as f:
-                    f.extractall(temp_dir_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner) 
+                        
+                    
+                    safe_extract(f, temp_dir_path)
             os.remove(file_path)
             files = os.listdir(temp_dir_path)
             if len(files) != 1:
--- tests/handler_tests/test_mariadb_handler.py
+++ tests/handler_tests/test_mariadb_handler.py
@@ -39,7 +39,26 @@ def get_certificates(container):
             f.write(chunk)
 
     with tarfile.open(archive_path) as tf:
-        tf.extractall(path=cur_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tf, path=cur_dir)
     certs = get_certs()
     HANDLER_KWARGS["connection_data"].update(certs)
 

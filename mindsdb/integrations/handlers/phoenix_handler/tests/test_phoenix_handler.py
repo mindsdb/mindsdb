d53f40e461499cba85cf866b54c4ac7b556f04f8
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-02-02 20:54:03 +0530

    added the skeleton for the unit tests of the Apache Phoenix handler

---
+++ mindsdb/integrations/handlers/phoenix_handler/tests/test_phoenix_handler.py
@@ -0,0 +1,32 @@
+import unittest
+from mindsdb.integrations.handlers.phoenix_handler.phoenix_handler import PhoenixHandler
+from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
+
+
+class PhoenixHandlerTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.kwargs = {
+
+        }
+        cls.handler = PhoenixHandler('test_phoenix_handler', cls.kwargs)
+
+    def test_0_check_connection(self):
+        assert self.handler.check_connection()
+
+    def test_1_native_query_select(self):
+        query = ""
+        result = self.handler.native_query(query)
+        assert result.type is RESPONSE_TYPE.TABLE
+
+    def test_2_get_tables(self):
+        tables = self.handler.get_tables()
+        assert tables.type is not RESPONSE_TYPE.ERROR
+
+    def test_3_get_columns(self):
+        columns = self.handler.get_columns('')
+        assert columns.type is not RESPONSE_TYPE.ERROR
+
+
+if __name__ == '__main__':
+    unittest.main()

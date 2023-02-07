15601b4f26781a8b76c06d688d532165d75a6549
Sam Miller <sam_miller@AUTO-ATI0894.localdomain>
2023-02-07 15:13:48 +0000

    another attempt at CI fixing

--- mindsdb/integrations/handlers/statsforecast_handler/__init__.py
+++ mindsdb/integrations/handlers/statsforecast_handler/__init__.py
@@ -11,7 +11,7 @@ except Exception as e:
 title = 'StatsForecast'
 name = 'statsforecast'
 type = HANDLER_TYPE.ML
-permanent = True
+permanent = False
 
 __all__ = [
     'Handler', 'version', 'name', 'type', 'title', 'description', 'import_error'

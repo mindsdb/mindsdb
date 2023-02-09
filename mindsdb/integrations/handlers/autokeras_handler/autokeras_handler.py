5cee752f9a24ee226793f0746d03d919634e8428
Sam Miller <sam_miller@AUTO-ATI0894.localdomain>
2023-02-09 18:07:27 +0000

    first working version of create and predict (but preds suck)

--- mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
+++ mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
@@ -5,6 +5,7 @@ import dill
 import pandas as pd
 import autokeras as ak
 from mindsdb.integrations.libs.base import BaseMLEngine
+from tensorflow.keras.models import load_model
 
 # Makes this run on Windows Subsystem for Linux
 os.environ["XLA_FLAGS"]="--xla_gpu_cuda_data_dir=/usr/lib/cuda"
@@ -17,9 +18,10 @@ DEFAULT_MODE = "regression"
 DEFAULT_EPOCHS = 1
 DEFAULT_TRIALS = 1
 
-def train_autokeras_model(training_df, target, mode=DEFAULT_MODE):
+def train_autokeras_model(df, target, mode=DEFAULT_MODE):
+    training_df = df.drop(target, axis=1)
     trainer = trainer_dict[mode](overwrite=True, max_trials=DEFAULT_TRIALS)
-    trainer.fit(training_df.drop(target, axis=1), training_df[target], epochs=DEFAULT_EPOCHS)
+    trainer.fit(training_df, df[target], epochs=DEFAULT_EPOCHS)
     return trainer.export_model()
 
 
@@ -35,10 +37,28 @@ class AutokerasHandler(BaseMLEngine):
         Create and train model on the input df
         """
         args = args['using']  # ignore the rest of the problem definition
+        args["target"] = target
+        args["folder_path"] = "autokeras"
 
         
-        training_df = df.drop(target, axis=1)
-        model = train_autokeras_model(df, training_df)
+        model = train_autokeras_model(df, target)
 
-        with open("hi.txt", "w+") as f:
-            f.write("hi")
+        model.save(args["folder_path"])
+        self.model_storage.json_set("predict_args", args)
+
+    
+    def predict(self, df, args=None):
+        args = self.model_storage.json_get("predict_args")
+        model = load_model(args["folder_path"], custom_objects=ak.CUSTOM_OBJECTS)
+
+        df_to_predict = df.copy()
+        cols_to_drop = ["__mindsdb_row_id", args["target"]]
+        for col in cols_to_drop:
+            if col in df_to_predict.columns:
+                df_to_predict = df_to_predict.drop(col, axis=1)
+        df_to_predict.to_csv("raw_df.csv")
+        preds = model.predict(df_to_predict)
+
+        df_to_predict[args["target"]] = preds
+        return df_to_predict
+

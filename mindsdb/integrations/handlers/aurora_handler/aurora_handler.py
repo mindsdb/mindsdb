a00ec310722f349863ea26740fdfdf59d0198470
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-02-13 23:36:39 +0530

    added a couple of TODOs for the use of the name parameter

--- mindsdb/integrations/handlers/aurora_handler/aurora_handler.py
+++ mindsdb/integrations/handlers/aurora_handler/aurora_handler.py
@@ -23,6 +23,7 @@ class AuroraHandler(DatabaseHandler):
             connection_data (dict): parameters for connecting to the database
             **kwargs: arbitrary keyword arguments.
         """
+        # TODO: check if using the same name parameter here and when instantiating the objects will be feasible
         super().__init__(name)
         self.parser = parse_sql
         self.dialect = 'aurora'
@@ -38,11 +39,13 @@ class AuroraHandler(DatabaseHandler):
 
         if self.connection_data['db_engine'] == 'mysql' or database_engine == 'aurora':
             self.db = MySQLHandler(
+                # TODO: check if using the same name parameter here again will be feasible
                 name=name,
                 connection_data=self.connection_data
             )
         elif self.connection_data['db_engine'] == 'postgresql' or database_engine == 'aurora-postgresql':
             self.db = PostgresHandler(
+                # TODO: check if using the same name parameter here again will be feasible
                 name=name,
                 connection_data=self.connection_data
             )

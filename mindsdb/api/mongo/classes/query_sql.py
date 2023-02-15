9ac8f028743db8e71cbdfc1db7c0999999234f6f
Ilia Tsyplenkov <ilya@mindsdb.com>
2023-02-15 11:06:18 +0300

    Seems done (need fully test)

--- mindsdb/api/mongo/classes/query_sql.py
+++ mindsdb/api/mongo/classes/query_sql.py
@@ -1,7 +1,5 @@
-from mindsdb.api.mysql.mysql_proxy.controllers.session_controller import SessionController
-# from mindsdb.api.mysql.mysql_proxy.executor.executor_commands import ExecuteCommands
+from mindsdb.api.mysql.mysql_proxy.controllers import SessionController
 from mindsdb.api.mysql.mysql_proxy.executor import Executor
-from mindsdb.api.mysql.mysql_proxy.executor.executor_commands import ExecuteCommands
 
 class SqlServerStub:
     """This class is just an emulation of Server object,
@@ -22,7 +20,7 @@ def run_sql_command(mindsdb_env, ast_query):
 
     executor = Executor(sql_session, sqlserver)
     executor.query_execute(ast_query.to_string())
-    if executor.error_code is not None:
+    if executor.error_code:
         raise Exception(executor.error_message)
 
     if executor.columns is None:
@@ -39,29 +37,3 @@ def run_sql_command(mindsdb_env, ast_query):
         data.append(dict(zip(column_names, row)))
 
     return data
-
-
-# def run_sql_command(mindsdb_env, ast_query):
-#     sql_session = SessionController()
-#     sql_session.database = 'mindsdb'
-# 
-#     print(f"execute ast_query - {ast_query} of type {type(ast_query)}")
-#     command_executor = ExecuteCommands(sql_session, executor=None)
-#     ret = command_executor.execute_command(ast_query)
-#     if ret.error_code is not None:
-#         raise Exception(ret.error_message)
-# 
-#     if ret.columns is None:
-#         # return no data
-#         return []
-# 
-#     column_names = [
-#         c.name is c.alias is None or c.alias
-#         for c in ret.columns
-#     ]
-# 
-#     data = []
-#     for row in ret.data:
-#         data.append(dict(zip(column_names, row)))
-# 
-#     return data

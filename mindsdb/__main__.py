8955fadd3240f61ea309c3f38ef9e619aed1b008
Max Stepanov <stpmax@yandex.ru>
2023-02-17 14:29:52 +0000

    better handler aws_metadata

--- mindsdb/__main__.py
+++ mindsdb/__main__.py
@@ -137,24 +137,37 @@ if __name__ == '__main__':
     is_marketplace = config.get('environment') == 'aws_marketplace'
     if is_marketplace:
         try:
-            resp = requests.get('http://169.254.169.254/latest/meta-data/public-hostname', timeout=1)
-            if resp.status_code != 200:
-                raise Exception()
-            public_host = resp.text
+            aws_meta_data = {
+                'public-hostname': None,
+                'ami-id': None,
+                'instance-id': None
+            }
+            for key in aws_meta_data.keys():
+                resp = requests.get(
+                    f'http://169.254.169.254/latest/meta-data/{key}',
+                    timeout=1
+                )
+                if resp.status_code != 200:
+                    raise Exception()
+                aws_meta_data[key] = resp.text
+
+            current_aws_meta_data = config.get('aws_meta_data', {})
+            oauth_meta = config.get('auth', {}).get('oauth', {})
+
+            public_hostname = aws_meta_data['public-hostname']
             if (
-                config.get('public_host') != public_host
-                or config.get('auth', {}).get('oauth', {}).get('client_id') is None
+                current_aws_meta_data.get('public-hostname') != public_hostname
+                or oauth_meta.get('client_id') is None
             ):
-                auth_server_host = config['auth']['oauth']['server_host']
                 resp = requests.post(
-                    f'https://{auth_server_host}/auth/register_client',
+                    f'https://{oauth_meta["server_host"]}/auth/register_client',
                     json={
-                        'client_name': f'aws_marketplace_{public_host}',
-                        'client_uri': public_host,
+                        'client_name': f'aws_marketplace_{public_hostname}',
+                        'client_uri': public_hostname,
                         'grant_types': 'authorization_code',
                         'redirect_uris': textwrap.dedent(f'''
-                            https://{public_host}/api/auth/callback
-                            https://{public_host}/api/auth/callback/cloud_home
+                            https://{public_hostname}/api/auth/callback
+                            https://{public_hostname}/api/auth/callback/cloud_home
                         '''),
                         'response_types': 'code',
                         'scope': 'openid profile aws_marketplace',
@@ -166,7 +179,7 @@ if __name__ == '__main__':
                     raise Exception()
                 keys = resp.json()
                 Config().update({
-                    'public_host': public_host,
+                    'aws_meta_data': aws_meta_data,
                     'auth': {
                         'oauth': {
                             'client_id': keys['client_id'],

4201d864b5f431261a1f36217e37f03204e96496
Sam Miller <sam@atlasai.co.uk>
2023-02-20 17:41:09 +0000

    clean up into smaller funcs

--- mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
+++ mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
@@ -19,10 +19,9 @@ trainer_dict = {
     "classification": ak.StructuredDataClassifier
 }
 
-DEFAULT_EPOCHS = 3
-DEFAULT_TRIALS = 3
+DEFAULT_TRIALS = 2
 
-def train_autokeras_model(df, target):
+def train_model(df, target):
     # Choose regressor of classifier based on target data type
     if np.issubdtype(df[target].dtype, np.number):
         mode = "regression"
@@ -37,17 +36,36 @@ def train_autokeras_model(df, target):
 
     # Save the column names of all numeric columns before transforming any categorical columns into dummies
     numeric_column_names = training_df.select_dtypes(include=[np.number]).columns.values.tolist()
-
+    # Extract all dummy column names - these are used in the prediction method
     training_df = pd.get_dummies(training_df)
-
-    # Extract all dummy column names. These will be used to prepare the predict df before the prediction
     categorical_dummy_column_names = [col for col in training_df.columns.values.tolist() if col not in numeric_column_names]
-
-    trainer.fit(training_df, y_train, epochs=DEFAULT_EPOCHS)
+    trainer.fit(training_df, y_train, verbose=2, epochs=10)
     return trainer.export_model(), categorical_dummy_column_names
 
 
-def get_preds_from_autokeras_model(df, model, target, categorical_dummy_column_names):
+def get_prediction_df(mindsdb_df, training_df):
+    """
+    mindsdb_df is passed in from the .predict() method
+    training_df is the full df from training
+    """
+    # Remove column that didn't exist in the training df so that we can do filtering
+    df_to_predict = mindsdb_df.copy()
+    if "__mindsdb_row_id" in df_to_predict.columns.values.tolist():
+        df_to_predict = df_to_predict.drop("__mindsdb_row_id", axis=1)
+
+    # Filter the training df based on the predict df
+    keys = list(df_to_predict.columns.values)
+    i1 = training_df.set_index(keys).index
+    i2 = df_to_predict.set_index(keys).index
+    filtered_df = training_df[i1.isin(i2)]
+
+    if filtered_df.empty:
+        # TODO: Rephrase the exception message in a more user-friendly way
+        raise Exception("The condition(s) in the WHERE clause filtered out all the data. Please refine these and try again")
+    return filtered_df
+
+
+def get_preds_from_model(df, model, target, categorical_dummy_column_names):
     # Remove columns that didn't exist in the training df
     cols_to_drop = ["__mindsdb_row_id", target]
     for col in cols_to_drop:
@@ -60,8 +78,19 @@ def get_preds_from_autokeras_model(df, model, target, categorical_dummy_column_n
         if col not in prediction_df.columns.values.tolist():
             prediction_df[col] = 0
 
-    return model.predict(prediction_df)
+    return model.predict(prediction_df, verbose=2)
 
+
+def format_categorical_preds(predictions, original_y, df_to_predict, target_col):
+    # Turn prediction back into categorical value
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(original_y)
+    preds = lb.inverse_transform(predictions)
+
+    # Add the confidence score next to the prediction
+    df_to_predict[target_col] = pd.Series(preds).astype(original_y.dtype)
+    df_to_predict["confidence"] = [max(row) for _, row in enumerate(predictions)]
+    return df_to_predict
 
 
 class AutokerasHandler(BaseMLEngine):
@@ -84,46 +113,22 @@ class AutokerasHandler(BaseMLEngine):
         random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=24))
         args["folder_path"] = os.path.join("autokeras", random_string)
 
-        model, args["data_column_names"] = train_autokeras_model(df, target)
+        model, args["data_column_names"] = train_model(df, target)
         model.save(args["folder_path"])
         self.model_storage.json_set("predict_args", args)
     
     def predict(self, df, args=None):
         args = self.model_storage.json_get("predict_args")
         training_df = pd.read_json(args["training_df"])
-
         model = load_model(args["folder_path"], custom_objects=ak.CUSTOM_OBJECTS)
 
-        # Remove column that didn't exist in the training df so that we can do filtering
-        df_to_predict = df.copy()
-        if "__mindsdb_row_id" in df_to_predict.columns.values.tolist():
-            df_to_predict = df_to_predict.drop("__mindsdb_row_id", axis=1)
-
-        # Filter the training df based on the predict df
-        keys = list(df_to_predict.columns.values)
-        i1 = training_df.set_index(keys).index
-        i2 = df_to_predict.set_index(keys).index
-        filtered_df = training_df[i1.isin(i2)]
-
-        if filtered_df.empty:
-            # TODO: Rephrase the exception message in a more user-friendly way
-            raise Exception("The condition(s) in the WHERE clause filtered out all the data. Please refine these and try again")
-
-        original_y = training_df[args["target"]]
-        predictions = get_preds_from_autokeras_model(filtered_df, model, args["target"], args["data_column_names"])
+        df_to_predict = get_prediction_df(df, training_df)
+        predictions = get_preds_from_model(df_to_predict, model, args["target"], args["data_column_names"])
 
         # If we used the classifier we need to pre-process the predictions before returning them
+        original_y = training_df[args["target"]]
         if not np.issubdtype(original_y.dtype, np.number):
-            # Turn prediction back into categorical value
-            lb = preprocessing.LabelBinarizer()
-            lb.fit(original_y)
-            preds = lb.inverse_transform(predictions)
+            return format_categorical_preds(predictions, original_y, df_to_predict, args["target"])
 
-            # Add the confidence score next to the prediction
-            filtered_df[args["target"]] = pd.Series(preds).astype(original_y.dtype)
-            filtered_df["confidence"] = [max(row) for _, row in enumerate(predictions)]
-            return filtered_df
-
-
-        filtered_df[args["target"]] = predictions
-        return filtered_df
+        df_to_predict[args["target"]] = predictions
+        return df_to_predict

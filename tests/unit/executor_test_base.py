44ae4ce31cddbcc01e13e9240acba4ed34adcd33
Dipti Sengupta <diptisengupta@Diptis-Own-MacBook.local>
2023-03-02 12:48:01 +0100

    Adding Failing AutoGluon Test case
    Added the failing test class before creating the handler.

--- tests/unit/executor_test_base.py
+++ tests/unit/executor_test_base.py
@@ -99,6 +99,8 @@ class BaseUnitTest:
         db.session.add(r)
         r = db.Integration(name='dummy_ml', data={}, engine='dummy_ml')
         db.session.add(r)
+        r = db.Integration(name='autogluon', data={}, engine='autogluon')
+        db.session.add(r)
         r = db.Integration(name='lightwood', data={}, engine='lightwood')
         db.session.add(r)
         db.session.flush()

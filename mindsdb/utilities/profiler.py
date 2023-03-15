c2740235a942e46cd63e453758e5fbe5baa6ca71
Max Stepanov <stpmax@yandex.ru>
2023-03-15 18:12:55 +0300

    improve

--- mindsdb/utilities/profiler.py
+++ mindsdb/utilities/profiler.py
@@ -18,18 +18,6 @@ def stop():
         ctx.profiling_stop_current_node()
 
 
-def profile(tag: str = None):
-    def decorator(function):
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            start(tag or f'{function.__module__}.{function.__name__}')
-            result = function(*args, **kwargs)
-            stop()
-            return result
-        return wrapper
-    return decorator
-
-
 class Context:
     def __init__(self, tag):
         self.tag = tag
@@ -39,3 +27,14 @@ class Context:
 
     def __exit__(self, exc_type, exc_value, traceback):
         stop()
+
+
+def profile(tag: str = None):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            with Context(tag or f'{function.__module__}.{function.__name__}'):
+                result = function(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator

6b48d7f2bdd69b5dcb269a7db9337a928086f4a0
Dipti Sengupta <diptisengupta@DiptisOnMacBook.fritz.box>
2023-03-15 19:38:09 +0100

    Adding Features
    Autogluon requires the features to be listed in the df while predicting. Even empty features but it needs to be present. Adding the features when not provided in the test
    Also added the sample autogluon file.

--- mindsdb/integrations/handlers/autogluon_handler/autogluon_handler.py
+++ mindsdb/integrations/handlers/autogluon_handler/autogluon_handler.py
@@ -3,6 +3,7 @@ import pandas as pd
 from typing import Optional
 import logging
 import dill
+import os
 from mindsdb.integrations.libs.base import BaseMLEngine
 from autogluon.tabular import TabularDataset, TabularPredictor
 
@@ -22,23 +23,29 @@ class AutoGluonHandler(BaseMLEngine):
             model_name = args['model_name']
         except KeyError:
             model_name = 'default'
-
-        save_path = 'mindsdb-predict'  # specifies folder to store trained models
+        store_path = os.environ.get('MINDSDB_STORAGE_DIR')
+        save_path = os.path.join(store_path, 'mindsdb-predict') # specifies folder to store trained models
         predictor = TabularPredictor(label=args['target'], path=save_path).fit(df)
 
         # persist changes to handler folder
-        self.engine_storage.folder_sync(model_name)
-
-        ###### persist changes to handler folder
+        # self.engine_storage.folder_sync(model_name)
+        #
+        # ###### persist changes to handler folder
         self.model_storage.json_set("model_args", args)
-        self.model_storage.file_set("training_df", dill.dumps(df))
-        self.model_storage.file_set("trained_model", dill.dumps(predictor))
+        # self.model_storage.file_set("training_df", dill.dumps(df))
+        # self.model_storage.file_set("trained_model", dill.dumps(predictor))
 
 
     def predict(self, df: pd.DataFrame, args: Optional[Dict] = None) -> pd.DataFrame:
         logging.debug('Predict!')
         args = self.model_storage.json_get('args')
-        predictor = self.model_storage.file_get('trained_model')
+        store_path = os.environ.get('MINDSDB_STORAGE_DIR')
+        save_path = os.path.join(store_path, 'mindsdb-predict')
+        predictor = TabularPredictor.load(path=save_path)
+        for feat in predictor.features():
+            datatype = predictor.feature_metadata_in.get_feature_type_raw(feat)
+            if feat not in df.columns:
+                df[feat] = pd.Series(dtype=datatype)
         y_pred = predictor.predict(df)
         return pd.DataFrame(y_pred)
 
--- mindsdb/integrations/handlers/autogluon_handler/sample_autogluon.py
+++ mindsdb/integrations/handlers/autogluon_handler/sample_autogluon.py
@@ -1,21 +1,27 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+import glob
 
-
-if __name__ == '__main__':
+def find(name, path):
+    for f in glob.glob('/Users/diptisengupta/Desktop/CODEWORK/GitHub/OpenSource/mindsdb/**/*.pkl', recursive=True):
+        print(f)
+def sample_autogluon():
     train_data = TabularDataset('https://autogluon.s3.amazonaws.com/datasets/Inc/train.csv')
     subsample_size = 500  # subsample subset of data for faster demo, try setting this to much larger values
     train_data = train_data.sample(n=subsample_size, random_state=0)
     print(train_data.head(4))
     label = 'class'
     print("Summary of class variable: \n", train_data[label].describe())
-    save_path = 'agModels-predictClass'  # specifies folder to store trained models
+    store_path = os.environ.get('MINDSDB_STORAGE_DIR')
+    save_path = os.path.join(store_path,'agModels-predictClass')  # specifies folder to store trained models
     predictor = TabularPredictor(label=label, path=save_path).fit(train_data)
 
     test_data = TabularDataset('https://autogluon.s3.amazonaws.com/datasets/Inc/test.csv')
     y_test = test_data[label]  # values to predict
     test_data_nolab = test_data.drop(columns=[label])  # delete label column to prove we're not cheating
     test_data_nolab.head()
-
     predictor = TabularPredictor.load(
         save_path)  # unnecessary, just demonstrates how to load previously-trained predictor from file
 
@@ -24,3 +30,68 @@ if __name__ == '__main__':
     perf = predictor.evaluate_predictions(y_true=y_test, y_pred=y_pred, auxiliary_metrics=True)
 
     predictor.leaderboard(test_data, silent=True)
+
+def train_books():
+    df = pd.read_csv('data.csv')
+    df = df.dropna()
+    train, test = train_test_split(df, test_size=0.2)
+    train.to_csv('data_train.csv', index=False)
+    test.to_csv('data_test.csv', index=False)
+    train_data = TabularDataset('data_train.csv')
+    # subsample_size = 5000  # subsample subset of data for faster demo, try setting this to much larger values
+    # train_data = train_data.sample(n=subsample_size, random_state=0)
+    print(train_data.head(4))
+    label = 'genre'
+    print("Summary of class variable: \n", train_data[label].describe())
+    store_path = os.environ.get('MINDSDB_STORAGE_DIR') or ''
+    save_path = os.path.join(store_path,'agModels-predictClass')
+
+    # AutoGluon will gauge predictive performance using evaluation metric: 'accuracy'
+    # 	To change this, specify the eval_metric parameter of Predictor()
+    predictor = TabularPredictor(label=label, path=save_path).fit(train_data)
+
+    test_data = TabularDataset('data_test.csv')
+    y_test = test_data[label]  # values to predict
+    test_data_nolab = test_data.drop(columns=[label])  # delete label column to prove we're not cheating
+    test_data_nolab.head()
+    predictor = TabularPredictor.load(
+        save_path)  # unnecessary, just demonstrates how to load previously-trained predictor from file
+
+    y_pred = predictor.predict(test_data_nolab)
+
+    for feat in predictor.features():
+        datatype = predictor.feature_metadata_in.get_feature_type_raw(feat)
+        if feat not in test_data_nolab.columns:
+            test_data_nolab[feat] = pd.Series(dtype=datatype)
+    test_data_nolab.set_index('summary')
+
+    print("Predictions:  \n", y_pred)
+    perf = predictor.evaluate_predictions(y_true=y_test, y_pred=y_pred, auxiliary_metrics=True)
+
+    predictor.leaderboard(test_data, silent=True)
+    print(test_data.head(4))
+
+
+def book_pred():
+    label = 'genre'
+    store_path = os.environ.get('MINDSDB_STORAGE_DIR') or ''
+    save_path = os.path.join(store_path, 'agModels-predictClass')
+    test_data = TabularDataset('data_test.csv')
+    y_test = test_data[label]  # values to predict
+    test_data_nolab = test_data.drop(columns=['genre']) # delete label column to prove we're not cheating
+    test_data_nolab.drop(['index', 'title'],axis=1)
+    predictor = TabularPredictor.load(
+        save_path)# unnecessary, just demonstrates how to load previously-trained predictor from file
+
+
+    for feat in predictor.features():
+        datatype = predictor.feature_metadata_in.get_feature_type_raw(feat)
+        if feat not in test_data_nolab.columns:
+            test_data_nolab[feat] = pd.Series(dtype=datatype)
+
+    y_pred = predictor.predict(test_data_nolab)
+    perf = predictor.evaluate_predictions(y_true=y_test, y_pred=y_pred, auxiliary_metrics=True)
+    print("Performance:  \n", perf)
+
+if __name__ == '__main__':
+    book_pred()

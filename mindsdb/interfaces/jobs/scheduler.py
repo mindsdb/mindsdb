f64883e06666c0b2c73985b92a20ba52d8e2161c
andrew <elkin@raketa.im>
2023-03-15 16:04:39 +0300

    flake8

--- mindsdb/interfaces/jobs/scheduler.py
+++ mindsdb/interfaces/jobs/scheduler.py
@@ -78,10 +78,10 @@ def task_process(record_id, exec_method):
     if exec_method == 'local':
         try:
             history_id = scheduler.lock_record(record_id)
-        except Exception as e:
+        except Exception:
             db.session.rollback()
             db.session.remove()
-            logger.error(f'Unable create history record, is locked?')
+            logger.error('Unable create history record, is locked?')
             return
 
         scheduler.execute_task_local(record_id, history_id)

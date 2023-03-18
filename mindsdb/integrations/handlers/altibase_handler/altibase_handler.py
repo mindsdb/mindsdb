5e2d52dd7f01173c1d4045a7a7a826188138b78f
topefolorunso <folorunsotemitope10@gmail.com>
2023-03-18 23:47:23 +0100

    implement disconnect, check_connection, native_query and query methods for altibase handler

--- mindsdb/integrations/handlers/altibase_handler/altibase_handler.py
+++ mindsdb/integrations/handlers/altibase_handler/altibase_handler.py
@@ -1,11 +1,17 @@
-from typing import Optional
+from typing import Any, Optional
 
 from mindsdb.integrations.libs.base import DatabaseHandler
-from mindsdb.integrations.libs.response import HandlerStatusResponse
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+    HandlerResponse as Response,
+    RESPONSE_TYPE
+)
 from mindsdb.utilities import log
 from mindsdb_sql import parse_sql
+from mindsdb_sql.parser.ast.base import ASTNode
 
 import jaydebeapi as jdbcconnector
+import pandas as pd
 
 
 
@@ -34,57 +40,125 @@ class AltibaseHandler(DatabaseHandler):
         self.connection = None
         self.is_connected = False
 
-    def connect(self) -> HandlerStatusResponse:
+    def connect(self):
         """ Set up any connections required by the handler
-        Should return output of check_connection() method after attempting
-        connection. Should switch self.is_connected.
+        Should return output of check_connection() method after attempting connection.
+        Should switch self.is_connected.
         Returns:
-            HandlerStatusResponse
+            connection
         """
         if self.is_connected is True:
             return self.connection
                 
         user = self.connection_args.get('user')
         password = self.connection_args.get('password')
-        jdbc_class = self.connection_args.get('jdbcClass', 'Altibase.jdbc.driver.AltibaseDriver')
         jar_location = self.connection_args.get('jdbcJarLocation')
 
-        jdbc_url = f"jdbc:Altibase://{self.host}:{self.port}/{self.database};"
+        jdbc_class = self.connection_args.get('jdbcClass', 'Altibase.jdbc.driver.AltibaseDriver')
+        jdbc_url = f"jdbc:Altibase://{self.host}:{self.port}/{self.database}"
 
         try:
             if user and password and jar_location: 
-                self.connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, driver_args=[user, password], jars=jar_location.split(","))
+                connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, driver_args=[user, password], jars=jar_location.split(","))
             elif user and password: 
-                self.connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, driver_args=[user, password])
+                connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, driver_args=[user, password])
             elif jar_location: 
-                self.connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, jars=jar_location.split(","))
+                connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url, jars=jar_location.split(","))
             else:
-                self.connection = jdbcconnector.connect(jdbc_class, jdbc_url)
+                connection = jdbcconnector.connect(jclassname=jdbc_class, url=jdbc_url)
         except Exception as e:
             log.logger.error(f"Error while connecting to {self.database}, {e}")
-
+        
+        self.is_connected = True
+        self.connection = connection
         return self.connection
 
 
-    def disconnect():
+    def disconnect(self):
+        """ Close any existing connections
+        Should switch self.is_connected.
         """
-        It gracefully closes connections established in the connect method.
-        """
+        if self.is_connected is False:
+            return
+        try:
+            self.connection.close()
+            self.is_connected = False
+        except Exception as e:
+            log.logger.error(f"Error while disconnecting to {self.database}, {e}")
+        return 
 
-    def check_connection():
+    def check_connection(self) -> StatusResponse:
+        """ Check connection to the handler
+        Returns:
+            HandlerStatusResponse
         """
-        It evaluates if the connection is alive and healthy. This method is called frequently.
-        """
+        responseCode = StatusResponse(success=False)
+        need_to_close = self.is_connected is False
 
-    def native_query():
-        """
-        It parses any native statement string and acts upon it (for example, raw SQL commands).
-        """
+        try:
+            self.connect()
+            responseCode.success = True
+        except Exception as e:
+            log.logger.error(f'Error connecting to database {self.database}, {e}!')
+            responseCode.error_message = str(e)
+        finally:
+            if responseCode.success is True and need_to_close:
+                self.disconnect()
+            if responseCode.success is False and self.is_connected is True:
+                self.is_connected = False
 
-    def query():
+        return responseCode
+
+    def native_query(self, query: str) -> Response:
+        """Receive raw query and act upon it somehow.
+        Args:
+            query (str): query in native format
+        Returns:
+            HandlerResponse
         """
-        It takes a parsed SQL command in the form of an abstract syntax tree and executes it.
+        need_to_close = self.is_connected is False
+        connection = self.connect()
+        with connection.cursor() as cur:
+            try:
+                cur.execute(query)
+                if cur.description:
+                    result = cur.fetchall() 
+                    response = Response(
+                        RESPONSE_TYPE.TABLE,
+                        data_frame=pd.DataFrame(
+                            result,
+                            columns=[x[0] for x in cur.description]
+                        )
+                    )
+                else:
+                    response = Response(RESPONSE_TYPE.OK)
+                connection.commit()
+            except Exception as e:
+                log.logger.error(f'Error running query: {query} on {self.database}!')
+                response = Response(
+                    RESPONSE_TYPE.ERROR,
+                    error_message=str(e)
+                )
+                connection.rollback()
+
+        if need_to_close is True:
+            self.disconnect()
+
+        return response
+
+    def query(self, query: ASTNode) -> Response:
+        """Receive query as AST (abstract syntax tree) and act upon it somehow.
+        Args:
+            query (ASTNode): sql query represented as AST. May be any kind of query: SELECT, INSERT, DELETE, etc
+        Returns:
+            HandlerResponse
         """
+        if isinstance(query, ASTNode):
+            query_str = query.to_string()
+        else:
+            query_str = str(query)
+
+        return self.native_query(query_str)
 
     def get_tables():
         """

3789b2e0208143e4f0e6eb26382eca910548ca4b
topefolorunso <folorunsotemitope10@gmail.com>
2023-03-28 19:21:47 +0100

    add edgelessdb handler

---
+++ mindsdb/integrations/handlers/edgelessdb_handler/edgelessdb_handler.py
@@ -0,0 +1,11 @@
+from ..mysql_handler import Handler as MySQLHandler
+
+
+class EdgelessDBHandler(MySQLHandler):
+    """
+    This handler handles connection and execution of the EdgelessDB statements.
+    """
+    name = 'edgelessdb'
+
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)

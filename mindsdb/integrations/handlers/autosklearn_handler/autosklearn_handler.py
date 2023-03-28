0fa5795dead7b73890e66d4b4bc5aaf20cef5d00
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-03-28 23:55:11 +0530

    updated the create() method to use the passed in target variable to run the ML workload

--- mindsdb/integrations/handlers/autosklearn_handler/autosklearn_handler.py
+++ mindsdb/integrations/handlers/autosklearn_handler/autosklearn_handler.py
@@ -17,7 +17,7 @@ class AutoSklearnHandler(BaseMLEngine):
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[dict] = None) -> None:
         automl_classifier = automl.AutoSklearnClassifier(time_left_for_this_task=3600, per_run_time_limit=360, n_jobs=-1)
 
-        automl_classifier.fit(df.drop('y', axis=1), df['y'], metric='accuracy', cv=5, ensemble_size=1)
+        automl_classifier.fit(df.drop(target, axis=1), df[target], metric='accuracy', cv=5, ensemble_size=1)
 
         best_model = automl_classifier.get_models_with_weights()[0][0]
 

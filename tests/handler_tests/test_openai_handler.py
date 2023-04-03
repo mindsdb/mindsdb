cb9aeaddab4ed8443906c31feb25d60f7d550f37
Fayez <fayezkhan680@gmail.com>
2023-04-04 00:57:22 +0530

    added 2 blank lines

--- tests/handler_tests/test_openai_handler.py
+++ tests/handler_tests/test_openai_handler.py
@@ -3,10 +3,12 @@ import pytest
 from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
 from integration_tests.flows.http_test_helpers import HTTPHelperMixin
 
+
 OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 
 @pytest.mark.usefixtures("mindsdb_app")
 class TestOpenAIHandler(HTTPHelperMixin):
+    
 
     def test_missing_required_keys(self):
         query = f"""
@@ -17,6 +19,7 @@ class TestOpenAIHandler(HTTPHelperMixin):
             api_key = '{OPEN_AI_API_KEY}';
         """
         self.sql_via_http(query, RESPONSE_TYPE.ERROR)
+
 
     def test_invalid_openai_name_parameter(self):
         query = f"""

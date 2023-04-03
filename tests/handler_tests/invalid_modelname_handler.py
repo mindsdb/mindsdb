35d3da267bd2648d180138b9f9d76180f32135a5
Fayez <fayezkhan680@gmail.com>
2023-04-03 16:05:07 +0530

    integration test for invalid_openai name params

--- tests/handler_tests/invalid_modelname_handler.py
+++ tests/handler_tests/invalid_modelname_handler.py
@@ -2,8 +2,9 @@ import pytest
 import mindsdb
 import openai
 
+
 def test_invalid_openai_name_parameter():
-    openai.api_key = 'YOUR_API_KEY' # Replace with your OpenAI API key
+    openai.api_key = 'YOUR_API_KEY'  # Replace with your OpenAI API key
     available_models = [model.id for model in openai.Model.list()]
     with pytest.raises(ValueError, match=r'.*Invalid model name.*'):
         mindsdb.Predictor(name='invalid_model_name', model_type='nlp', predict_when='accuracy', query={

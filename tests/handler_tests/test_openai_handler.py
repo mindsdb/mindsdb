a97d2a57e2703a16e04e97d806e70cfa038d526f
Fayez <fayezkhan680@gmail.com>
2023-04-04 01:20:04 +0530

    possible errors removed

--- tests/handler_tests/test_openai_handler.py
+++ tests/handler_tests/test_openai_handler.py
@@ -6,10 +6,10 @@ from integration_tests.flows.http_test_helpers import HTTPHelperMixin
 
 OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 
+
 @pytest.mark.usefixtures("mindsdb_app")
 class TestOpenAIHandler(HTTPHelperMixin):
     
-
     def test_missing_required_keys(self):
         query = f"""
             CREATE MODEL sentiment_classifier_gpt3
@@ -20,14 +20,13 @@ class TestOpenAIHandler(HTTPHelperMixin):
         """
         self.sql_via_http(query, RESPONSE_TYPE.ERROR)
 
-
-    def test_invalid_openai_name_parameter(self):
-        query = f"""
-            CREATE MODEL sentiment_classifier_gpt3
-            PREDICT sentiment
-            USING
-            engine = 'openai',
-            api_key = '{OPEN_AI_API_KEY}',
-            model_name = 'invalid_model_name';
+        def test_invalid_openai_name_parameter(self):
+            query = f"""
+                CREATE MODEL sentiment_classifier_gpt3
+                PREDICT sentiment
+                USING
+                engine = 'openai',
+                api_key = '{OPEN_AI_API_KEY}',
+                model_name = 'invalid_model_name';
         """
         self.sql_via_http(query, RESPONSE_TYPE.ERROR)

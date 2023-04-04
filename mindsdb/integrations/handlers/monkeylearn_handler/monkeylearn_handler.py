bade3c8f883f7ef4fbeb78551a117a06682423fd
Pranav <pranavprajapati586@gmail.com>
2023-04-04 17:15:18 +0530

    Completed the predict method of the Handler

--- mindsdb/integrations/handlers/monkeylearn_handler/monkeylearn_handler.py
+++ mindsdb/integrations/handlers/monkeylearn_handler/monkeylearn_handler.py
@@ -14,12 +14,13 @@ class monkeylearnHandler(BaseMLEngine):
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
         args = args['using']
         model_id = args['MODEL_ID']
-
+        API_key = args['YOUR_API_KEY']
         # if 'production_model' in args:
         #     raise Exception("Custom models are not supported currently")
 
+        # Check whether the model_id given by user exists in the user account or monkeylearn pre-trained models
         url = 'https://api.monkeylearn.com/v3/classifiers/'
-        response = requests.get(url, headers={'Authorization': 'Token {}'.format(args['YOUR_API_KEY'])})
+        response = requests.get(url, headers={'Authorization': 'Token {}'.format(API_key)})
         if response.status_code == 200:
             models = response.json()
             models_list = [model['id'] for model in models]
@@ -27,23 +28,27 @@ class monkeylearnHandler(BaseMLEngine):
             raise Exception(f"Server response {response.status_code}")
 
         if model_id not in models_list:
-            raise Exception(f"Model_id not found {model_id} in MonkeyLearn pre-trained models")
+            raise Exception(f"Model_id {model_id} not found in MonkeyLearn pre-trained models")
 
         self.model_storage.json_set('args',args)
-        self.engine_storage.folder_sync(model_id)
 
-        def predict(self, df, args= None):
+        def predict(self, df, args=None):
             args = self.model_storage.json_get('args')
-            model_id = self.engine_storage.folder_get(args['model_id'], update=False)
             input_list = df[args['input_column']]
+            ml = MonkeyLearn(args['YOUR_API_KEY'])
+            classifier_response = ml.classifiers.classify(args['MODEL_ID'],input_list)
+            df_dict = []
+            for res_dict in classifier_response.body:
+                text = res_dict['text']
+                pred_dict = res_dict['classifications'][0]  #Only add the one which model is more confident about
+                pred_dict['text'] = text
+                df_dict.append(pred_dict)
+            pred_df = pd.DataFrame(df_dict)
+            return pred_df
 
-
-
-
-
-
-
-
+        def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
+            args = self.model_storage.json_get('args')
+            ml = MonkeyLearn(args['YOUR_API_KEY'])
 
 
 

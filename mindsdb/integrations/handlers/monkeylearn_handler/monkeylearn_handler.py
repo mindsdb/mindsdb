14c1e5c4bec65bdb2efb0f7fcc3f26885c47207c
Pranav <pranavprajapati586@gmail.com>
2023-04-04 11:16:40 +0530

    Fixed the models list that gets response of resources

--- mindsdb/integrations/handlers/monkeylearn_handler/monkeylearn_handler.py
+++ mindsdb/integrations/handlers/monkeylearn_handler/monkeylearn_handler.py
@@ -13,24 +13,39 @@ class monkeylearnHandler(BaseMLEngine):
 
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
         args = args['using']
-        connection = MonkeyLearn(args['YOUR_API_KEY'])
         model_id = args['MODEL_ID']
 
-        if 'production_model' in args:
-            raise Exception("Custom models are not supported currently")
+        # if 'production_model' in args:
+        #     raise Exception("Custom models are not supported currently")
 
         url = 'https://api.monkeylearn.com/v3/classifiers/'
         response = requests.get(url, headers={'Authorization': 'Token {}'.format(args['YOUR_API_KEY'])})
         if response.status_code == 200:
             models = response.json()
-            models_list = [model for model in models[id]]
+            models_list = [model['id'] for model in models]
         else:
             raise Exception(f"Server response {response.status_code}")
 
         if model_id not in models_list:
             raise Exception(f"Model_id not found {model_id} in MonkeyLearn pre-trained models")
 
-        self.
+        self.model_storage.json_set('args',args)
+        self.engine_storage.folder_sync(model_id)
+
+        def predict(self, df, args= None):
+            args = self.model_storage.json_get('args')
+            model_id = self.engine_storage.folder_get(args['model_id'], update=False)
+            input_list = df[args['input_column']]
+
+
+
+
+
+
+
+
+
+
 
 
 

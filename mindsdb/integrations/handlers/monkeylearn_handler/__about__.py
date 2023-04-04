7fc71aeab63e4c8318f9886b2205c4eaf592fdd5
Pranav <pranavprajapati586@gmail.com>
2023-04-04 21:11:41 +0530

    Added the about.py file in the handler directory

---
+++ mindsdb/integrations/handlers/monkeylearn_handler/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'MindsDB MonkeyLearn handler'
+__package_name__ = 'mindsdb_monkeylearn_handler'
+__version__ = '0.0.2'
+__description__ = "MindsDB handler for monkeylearn"
+__author__ = 'Pranav Prajapati'
+__github__ = 'https://github.com/mindsdb/mindsdb'
+__pypi__ = 'https://pypi.org/project/mindsdb/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2022- mindsdb'

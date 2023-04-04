4dc51ab967bc9046d180df9a00a9ba0d18e349a9
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2023-04-03 18:43:06 -0600

    fix import

--- tests/handler_tests/conftest.py
+++ tests/handler_tests/conftest.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")  # noqa: F405
+
 from ..integration_tests.flows.conftest import *  # noqa: F403
 
 """
@@ -5,4 +8,5 @@ Temporary workaround to import these fixtures, but ideally they would
 be stored in a path that is accessible from both integration_tests,
 handler_tests, and any others that may be added in the future.
 """
+
 sys.path.append("../../integration_tests/flows/conftest.py")  # noqa: F405

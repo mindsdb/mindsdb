cfda31627b9fb0309129e1de88268cb2ff6fcefc
Γιώργος Αρτόπουλος <t8200016@aueb.gr>
2023-04-05 21:31:42 +0300

    Add README.md and more tests in test_surrealdb_handler.py

--- mindsdb/integrations/handlers/surrealdb_handler/tests/test_surrealdb_handler.py
+++ mindsdb/integrations/handlers/surrealdb_handler/tests/test_surrealdb_handler.py
@@ -1,6 +1,7 @@
 import unittest
 
 from mindsdb.integrations.handlers.surrealdb_handler.surrealdb_handler import SurrealDBHandler
+from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
 
 
 class SurrealdbHandlerTest(unittest.TestCase):
@@ -22,7 +23,33 @@ class SurrealdbHandlerTest(unittest.TestCase):
     def test_0_check_connection(self):
         assert self.handler.check_connection()
 
-    def test_1_disconnect(self):
+    def test_1_create_table(self):
+        res = self.handler.native_query("CREATE person SET name = 'Tobie', company = 'SurrealDB', "
+                                        "skills = ['Rust', 'Go', 'JavaScript'];")
+        assert res.type is not RESPONSE_TYPE.ERROR
+
+    def test_2_insert(self):
+        res = self.handler.native_query("UPDATE person SET name = 'Jamie'")
+        assert res.type is not RESPONSE_TYPE.ERROR
+
+    def test_3_select_query(self):
+        query = "SELECT * FROM person"
+        result = self.handler.native_query(query)
+        assert result.type is RESPONSE_TYPE.TABLE
+
+    def test_4_get_columns(self):
+        columns = self.handler.get_columns('person')
+        assert columns.type is not RESPONSE_TYPE.ERROR
+
+    def test_5_get_tables(self):
+        tables = self.handler.get_tables()
+        assert tables.type is not RESPONSE_TYPE.ERROR
+
+    def test_6_drop_table(self):
+        res = self.handler.native_query("REMOVE table person")
+        assert res.type is not RESPONSE_TYPE.ERROR
+
+    def test_7_disconnect(self):
         assert self.handler.disconnect() is None
 
 

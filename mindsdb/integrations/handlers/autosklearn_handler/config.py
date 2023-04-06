f8b014498faa6c11d0dc2c7a8c410d2445eea292
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-06 19:47:36 +0530

    added the RegressionConfig to config

--- mindsdb/integrations/handlers/autosklearn_handler/config.py
+++ mindsdb/integrations/handlers/autosklearn_handler/config.py
@@ -11,6 +11,22 @@ class ClassificationConfig:
     n_jobs: int = None
     metric: Scorer = 'accuracy'
     ensemble_size: int = None
+    ensemble_nbest: int = 50
+    initial_configurations_via_metalearning: int = 25
+    resampling_strategy: str = 'holdout'
+
+    def __post_init__(self):
+        object.__setattr__(self, 'metric', getattr(sys.modules[__name__], self.metric))
+
+
+@dataclass(frozen=True)
+class RegressionConfig:
+    time_left_for_this_task: int = 3600
+    per_run_time_limit: int = None
+    n_jobs: int = None
+    metric: Scorer = 'accuracy'
+    ensemble_size: int = None
+    ensemble_nbest: int = 50
     initial_configurations_via_metalearning: int = 25
     resampling_strategy: str = 'holdout'
 

5771535cb25270e7f173d5106420d24426226f9d
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-04-07 08:31:41 +0530

    added the BaseConfig class and used it as the parent for ClassificationConfig and RegressionConfig

--- mindsdb/integrations/handlers/autosklearn_handler/config.py
+++ mindsdb/integrations/handlers/autosklearn_handler/config.py
@@ -5,7 +5,7 @@ from autosklearn.metrics import Scorer
 
 
 @dataclass(frozen=True)
-class ClassificationConfig:
+class BaseConfig:
     time_left_for_this_task: int = 3600
     per_run_time_limit: int = None
     n_jobs: int = None
@@ -20,15 +20,10 @@ class ClassificationConfig:
 
 
 @dataclass(frozen=True)
-class RegressionConfig:
-    time_left_for_this_task: int = 3600
-    per_run_time_limit: int = None
-    n_jobs: int = None
-    metric: Scorer = 'accuracy'
-    ensemble_size: int = None
-    ensemble_nbest: int = 50
-    initial_configurations_via_metalearning: int = 25
-    resampling_strategy: str = 'holdout'
+class ClassificationConfig(BaseConfig):
+    pass
 
-    def __post_init__(self):
-        object.__setattr__(self, 'metric', getattr(sys.modules[__name__], self.metric))
+
+@dataclass(frozen=True)
+class RegressionConfig(BaseConfig):
+    pass

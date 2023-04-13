9f35c7e7d072e242fa8f800887b134ef28c42b80
parthiv11 <parthivmakwana11@gmail.com>
2023-04-13 17:55:00 +0530

    imported al required fun and classes

---
+++ mindsdb/integrations/handlers/plaid_handler/__init__.py
@@ -0,0 +1,21 @@
+from mindsdb.integrations.libs.const import HANDLER_TYPE
+
+from .__about__ import __version__ as version, __description__ as description
+try:
+    from .plaid_handler import (
+        PlaidHandler as Handler
+    )
+    import_error = None
+except Exception as e:
+    Handler = None
+    import_error = e
+
+title = 'Plaid'
+name = 'plaid'
+type = HANDLER_TYPE.DATA
+icon_path = 'icon.svg'
+
+__all__ = [
+    'Handler', 'version', 'name', 'type', 'title', 'description',
+    'import_error', 'icon_path'
+]

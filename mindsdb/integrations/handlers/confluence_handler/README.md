2a7c1f2bb8fbdb78aace7a7bda79d9acf072932d
bala_ceg <balaji_ceg@outlook.com>
2023-04-19 08:39:32 +0530

    Initial Implementation of Confluence Handler

---
+++ mindsdb/integrations/handlers/confluence_handler/README.md
@@ -0,0 +1,52 @@
+# Confluence Handler
+
+Confluence handler for MindsDB provides interfaces to connect with Confluence via APIs and pull confluence data into MindsDB.
+
+## Confluence
+
+Confluence is a collaborative documentation tool,it can be used to host Wiki pages. In this handler,python client of api is used and more information about this python client can be found (here)[https://pypi.org/project/atlassian-python-api/]
+
+
+## Confluence Handler Initialization
+
+The Confluence handler is initialized with the following parameters:
+
+- `url`: Confluence hosted url instance
+- `confluence_api_token`: Confluence API key to use for authentication
+
+## Implemented Features
+
+- [x] Confluence spaces table for a given confluence hosted url instance
+  - [x] Support LIMIT
+  - [x] Support WHERE
+  - [x] Support ORDER BY
+  - [x] Support column selection
+
+## Example Usage
+
+The first step is to create a database with the new `confluence` engine.
+
+~~~~sql
+CREATE DATABASE mindsdb_confluence
+WITH ENGINE = 'confluence',
+PARAMETERS = {
+  "url": "https://wiki.onap.org/",
+  "confluence_api_token": "MDk5NzgyNzY1Mzc3OlP5jEKnCL/z1+jIyEfIVIwERbJF"  
+};
+~~~~
+
+Use the established connection to query your database:
+
+~~~~sql
+SELECT * FROM mindsdb_confluence.pages
+~~~~
+
+Advanced queries for the confluence handler
+
+~~~~sql
+SELECT id,key,name,type
+FROM mindsdb_confluence.pages
+WHERE type="personal"
+ORDER BY id ASC, name DESC
+LIMIT 10
+~~~~
---
+++ mindsdb/integrations/handlers/confluence_handler/confluence_handler.py
@@ -0,0 +1,91 @@
+from mindsdb.integrations.handlers.confluence_handler.confluence_table import ConfluenceSpacesTable
+from mindsdb.integrations.libs.api_handler import APIHandler
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+)
+from mindsdb.utilities.log import get_log
+from mindsdb_sql import parse_sql
+from mindsdb.utilities.log import get_log
+
+from atlassian import Confluence
+from typing import Optional
+import requests
+
+logger = get_log("integrations.confluence_handler")
+
+class ConfluenceHandler(APIHandler):
+    """Confluence handler implementation"""
+
+    def __init__(self, name=None, **kwargs):
+        """Initialize the Confluence handler.
+        Parameters
+        ----------
+        name : str
+            name of a handler instance
+        """
+        super().__init__(name)
+        
+        connection_data = kwargs.get("connection_data", {})
+        
+        self.parser = parse_sql
+        self.dialect = 'confluence'
+        self.connection_data = connection_data
+        self.kwargs = kwargs
+        self.connection = None
+        self.is_connected = False
+
+        confluence_pages_data = ConfluenceSpacesTable(self)
+        self._register_table("pages", confluence_pages_data)
+
+    def connect(self) -> StatusResponse:
+        """Set up the connection required by the handler.
+        Returns
+        -------
+        StatusResponse
+            connection object
+        """
+        if self.is_connected is True:
+            return self.connection
+       
+        s = requests.Session()
+        s.headers['Authorization'] =  f"Bearer {self.connection_data['confluence_api_token']}"
+
+        self.connection = Confluence(url= self.connection_data['url'], session=s)
+        self.is_connected = True
+        
+        return self.connection
+
+    def check_connection(self) -> StatusResponse:
+        """Check connection to the handler.
+        Returns
+        -------
+        StatusResponse
+            Status confirmation
+        """
+        response = StatusResponse(False)
+        need_to_close = self.is_connected is False
+        
+        try:
+            self.connect()
+            response.success = True
+        except Exception as e:
+            logger.error(f"Error connecting to Confluence API: {e}!")
+            response.error_message = e
+
+        self.is_connected = response.success
+
+        return response
+
+    def native_query(self, query: str) -> StatusResponse:
+        """Receive and process a raw query.
+        Parameters
+        ----------
+        query : str
+            query in a native format
+        Returns
+        -------
+        StatusResponse
+            Request status
+        """
+        ast = parse_sql(query, dialect="mindsdb")
+        return self.query(ast)

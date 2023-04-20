d39d8b6c56accca5e6c9d7fdeb39457dc225054d
bala_ceg <balaji_ceg@outlook.com>
2023-04-20 16:39:44 +0530

    Revamp of existing Jira handler as per API Handler framework

--- mindsdb/integrations/handlers/jira_handler/README.md
+++ mindsdb/integrations/handlers/jira_handler/README.md
@@ -8,18 +8,14 @@ In this handler. python client of api is used and more information about this py
 
 
 ## Implementation
-This handler was implemented using `duckdb`, a library that allows SQL queries to be executed on `pandas` DataFrames.
+This handler was implemented as per the MindsDB API Handler documentation.
 
-In essence, when querying a particular table, the entire table will first be pulled into a `pandas` DataFrame using the Jira API. Once this is done, SQL queries can be run on the DataFrame using `duckdb`.
-
-Note: Since the entire table needs to be pulled into memory first (DataFrame), it is recommended to be somewhat careful when querying large tables so as not to overload your machine.
 
 The required arguments to establish a connection are,
-* `table_name`: Sample Jira table name.
-* `jira_url`: Jira  url
-* `user_id`: Jira User ID.
-* `api_key`: API key for the Jira API.
-* `jira_query`: Jira Search Query
+* `jira_url`: Jira  hosted url instance
+* `jira_api_token`: API key for acessing the Jira url instance
+* `project`: Jira project name 
+
 
 ## Usage
 In order to make use of this handler and connect to an Jira in MindsDB, the following syntax can be used,
@@ -29,15 +25,28 @@ WITH
 engine='Jira',
 parameters={
     "jira_url": "https://jira.linuxfoundation.org",
-    "user_id": "balaceg",
-    "api_key": "4Rhq&Ehd#KV4an!",
-    "jira_query": "project = RELENG and status != Done"
+     "jira_api_token": "Bearer NDU4OTIzMDE2MTgxOkU5CjVFV7L01piwU8WDYiEwuiOL",
+     "project": "RELENG"   
 };
 ~~~~
 
+## Implemented Features
+
+- [x] Jira project table for a given Jira hosted url instance
+  - [x] Support LIMIT
+  - [x] Support ORDER BY
+  - [x] Support column selection
+
 Now, you can use this established connection to query your table as follows,
 ~~~~sql
-SELECT * FROM jira_source.example_tbl
+SELECT * FROM jira_source.project
 ~~~~
 
-At the moment, only `SELECT` queries are allowed to be executed through `duckdb`. This, however, has no restriction on running machine learning algorithms against your data in Jira using `CREATE PREDICTOR` statements.
+Advanced queries for the jira handler
+
+~~~~sql
+SELECT key,summary,status
+FROM jira_source.project
+ORDER BY key ASC
+LIMIT 10
+~~~~ 
--- mindsdb/integrations/handlers/jira_handler/jira_handler.py
+++ mindsdb/integrations/handlers/jira_handler/jira_handler.py
@@ -1,33 +1,26 @@
-from typing import Optional
-from collections import OrderedDict
-
-import pandas as pd
-import duckdb
-import json
-from atlassian import Jira
-
-from mindsdb_sql import parse_sql
-from mindsdb.integrations.libs.base import DatabaseHandler
-from mindsdb.utilities.log import get_log
-from mindsdb_sql.parser.ast.base import ASTNode
-
+from mindsdb.integrations.handlers.jira_handler.jira_table import JiraProjectsTable
+from mindsdb.integrations.libs.api_handler import APIHandler
 from mindsdb.integrations.libs.response import (
     HandlerStatusResponse as StatusResponse,
-    HandlerResponse as Response,
-    RESPONSE_TYPE
 )
-from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_TYPE
+from mindsdb.utilities.log import get_log
+from mindsdb_sql import parse_sql
 
-log = get_log()
+from atlassian import Jira
+from typing import Optional
+import requests
 
 
-class JiraHandler(DatabaseHandler):
+log = get_log("integrations.jira_handler")
+
+
+class JiraHandler(APIHandler):
     """
     This handler handles connection and execution of the Airtable statements.
     """
 
-    name = 'jira'
-    def __init__(self, name: str, connection_data: Optional[dict], **kwargs):
+
+    def __init__(self, name=None, **kwargs):
         """
         Initialize the handler.
         Args:
@@ -36,13 +29,18 @@ class JiraHandler(DatabaseHandler):
             **kwargs: arbitrary keyword arguments.
         """
         super().__init__(name)
+        connection_data = kwargs.get("connection_data", {})
+
         self.parser = parse_sql
         self.dialect = 'jira'
         self.connection_data = connection_data
         self.kwargs = kwargs
-
         self.connection = None
         self.is_connected = False
+
+
+        jira_projects_data = JiraProjectsTable(self)
+        self._register_table("project", jira_projects_data)
 
     def __del__(self):
         if self.is_connected is True:
@@ -58,11 +56,12 @@ class JiraHandler(DatabaseHandler):
         if self.is_connected is True:
             return self.connection
         
-        self.connection = Jira(
-            url=self.connection_data['jira_url'],
-            username=self.connection_data['user_id'],
-            password=self.connection_data['api_key'],
-            cloud=True)
+        s = requests.Session()
+        s.headers['Authorization'] =  f"Bearer {self.connection_data['jira_api_token']}"
+
+        self.connection = Jira(url= self.connection_data['jira_url'], session=s)
+        self.is_connected = True
+        
         
         self.is_connected = True
 
@@ -89,137 +88,41 @@ class JiraHandler(DatabaseHandler):
 
         response = StatusResponse(False)
         need_to_close = self.is_connected is False
-
+        
         try:
             self.connect()
             response.success = True
         except Exception as e:
-            log.error(f'Error connecting to Jira portal {self.connection_data["jira_url"]}, {e}!')
-            response.error_message = str(e)
-        finally:
-            if response.success is True and need_to_close:
-                self.disconnect()
-            if response.success is False and self.is_connected is True:
-                self.is_connected = False
+            logger.error(f"Error connecting to Jira API: {e}!")
+            response.error_message = e
+
+        self.is_connected = response.success
 
         return response
 
     def native_query(self, query: str) -> StatusResponse:
+        """Receive and process a raw query.
+        Parameters
+        ----------
+        query : str
+            query in a native format
+        Returns
+        -------
+        StatusResponse
+            Request status
         """
-        Receive raw query and act upon it somehow.
-        Args:
-            query (str): query in native format
-        Returns:
-            HandlerResponse
-        """
-
-        need_to_close = self.is_connected is False
-
-        connection = self.connect()
-        results = connection.jql(self.connection_data['jira_query'])
-        df = pd.json_normalize(results["issues"])
-        fields_name= ["key", "fields.summary","fields.status.name", "fields.reporter.name","fields.assignee.name","fields.priority.name"]
-        locals()[self.connection_data['table_name']] = df[fields_name]
-        try:            
-            result = duckdb.query(query)
-            if result:
-                response = Response(RESPONSE_TYPE.TABLE,data_frame=result.df())                   
-            else:
-                response = Response(RESPONSE_TYPE.OK)
-
-        except Exception as e:
-            log.error(f'Error running query  {query} on jira handler')
-            response = Response(
-                RESPONSE_TYPE.ERROR,
-                error_message=str(e)
-            )
-        
-        if need_to_close is True:
-            self.disconnect()
-
-        return response
-
-    def query(self, query: ASTNode) -> StatusResponse:
-        """
-        Receive query as AST (abstract syntax tree) and act upon it somehow.
-        Args:
-            query (ASTNode): sql query represented as AST. May be any kind
-                of query: SELECT, INTSERT, DELETE, etc
-        Returns:
-            HandlerResponse
-        """
-
-        return self.native_query(query.to_string())
-
-    def get_tables(self) -> StatusResponse:
-        """
-        Return list of entities that will be accessible as tables.
-        Returns:
-            HandlerResponse
-        """
-
-        response = Response(
-            RESPONSE_TYPE.TABLE,
-            data_frame=pd.DataFrame(
-                [self.connection_data['table_name']],
-                columns=['table_name']
-            )
-        )
-
-        return response
-
-    def get_columns(self) -> StatusResponse:
-        """
-        Returns a list of entity columns.
-        Args:
-            table_name (str): name of one of tables returned by self.get_tables()
-        Returns:
-            HandlerResponse
-        """
-        query = 'SELECT * FROM '+ str(self.connection_data['table_name'])+ ' LIMIT 10'
-        result = self.native_query(query)
-
-        response = Response(
-            RESPONSE_TYPE.TABLE,
-            data_frame=pd.DataFrame(
-                {
-                    'column_name': result.data_frame.columns,
-                    'data_type': result.data_frame.dtypes
-                }
-            )
-        )
-
-        return response
-
-
-connection_args = OrderedDict(
-    table_name={
-        'type': ARG_TYPE.STR,
-        'description': 'Sample Jira table name.'
-    },
-    jira_url={
-        'type': ARG_TYPE.STR,
-        'description': 'Jira  url'
-    },
-    user_id={
-        'type': ARG_TYPE.STR,
-        'description': 'Jira User ID.'
-    },
-    api_key={
-        'type': ARG_TYPE.STR,
-        'description': 'API key for the Jira API.'
-    },
-    jira_query={
-        'type': ARG_TYPE.STR,
-        'description': 'Jira Search Query '
-    }
+        ast = parse_sql(query, dialect="mindsdb")
+        return self.query(ast)
    
-)
+    def construct_jql(self):
+        """construct jql & returns it to JiraProjectsTable class
+        Returns
+        -------
+        Str
+        """
+        return 'project = ' + str(self.connection_data['project'])
 
-connection_args_example = OrderedDict(
-    table_name ='project',
-    jira_url ='https://jira.linuxfoundation.org/',
-    user_id='balaceg',
-    api_key='4Rhq&Ehd#KV4an!',
-    jira_query = 'project = RELENG and status = In Progress'
-)
+
+
+
+

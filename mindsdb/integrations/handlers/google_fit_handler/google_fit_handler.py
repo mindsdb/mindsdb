73882aab4b3c19a1f6ac1452ccceb57372dca868
2170chm <2170chm@gmail.com>
2023-04-24 12:52:55 -0400

    Added required TODO functions

--- mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
+++ mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
@@ -1,4 +1,5 @@
 import os.path
+import pandas as pd
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -13,13 +14,56 @@ epoch0 = datetime(1970, 1, 1, tzinfo=pytz.utc)
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/fitness.activity.read']
 
-def retrieve_data(service, startTimeMillis, endTimeMillis, dataSourceId):
-    return service.users().dataset().aggregate(userId="me", body={
-        "aggregateBy": [{
-            "dataTypeName": "com.google.step_count.delta",
-            "dataSourceId": dataSourceId
-        }],
-        "bucketByTime": {"durationMillis": 86400000},
-        "startTimeMillis": startTimeMillis,
-        "endTimeMillis": endTimeMillis
-    }).execute()
+from mindsdb.integrations.libs.api_handler import APIHandler,FuncParser
+
+class GoogleFitTable(APIHandler):
+    
+    def __init__(self, name: str):
+    super().__init__(name)
+    """ constructor
+    Args:
+        name (str): the handler name
+    """
+
+    def retrieve_data(service, startTimeMillis, endTimeMillis, dataSourceId):
+        return service.users().dataset().aggregate(userId="me", body={
+            "aggregateBy": [{
+                "dataTypeName": "com.google.step_count.delta",
+                "dataSourceId": dataSourceId
+            }],
+            "bucketByTime": {"durationMillis": 86400000},
+            "startTimeMillis": startTimeMillis,
+            "endTimeMillis": endTimeMillis
+        }).execute()
+
+    def connect(self) -> HandlerStatusResponse:
+    """ Set up any connections required by the handler
+    Should return output of check_connection() method after attempting
+    connection. Should switch self.is_connected.
+    Returns:
+        HandlerStatusResponse
+    """
+
+    def check_connection(self) -> HandlerStatusResponse:
+    """ Check connection to the handler
+    Returns:
+        HandlerStatusResponse
+    """
+
+    def native_query(self, query: Any) -> HandlerResponse:
+    """Receive raw query and act upon it somehow.
+    Args:
+        query (Any): query in native format (str for sql databases,
+            dict for mongo, api's json etc)
+    Returns:
+        HandlerResponse
+    """
+
+    def call_google_fit_api(self, method_name:str = None, params:dict = None) -> DataFrame:
+    """Receive query as AST (abstract syntax tree) and act upon it somehow.
+    Args:
+        query (ASTNode): sql query represented as AST. May be any kind
+            of query: SELECT, INSERT, DELETE, etc
+    Returns:
+        DataFrame
+    """

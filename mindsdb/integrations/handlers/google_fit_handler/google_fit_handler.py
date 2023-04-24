983e4c9b0428f5a2d4de78e7a43e362f713113ca
2170chm <2170chm@gmail.com>
2023-04-24 17:39:33 -0400

    Finished draft of the connect() function for Google Fit's handler

--- mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
+++ mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -26,8 +27,9 @@ class GoogleFitHandler(APIHandler):
         args = kwargs.get('connection_data', {})
         self.connection_args = {}
         handler_config = Config().get('google_handler', {})
+        #TODO: make sure the arguments can read a list from user input when the database is created, since "redirect_uris" is a list.
         for k in ['client_id', 'project_id', 'auth_uri',
-                  'token_uri', 'auth_provider_x509_cert_url', 'client_secret', 'redirect_uris']:
+                  'token_uri', 'auth_provider_x509_cert_url', 'client_secret',"redirect_uris"]:
             if k in args:
                 self.connection_args[k] = args[k]
             elif f'GOOGLE_FIT_{k.upper()}' in os.environ:
@@ -41,6 +43,37 @@ class GoogleFitHandler(APIHandler):
         aggregated_data = GoogleFitTable(self)
         self._register_table('aggregated_data', aggregated_data)
 
+    def connect(self) -> Resource:
+        if self.is_connected is True and self.api:
+            return self.api
+        if len(self.connection_args) == 6:
+            credentialDict = {"installed":self.connection_args}
+            f = open("credentials.json", "a")
+            f.write(json.dumps(connection_args).replace(" ", ""))
+            f.close()
+        
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        self.api = fit_service = build('fitness', 'v1', credentials=creds)
+        
+        self.is_connected = True
+        return self.api
+
+    def check_connection(self) -> HandlerStatusResponse:
+    """ Check connection to the handler
+    Returns:
+        HandlerStatusResponse
+    """
+
     def retrieve_data(service, startTimeMillis, endTimeMillis, dataSourceId):
         return service.users().dataset().aggregate(userId="me", body={
             "aggregateBy": [{
@@ -51,20 +84,6 @@ class GoogleFitHandler(APIHandler):
             "startTimeMillis": startTimeMillis,
             "endTimeMillis": endTimeMillis
         }).execute()
-
-    def connect(self) -> HandlerStatusResponse:
-    """ Set up any connections required by the handler
-    Should return output of check_connection() method after attempting
-    connection. Should switch self.is_connected.
-    Returns:
-        HandlerStatusResponse
-    """
-
-    def check_connection(self) -> HandlerStatusResponse:
-    """ Check connection to the handler
-    Returns:
-        HandlerStatusResponse
-    """
 
     def native_query(self, query: str = None) -> Response:
     """Receive raw query and act upon it somehow.

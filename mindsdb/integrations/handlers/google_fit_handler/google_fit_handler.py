4e215cf601509625e604915f71a811da8cbf0068
2170chm <2170chm@gmail.com>
2023-04-24 16:15:04 -0400

    Finished draft of Google Fit handler's init function

--- mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
+++ mindsdb/integrations/handlers/google_fit_handler/google_fit_handler.py
@@ -1,5 +1,7 @@
 import os.path
 import pandas as pd
+import pytz
+from datetime import datetime, timedelta
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -7,23 +9,37 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-import pytz
-from datetime import datetime, timedelta
+from mindsdb.integrations.handlers.google_fit_handler.google_fit_tables import GoogleFitTable
+from mindsdb.integrations.libs.api_handler import APIHandler,FuncParser
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+    HandlerResponse as Response,
+)
 epoch0 = datetime(1970, 1, 1, tzinfo=pytz.utc)
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/fitness.activity.read']
 
-from mindsdb.integrations.libs.api_handler import APIHandler,FuncParser
+class GoogleFitHandler(APIHandler):
 
-class GoogleFitTable(APIHandler):
-    
     def __init__(self, name: str):
-    super().__init__(name)
-    """ constructor
-    Args:
-        name (str): the handler name
-    """
+        super().__init__(name)
+
+        args = kwargs.get('connection_data', {})
+        self.connection_args = {}
+        handler_config = Config().get('google_handler', {})
+        for k in ['client_id', 'project_id', 'auth_uri',
+                  'token_uri', 'auth_provider_x509_cert_url', 'client_secret', 'redirect_uris']:
+            if k in args:
+                self.connection_args[k] = args[k]
+            elif f'GOOGLE_FIT_{k.upper()}' in os.environ:
+                self.connection_args[k] = os.environ[f'GOOGLE_FIT_{k.upper()}']
+            elif k in handler_config:
+                self.connection_args[k] = handler_config[k]
+        
+        self.api = None
+        self.is_connected = False
+
+        aggregated_data = GoogleFitTable(self)
+        self._register_table('aggregated_data', aggregated_data)
 
     def retrieve_data(service, startTimeMillis, endTimeMillis, dataSourceId):
         return service.users().dataset().aggregate(userId="me", body={
@@ -50,7 +66,7 @@ class GoogleFitTable(APIHandler):
         HandlerStatusResponse
     """
 
-    def native_query(self, query: Any) -> HandlerResponse:
+    def native_query(self, query: str = None) -> Response:
     """Receive raw query and act upon it somehow.
     Args:
         query (Any): query in native format (str for sql databases,
---
+++ mindsdb/integrations/handlers/google_fit_handler/google_fit_tables.py
@@ -0,0 +1,4 @@
+from mindsdb.integrations.libs.api_handler import APITable
+from mindsdb.integrations.libs.response import HandlerResponse as Response
+
+class GoogleFitTable(APITable):

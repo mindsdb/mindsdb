5cdba900141dceada93de256d8abae6eb399aed5
Panagiotis Spanakis <t8200158@aueb.gr>
2023-04-26 20:05:18 +0300

    Add implementation for Google content api for shopping handler

---
+++ mindsdb/integrations/handlers/google_content_shopping_handler/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'MindsDB Google Content API for Shopping handler'
+__package_name__ = 'mindsdb_google_content_shopping_handler'
+__version__ = '0.0.1'
+__description__ = "MindsDB handler for the Google Content API for Shopping"
+__author__ = 'Panagiotis-Alexios Spanakis'
+__github__ = 'https://github.com/mindsdb/mindsdb'
+__pypi__ = 'https://pypi.org/project/mindsdb/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2023 - mindsdb'

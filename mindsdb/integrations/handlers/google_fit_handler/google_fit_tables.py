82f3d3f6b6f9ace8cfd74f990dc3589aa2b76fe4
sid <sidwang2002@gmail.com>
2023-05-01 23:43:01 -0400

    google fit table done, handler nearly done

--- mindsdb/integrations/handlers/google_fit_handler/google_fit_tables.py
+++ mindsdb/integrations/handlers/google_fit_handler/google_fit_tables.py
@@ -5,15 +5,32 @@ from mindsdb_sql.parser import ast
 
 class GoogleFitTable(APITable):
     def select(self, query: ast.Select) -> Response:
+
         conditions = extract_comparison_conditions(query.where)
+        
+        params = {}
+        filters = []
+        steps = {}
         for op, arg1, arg2 in conditions:
-            # To be done
-            pass
-        result = self.handler.call_google_fit_api(
-            method_name='get_steps'
-            #params=params
+
+            if op == 'or':
+                raise NotImplementedError(f'OR is not supported')
+            if arg1 == 'date':
+                date = time_parser(arg2)
+                if op == '>':
+                    params['start_time'] = date
+                elif op == '<':
+                    params['end_time'] = date
+                else:
+                    raise NotImplementedError
+            else:
+                raise NotImplementedError(f'This query is not supported')
+
+        result = self.handler.call_twitter_api(
+            method_name='get_steps',
+            params=params
         )
-        return 
+        return result
 
     def get_columns(self):
         return [

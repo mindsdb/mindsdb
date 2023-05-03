c6159d8e64cd7db52f3d726978354a18d5957ff8
dusvyat <usvyat@gmail.com>
2023-05-03 10:57:48 +0300

    -refactor helpers.py - lightfm_handler.py create function -test wip for create function

--- mindsdb/integrations/handlers/lightfm_handler/lightfm_handler.py
+++ mindsdb/integrations/handlers/lightfm_handler/lightfm_handler.py
@@ -3,7 +3,7 @@ from typing import Optional, Dict
 import dill
 import pandas as pd
 from lightfm import LightFM
-from helpers import preprocess
+from mindsdb.integrations.handlers.lightfm_handler.helpers import RecommenderPreprocessor
 from mindsdb.integrations.libs.base import BaseMLEngine
 
 
@@ -14,13 +14,20 @@ class LightFMHandler(BaseMLEngine):
 
     name = 'lightfm'
 
-    def create(self, data: pd.DataFrame = None, meta_data: dict = None, args: Optional[dict] = None) -> None:
+    def create(self, interaction_data: pd.DataFrame = None, item_data: pd.DataFrame = None, meta_data: Dict = None, args: Optional[Dict] = None) -> None:
 
-        preprocess(data, **meta_data)
+        rec_preprocessor = RecommenderPreprocessor(
+            interaction_data=interaction_data,
+            item_data=item_data,
+            **meta_data
+        )
+
+        preprocessed_data = rec_preprocessor.preprocess()
 
         model = LightFM(learning_rate=0.05, loss='warp')
-        model.fit(data, epochs=10)
+        model.fit(preprocessed_data.interaction_matrix, epochs=10)
 
+        # todo sort model storage
         self.model_storage.file_set('model', dill.dumps(model))
         self.model_storage.json_set('args', args)
 

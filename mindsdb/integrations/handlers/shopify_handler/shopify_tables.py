72c5a4ad5408af1d960ace19fe84a8194ddfae18
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-09 22:48:02 +0530

    separated the logic for parsing the query and getting the final result outside of the ProductsTable into functions

--- mindsdb/integrations/handlers/shopify_handler/shopify_tables.py
+++ mindsdb/integrations/handlers/shopify_handler/shopify_tables.py
@@ -11,6 +11,71 @@ from mindsdb_sql.parser import ast
 from mindsdb.utilities import log
 
 
+def parse_statement(query: ast.Select, table: Text, columns: List[Text]):
+    # SELECT
+    selected_columns = []
+    for target in query.targets:
+        if isinstance(target, ast.Star):
+            selected_columns = columns
+            break
+        elif isinstance(target, ast.Identifier):
+            selected_columns.append(target.parts[-1])
+        else:
+            raise ValueError(f"Unknown query target {type(target)}")
+
+    # WHERE
+    where_conditions = extract_comparison_conditions(query.where)
+
+    # ORDER BY
+    order_by_conditions = {}
+    if query.order_by and len(query.order_by) > 0:
+        order_by_conditions["columns"] = []
+        order_by_conditions["ascending"] = []
+
+        for an_order in query.order_by:
+            if an_order.field.parts[0] == table:
+                if an_order.field.parts[1] in columns:
+                    order_by_conditions["columns"].append(an_order.field.parts[1])
+
+                    if an_order.direction == "ASC":
+                        order_by_conditions["ascending"].append(True)
+                    else:
+                        order_by_conditions["ascending"].append(False)
+                else:
+                    raise ValueError(
+                        f"Order by unknown column {an_order.field.parts[1]}"
+                    )
+
+    # LIMIT
+    if query.limit:
+        result_limit = query.limit.value
+    else:
+        result_limit = 20
+
+    return selected_columns, where_conditions, order_by_conditions, result_limit
+
+
+def get_results(df, selected_columns, where_conditions, order_by_conditions, result_limit=None):
+    if result_limit:
+        df = df.head(result_limit)
+
+    if len(where_conditions) > 0:
+        df = filter_df(df, where_conditions)
+
+    if len(df) == 0:
+        df = pd.DataFrame([], columns=selected_columns)
+    else:
+        df = df[selected_columns]
+
+        if len(order_by_conditions.get("columns", [])) > 0:
+            df = df.sort_values(
+                by=order_by_conditions["columns"],
+                ascending=order_by_conditions["ascending"],
+            )
+
+    return df
+
+
 def filter_df(df, conditions):
     for condition in conditions:
         column = condition[1]
@@ -45,58 +110,15 @@ class ProductsTable(APITable):
             If the query contains an unsupported condition
         """
 
-        # SELECT
-        selected_columns = []
-        for target in query.targets:
-            if isinstance(target, ast.Star):
-                selected_columns = self.get_columns()
-                break
-            elif isinstance(target, ast.Identifier):
-                selected_columns.append(target.parts[-1])
-            else:
-                raise ValueError(f"Unknown query target {type(target)}")
-
-        # WHERE
-        conditions = extract_comparison_conditions(query.where)
-
-        # ORDER BY
-        order_by_conditions = {}
-        if query.order_by and len(query.order_by) > 0:
-            order_by_conditions["columns"] = []
-            order_by_conditions["ascending"] = []
-
-            for an_order in query.order_by:
-                if an_order.field.parts[0] == "products":
-                    if an_order.field.parts[1] in self.get_columns():
-                        order_by_conditions["columns"].append(an_order.field.parts[1])
-
-                        if an_order.direction == "ASC":
-                            order_by_conditions["ascending"].append(True)
-                        else:
-                            order_by_conditions["ascending"].append(False)
-                    else:
-                        raise ValueError(
-                            f"Order by unknown column {an_order.field.parts[1]}"
-                        )
-
-        # LIMIT
-        if query.limit:
-            total_results = query.limit.value
-        else:
-            total_results = 20
+        selected_columns, where_conditions, order_by_conditions, total_results = parse_statement(
+            query,
+            'products',
+            self.get_columns()
+        )
 
         products_df = pd.json_normalize(self.get_products(limit=total_results))
 
-        if len(products_df) == 0:
-            products_df = pd.DataFrame([], columns=selected_columns)
-        else:
-            products_df = products_df[selected_columns]
-
-            if len(order_by_conditions.get("columns", [])) > 0:
-                products_df = products_df.sort_values(
-                    by=order_by_conditions["columns"],
-                    ascending=order_by_conditions["ascending"],
-                )
+        products_df = get_results(products_df, selected_columns, where_conditions, order_by_conditions)
 
         return products_df
 

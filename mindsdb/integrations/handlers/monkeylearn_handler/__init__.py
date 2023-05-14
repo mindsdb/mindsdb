e6a3e67a3c9ef39b0c736b3fa669cb58d94b8884
Pranav <pranavprajapati586@gmail.com>
2023-05-14 10:19:41 +0530

    Fixed import error

--- mindsdb/integrations/handlers/monkeylearn_handler/__init__.py
+++ mindsdb/integrations/handlers/monkeylearn_handler/__init__.py
@@ -2,7 +2,7 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 
 from .__about__ import __version__ as version, __description__ as description
 try:
-    from monkeylearn_handler import monkeylearnHandler as Handler
+    from .monkeylearn_handler import monkeylearnHandler as Handler
     import_error = None
 except Exception as e:
     Handler = None

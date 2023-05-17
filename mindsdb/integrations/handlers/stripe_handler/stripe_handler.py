d699a7317e99fb3ca6b157b7a6cf4ce699666dd1
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-18 00:04:24 +0530

    added the class for the Stripe handler and implemented the connect() method

---
+++ mindsdb/integrations/handlers/stripe_handler/stripe_handler.py
@@ -0,0 +1,53 @@
+import stripe
+
+from mindsdb.integrations.handlers.stripe_handler.stripe_tables import *
+from mindsdb.integrations.libs.api_handler import APIHandler
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+)
+
+from mindsdb.utilities import log
+from mindsdb_sql import parse_sql
+
+
+class StripeHandler(APIHandler):
+    """
+    The Stripe handler implementation.
+    """
+
+    name = 'stripe'
+
+    def __init__(self, name: str, **kwargs):
+        """
+        Initialize the handler.
+        Args:
+            name (str): name of particular handler instance
+            **kwargs: arbitrary keyword arguments.
+        """
+        super().__init__(name)
+
+        connection_data = kwargs.get("connection_data", {})
+        self.connection_data = connection_data
+        self.kwargs = kwargs
+
+        self.connection = None
+        self.is_connected = False
+
+    def connect(self):
+        """
+        Set up the connection required by the handler.
+        Returns
+        -------
+        StatusResponse
+            connection object
+        """
+        if self.is_connected is True:
+            return self.connection
+
+        stripe.api_key = self.connection_data['access_token']
+
+        self.connection = stripe
+
+        self.is_connected = True
+
+        return self.connection

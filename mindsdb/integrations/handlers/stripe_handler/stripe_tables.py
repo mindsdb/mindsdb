03cb13193146e4b30e209a98b0a610d4432b3e06
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-19 15:16:54 +0530

    implemented the select() and get_customers() methods

--- mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
+++ mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
@@ -5,6 +5,8 @@ from typing import Text, List, Dict
 from mindsdb_sql.parser import ast
 from mindsdb.integrations.libs.api_handler import APITable
 
+from .utils import parse_statement, get_results
+
 
 class CustomersTable(APITable):
     """The Stripe Customers Table implementation"""
@@ -29,10 +31,22 @@ class CustomersTable(APITable):
             If the query contains an unsupported condition
         """
 
-        pass
+        selected_columns, where_conditions, order_by_conditions, result_limit = parse_statement(
+            query,
+            'products',
+            self.get_columns()
+        )
+
+        customers_df = pd.json_normalize(self.get_customers(limit=result_limit))
+
+        customers_df = get_results(customers_df, selected_columns, where_conditions, order_by_conditions)
+
+        return customers_df
 
     def get_columns(self) -> List[Text]:
         pass
 
     def get_customers(self, **kwargs) -> List[Dict]:
         stripe = self.handler.connect()
+        customers = stripe.Customer.list(**kwargs)
+        return [customer.to_dict() for customer in customers]

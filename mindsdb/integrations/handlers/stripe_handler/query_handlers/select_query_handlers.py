bc4a84664970182c77201f84c9fd42537168383b
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-20 00:11:22 +0530

    added docstrings and type hints to the functions of the SELECTQueryParser and SELECTQueryExecutor

--- mindsdb/integrations/handlers/stripe_handler/query_handlers/select_query_handlers.py
+++ mindsdb/integrations/handlers/stripe_handler/query_handlers/select_query_handlers.py
@@ -6,12 +6,27 @@ from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditio
 
 
 class SELECTQueryParser:
+    """
+    Parses a SELECT query into its component parts.
+
+    Parameters
+    ----------
+    query : ast.Select
+        Given SQL SELECT query.
+    table : Text
+        Name of the table to query.
+    columns : List[Text]
+        List of columns in the table.
+    """
     def __init__(self, query: ast.Select, table: Text, columns: List[Text]):
         self.query = query
         self.table = table
         self.columns = columns
 
     def parse_query(self) -> Tuple[List[Text], List[List[Text]], Dict[Text, List[Text]], int]:
+        """
+        Parses a SQL SELECT statement into its components: SELECT, WHERE, ORDER BY, LIMIT.
+        """
         selected_columns = self.parse_select_clause()
         where_conditions = self.parse_where_clause()
         order_by_conditions = self.parse_order_by_clause()
@@ -19,7 +34,10 @@ class SELECTQueryParser:
 
         return selected_columns, where_conditions, order_by_conditions, result_limit
 
-    def parse_select_clause(self):
+    def parse_select_clause(self) -> List[Text]:
+        """
+        Parses the SELECT (column selection) clause of the query.
+        """
         selected_columns = []
         for target in self.query.targets:
             if isinstance(target, ast.Star):
@@ -32,11 +50,17 @@ class SELECTQueryParser:
 
         return selected_columns
 
-    def parse_where_clause(self):
+    def parse_where_clause(self) -> List[List[Text]]:
+        """
+        Parses the WHERE clause of the query.
+        """
         where_conditions = extract_comparison_conditions(self.query.where)
         return where_conditions
 
-    def parse_order_by_clause(self):
+    def parse_order_by_clause(self) -> Dict[Text, List[Text]]:
+        """
+        Parses the ORDER BY clause of the query.
+        """
         order_by_conditions = {}
         if self.query.order_by and len(self.query.order_by) > 0:
             order_by_conditions["columns"] = []
@@ -58,7 +82,10 @@ class SELECTQueryParser:
 
         return order_by_conditions
 
-    def parse_limit_clause(self):
+    def parse_limit_clause(self) -> int:
+        """
+        Parses the LIMIT clause of the query.
+        """
         if self.query.limit:
             result_limit = self.query.limit.value
         else:
@@ -68,6 +95,22 @@ class SELECTQueryParser:
 
 
 class SELECTQueryExecutor:
+    """
+    Executes a SELECT query.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to query.
+    selected_columns : List[Text]
+        List of columns to select.
+    where_conditions : List[List[Text]]
+        List of where conditions.
+    order_by_conditions : Dict[Text, List[Text]]
+        Dictionary of order by conditions.
+    result_limit : int
+        Number of results to return.
+    """
     def __init__(self, df: pd.DataFrame, selected_columns: List[Text], where_conditions: List[List[Text]], order_by_conditions: Dict[Text, List[Text]], result_limit: int = None):
         self.df = df
         self.selected_columns = selected_columns
@@ -76,6 +119,9 @@ class SELECTQueryExecutor:
         self.result_limit = result_limit
 
     def execute_query(self):
+        """
+        Execute the query.
+        """
         self.execute_limit_clause()
 
         self.execute_where_clause()
@@ -87,12 +133,18 @@ class SELECTQueryExecutor:
         return self.df
 
     def execute_select_clause(self):
+        """
+        Execute the select clause of the query.
+        """
         if len(self.df) == 0:
             self.df = pd.DataFrame([], columns=self.selected_columns)
         else:
             self.df = self.df[self.selected_columns]
 
     def execute_where_clause(self):
+        """
+        Execute the where clause of the query.
+        """
         if len(self.where_conditions) > 0:
             for condition in self.where_conditions:
                 column = condition[1]
@@ -103,6 +155,9 @@ class SELECTQueryExecutor:
                 self.df.query(query, inplace=True)
 
     def execute_order_by_clause(self):
+        """
+        Execute the order by clause of the query.
+        """
         if len(self.order_by_conditions.get("columns", [])) > 0:
             self.df = self.df.sort_values(
                 by=self.order_by_conditions["columns"],
@@ -110,5 +165,8 @@ class SELECTQueryExecutor:
             )
 
     def execute_limit_clause(self):
+        """
+        Execute the limit clause of the query.
+        """
         if self.result_limit:
             self.df = self.df.head(self.result_limit)

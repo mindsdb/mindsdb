76d0aa7a2aade765d4eebdfea203f7b5a66c286a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-19 15:35:13 +0530

    added the Products Table

--- mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
+++ mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
@@ -50,3 +50,47 @@ class CustomersTable(APITable):
         stripe = self.handler.connect()
         customers = stripe.Customer.list(**kwargs)
         return [customer.to_dict() for customer in customers]
+
+
+class ProductsTable(APITable):
+    """The Stripe Products Table implementation"""
+
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """
+        Pulls data from the Stripe Products.
+
+        Parameters
+        ----------
+        query : ast.Select
+           Given SQL SELECT query
+
+        Returns
+        -------
+        pd.DataFrame
+            Stripe Products matching the query
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+
+        selected_columns, where_conditions, order_by_conditions, result_limit = parse_statement(
+            query,
+            'products',
+            self.get_columns()
+        )
+
+        products_df = pd.json_normalize(self.get_products(limit=result_limit))
+
+        products_df = get_results(products_df, selected_columns, where_conditions, order_by_conditions)
+
+        return products_df
+
+    def get_columns(self) -> List[Text]:
+        return pd.json_normalize(self.get_products(limit=1)).columns.tolist()
+
+    def get_products(self, **kwargs) -> List[Dict]:
+        stripe = self.handler.connect()
+        products = stripe.Product.list(**kwargs)
+        return [product.to_dict() for product in products]

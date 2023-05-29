65b4fa489390bfd06634343fb6a66e62661db463
Parthiv Makwana <75653580+parthiv11@users.noreply.github.com>
2023-05-30 00:47:36 +0530

    Improved condition for df=None

--- mindsdb/integrations/handlers/flaml_handler/flaml_handler.py
+++ mindsdb/integrations/handlers/flaml_handler/flaml_handler.py
@@ -12,26 +12,25 @@ class FLAMLHandler(BaseMLEngine):
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
         if args is None:
             args = {}
+        if df is not None:
+            target_dtype = infer_types(df, 0).to_dict()["dtypes"][target]
+            model = AutoML(verbose=0)
 
-        target_dtype = infer_types(df, 0).to_dict()["dtypes"][target]
-        model = AutoML(verbose=0)
-
-        if target_dtype in ['binary', 'categorical', 'tags']:
-            model.fit(X_train=df.drop(columns=[target]),
+            if target_dtype in ['binary', 'categorical', 'tags']:
+                model.fit(X_train=df.drop(columns=[target]),
                       y_train=df[target],
                       task='classification',
                       **args.get('using'))
 
-        elif target_dtype in ['integer', 'float', 'quantity']:
-            model.fit(X_train=df.drop(columns=[target]),
+            elif target_dtype in ['integer', 'float', 'quantity']:
+                model.fit(X_train=df.drop(columns=[target]),
                       y_train=df[target],
                       task='regression',
                       **args.get('using'))
-
-        if df is not None:
-
+                
             self.model_storage.json_set('args', args)
             self.model_storage.file_set('model', dill.dumps(model))
+            
         else:
             raise Exception(
                 "Data is empty!!"

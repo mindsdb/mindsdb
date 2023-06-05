98aa778dead06cdbf81d74ee91beac142eee28ac
Max Stepanov <stpmax@yandex.ru>
2023-06-05 16:55:25 +0300

    fix

--- mindsdb/utilities/profiler/profiler.py
+++ mindsdb/utilities/profiler/profiler.py
@@ -59,8 +59,8 @@ def stop_current_node():
         return
     current_node = _get_current_node(profiling)
     current_node['stop_at'] = time.perf_counter()
-    current_node['start_at_thread'] = time.thread_time()
-    current_node['start_at_process'] = time.process_time()
+    current_node['stop_at_thread'] = time.thread_time()
+    current_node['stop_at_process'] = time.process_time()
     if len(profiling['pointer']) > 0:
         profiling['pointer'] = profiling['pointer'][:-1]
     else:

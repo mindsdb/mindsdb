c3cdf891aae9f1367de2b1b7a38db280d02d62f1
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-06 12:20:30 +0530

    added the logic to check for existence of supported columns in parse_columns() method

--- mindsdb/integrations/handlers/utilities/query_utilities/insert_query_utilities.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/insert_query_utilities.py
@@ -1,14 +1,21 @@
-from typing import Text, List, Dict, Tuple
+from typing import Text, List
 
 from mindsdb_sql.parser import ast
 
+from .exceptions import UnsupportedColumnException
+
 
 class INSERTQueryParser:
-    def __init__(self, query: ast.Select, columns: List[Text]):
+    def __init__(self, query: ast.Insert, supported_columns: List[Text]):
         self.query = query
-        self.columns = columns
+        self.supported_columns = supported_columns
 
     def parse_columns(self):
+        columns = self.query.columns
+        if not set(columns).issubset(self.supported_columns):
+            unsupported_columns = set(columns).difference(self.supported_columns)
+            raise UnsupportedColumnException(f"Unsupported columns: {', '.join(unsupported_columns)}")
+
         return self.query.columns
 
     def parse_values(self):

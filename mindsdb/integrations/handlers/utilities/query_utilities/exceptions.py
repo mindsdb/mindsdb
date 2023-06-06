c62ebb4c6430a439637fedc4a7d664ea46179a3f
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-06 12:22:52 +0530

    added the MandatoryColumnException

--- mindsdb/integrations/handlers/utilities/query_utilities/exceptions.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/exceptions.py
@@ -3,3 +3,9 @@ class UnsupportedColumnException(Exception):
     """
     Exception raised when a column that is not supported is used in a query.
     """
+
+
+class MandatoryColumnException(Exception):
+    """
+    Exception raised when a mandatory column is missing from a query.
+    """

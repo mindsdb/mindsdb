f8496a93e8765f28019a06365e7496049b9ec921
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-06-06 23:16:13 +0300

    linting

--- tests/unit/ml_handlers/conftest.py
+++ tests/unit/ml_handlers/conftest.py
@@ -1,19 +1,24 @@
-import pytest
 from pathlib import Path
-import pandas as pd
 
-TEST_DATA_PATH = Path(__file__).parent.resolve() / 'data'
+import pandas as pd
+import pytest
+
+TEST_DATA_PATH = Path(__file__).parent.resolve() / "data"
 
 
 def get_df(file_name: str, dtype: dict = None) -> pd.DataFrame:
-	return pd.read_csv(TEST_DATA_PATH / file_name, dtype=None)
+    return pd.read_csv(TEST_DATA_PATH / file_name, dtype=None)
 
 
 @pytest.fixture
 def interaction_data() -> pd.DataFrame:
-	return get_df('ratings.csv', dtype={'userId': 'str', 'movieId': 'str', 'rating': 'float64'})
+    return get_df(
+        "ratings.csv", dtype={"userId": "str", "movieId": "str", "rating": "float64"}
+    )
 
 
 @pytest.fixture
 def item_data() -> pd.DataFrame:
-	return get_df('movies.csv', dtypes={'movieId': 'str', 'title': 'str', 'genres': 'str'})
+    return get_df(
+        "movies.csv", dtypes={"movieId": "str", "title": "str", "genres": "str"}
+    )
--- tests/unit/ml_handlers/test_lightfm.py
+++ tests/unit/ml_handlers/test_lightfm.py
@@ -1,152 +1,170 @@
-import pandas as pd
 import time
-import pytest
-from pathlib import Path
-import scipy as sp
 from unittest.mock import patch
 
+import pandas as pd
+import scipy as sp
 from mindsdb_sql import parse_sql
-from mindsdb.integrations.handlers.lightfm_handler.helpers import RecommenderPreprocessor
-from mindsdb.integrations.handlers.lightfm_handler.lightfm_handler import LightFMHandler
 from unit.executor_test_base import BaseExecutorTest
+
+from mindsdb.integrations.handlers.lightfm_handler.helpers import (
+    RecommenderPreprocessor,
+)
 
 
 def test_preprocessing_cf(interaction_data):
-	"""Tests helper function for preprocessing"""
+    """Tests helper function for preprocessing"""
 
-	rec_preprocessor = RecommenderPreprocessor(
-		interaction_data=interaction_data,
-		user_id_column_name="userId",
-		item_id_column_name="movieId"
-	)
+    rec_preprocessor = RecommenderPreprocessor(
+        interaction_data=interaction_data,
+        user_id_column_name="userId",
+        item_id_column_name="movieId",
+    )
 
-	preprocessed_data = rec_preprocessor.preprocess()
+    preprocessed_data = rec_preprocessor.preprocess()
 
-	# check ids are int64
-	assert preprocessed_data.interaction_df[[rec_preprocessor.user_id_column_name, rec_preprocessor.item_id_column_name]] \
-		.dtypes[preprocessed_data.interaction_df.dtypes == 'int64'].all()
+    # check ids are int64
+    assert (
+        preprocessed_data.interaction_df[
+            [rec_preprocessor.user_id_column_name, rec_preprocessor.item_id_column_name]
+        ]
+        .dtypes[preprocessed_data.interaction_df.dtypes == "int64"]
+        .all()
+    )
 
-	# check interaction are equal to 1 or -1 e.g. positive or negative
-	assert preprocessed_data.interaction_df['interaction'].apply(lambda x: x == -1 or x == 1).all()
+    # check interaction are equal to 1 or -1 e.g. positive or negative
+    assert (
+        preprocessed_data.interaction_df["interaction"]
+        .apply(lambda x: x == -1 or x == 1)
+        .all()
+    )
 
-	print("d")
-	# check interaction matrix is the expected shape
-	assert preprocessed_data.interaction_matrix.shape == (503, 89)
-	assert isinstance(preprocessed_data.interaction_matrix, sp.sparse.coo_matrix)
+    print("d")
+    # check interaction matrix is the expected shape
+    assert preprocessed_data.interaction_matrix.shape == (503, 89)
+    assert isinstance(preprocessed_data.interaction_matrix, sp.sparse.coo_matrix)
 
 
 class TestLightFM(BaseExecutorTest):
+    def wait_predictor(self, project, name):
+        # wait
+        done = False
+        for attempt in range(200):
+            ret = self.run_sql(f"select * from {project}.models where name='{name}'")
+            if not ret.empty:
+                if ret["STATUS"][0] == "complete":
+                    done = True
+                    break
+                elif ret["STATUS"][0] == "error":
+                    break
+            time.sleep(0.5)
+        if not done:
+            raise RuntimeError("predictor wasn't created")
 
-	def wait_predictor(self, project, name):
-		# wait
-		done = False
-		for attempt in range(200):
-			ret = self.run_sql(f"select * from {project}.models where name='{name}'")
-			if not ret.empty:
-				if ret["STATUS"][0] == "complete":
-					done = True
-					break
-				elif ret["STATUS"][0] == "error":
-					break
-			time.sleep(0.5)
-		if not done:
-			raise RuntimeError("predictor wasn't created")
+    def run_sql(self, sql):
+        ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
+        assert ret.error_code is None
+        if ret.data is not None:
+            columns = [
+                col.alias if col.alias is not None else col.name for col in ret.columns
+            ]
+            return pd.DataFrame(ret.data, columns=columns)
 
-	def run_sql(self, sql):
-		ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
-		assert ret.error_code is None
-		if ret.data is not None:
-			columns = [col.alias if col.alias is not None else col.name for col in ret.columns]
-			return pd.DataFrame(ret.data, columns=columns)
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_collaborative_filter_user_item_recommendation_light_fm_handler(
+        self, mock_handler, interaction_data
+    ):
 
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_collaborative_filter_user_item_recommendation_light_fm_handler(self, mock_handler, interaction_data):
+        self.set_handler(mock_handler, name="pg", tables={"df": interaction_data})
 
-		self.set_handler(mock_handler, name="pg", tables={"df": interaction_data})
+        # create project
+        self.run_sql("create database proj")
 
-		# create project
-		self.run_sql("create database proj")
+        # create predictor
+        self.run_sql(
+            """
+            create model proj.useritemtest
+            from pg (select * from df)
+            predict movieId
+            using
+                engine='lightfm',
+                item_id='movieId',
+                user_id='userId',
+                recommendation_type='user_item',
+                threshold=4,
+                n_recommendations=10
+                """
+        )
+        self.wait_predictor("proj", "useritemtest")
 
-		# create predictor
-		self.run_sql(
-			"""
-			create model proj.useritemtest
-			from pg (select * from df)
-			predict movieId
-			using
-				engine='lightfm',
-				item_id='movieId',
-				user_id='userId',
-				recommendation_type='user_item',
-				threshold=4,
-				n_recommendations=10
-				"""
-		)
-		self.wait_predictor("proj", "useritemtest")
+        result_df = self.run_sql(
+            """
+            SELECT p.*
+            FROM pg.df as t
+            JOIN proj.useritemtest as p
+            """
+        )
 
-		result_df = self.run_sql(
-			"""
-		   SELECT p.*
-		   FROM pg.df as t
-		   JOIN proj.useritemtest as p
-		"""
-		)
+        # check that the result is the expected shape e.g. 10 recommendations per user  * 503 users
+        assert result_df.shape == (5030, 3)
 
-		# check that the result is the expected shape e.g. 10 recommendations per user  * 503 users
-		assert result_df.shape == (5030, 3)
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_collaborative_filter_item_item_recommendation_light_fm_handler(
+        self, mock_handler, interaction_data
+    ):
 
+        self.set_handler(mock_handler, name="pg", tables={"df": interaction_data})
 
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_collaborative_filter_item_item_recommendation_light_fm_handler(self, mock_handler, interaction_data):
+        # create project
+        self.run_sql("create database proj")
 
-		self.set_handler(mock_handler, name="pg", tables={"df": interaction_data})
+        # create predictor
+        self.run_sql(
+            """
+            create model proj.itemitemtest
+            from pg (select * from df)
+            predict movieId
+            using
+                engine='lightfm',
+                item_id='movieId',
+                user_id='userId',
+                threshold=4,
+                recommendation_type='item_item',
+                n_recommendations=10
+                """
+        )
+        self.wait_predictor("proj", "itemitemtest")
 
-		# create project
-		self.run_sql("create database proj")
+        result_df = self.run_sql(
+            """
+            SELECT p.*
+            FROM pg.df as t
+            JOIN proj.itemitemtest as p
+            """
+        )
 
-		# create predictor
-		self.run_sql(
-			"""
-			create model proj.itemitemtest
-			from pg (select * from df)
-			predict movieId
-			using
-				engine='lightfm',
-				item_id='movieId',
-				user_id='userId',
-				threshold=4,
-				recommendation_type='item_item',
-				n_recommendations=10
-				
-				"""
-		)
-		self.wait_predictor("proj", "itemitemtest")
+        # check that the result is the expected shape e.g. 10 recommendations per user  * 89 users
+        assert result_df.shape == (890, 3)
 
-		result_df = self.run_sql(
-			"""
-		   SELECT p.*
-		   FROM pg.df as t
-		   JOIN proj.itemitemtest as p
-		"""
-		)
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_collaborative_filter_user_user_recommendation_light_fm_handler_with_item_data(
+        self, mock_handler, interaction_data
+    ):
+        ...
 
-		# check that the result is the expected shape e.g. 10 recommendations per user  * 89 users
-		assert result_df.shape == (890, 3)
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_hybrid_user_item_recommendation_light_fm_handler(
+        self, mock_handler, interaction_data
+    ):
+        ...
 
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_collaborative_filter_user_user_recommendation_light_fm_handler_with_item_data(self, mock_handler, interaction_data):
-		...
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_hybrid_item_item_recommendation_light_fm_handler(
+        self, mock_handler, interaction_data
+    ):
+        ...
 
-
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_hybrid_user_item_recommendation_light_fm_handler(self, mock_handler, interaction_data):
-		...
-
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_hybrid_item_item_recommendation_light_fm_handler(self, mock_handler, interaction_data):
-		...
-
-	@patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-	def test_hybrid_user_user_recommendation_light_fm_handler(self, mock_handler, interaction_data):
-		...
-
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_hybrid_user_user_recommendation_light_fm_handler(
+        self, mock_handler, interaction_data
+    ):
+        ...

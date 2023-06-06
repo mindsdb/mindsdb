d18adbb8c72826829d300c60a3598f3f5289f04a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-06 12:23:26 +0530

    added the logic to check for existence of mandatory columns in parse_columns() method

--- mindsdb/integrations/handlers/utilities/query_utilities/insert_query_utilities.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/insert_query_utilities.py
@@ -2,19 +2,24 @@ from typing import Text, List
 
 from mindsdb_sql.parser import ast
 
-from .exceptions import UnsupportedColumnException
+from .exceptions import UnsupportedColumnException, MandatoryColumnException
 
 
 class INSERTQueryParser:
-    def __init__(self, query: ast.Insert, supported_columns: List[Text]):
+    def __init__(self, query: ast.Insert, supported_columns: List[Text], mandatory_columns: List[Text]):
         self.query = query
         self.supported_columns = supported_columns
+        self.mandatory_columns = mandatory_columns
 
     def parse_columns(self):
         columns = self.query.columns
         if not set(columns).issubset(self.supported_columns):
             unsupported_columns = set(columns).difference(self.supported_columns)
             raise UnsupportedColumnException(f"Unsupported columns: {', '.join(unsupported_columns)}")
+
+        if not set(self.mandatory_columns).issubset(columns):
+            missing_mandatory_columns = set(self.mandatory_columns).difference(columns)
+            raise MandatoryColumnException(f"Mandatory columns missing: {', '.join(missing_mandatory_columns)}")
 
         return self.query.columns
 

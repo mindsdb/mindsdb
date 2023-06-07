4bd7788f43d0c9866e5122464056cb1780801cfc
Jorge Torres <jorge.torres.maldonado@gmail.com>
2023-06-07 00:41:27 -0700

    Update __init__.py
    urls handler

--- mindsdb/integrations/handlers/urls_handler/__init__.py
+++ mindsdb/integrations/handlers/urls_handler/__init__.py
@@ -2,16 +2,16 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 
 from .__about__ import __version__ as version, __description__ as description
 try:
-    from .twitter_handler import (
-        TwitterHandler as Handler
+    from .urls_handler import (
+        UrlsHandler as Handler
     )
     import_error = None
 except Exception as e:
     Handler = None
     import_error = e
 
-title = 'Twitter'
-name = 'twitter'
+title = 'Urls'
+name = 'urls'
 type = HANDLER_TYPE.DATA
 icon_path = 'icon.svg'
 

f06a41c48ce9b412ed56690931ec1d27c2910033
Ilia Tsyplenkov <ilya@mindsdb.com>
2023-06-22 09:32:37 +0300

    Proper setup for huggingface handler

--- mindsdb/integrations/handlers/huggingface_handler/setup.py
+++ mindsdb/integrations/handlers/huggingface_handler/setup.py
@@ -1,22 +1,27 @@
-from setuptools import setup, find_packages
+import setuptools
 
-pkgs, new_links = install_deps()
+about = {}
+with open("__about__.py") as fp:
+    exec(fp.read(), about)
 
-setup(
+with open('requirements.txt') as req_file:
+    requirements = [req.strip() for req in req_file.read().splitlines()]
+
+with open('requirements_standalone.txt') as req_file:
+    requirements = [req.strip() for req in req_file.read().splitlines()]
+
+
+setuptools.setup(
     name=about['__title__'],
     version=about['__version__'],
     url=about['__github__'],
     download_url=about['__pypi__'],
     license=about['__license__'],
     author=about['__author__'],
-    author_email=about['__email__'],
     description=about['__description__'],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=pkgs,
-    dependency_links=new_links,
-    include_package_data=True,
+    packages=setuptools.find_packages(),
+    package_data={'project': ['requirements.txt']},
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",

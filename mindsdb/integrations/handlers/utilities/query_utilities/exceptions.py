2586a191aadae21ddccce3f9e4a6d21784a0aa15
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-27 10:42:47 +0530

    added ColumnCountMismatchException to be raised when the number of columns in the query does not match the number of values

--- mindsdb/integrations/handlers/utilities/query_utilities/exceptions.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/exceptions.py
@@ -9,3 +9,9 @@ class MandatoryColumnException(Exception):
     """
     Exception raised when a mandatory column is missing from a query.
     """
+
+
+class ColumnCountMismatchException(Exception):
+    """
+    Exception raised when the number of columns in the query does not match the number of values.
+    """

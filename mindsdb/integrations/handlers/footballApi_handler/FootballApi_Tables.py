4edbbd55d792576bad852bfe81a6eb3cd601c6b7
rishavganguly007 <rishavganguly007@gmail.com>
2023-07-08 23:40:57 +0530

    API-FOOTBALL API Integration #5646

---
+++ mindsdb/integrations/handlers/footballApi_handler/FootballApi_Tables.py
@@ -0,0 +1,150 @@
+import pandas as pd
+from mindsdb_sql.parser import ast
+
+from mindsdb.integrations.libs.api_handler import APITable
+from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
+
+
+class FootballApiTable(APITable):
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """Pulls data from the Football API
+
+                Parameters
+                ----------
+                query : ast.Select
+                   Given SQL SELECT query
+
+                Returns
+                -------
+                pd.DataFrame
+                    List of Football players matching the query
+
+                Raises
+                ------
+                ValueError
+                    If the query contains an unsupported condition
+                """
+        conditions = extract_comparison_conditions(query.where)
+        player_params = {}
+        for a_where in conditions:
+            if a_where[1] == "league":
+                if a_where[0] != "=":
+                    raise ValueError("Unsupported where operation for state")
+                player_params["league"] = int(a_where[2])
+                continue
+
+            if a_where[1] == "season":
+                if a_where[0] != "=":
+                    raise ValueError("Unsupported where operation for labels")
+                player_params["season"] = int(a_where[2])
+                continue
+
+            if a_where[1] in "team":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["team"] = int(a_where[2])
+
+            if a_where[1] in "id":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["id"] = int(a_where[2])
+
+            if a_where[1] in "search":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["search"] = a_where[2]
+
+            if a_where[1] in "page":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["page"] = int(a_where[2])
+            else:
+                raise ValueError(f"Unsupported where argument {a_where[1]}")
+
+        player_data = self.handler.call_football_api("get_players", **player_params)
+        columns = []
+        for target in query.targets:
+            if isinstance(target, ast.Star):
+                columns = player_data.columns
+                break
+            elif isinstance(target, ast.Identifier):
+                columns.append(target.parts[-1])
+            else:
+                raise NotImplementedError
+
+        if len(player_data) == 0:
+            return pd.DataFrame([], columns=columns)
+
+        # Remove columns not part of select.
+        for col in set(player_data.columns).difference(set(columns)):
+            player_data = player_data.drop(col, axis=1)
+
+        return player_data
+
+    def get_columns(self) -> list:
+        """Gets all columns to be returned in pandas DataFrame responses"""
+
+        return [
+            "player_id",
+            "player_name",
+            "player_firstname",
+            "player_lastname",
+            "player_age",
+            "player_birth_date",
+            "player_birth_place",
+            "player_birth_country",
+            "player_nationality",
+            "player_height",
+            "player_weight",
+            "player_injured",
+            "player_photo",
+            "statistics_0_team_id",
+            "statistics_0_team_name",
+            "statistics_0_team_logo",
+            "statistics_0_league_id",
+            "statistics_0_league_name",
+            "statistics_0_league_country",
+            "statistics_0_league_logo",
+            "statistics_0_league_flag",
+            "statistics_0_league_season",
+            "statistics_0_games_appearences",
+            "statistics_0_games_lineups",
+            "statistics_0_games_minutes",
+            "statistics_0_games_number",
+            "statistics_0_games_position",
+            "statistics_0_games_rating",
+            "statistics_0_games_captain",
+            "statistics_0_substitutes_in",
+            "statistics_0_substitutes_out",
+            "statistics_0_substitutes_bench",
+            "statistics_0_shots_total",
+            "statistics_0_shots_on",
+            "statistics_0_goals_total",
+            "statistics_0_goals_conceded",
+            "statistics_0_goals_assists",
+            "statistics_0_goals_saves",
+            "statistics_0_passes_total",
+            "statistics_0_passes_key",
+            "statistics_0_passes_accuracy",
+            "statistics_0_tackles_total",
+            "statistics_0_tackles_blocks",
+            "statistics_0_tackles_interceptions",
+            "statistics_0_duels_total",
+            "statistics_0_duels_won",
+            "statistics_0_dribbles_attempts",
+            "statistics_0_dribbles_success",
+            "statistics_0_dribbles_past",
+            "statistics_0_fouls_drawn",
+            "statistics_0_fouls_committed",
+            "statistics_0_cards_yellow",
+            "statistics_0_cards_yellowred",
+            "statistics_0_cards_red",
+            "statistics_0_penalty_won",
+            "statistics_0_penalty_commited",
+            "statistics_0_penalty_scored",
+            "statistics_0_penalty_missed",
+            "statistics_0_penalty_saved"
+        ]
+
+
+
---
+++ mindsdb/integrations/handlers/footballApi_handler/footballApi_handler.py
@@ -0,0 +1,175 @@
+import os
+import time
+from typing import Dict
+
+import pandas as pd
+from mindsdb_sql import parse_sql
+
+from mindsdb.integrations.handlers.footballApi_handler.FootballApi_Tables import FootballApiTable
+from mindsdb.integrations.libs.api_handler import APIHandler
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse, HandlerResponse,
+)
+from mindsdb.utilities import log
+from footballAPIClient import footballAPI, FootballAPI
+
+
+class FootballApiHandler(APIHandler):
+    """The Football API handler implementation"""
+
+    def __init__(self, name: str = None, **kwargs):
+        """Initialize the Football API handler.
+
+                Parameters
+                ----------
+                name : str
+                    name of a handler instance
+                """
+        super().__init__(name)
+        self.api_key = None
+        self.account_type = None
+
+        connection_data = kwargs.get('connection_data', {})
+        if 'api_key' in connection_data:
+            self.api_key = connection_data['api_key']
+        elif 'API_KEY' in os.environ:
+            self.api_key = os.environ['API_KEY']
+        if 'account_type' in connection_data:
+            self.account_type = connection_data['account_type']
+
+        self.is_connected = False
+        self.connection_data = connection_data
+        self.connection = None
+        football_api_players = FootballApiTable(self)
+        self._register_table("get_players", football_api_players)
+
+    def connect(self) -> FootballAPI:
+        """Set up the connection required by the handler.
+
+                Returns
+                -------
+                StatusResponse
+                    connection object
+                """
+
+        if self.is_connected is True:
+            return self.connection
+
+        football_client = footballAPI.FootballAPI(self.account_type, self.api_key)
+        self.connection = football_client
+        self.is_connected = True
+        return self.connection
+
+    def check_connection(self) -> StatusResponse:
+        """Check connection to the handler.
+
+                Returns
+                -------
+                StatusResponse
+                    Status confirmation
+                """
+
+        response = StatusResponse(False)
+
+        try:
+            client = self.connect()
+            client.get_status()
+            response.success = True
+        except Exception as e:
+            log.logger.error(f"Error Connecting to Football API: {e}")
+            response.error_message = e
+
+        self.is_connected = response.success
+        return response
+
+    def native_query(self, query: str = None) -> HandlerResponse:
+        """Receive and process a raw query.
+
+                Parameters
+                ----------
+                query : str
+                    query in a native format
+
+                Returns
+                -------
+                HandlerResponse
+                    Request status
+                """
+
+        ast = parse_sql(query, dialect='mindsdb')
+        return self.query(ast)
+
+    def call_football_api(self, method_name: str, **player_params) -> pd.DataFrame:
+        """Calls the Football API.
+
+                        Parameters
+                        ----------
+                        method_name : str
+                            name of the calling method from Football api
+                        **player_params:
+                            Additional keyword arguments representing parameters specific
+                            to the API method being called.
+
+                        Returns
+                        -------
+                        pd.DataFrame
+                            A Pandas DataFrame containing the retrieved player data.
+                        """
+        if method_name == "get_players":
+            return self._get_players(**player_params)
+        raise NotImplementedError(f"Method name {method_name} not supported by Football API Handler. ")
+
+    def _get_players_data(self, client, id=None, team=None, league=None, season=None, page=1, search=None,
+                          player_data_list=None, page_required=1):
+        if player_data_list is None:
+            player_data_list = []
+
+        players = client.get_player(id=id, team=team, league=league, season=season, page=page, search=search)
+        if players["errors"]:
+            raise Exception(players["errors"])
+        player_data_list.extend(players['response'])
+
+        if players['paging']['current'] < page_required and page_required <= players['paging']['total']:
+            next_page = players['paging']['current'] + 1
+            if next_page % 2 == 1:
+                time.sleep(2)  # to avoid api rate-limit
+            player_data_list = self._get_players_data(client, id=id, team=team, league=league, season=season, page=next_page,
+                                                      search=search,
+                                                      player_data_list=player_data_list,
+                                                      page_required=page_required)
+
+        return player_data_list
+
+    def _flatten_json(self, json_data, prefix=''):
+        flattened_dict = {}
+        for key, value in json_data.items():
+            new_key = prefix + key if prefix else key
+            if isinstance(value, dict):
+                flattened_dict.update(self._flatten_json(value, new_key + '_'))
+            elif isinstance(value, list):
+                for index, item in enumerate(value):
+                    if isinstance(item, dict):
+                        flattened_dict.update(self._flatten_json(item, new_key + f'_{index}_'))
+                    else:
+                        flattened_dict[new_key + f'_{index}'] = item
+            else:
+                flattened_dict[new_key] = value
+        return flattened_dict
+
+    def _get_players(self, **params) -> pd.DataFrame:
+        client = self.connect()
+        league = params.get('league', None)
+        season = params.get('season', None)
+        team = params.get('team', None)
+        id = params.get('id', None)
+        search = params.get('search', None)
+        page = params.get('page', None)
+
+        try:
+            page_required = page if page is not None else 1
+            data = self._get_players_data(client, id=id, team=team, league=league, season=season,
+                                          search=search, page_required=page_required)
+            player_data = pd.DataFrame.from_dict([self._flatten_json(d) for d in data])
+        except Exception as e:
+            raise e
+        return player_data

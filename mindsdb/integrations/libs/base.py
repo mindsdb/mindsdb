36998406d86608008dd36e8fb3acc7a764922b17
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-07-10 00:14:59 +0100

    first commit for chromadb handler

--- mindsdb/integrations/libs/base.py
+++ mindsdb/integrations/libs/base.py
@@ -1,19 +1,20 @@
-from typing import Any, Optional, Dict
+from typing import Any, Dict, Optional
 
 import pandas as pd
 from mindsdb_sql.parser.ast.base import ASTNode
+
 from mindsdb.integrations.libs.response import HandlerResponse, HandlerStatusResponse
 
 
 class BaseHandler:
-    """ Base class for database handlers
+    """Base class for database handlers
 
     Base class for handlers that associate a source of information with the
     broader MindsDB ecosystem via SQL commands.
     """
 
     def __init__(self, name: str):
-        """ constructor
+        """constructor
         Args:
             name (str): the handler name
         """
@@ -21,7 +22,7 @@ class BaseHandler:
         self.name = name
 
     def connect(self):
-        """ Set up any connections required by the handler
+        """Set up any connections required by the handler
 
         Should return connection
 
@@ -29,7 +30,7 @@ class BaseHandler:
         raise NotImplementedError()
 
     def disconnect(self):
-        """ Close any existing connections
+        """Close any existing connections
 
         Should switch self.is_connected.
         """
@@ -37,7 +38,7 @@ class BaseHandler:
         return
 
     def check_connection(self) -> HandlerStatusResponse:
-        """ Check connection to the handler
+        """Check connection to the handler
 
         Returns:
             HandlerStatusResponse
@@ -69,7 +70,7 @@ class BaseHandler:
         raise NotImplementedError()
 
     def get_tables(self) -> HandlerResponse:
-        """ Return list of entities
+        """Return list of entities
 
         Return list of entities that will be accesible as tables.
 
@@ -81,7 +82,7 @@ class BaseHandler:
         raise NotImplementedError()
 
     def get_columns(self, table_name: str) -> HandlerResponse:
-        """ Returns a list of entity columns
+        """Returns a list of entity columns
 
         Args:
             table_name (str): name of one of tables returned by self.get_tables()
@@ -99,6 +100,15 @@ class BaseHandler:
 class DatabaseHandler(BaseHandler):
     """
     Base class for handlers associated to data storage systems (e.g. databases, data warehouses, streaming services, etc.)
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+class VectorStoreHandler(BaseHandler):
+    """
+    Base class for handlers associated to vector databases.
     """
 
     def __init__(self, name: str):
@@ -145,12 +155,19 @@ class BaseMLEngine:
         self.engine_storage = engine_storage
         self.generative = False  # if True, the target column name does not have to be specified at creation time
 
-        if kwargs.get('base_model_storage'):
-            self.base_model_storage = kwargs['base_model_storage']  # available when updating a model
+        if kwargs.get("base_model_storage"):
+            self.base_model_storage = kwargs[
+                "base_model_storage"
+            ]  # available when updating a model
         else:
             self.base_model_storage = None
 
-    def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
+    def create(
+        self,
+        target: str,
+        df: Optional[pd.DataFrame] = None,
+        args: Optional[Dict] = None,
+    ) -> None:
         """
         Saves a model inside the engine registry for later usage.
 
@@ -170,7 +187,9 @@ class BaseMLEngine:
         """
         raise NotImplementedError
 
-    def finetune(self, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
+    def finetune(
+        self, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None
+    ) -> None:
         """
         Optional.
 

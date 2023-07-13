12fa6420a116e5c87e8a13762ae7718d62f1768a
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-07-13 23:33:09 +0530

    added the test module for the HuggingFace API handler

---
+++ tests/unit/ml_handlers/test_huggingface_api.py
@@ -0,0 +1,11 @@
+import time
+from unittest.mock import patch
+import pandas as pd
+
+from mindsdb_sql import parse_sql
+
+from tests.unit.executor_test_base import BaseExecutorTest
+
+
+class TestHuggingFaceAPI(BaseExecutorTest):
+    pass

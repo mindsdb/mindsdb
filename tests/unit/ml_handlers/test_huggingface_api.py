190ad2281c805a16ada4b4741b3b855caa1fa16e
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-07-13 23:35:31 +0530

    added the run_sql() method for executing SQL queries to TestHuggingFaceAPI

--- tests/unit/ml_handlers/test_huggingface_api.py
+++ tests/unit/ml_handlers/test_huggingface_api.py
@@ -8,4 +8,9 @@ from tests.unit.executor_test_base import BaseExecutorTest
 
 
 class TestHuggingFaceAPI(BaseExecutorTest):
-    pass
+    def run_sql(self, sql):
+        ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
+        assert ret.error_code is None
+        if ret.data is not None:
+            columns = [col.alias if col.alias is not None else col.name for col in ret.columns]
+            return pd.DataFrame(ret.data, columns=columns)

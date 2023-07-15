934a9a9bcde659f94eb0436b2410a7ea89350513
rishavganguly007 <rishavganguly007@gmail.com>
2023-07-15 21:56:12 +0530

    added api support

--- mindsdb/integrations/handlers/footballApi_handler/FootballApi_Tables.py
+++ mindsdb/integrations/handlers/footballApi_handler/FootballApi_Tables.py
@@ -1,6 +1,84 @@
+import pandas as pd
+from mindsdb_sql.parser import ast
+
 from mindsdb.integrations.libs.api_handler import APITable
+from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
 
 
 class FootballApiTable(APITable):
-    def __init__(self, handler):
-        super().__init__(handler)
+    def select(self, query: ast.Select) -> pd.Dataframe:
+        """Pulls data from the Football API
+
+                Parameters
+                ----------
+                query : ast.Select
+                   Given SQL SELECT query
+
+                Returns
+                -------
+                pd.DataFrame
+                    List of Football players matching the query
+
+                Raises
+                ------
+                ValueError
+                    If the query contains an unsupported condition
+                """
+        conditions = extract_comparison_conditions(query.where)
+        player_params = {}
+        for a_where in conditions:
+            if a_where[1] == "league":
+                if a_where[0] != "=":
+                    raise ValueError("Unsupported where operation for state")
+                player_params["league"] = a_where[2]
+                continue
+
+            if a_where[1] == "season":
+                if a_where[0] != "=":
+                    raise ValueError("Unsupported where operation for labels")
+                player_params["season"] = a_where[2].split(",")
+                continue
+
+            if a_where[1] in "team":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["team"] = a_where[2]
+
+            if a_where[1] in "id":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["id"] = a_where[2]
+
+            if a_where[1] in "search":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["search"] = a_where[2]
+
+            if a_where[1] in "page":
+                if a_where[0] != "=":
+                    raise ValueError(f"Unsupported where operation for {a_where[1]}")
+                player_params["page"] = a_where[2]
+            else:
+                raise ValueError(f"Unsupported where argument {a_where[1]}")
+
+        player_data = self.handler.call_football_api("get_players", **player_params)
+        columns = []
+        for target in query.targets:
+            if isinstance(target, ast.Star):
+                columns = player_data.columns
+                break
+            elif isinstance(target, ast.Identifier):
+                columns.append(target.parts[-1])
+            else:
+                raise NotImplementedError
+
+        if len(player_data) == 0:
+            return pd.DataFrame([], columns=columns)
+
+        # Remove columns not part of select.
+        for col in set(player_data.columns).difference(set(columns)):
+            player_data = player_data.drop(col, axis=1)
+
+        return player_data
+
+
--- mindsdb/integrations/handlers/footballApi_handler/footballApi_handler.py
+++ mindsdb/integrations/handlers/footballApi_handler/footballApi_handler.py
@@ -1,6 +1,8 @@
 import os
+import time
 from typing import Dict
 
+import pandas as pd
 from mindsdb_sql import parse_sql
 
 from mindsdb.integrations.handlers.footballApi_handler.FootballApi_Tables import FootballApiTable
@@ -30,8 +32,8 @@ class FootballApiHandler(APIHandler):
         self.is_connected = False
         self.connection_data = connection_data
         self.connection = None
-        football_api_teams_info = FootballApiTable(self)
-        self._register_table("get_teams_info", football_api_teams_info)
+        football_api_players = FootballApiTable(self)
+        self._register_table("get_players", football_api_players)
 
     def connect(self) -> FootballAPI:
         if self.is_connected is True:
@@ -60,20 +62,60 @@ class FootballApiHandler(APIHandler):
         ast = parse_sql(query, dialect='mindsdb')
         return self.query(ast)
 
-    def call_football_api(self, method_name: str = None, params: Dict = None):
-        if method_name == "get_team_statistics":
-            return self._get_team_statistics(params)
+    def call_football_api(self, method_name: str = None, **player_params):
+        if method_name == "get_players":
+            return self._get_players(**player_params)
         raise NotImplementedError(f"Method name {method_name} not supported by Football API. ")
 
-    def _get_team_statistics(self, params: Dict = None):
+    def _get_players_data(self, client, id=None, team=None, league=None, season=None, page=1, search=None,
+                          player_data_list=None):
+        if player_data_list is None:
+            player_data_list = []
+
+        players = client.get_player(id=id, team=team, league=league, season=season, page=page, search=search)
+        if players["errors"]:
+            raise Exception(players["errors"])
+        player_data_list.extend(players['response'])
+
+        if players['paging']['current'] < players['paging']['total']:
+            next_page = players['paging']['current'] + 1
+            if next_page % 2 == 1:
+                time.sleep(2)  # to avoid api rate-limit
+            player_data_list = self._get_players_data(client, id=id, team=team, league=league, season=season, page=page,
+                                                      search=search,
+                                                      player_data_list=player_data_list)
+
+        return player_data_list
+
+    def _flatten_json(self, json_data, prefix=''):
+        flattened_dict = {}
+        for key, value in json_data.items():
+            new_key = prefix + key if prefix else key
+            if isinstance(value, dict):
+                flattened_dict.update(self._flatten_json(value, new_key + '_'))
+            elif isinstance(value, list):
+                for index, item in enumerate(value):
+                    if isinstance(item, dict):
+                        flattened_dict.update(self._flatten_json(item, new_key + f'_{index}_'))
+                    else:
+                        flattened_dict[new_key + f'_{index}'] = item
+            else:
+                flattened_dict[new_key] = value
+        return flattened_dict
+
+    def _get_players(self, **params) -> pd.Dataframe:
         client = self.connect()
         league = params.get('league', None),
         season = params.get('season', None)
         team = params.get('team', None)
-        date = params.get('date', None)
+        id = params.get('id', None)
+        search = params.get('search', None)
+        page = params.get('page', None)
 
         try:
-            data = client.get_team_statistics(league=league, season=season, team=team, date=date)
+            data = self._get_players_data(client, id=id, team=team, league=league, season=season, page=page,
+                                          search=search)
+            player_data = pd.DataFrame.from_dict([self._flatten_json(d) for d in data])
         except Exception as e:
             raise e
-        return data
+        return player_data

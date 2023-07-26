e48f2acd307eeaeb8d654d123aeea8ec1d1c4cd3
Yuhui Shi <yuhui@convect.ai>
2023-07-26 13:42:24 -0700

    Remove unused import

--- tests/unit/ml_handlers/test_openai.py
+++ tests/unit/ml_handlers/test_openai.py
@@ -9,7 +9,6 @@ from mindsdb_sql import parse_sql
 
 from tests.unit.executor_test_base import BaseExecutorTest
 
-from mindsdb.integrations.handlers.openai_handler.openai_handler import OpenAIHandler
 
 OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPEN_AI_API_KEY

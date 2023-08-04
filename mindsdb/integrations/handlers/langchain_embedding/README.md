c2d0b0d0bf3f45ca200e20964a6c829cbab17795
Yuhui Shi <yuhui@convect.ai>
2023-08-04 16:12:31 -0700

    Fix model class serialization

---
+++ mindsdb/integrations/handlers/langchain_embedding/README.md
@@ -0,0 +1,66 @@
+# Langchain Embedding Handler
+
+## Overview
+
+This is a bridge handler that brings all modules that are supported under
+`langchain.embeddings` to MindsDB. Here is a [comprehensive list](https://python.langchain.com/docs/integrations/text_embedding/) of supported embeddings models in Langchain.
+
+To use individual embedding modules, additional deps need to be installed.
+If you failed to create an embedding model, use `DESCRIBE <model_name>` to check if missing dependencies.
+
+## Usage
+
+Create the ML engine
+```sql
+CREATE ML_ENGINE langchain_embedding
+FROM langchain_embedding;
+```
+
+Create an embedding model
+```sql
+CREATE MODEL openai_embedding_model
+PREDICT content
+-- the name of the column in the input data that contains the text content
+USING
+  engine = 'langchain_embedding',
+  class = 'OpenAIEmbeddings', -- the name of the embedding class in langchain.embeddings
+  openai_api_key = '<your_api_key>'  -- alternative set your env variable OPEN_AI_API_KEY on the machine that runs MindsDB
+;
+```
+All arguments specified after the `USING` clause, except `engine` and `class` will be passed to the constructor when initializing the embedding models. Please refer to the detailed doc page for each embedding models in langchain for acceptable arguments. For example, here is the [doc for OpenAI](https://api.python.langchain.com/en/latest/embeddings/langchain.embeddings.openai.OpenAIEmbeddings.html).
+
+Make a prediction for one row
+```sql
+SELECT
+  content, embedding
+FROM
+  openai_embedding_model
+WHERE
+  content = 'A sample text to be embeded'
+;
+
+```
+
+Make a batch prediction
+```sql
+CREATE DATABASE mysql_demo_db
+WITH ENGINE = "mysql",
+PARAMETERS = {
+    "user": "user",
+    "password": "MindsDBUser123!",
+    "host": "db-demo-data.cwoyhfn6bzs0.us-east-1.rds.amazonaws.com",
+    "port": "3306",
+    "database": "public"
+    };
+
+CREATE VIEW amazon_review (
+    SELECT review as content FROM mysql_demo_db.amazon_reviews LIMIT 10
+);
+
+SELECT
+  content, m.embeddings
+FROM
+  amazon_review as s
+JOIN
+    openai_embedding_model as m;
+```
--- mindsdb/integrations/handlers/langchain_embedding/langchain_embedding_handler.py
+++ mindsdb/integrations/handlers/langchain_embedding/langchain_embedding_handler.py
@@ -32,26 +32,18 @@ def get_langchain_class(class_name: str) -> Embeddings:
     return class_
 
 
-def serialize_model_to_storage(embedding_model: Embeddings, model_storage):
-    """
-    Serializes the model to the model storage
-    """
-
-    # save the model to the model storage
-    serialized_dict = embedding_model.dict()
-    serialized_dict["_model_class"] = embedding_model.__class__.__name__
-    model_storage.json_set("_model", serialized_dict)
-
-
-def deserialize_model_from_storage(model_storage) -> Embeddings:
+def construct_model_from_args(args: Dict) -> Embeddings:
     """
     Deserializes the model from the model storage
     """
-    model_info = model_storage.json_get("_model")
-    class_name = model_info.pop("_model_class")
+    target = args.pop("target", None)
+    class_name = args.pop("class", LangchainEmbeddingHandler.DEFAULT_EMBEDDING_CLASS)
     MODEL_CLASS = get_langchain_class(class_name)
-    serialized_dict = model_info
+    serialized_dict = args
     model = MODEL_CLASS(**serialized_dict)
+    if target is not None:
+        args["target"] = target
+    args["class"] = class_name
     return model
 
 
@@ -73,30 +65,28 @@ class LangchainEmbeddingHandler(BaseMLEngine):
     ) -> None:
         # get the class name from the args
         user_args = args.get("using", {})
-        class_ = user_args.pop("class", self.DEFAULT_EMBEDDING_CLASS)
-
-        # get the class object
-        MODEL_CLASS = get_langchain_class(class_)
         # this may raise an exception if
         # the arguments are not sufficient to create such as class
         # due to e.g., lack of API key
         # But the validation logic is handled by langchain and pydantic
-        model = MODEL_CLASS(**user_args)
+        construct_model_from_args(user_args)
 
         # save the model to the model storage
-        serialize_model_to_storage(model, self.model_storage)
         user_args["target"] = target
         self.model_storage.json_set("args", user_args)
 
     def predict(self, df: DataFrame, args) -> DataFrame:
         # reconstruct the model from the model storage
-        model = deserialize_model_from_storage(self.model_storage)
+        user_args = self.model_storage.json_get("args")
+        model = construct_model_from_args(user_args)
 
         # get the target from the model storage
-        target = self.model_storage.json_get("args").get("target")
+        target = user_args.get("target")
         # check if the target is in the df
-        if target not in df.columns:
-            raise Exception(f"Target column {target} not found in the input dataframe.")
+        if target is None or target not in df.columns:
+            raise Exception(
+                f"Target column {target} not found in the input dataframe. Available columns are {df.columns}"
+            )
 
         # run the actual embedding vector generation
         texts = df[target].tolist()

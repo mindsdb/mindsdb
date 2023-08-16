d31966b7f796c1f3870a415febf6ff336958d656
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-08-16 16:12:05 +0330

    0.0.1 → 0.1.0

--- mindsdb/integrations/handlers/pypi_handler/__about__.py
+++ mindsdb/integrations/handlers/pypi_handler/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "MindsDB PyPI handler"
 __package_name__ = "mindsdb_pypi_handler"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __description__ = "MindsDB handler for PyPI"
 __author__ = "Sadra Yahyapour"
 __github__ = "https://github.com/mindsdb/mindsdb"

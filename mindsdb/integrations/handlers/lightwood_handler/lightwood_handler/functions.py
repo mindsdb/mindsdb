17e86ccb35fcc3c42382e01f5ead76521df47929
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2023-09-01 14:23:55 -0700

    fix: alert missing data source in lightwood handler when training

--- mindsdb/integrations/handlers/lightwood_handler/lightwood_handler/functions.py
+++ mindsdb/integrations/handlers/lightwood_handler/lightwood_handler/functions.py
@@ -151,12 +151,15 @@ def run_learn_remote(df: DataFrame, predictor_id: int) -> None:
 
 @mark_process(name='learn')
 def run_learn(df: DataFrame, args: dict, model_storage) -> None:
-    # FIXME
+    if df is None or df.shape[0] == 0:
+        raise Exception('No input data. Ensure the data source is healthy and try again.')
+
     predictor_id = model_storage.predictor_id
 
     predictor_record = db.Predictor.query.with_for_update().get(predictor_id)
     predictor_record.training_start_at = datetime.now()
     db.session.commit()
+
 
     run_generate(df, predictor_id, model_storage, args)
     run_fit(predictor_id, df, model_storage)
@@ -169,6 +172,9 @@ def run_learn(df: DataFrame, args: dict, model_storage) -> None:
 @mark_process(name='finetune')
 def run_finetune(df: DataFrame, args: dict, model_storage):
     try:
+        if df is None or df.shape[0] == 0:
+            raise Exception('No input data. Ensure the data source is healthy and try again.')
+
         base_predictor_id = args['base_model_id']
         base_predictor_record = db.Predictor.query.get(base_predictor_id)
         if base_predictor_record.status != PREDICTOR_STATUS.COMPLETE:

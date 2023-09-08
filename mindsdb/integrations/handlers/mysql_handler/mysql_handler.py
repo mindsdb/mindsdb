69a512adde702ef20fd0f2afb635500fabb46979
Federico Razzoli <federico.razzoli@vettabase.com>
2023-09-08 03:09:28 +0100

    Remove column renaming, which became redundant

--- mindsdb/integrations/handlers/mysql_handler/mysql_handler.py
+++ mindsdb/integrations/handlers/mysql_handler/mysql_handler.py
@@ -167,8 +167,6 @@ class MySQLHandler(DatabaseHandler):
             ;
         """
         result = self.native_query(sql)
-        df = result.data_frame
-        result.data_frame = df.rename(columns={df.columns[0]: 'table_name'})
         return result
 
     def get_columns(self, table_name) -> Response:

8825c1aebe508ef3da24ee67e80f44ac010be615
Pranav <pranavprajapati586@gmail.com>
2023-09-19 17:52:07 +0530

    Add about.py file

---
+++ mindsdb/integrations/handlers/pytorch_tabular_handler/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'MindsDB pytorch tabular handler'
+__package_name__ = 'pytorch_tabular_handler'
+__version__ = '0.0.1'
+__description__ = "MindsDB handler for PyTorch Tabular"
+__author__ = 'Pranav Prajapati'
+__github__ = 'https://github.com/mindsdb/mindsdb'
+__pypi__ = 'https://pypi.org/project/mindsdb/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2023 - mindsdb'

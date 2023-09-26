b02ba6a0c390d970c4ba5355e4d927205f79f2e6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-09-26 21:22:56 +0530

    added the skeleton for the UPDATEQueryParser

---
+++ mindsdb/integrations/handlers/utilities/query_utilities/update_query_utilities.py
@@ -0,0 +1,21 @@
+
+
+class UPDATEQueryParser:
+    """
+    Parses an UPDATE query into its component parts.
+
+    Parameters
+    ----------
+    query : ast.Select
+        Given SQL UPDATE query.
+    """
+    
+    def parse_query():
+        pass
+
+    def parse_set_clause():
+        pass
+
+    def parse_where_clause():
+        pass
+

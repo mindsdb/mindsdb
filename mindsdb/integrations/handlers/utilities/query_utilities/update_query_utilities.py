9415bfe28d1e05f0c02395804fde160d656164a2
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-09-27 10:10:39 +0530

    updated the UPDATEQueryParser to inheir from the BaseQueryParser

--- mindsdb/integrations/handlers/utilities/query_utilities/update_query_utilities.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/update_query_utilities.py
@@ -1,21 +1,22 @@
+from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
+from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryParser
 
 
-class UPDATEQueryParser:
+class UPDATEQueryParser(BaseQueryParser):
     """
     Parses an UPDATE query into its component parts.
 
     Parameters
     ----------
-    query : ast.Select
+    query : ast.Update
         Given SQL UPDATE query.
     """
+    def __init__(self, query):
+        super().__init__(query)
     
     def parse_query():
         pass
 
     def parse_set_clause():
-        pass
-
-    def parse_where_clause():
         pass
 

4027d3c0958226593bc74f0991477ccb8bbf684b
Abhi <abhi@Abhis-MacBook-Air.local>
2023-10-03 02:48:56 +0530

    added inventory table implementation

--- mindsdb/integrations/handlers/shopify_handler/shopify_tables.py
+++ mindsdb/integrations/handlers/shopify_handler/shopify_tables.py
@@ -178,7 +178,7 @@ class OrdersTable(APITable):
             self.get_columns()
         )
         selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
-
+        logger.info(where_conditions)
         orders_df = pd.json_normalize(self.get_orders(limit=result_limit))
 
         select_statement_executor = SELECTQueryExecutor(
@@ -199,3 +199,85 @@ class OrdersTable(APITable):
         shopify.ShopifyResource.activate_session(api_session)
         orders = shopify.Order.find(**kwargs)
         return [order.to_dict() for order in orders]
+
+class InventoryTable(APITable):
+    """The Shopify Inventory Table implementation"""
+
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """Pulls data from the Shopify "GET /inventory" API endpoint.
+
+        Parameters
+        ----------
+        query : ast.Select
+           Given SQL SELECT query
+
+        Returns
+        -------
+        pd.DataFrame
+            Shopify Inventory matching the inventory_item_id or/and location_id
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+        select_statement_parser = SELECTQueryParser(
+            query,
+            'inventory',
+            self.get_columns()
+        )
+        selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
+
+        search_params = {}
+        subset_where_conditions = []
+        for op, arg1, arg2 in where_conditions:
+            if arg1 == 'inventory_item_ids':
+                if op == '=':
+                    search_params["inventory_item_ids"] = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for inventory_item_ids column.")
+            elif arg1 == 'location_ids':
+                if op == '=':
+                    search_params["location_ids"] = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for location_ids column.")
+            elif arg1 in ['available', 'updated_at']:
+                subset_where_conditions.append([op, arg1, arg2])
+
+        filter_flag = ("inventory_item_ids" in search_params) or ("location_ids" in search_params)
+
+        if not filter_flag:
+            raise NotImplementedError("inventory_item_ids or location_ids column has to be present in where clause.")
+
+        search_params["limit"] = result_limit
+
+        inventory_df = pd.json_normalize(self.get_inventory(search_params))
+
+        self.clean_selected_columns(selected_columns)
+
+        select_statement_executor = SELECTQueryExecutor(
+            inventory_df,
+            selected_columns,
+            subset_where_conditions,
+            order_by_conditions
+        )
+        inventory_df = select_statement_executor.execute_query()
+
+        return inventory_df
+
+    def clean_selected_columns(self, selected_cols) -> List[Text]:
+        if "inventory_item_ids" in selected_cols:
+            selected_cols.remove("inventory_item_ids")
+            selected_cols.append("inventory_item_id")
+        if "location_ids" in selected_cols:
+            selected_cols.remove("location_ids")
+            selected_cols.append("location_id")
+
+    def get_columns(self) -> List[Text]:
+        return ["inventory_item_ids", "location_ids", "available", "updated_at"]
+
+    def get_inventory(self, kwargs) -> List[Dict]:
+        api_session = self.handler.connect()
+        shopify.ShopifyResource.activate_session(api_session)
+        inventories = shopify.InventoryLevel.find(**kwargs)
+        return [inventory.to_dict() for inventory in inventories]

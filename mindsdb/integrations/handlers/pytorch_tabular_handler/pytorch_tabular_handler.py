aa2e48bb11863168cd4fdcbb52031433df5246a5
Pranav <pranavprajapati586@gmail.com>
2023-10-05 00:31:44 +0530

    Added layers parameter

--- mindsdb/integrations/handlers/pytorch_tabular_handler/pytorch_tabular_handler.py
+++ mindsdb/integrations/handlers/pytorch_tabular_handler/pytorch_tabular_handler.py
@@ -1,9 +1,11 @@
 from typing import Optional, Dict
+import re
 
 import dill
 import pandas as pd
 import ast
 
+import io
 import torch
 from pytorch_tabular.config import DataConfig, OptimizerConfig, TrainerConfig
 from pytorch_tabular.models import CategoryEmbeddingModelConfig
@@ -23,21 +25,26 @@ class Pytorch_Tabular_Handler(BaseMLEngine):
         task_supported = ['regression','classification','backbone']
         initialization_supported = ['kaiming','xavier','random']
         args = args["using"]
-        if "task" not in args:
+        #pattern for layers regex
+        pattern = r'^\d+-\d+-\d+$'
+        if "task" in args:
             if args["task"] not in task_supported:
                 raise Exception(f"Please specify task parameter supported : {task_supported}")
         if "initialization" not in args or args["initialization"] not in initialization_supported:
             raise Exception(f"Initialization scheme choices are : {initialization_supported}")
-        #if not target:
-            #raise Exception("Please provide the target column")
+        if "layers" in args:
+            if not re.match(pattern,args["layers"]):
+                raise Exception(f"Please specify layers in format : '128-64-32'")
 
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[dict] = None) -> None:
         train_data = df
+        # Set default values if user does not specify
         categorical_columns = None
         dropout = 0.0
         epochs = 3
         continuous_columns = None
         batch_size = 32
+        layers = '128-64-32'
         args = args["using"]
         if 'categorical_cols' in args:
             categorical_columns = ast.literal_eval(args["categorical_cols"])
@@ -49,6 +56,8 @@ class Pytorch_Tabular_Handler(BaseMLEngine):
             epochs = int(args['epochs'])
         if 'batch_size' in args:
             batch_size = int(args['batch_size'])
+        if 'layers' in args:
+            layers = args['layers']
         data_config = DataConfig(
             target = [target],
             continuous_cols = continuous_columns,
@@ -57,7 +66,7 @@ class Pytorch_Tabular_Handler(BaseMLEngine):
             normalize_continuous_features=True,
         )
         head_config = LinearHeadConfig(
-            layers='', dropout=dropout, initialization=args['initialization']
+            layers=layers, dropout=dropout, initialization=args['initialization']
             # No additional layer in head, just a mapping layer to output_dim
         ).__dict__
         model_config = CategoryEmbeddingModelConfig(
@@ -82,12 +91,13 @@ class Pytorch_Tabular_Handler(BaseMLEngine):
 
         # Save the trained model
         self.model_storage.json_set('args', args)
-        self.model_storage.file_set(torch.save(tabular_model,'pytorch_tabular.pt'))
+        self.model_storage.file_set('model.pt', dill.dumps(tabular_model))
 
     def predict(self, df: pd.DataFrame, args: Optional[Dict] = None) -> pd.DataFrame:
-        tabular_model = dill.loads(self.model_storage.file_get("model"))
+        tabular_model = torch.load(self.model_storage.file_get('model'))
         predictions = tabular_model.predict(df)
         return predictions
+
     def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
         args = self.model_storage.json_get('args')
         des_dict = {'epochs': args['epochs'], 'initialization': args['initialization'], 'task': args['task'],

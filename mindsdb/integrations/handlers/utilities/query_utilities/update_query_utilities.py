e96f9069645ba12fa8927f8730645de8f9085530
Drew Springfield <drew@robosim.com>
2023-10-06 10:06:10 -0700

    Please git dont fail me now

--- mindsdb/integrations/handlers/utilities/query_utilities/update_query_utilities.py
+++ mindsdb/integrations/handlers/utilities/query_utilities/update_query_utilities.py
@@ -1,146 +1,14 @@
-from mindsdb_sql.parser import ast
-from typing import Text, List, Optional
-
-from .exceptions import UnsupportedColumnException
-
-from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryParser
-from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryExecutor
-
-
-class UPDATEQueryParser(BaseQueryParser):
-    """
-    Parses an UPDATE query into its component parts.
-
-    Parameters
-    ----------
-    query : ast.Update
-        Given SQL UPDATE query.
-    supported_columns : List[Text], Optional
-        List of columns supported by the table for updating.
-    """
-    def __init__(self, query: ast.Update, supported_columns: Optional[List[Text]] = None):
-        super().__init__(query)
-        self.supported_columns = supported_columns
-    
-    def parse_query(self):
-        """
-        Parses a SQL UPDATE statement into its components: the columns and values to update as a dictionary, and the WHERE conditions.
-        """
-        values_to_update = self.parse_set_clause()
-        where_conditions = self.parse_where_clause()
-
-        return values_to_update, where_conditions
-
-    def parse_set_clause(self):
-        """
-        Parses the SET clause of the query and returns a dictionary of columns and values to update.
-        """
-        values = list(self.query.update_columns.items())
-
-        values_to_update = {}
-        for value in values:
-            if self.supported_columns:
-                if value[0] not in self.supported_columns:
-                    raise UnsupportedColumnException(f"Unsupported column: {value[0]}")
-            
-            values_to_update[value[0]] = value[1].value
-
-        return values_to_update
-
-
-class UPDATEQueryExecutor(BaseQueryExecutor):
-    """
-    Executes an UPDATE query.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Given table.
-    where_conditions : List[List[Text]]
-        WHERE conditions of the query.
-
-    NOTE: This class DOES NOT update the relevant records of the entity for you, it will simply return the records that need to be updated based on the WHERE conditions.
-          
-          This class expects all of the entities to be passed in as a DataFrane and filters out the relevant records based on the WHERE conditions.
-          Because all of the records need to be extracted to be passed in as a DataFrame, this class is not very computationally efficient.
-          Therefore, DO NOT use this class if the API/SDK that you are using supports updating records in bulk.
-    """
-from mindsdb_sql.parser import ast
-from typing import Text, List, Optional
-
-from .exceptions import UnsupportedColumnException
-
-from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryParser
-from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryExecutor
-
-
-class UPDATEQueryParser(BaseQueryParser):
-    """
-    Parses an UPDATE query into its component parts.
-
-    Parameters
-    ----------
-    query : ast.Update
-        Given SQL UPDATE query.
-    supported_columns : List[Text], Optional
-        List of columns supported by the table for updating.
-    """
-    def __init__(self, query: ast.Update, supported_columns: Optional[List[Text]] = None):
-        super().__init__(query)
-        self.supported_columns = supported_columns
-    
-    def parse_query(self):
-        """
-        Parses a SQL UPDATE statement into its components: the columns and values to update as a dictionary, and the WHERE conditions.
-        """
-        values_to_update = self.parse_set_clause()
-        where_conditions = self.parse_where_clause()
-
-        return values_to_update, where_conditions
-
-    def parse_set_clause(self):
-        """
-        Parses the SET clause of the query and returns a dictionary of columns and values to update.
-        """
-        values = list(self.query.update_columns.items())
-
-        values_to_update = {}
-        for value in values:
-            if self.supported_columns:
-                if value[0] not in self.supported_columns:
-                    raise UnsupportedColumnException(f"Unsupported column: {value[0]}")
-            
-            values_to_update[value[0]] = value[1].value
-
-        return values_to_update
-
-
-class UPDATEQueryExecutor(BaseQueryExecutor):
-    """
-    Executes an UPDATE query.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Given table.
-    where_conditions : List[List[Text]]
-        WHERE conditions of the query.
-
-    NOTE: This class DOES NOT update the relevant records of the entity for you, it will simply return the records that need to be updated based on the WHERE conditions.
-          
-          This class expects all of the entities to be passed in as a DataFrane and filters out the relevant records based on the WHERE conditions.
-          Because all of the records need to be extracted to be passed in as a DataFrame, this class is not very computationally efficient.
-          Therefore, DO NOT use this class if the API/SDK that you are using supports updating records in bulk.
-    """
 import pandas as pd
-from typing import Text, List, Tuple, Any, Dict
+from mindsdb_sql.parser import ast
+from typing import Text, List, Optional, Tuple, Any
 
 from .exceptions import UnsupportedColumnException
 
-from mindsdb_sql.parser import ast
-from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
+from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryParser
+from mindsdb.integrations.handlers.utilities.query_utilities.base_query_utilities import BaseQueryExecutor
 
-class UPDATEQueryParser():
+
+class UPDATEQueryParser(BaseQueryParser):
     """
     Parses an UPDATE query into its component parts.
 
@@ -148,61 +16,57 @@ class UPDATEQueryParser():
     ----------
     query : ast.Update
         Given SQL UPDATE query.
-    table : Text
-        Name of the table to query.
-    columns : List[Text]
-        List of columns in the table.
+    supported_columns : List[Text], Optional
+        List of columns supported by the table for updating.
     """
-    def __init__(self, query: ast.Update, table: Text, columns: List[Text]):
-        self.query = query
-        self.table = table
-        self.columns = columns
-
-    def parse_query(self) -> Tuple[Dict[Text, Any], List[List[Text]]]:
-        """
-        Parses a SQL UPDATE statement into its components: UPDATE, SET, WHERE.
-        """
-        set_clauses = self.parse_set_clause() #Columns and values to update
-        where_conditions = self.parse_where_clause() #Conditional write
-
-        return set_clauses, where_conditions
+    def __init__(self, query: ast.Update, supported_columns: Optional[List[Text]] = None):
+        super().__init__(query)
+        self.supported_columns = supported_columns
     
-    
-    def parse_set_clause(self) -> Dict[Text, Any]:
-        column_value_pairs = list(self.query.update_columns.items())
-
-        columns_to_update = {}
-        for c_v_pair in column_value_pairs:
-            columns_to_update[c_v_pair[0]] = c_v_pair[1]
-
-        return columns_to_update
-
-
-    def parse_where_clause(self) -> List[List[Text]]:
+    def parse_query(self):
         """
-        Parses the WHERE clause of the query.
+        Parses a SQL UPDATE statement into its components: the columns and values to update as a dictionary, and the WHERE conditions.
         """
-        where_conditions = extract_comparison_conditions(self.query.where)
-        return where_conditions
+        values_to_update = self.parse_set_clause()
+        where_conditions = self.parse_where_clause()
+
+        return values_to_update, where_conditions
+
+    def parse_set_clause(self):
+        """
+        Parses the SET clause of the query and returns a dictionary of columns and values to update.
+        """
+        values = list(self.query.update_columns.items())
+
+        values_to_update = {}
+        for value in values:
+            if self.supported_columns:
+                if value[0] not in self.supported_columns:
+                    raise UnsupportedColumnException(f"Unsupported column: {value[0]}")
+            
+            values_to_update[value[0]] = value[1].value
+
+        return values_to_update
 
 
-class UPDATEQueryExecutor():
+class UPDATEQueryExecutor(BaseQueryExecutor):
     """
-    Executes a UPDATE query.
+    Executes an UPDATE query.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Given table.
+    where_conditions : List[List[Text]]
+        WHERE conditions of the query.
+
+    NOTE: This class DOES NOT update the relevant records of the entity for you, it will simply return the records that need to be updated based on the WHERE conditions.
+          
+          This class expects all of the entities to be passed in as a DataFrane and filters out the relevant records based on the WHERE conditions.
+          Because all of the records need to be extracted to be passed in as a DataFrame, this class is not very computationally efficient.
+          Therefore, DO NOT use this class if the API/SDK that you are using supports updating records in bulk.
     """
     def __init__(self, df: pd.DataFrame, set_clauses: List[Tuple[Text, Any]], where_conditions: List[List[Text]]):
-        """
-        Initializes the UPDATEQueryExecutor,
-        
-        Parameters
-        ----------
-        df : pd.DataFrame
-            Dataframe to query.
-        set_clauses : List[Tuple[Text, Any]]
-            List of Tuples (Column, Value) to set.
-        where_conditions : List[List[Text]]
-            List of where conditions.
-        """
         self.df = df
         self.set_clauses = set_clauses
         self.where_conditions = where_conditions

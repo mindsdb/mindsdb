f96af58c80f8cb147ee2b2fae04179fb724cf3a4
Aditya Azad <adityaazad121@gmail.com>
2023-10-09 15:02:17 -0400

    Work in progress, select

--- mindsdb/integrations/handlers/milvus_handler/milvus_handler.py
+++ mindsdb/integrations/handlers/milvus_handler/milvus_handler.py
@@ -12,7 +12,7 @@ from mindsdb.integrations.libs.response import \
 from mindsdb.integrations.libs.vectordatabase_handler import (
     FilterCondition, FilterOperator, TableField, VectorStoreHandler)
 from mindsdb.utilities import log
-from pymilvus import connections, utility
+from pymilvus import Collection, connections, utility
 
 
 class MilvusHandler(VectorStoreHandler):
@@ -22,7 +22,18 @@ class MilvusHandler(VectorStoreHandler):
 
     def __init__(self, name: str, **kwargs):
         super().__init__(name)
-        self._connection_data = kwargs.get("connection_data")
+        self._connection_data = kwargs["connection_data"]
+        # Extract parameters used while searching and leave the rest for establishing connection
+        search_param_names = {
+            "search_metric_type": "metric_type",
+            "search_ignore_growing": "ignore_growing",
+            "search_params": "params"
+        }
+        self._search_params = {}
+        for search_param_alias, actual_search_param_name in search_param_names.items():
+            if search_param_alias in self._connection_data:
+                self._search_params[actual_search_param_name] = self._connection_data[search_param_alias]
+                del self._connection_data[search_param_alias]
         self.is_connected = False
         self.connect()
 
@@ -82,84 +93,6 @@ class MilvusHandler(VectorStoreHandler):
                 )
         return Response(resp_type=RESPONSE_TYPE.OK)
 
-
-
-
-
-
-
-    def _get_chromadb_operator(self, operator: FilterOperator) -> str:
-        mapping = {
-            FilterOperator.EQUAL: "$eq",
-            FilterOperator.NOT_EQUAL: "$ne",
-            FilterOperator.LESS_THAN: "$lt",
-            FilterOperator.LESS_THAN_OR_EQUAL: "$lte",
-            FilterOperator.GREATER_THAN: "$gt",
-            FilterOperator.GREATER_THAN_OR_EQUAL: "$gte",
-        }
-
-        if operator not in mapping:
-            raise Exception(f"Operator {operator} is not supported by Milvus!")
-
-        return mapping[operator]
-
-    def _translate_metadata_condition(
-        self, conditions: List[FilterCondition]
-    ) -> Optional[dict]:
-        """
-        Translate a list of FilterCondition objects a dict that can be used by Milvus.
-        E.g.,
-        [
-            FilterCondition(
-                column="metadata.created_at",
-                op=FilterOperator.LESS_THAN,
-                value="2020-01-01",
-            ),
-            FilterCondition(
-                column="metadata.created_at",
-                op=FilterOperator.GREATER_THAN,
-                value="2019-01-01",
-            )
-        ]
-        -->
-        {
-            "$and": [
-                {"created_at": {"$lt": "2020-01-01"}},
-                {"created_at": {"$gt": "2019-01-01"}}
-            ]
-        }
-        """
-        # we ignore all non-metadata conditions
-        if conditions is None:
-            return None
-        metadata_conditions = [
-            condition
-            for condition in conditions
-            if condition.column.startswith(TableField.METADATA.value)
-        ]
-        if len(metadata_conditions) == 0:
-            return None
-
-        # we translate each metadata condition into a dict
-        chroma_db_conditions = []
-        for condition in metadata_conditions:
-            metadata_key = condition.column.split(".")[-1]
-            chroma_db_conditions.append(
-                {
-                    metadata_key: {
-                        self._get_chromadb_operator(condition.op): condition.value
-                    }
-                }
-            )
-
-        # we combine all metadata conditions into a single dict
-        metadata_condition = (
-            {"$and": chroma_db_conditions}
-            if len(chroma_db_conditions) > 1
-            else chroma_db_conditions[0]
-        )
-        return metadata_condition
-
     def select(
         self,
         table_name: str,
@@ -168,6 +101,60 @@ class MilvusHandler(VectorStoreHandler):
         offset: int = None,
         limit: int = None,
     ) -> HandlerResponse:
+
+        # Load collection table
+        collection = Collection(table_name)
+        try:
+            collection.load()
+        except Exception as e:
+            return Response(
+                resp_type=RESPONSE_TYPE.ERROR,
+                error_message=f"Error loading collection {table_name}: {e}",
+            )
+        # Find vector filter in conditions
+        vector_filter = (
+            []
+            if conditions is None
+            else [
+                condition
+                for condition in conditions
+                if condition.column == TableField.SEARCH_VECTOR.value
+            ]
+        )
+        # NOTE: According to api offset and limit should be less than 16384.
+        api_limit = 16384
+        if limit is not None and offset is not None and limit + offset >= api_limit:
+            return Response(
+                resp_type=RESPONSE_TYPE.ERROR,
+                error_message=f"Sum of limit and offset should be less than {api_limit}",
+            )
+
+        search_arguments = {
+            "data": [vector_filter],  # search vector
+            # name of the field (vector) to search on
+            "anns_field": TableField.EMBEDDINGS.value,
+            "param": self._search_params,
+            "expr": None,  # boolean filters
+            # cols (in metadata for mindsdb, table for milvus) to return
+            "output_fields": ['title'],
+        }
+        if limit is not None:
+            search_arguments["limit"] = limit
+        if offset is not None:
+            search_arguments["param"]["offset"] = offset
+
+        results = collection.search(**search_arguments)
+
+        """
+        # get the IDs of all returned hits
+        results[0].ids
+        # get the distances to the query vector from all returned hits
+        results[0].distances
+        # get the value of an output field specified in the search request.
+        hit = results[0][0]
+        hit.entity.get('title')
+        """
+
         collection = self._client.get_collection(table_name)
         filters = self._translate_metadata_condition(conditions)
         # check if embedding vector filter is present
@@ -241,6 +228,78 @@ class MilvusHandler(VectorStoreHandler):
             payload[TableField.DISTANCE.value] = distances
         result_df = pd.DataFrame(payload)
         return Response(resp_type=RESPONSE_TYPE.TABLE, data_frame=result_df)
+
+    def _get_chromadb_operator(self, operator: FilterOperator) -> str:
+        mapping = {
+            FilterOperator.EQUAL: "$eq",
+            FilterOperator.NOT_EQUAL: "$ne",
+            FilterOperator.LESS_THAN: "$lt",
+            FilterOperator.LESS_THAN_OR_EQUAL: "$lte",
+            FilterOperator.GREATER_THAN: "$gt",
+            FilterOperator.GREATER_THAN_OR_EQUAL: "$gte",
+        }
+
+        if operator not in mapping:
+            raise Exception(f"Operator {operator} is not supported by Milvus!")
+
+        return mapping[operator]
+
+    def _translate_metadata_condition(
+        self, conditions: List[FilterCondition]
+    ) -> Optional[dict]:
+        """
+        Translate a list of FilterCondition objects a dict that can be used by Milvus.
+        E.g.,
+        [
+            FilterCondition(
+                column="metadata.created_at",
+                op=FilterOperator.LESS_THAN,
+                value="2020-01-01",
+            ),
+            FilterCondition(
+                column="metadata.created_at",
+                op=FilterOperator.GREATER_THAN,
+                value="2019-01-01",
+            )
+        ]
+        -->
+        {
+            "$and": [
+                {"created_at": {"$lt": "2020-01-01"}},
+                {"created_at": {"$gt": "2019-01-01"}}
+            ]
+        }
+        """
+        # we ignore all non-metadata conditions
+        if conditions is None:
+            return None
+        metadata_conditions = [
+            condition
+            for condition in conditions
+            if condition.column.startswith(TableField.METADATA.value)
+        ]
+        if len(metadata_conditions) == 0:
+            return None
+
+        # we translate each metadata condition into a dict
+        chroma_db_conditions = []
+        for condition in metadata_conditions:
+            metadata_key = condition.column.split(".")[-1]
+            chroma_db_conditions.append(
+                {
+                    metadata_key: {
+                        self._get_chromadb_operator(condition.op): condition.value
+                    }
+                }
+            )
+
+        # we combine all metadata conditions into a single dict
+        metadata_condition = (
+            {"$and": chroma_db_conditions}
+            if len(chroma_db_conditions) > 1
+            else chroma_db_conditions[0]
+        )
+        return metadata_condition
 
     def insert(
         self, table_name: str, data: pd.DataFrame, columns: List[str] = None

6005895756637505a1675892e560d4bbf5d2d141
Pavlo Shushkov <pavloshushkov@gmail.com>
2023-10-10 16:58:39 +0300

    cleanup(flake8): clean datastax_handler & db2_handler

--- mindsdb/integrations/handlers/datastax_handler/datastax_handler.py
+++ mindsdb/integrations/handlers/datastax_handler/datastax_handler.py
@@ -1,10 +1,12 @@
 from ..scylla_handler import Handler as ScyllaHandler, connection_args
 
+
 class DatastaxHandler(ScyllaHandler):
     """
     This handler handles connection and execution of the Datastax Astra DB statements.
     """
-    name = 'astra'
+
+    name = "astra"
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)

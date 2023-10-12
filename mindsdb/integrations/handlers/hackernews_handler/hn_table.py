66d5be02a17bf370a7002a0d70e9d25db758aee9
harshdeswal <harshdeswal03@yahoo.com>
2023-10-12 14:37:08 +0530

    implemented hackernews askstories endpoint

--- mindsdb/integrations/handlers/hackernews_handler/hn_table.py
+++ mindsdb/integrations/handlers/hackernews_handler/hn_table.py
@@ -77,6 +77,78 @@ class StoriesTable(APITable):
                 columns.append(target.value)
         df = df[columns]
         return df
+    
+class HNStoriesTable(APITable):
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """Select data from the stories table and return it as a pandas DataFrame.
+        Args:
+            query (ast.Select): The SQL query to be executed.
+        Returns:
+            pandas.DataFrame: A pandas DataFrame containing the selected data.
+        """
+        hn_handler = self.handler
+
+        # Extract the limit value from the SQL query, if it exists
+        limit = None
+        if query.limit is not None:
+            limit = query.limit.value
+
+        # Call the Hacker News API to get the top stories
+        url = f'{hn_handler.base_url}/askstories.json'
+        response = requests.get(url)
+        data = response.json()
+
+        # Fetch the details of the HN stories, up to the specified limit
+        stories_data = []
+        for story_id in data[:limit]:
+            url = f'{hn_handler.base_url}/item/{story_id}.json'
+            response = requests.get(url)
+            story_data = response.json()
+            stories_data.append(story_data)
+
+        # Create a DataFrame from the fetched data
+        df = pd.DataFrame(
+            stories_data, 
+            columns=['id', 'time', 'title', 'text', 'score', 'descendants']
+            )
+
+        # Apply any WHERE clauses in the SQL query to the DataFrame
+        conditions = extract_comparison_conditions(query.where)
+        for condition in conditions:
+            if condition[0] == '=' and condition[1] == 'id':
+                df = df[df['id'] == int(condition[2])]
+            elif condition[0] == '>' and condition[1] == 'time':
+                timestamp = int(condition[2])
+                df = df[df['time'] > timestamp]
+
+        # Filter the columns in the DataFrame according to the SQL query
+        self.filter_columns(df, query)
+
+        return df
+
+
+    def get_columns(self):
+        """Get the list of column names for the stories table.
+        Returns:
+            list: A list of column names for the stories table.
+        """
+        return ['id', 'time', 'title', 'text', 'score', 'descendants']
+
+    def filter_columns(self, df, query):
+        """Filter the columns in the DataFrame according to the SQL query.
+        Args:
+            df (pandas.DataFrame): The DataFrame to filter.
+            query (ast.Select): The SQL query to apply to the DataFrame.
+        """
+        columns = []
+        for target in query.targets:
+            if isinstance(target, ast.Star):
+                columns = self.get_columns()
+                break
+            elif isinstance(target, ast.Identifier):
+                columns.append(target.value)
+        df = df[columns]
+        return df
 
 
 class CommentsTable(APITable):

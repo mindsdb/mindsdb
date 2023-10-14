18d5a242b3464c4d4eb0a1d42623268415e42854
bsarthak16 <bsarthak16@gmail.com>
2023-10-14 21:33:01 +0530

    stipe update,delete,insert added

--- mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
+++ mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
@@ -1,3 +1,4 @@
+import stripe
 import pandas as pd
 from typing import Text, List, Dict
 
@@ -47,6 +48,97 @@ class CustomersTable(APITable):
         customers_df = select_statement_executor.execute_query()
 
         return customers_df
+     def insert(self, query: ast.Insert):
+        """
+        Inserts new customer data.
+
+        Parameters
+        ----------
+        query : ast.Insert
+            SQL INSERT query
+
+        Returns
+        -------
+        int
+            The number of rows inserted
+        """
+        values = query.values
+        stripe.api_key = 'stripe_api_key'
+
+        try:
+            customer = stripe.Customer.create(
+                name=values['name'],
+                email=values['email'],
+                # Add other attributes as needed
+            )
+            return 1  
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0  
+
+    def update(self, query: ast.Update):
+        """
+        Updates customer data.
+
+        Parameters
+        ----------
+        query : ast.Update
+            SQL UPDATE query
+
+        Returns
+        -------
+        int
+            The number of rows updated
+        """
+        values = query.values
+        conditions = query.where
+        stripe.api_key = 'your_stripe_api_key'
+
+        try:
+            customer_id = 'customer_id_to_update'  
+            updated_customer = stripe.Customer.modify(
+                customer_id,
+                name=values['name'],
+                email=values['email'],
+                # Add other attributes as needed
+            )
+            return 1 
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0  
+        
+
+    def delete(self, query: ast.Delete):
+        """
+        Deletes customer data.
+
+        Parameters
+        ----------
+        query : ast.Delete
+            SQL DELETE query
+
+        Returns
+        -------
+        int
+            The number of rows deleted
+        """
+        values = query.values
+        conditions = query.where
+        stripe.api_key = 'your_stripe_api_key'
+
+        try:
+            customer_id = 'customer_id_to_update' 
+            updated_customer = stripe.Customer.modify(
+                customer_id,
+                name=values['name'],
+                email=values['email'],
+                # Add other attributes as needed
+            )
+            return 1
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0  
+
 
     def get_columns(self) -> List[Text]:
         return pd.json_normalize(self.get_customers(limit=1)).columns.tolist()
@@ -97,7 +189,87 @@ class ProductsTable(APITable):
         products_df = select_statement_executor.execute_query()
 
         return products_df
+    def insert(self, query: ast.Insert):
+        """
+        Inserts new product data.
 
+        Parameters
+        ----------
+        query : ast.Insert
+            SQL INSERT query
+
+        Returns
+        -------
+        int
+            The number of rows inserted
+        """
+        values = query.values
+        stripe.api_key = 'your_stripe_api_key'
+
+        try:
+            new_product = stripe.Product.create(
+                name=values['name'],
+                description=values['description'],
+            )
+            return 1 
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0  
+
+    def update(self, query: ast.Update):
+        """
+        Updates product data.
+
+        Parameters
+        ----------
+        query : ast.Update
+            SQL UPDATE query
+
+        Returns
+        -------
+        int
+            The number of rows updated
+        """
+        values = query.values
+        conditions = query.where
+        stripe.api_key = 'your_stripe_api_key'
+
+        try:
+            product_id = 'product_id_to_update'  
+            updated_product = stripe.Product.modify(
+                product_id,
+                name=values['name'],
+                description=values['description'],
+            )
+            return 1  
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0
+    def delete(self, query: ast.Delete):
+        """
+        Deletes product data.
+
+        Parameters
+        ----------
+        query : ast.Delete
+            SQL DELETE query
+
+        Returns
+        -------
+        int
+            The number of rows deleted
+        """
+        conditions = query.where
+        stripe.api_key = 'your_stripe_api_key'
+
+        try:
+            product_id = 'product_id_to_delete' 
+            deleted_product = stripe.Product.delete(product_id)
+            return 1 
+        except stripe.error.StripeError as e:
+            print(f"Stripe Error: {e}")
+            return 0 
+    
     def get_columns(self) -> List[Text]:
         return pd.json_normalize(self.get_products(limit=1)).columns.tolist()
 

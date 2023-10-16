0ad1c2e4f3f9b6d3b957d88bb6d5c4549ce08389
Aditya Azad <adityaazad121@gmail.com>
2023-10-16 16:59:55 -0400

    Change implementation to use MySQL handler

--- mindsdb/integrations/handlers/apache_doris_handler/__init__.py
+++ mindsdb/integrations/handlers/apache_doris_handler/__init__.py
@@ -1,16 +1,13 @@
 from mindsdb.integrations.libs.const import HANDLER_TYPE
 
-from .__about__ import __version__ as version, __description__ as description
 try:
-    from .apache_doris_handler import (
-        ApacheDorisHandler as Handler,
-        connection_args_example,
-        connection_args
-    )
+    from .apache_doris_handler import ApacheDorisHandler as Handler
     import_error = None
 except Exception as e:
     Handler = None
     import_error = e
+from .__about__ import __version__ as version, __description__ as description
+
 
 title = 'Apache Doris'
 name = 'apache_doris'
@@ -18,14 +15,6 @@ type = HANDLER_TYPE.DATA
 icon_path = 'icon.png'
 
 __all__ = [
-    'Handler',
-    'version',
-    'name',
-    'type',
-    'title',
-    'description',
-    'connection_args',
-    'connection_args_example',
-    'import_error',
-    'icon_path'
+    'Handler', 'version', 'name', 'type', 'title', 'description',
+    'import_error', 'icon_path'
 ]

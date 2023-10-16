bbef728896f3fd14c877a593f72c708ebf7c8def
Lazy Monk <145913756+lazyMonk1010@users.noreply.github.com>
2023-10-17 04:39:56 +0530

    Updated darts_handler.py
    Updated darts handler.py to addresses the suggested issue

--- mindsdb/integrations/handlers/darts_handler/darts_handler.py
+++ mindsdb/integrations/handlers/darts_handler/darts_handler.py
@@ -1,71 +1,86 @@
 import pandas as pd
 from typing import Optional, Dict
 from darts import TimeSeries
-from darts.models import ARIMA
+from darts.models import AutoARIMA
 from mindsdb.integrations.libs.base import BaseMLEngine
-from mindsdb.integrations.libs.response import HandlerResponse
 
 class DartsModel(BaseMLEngine):
-    
+
     name = "dartsmodel"
 
-    def _init_(self, model_storage, engine_storage) -> None:
-        super()._init_(model_storage, engine_storage)
+    def __init__(self, model_storage, engine_storage) -> None:
+        super().__init__(model_storage, engine_storage)
 
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
         """
-        Train an ARIMA model and save it for later usage.
+        Train an AutoARIMA model and save it for later usage.
 
         Args:
             target (str): Name of the target column.
             df (pd.DataFrame): Input DataFrame containing the time series data.
             args (Dict): Additional arguments (if needed).
         """
-        # Ensure df contains a datetime index
-        df.set_index('timestamp', inplace=True)
+
+        # Find the timestamp column in the DataFrame
+        timestamp_column = None
+        for col in df.columns:
+            if pd.api.types.is_datetime_dtype(df[col]):
+                timestamp_column = col
+                break
+
+        if timestamp_column is None:
+            raise Exception('No datetime column found in the DataFrame.')
+
+        # Set the timestamp column as the index
+        df.set_index(timestamp_column, inplace=True)
 
         # Convert DataFrame to Darts TimeSeries
-        time_series = TimeSeries.from_dataframe(df, time_col='timestamp', value_cols=[target])
+        time_series = TimeSeries.from_dataframe(df, time_col=timestamp_column, value_cols=[target])
 
-        # Create and train ARIMA model
-        arima_model = ARIMA(order=(1, 1, 1))  # Can adjust the order as needed
-        arima_model.fit(time_series)
+        auto_arima_model = AutoARIMA()  # AutoARIMA automatically finds the best ARIMA model
+        auto_arima_model.fit(time_series)
 
         # Save the trained model to model_storage
-        self.model_storage.save_model('arima_model', arima_model)
+        self.model_storage.save_model('auto_arima_model', auto_arima_model)
 
-    def predict(self, df: pd.DataFrame, args: Optional[Dict] = None) -> HandlerResponse:
+    def predict(self, df: pd.DataFrame, args: Optional[Dict] = None) -> pd.DataFrame:
         """
-        Make predictions using the trained ARIMA model.
+        Make predictions using the trained AutoARIMA model.
 
         Args:
             df (pd.DataFrame): Input DataFrame containing the time series data for prediction.
             args (Dict): Additional arguments (if needed).
 
         Returns:
-            HandlerResponse: Predicted values in a DataFrame.
+            pd.DataFrame: Predicted values in a DataFrame.
         """
-        # Ensure df contains a datetime index
-        df.set_index('timestamp', inplace=True)
 
-        # Load the trained ARIMA model from model_storage
-        arima_model = self.model_storage.load_model('arima_model')
+        # Find the timestamp column in the DataFrame
+        timestamp_column = None
+        for col in df.columns:
+            if pd.api.types.is_datetime_dtype(df[col]):
+                timestamp_column = col
+                break
 
-        # Convert DataFrame to Darts TimeSeries
-        time_series = TimeSeries.from_dataframe(df, time_col='timestamp')
+        if timestamp_column is None:
+            raise Exception('No datetime column found in the DataFrame.')
+
+        # Set the timestamp column as the index
+        df.set_index(timestamp_column, inplace=True)
+
+        # Load the trained AutoARIMA model from model_storage
+        auto_arima_model = self.model_storage.load_model('auto_arima_model')
+
+        # Convert DataFrame to Darts TimeSeries for future extensions
+        time_series = TimeSeries.from_dataframe(df, time_col=timestamp_column)
 
         # Make predictions
-        predictions = arima_model.predict(len(df))
+        predictions = auto_arima_model.predict(len(df))
 
         # Convert predictions to a DataFrame
         prediction_df = predictions.pd_dataframe()
 
         # Rename the prediction column to match the target column
-        prediction_df = prediction_df.rename(columns={'0': 'target'})
+        prediction_df = prediction_df.rename(columns={predictions.name: 'target'})
 
-        return HandlerResponse(data=prediction_df)
-
-    # You can optionally implement update, describe, and create_engine methods based on your requirements
-
-    def close(self):
-        pass
+        return prediction_df

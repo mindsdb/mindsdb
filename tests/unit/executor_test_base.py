19ea1741697db8886eb31f878000770c3e798b09
Sam Miller <sam@atlasai.co.uk>
2023-10-16 15:30:51 +0100

    rerun CI

--- tests/unit/executor_test_base.py
+++ tests/unit/executor_test_base.py
@@ -124,7 +124,7 @@ class BaseUnitTest:
         r = db.Integration(name="vertex", data={}, engine="vertex")
         db.session.add(r)
 
-        # Lightwood should always be last (else tests break, because of lw_integration_id below)
+        # Lightwood should always be last (else tests break)
         r = db.Integration(name="lightwood", data={}, engine="lightwood")
         db.session.add(r)
 

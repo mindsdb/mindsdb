3888d708436be5d0eefee008ee37306d4436f0bd
Abhi <abhi@Abhis-MacBook-Air.local>
2023-10-17 23:05:14 +0530

    dockerhub repo tags implementation

--- mindsdb/integrations/handlers/dockerhub_handler/dockerhub.py
+++ mindsdb/integrations/handlers/dockerhub_handler/dockerhub.py
@@ -37,3 +37,7 @@ class DockerHubClient:
     def get_images_summary(self, namespace, repo):
         url = f'{self.docker_hub_base_endpoint}namespaces/{namespace}/repositories/{repo}/images-summary'
         return self.make_request(url)
+    
+    def get_repo_tags(self, namespace, repo):
+        url = f'{self.docker_hub_base_endpoint}namespaces/{namespace}/repositories/{repo}/tags'
+        return self.make_request(url)
--- mindsdb/integrations/handlers/dockerhub_handler/dockerhub_tables.py
+++ mindsdb/integrations/handlers/dockerhub_handler/dockerhub_tables.py
@@ -100,3 +100,106 @@ class DockerHubRepoImagesSummaryTable(APITable):
             "active",
             "inactive"
         ]
+
+class DockerHubRepoTagsTable(APITable):
+    """The DockerHub Repo Tags Table implementation"""
+
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """Pulls data from the https://hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags" API
+
+        Parameters
+        ----------
+        query : ast.Select
+           Given SQL SELECT query
+
+        Returns
+        -------
+        pd.DataFrame
+            Repo Tag matching the query
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+
+        select_statement_parser = SELECTQueryParser(
+            query,
+            'repo_tags',
+            self.get_columns()
+        )
+
+        selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
+
+        search_params = {}
+        subset_where_conditions = []
+        for op, arg1, arg2 in where_conditions:
+            if arg1 == 'namespace':
+                if op == '=':
+                    search_params["namespace"] = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for namespace column.")
+            elif arg1 == 'repository':
+                if op == '=':
+                    search_params["repository"] = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for repository column.")
+            elif arg1 in self.get_columns():
+                subset_where_conditions.append([op, arg1, arg2])
+
+        filter_flag = ("namespace" in search_params) and ("repository" in search_params)
+
+        if not filter_flag:
+            raise NotImplementedError("namespace and repository column has to be present in where clause.")
+
+        repo_tags_summary_df = pd.DataFrame(columns=self.get_columns())
+
+        response = self.handler.docker_client.get_repo_tags(search_params["namespace"], search_params["repository"])
+
+        self.check_res(res=response)
+
+        content = response["content"]
+
+        repo_tags_summary_df = pd.json_normalize(content["results"])
+
+        select_statement_executor = SELECTQueryExecutor(
+            repo_tags_summary_df,
+            selected_columns,
+            subset_where_conditions,
+            order_by_conditions,
+            result_limit
+        )
+
+        repo_tags_summary_df = select_statement_executor.execute_query()
+
+        return repo_tags_summary_df
+
+    def check_res(self, res):
+        if res["code"] != 200:
+            raise Exception("Error fetching results - " + res["error"])
+
+    def get_columns(self) -> List[str]:
+        """Gets all columns to be returned in pandas DataFrame responses
+
+        Returns
+        -------
+        List[str]
+            List of columns
+        """
+
+        return ["creator", 
+                "id", 
+                "images", 
+                "last_updated", 
+                "last_updater", 
+                "last_updater_username", 
+                "name", 
+                "repository", 
+                "full_size", 
+                "v2", 
+                "tag_status", 
+                "tag_last_pulled", 
+                "tag_last_pushed", 
+                "media_type", 
+                "content_type"
+            ]

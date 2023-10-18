6ca70b837040a4b7b527f7dd6fe8fb7559349cb4
Sam Miller <sam@atlasai.co.uk>
2023-10-18 17:43:57 +0100

    set permanent to false

--- mindsdb/integrations/handlers/vertex_handler/__init__.py
+++ mindsdb/integrations/handlers/vertex_handler/__init__.py
@@ -13,6 +13,6 @@ except Exception as e:
 title = "Vertex"
 name = "vertex"
 type = HANDLER_TYPE.ML
-permanent = True
+permanent = False
 
 __all__ = ["Handler", "version", "name", "type", "title", "description", "import_error"]

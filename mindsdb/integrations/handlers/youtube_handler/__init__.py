c0c6bccafebc036dabd11b6222f4487e9a4b8514
Aryan Parashar <aryanparashar24@gmail.com>
2023-10-19 23:07:54 +0530

    Update __init__.py
    Added transcript in the _about_.py file of the youtube_handler repo

--- mindsdb/integrations/handlers/youtube_handler/__init__.py
+++ mindsdb/integrations/handlers/youtube_handler/__init__.py
@@ -1,9 +1,19 @@
 from mindsdb.integrations.libs.const import HANDLER_TYPE
 from mindsdb.utilities.log import get_log
+from youtube_transcript_api import YouTubeTranscriptApi
 
 logger = get_log()
 
 from .__about__ import __version__ as version, __description__ as description
+
+class YoutubeHandler:
+    def get_transcript(self, video_id):
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            return transcript
+        except Exception as e:
+            logger.error(f"Error retrieving transcript for video {video_id}: {e}")
+            return []
 
 try:
     from .youtube_handler import YoutubeHandler as Handler

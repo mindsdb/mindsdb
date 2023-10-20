0f8e0d3efd0e7ad85fee230a455dd360dfc760a7
Aditya Azad <adityaazad121@gmail.com>
2023-10-20 18:35:13 -0400

    Create structure of handler

--- mindsdb/integrations/handlers/lightdash_handler/lightdash_handler.py
+++ mindsdb/integrations/handlers/lightdash_handler/lightdash_handler.py
@@ -1,7 +1,8 @@
 from mindsdb_sql import parse_sql
 
-from mindsdb.integrations.handlers.lightdash_handler.api import PyPI
-from mindsdb.integrations.handlers.lightdash_handler.pypi_tables import (
+from mindsdb.integrations.handlers.lightdash_handler.api import Lightdash
+from mindsdb.integrations.handlers.lightdash_handler.lightdash_tables import (
+    # TODO: Create tables
 )
 from mindsdb.integrations.libs.api_handler import APIHandler
 from mindsdb.integrations.libs.response import HandlerStatusResponse as StatusResponse
@@ -10,60 +11,31 @@ from mindsdb.integrations.libs.response import HandlerStatusResponse as StatusRe
 class LightdashHandler(APIHandler):
 
     def __init__(self, name: str, **kwargs) -> None:
-        """initializer method
-
-        Args:
-            name (str): handler's name
-        """
         super().__init__(name)
-
         self.connection = None
         self.is_connected = False
-
+        self.api_key = kwargs.get("connection_data", {}).get("api_key", "")
+        self.base_url = kwargs.get("connection_data", {}).get("base_url", "")
         _tables = [
-            PyPIOverallTable,
-            PyPIPythonMajorTable,
-            PyPIPythonMinorTable,
-            PyPIRecentTable,
-            PyPISystemTable,
+            # TODO: Create tables
         ]
-
         for Table in _tables:
             self._register_table(Table.name, Table(self))
+        self.connect()
+
+    def connect(self) -> Lightdash:
+        self.connection = Lightdash(self.base_url, self.api_key)
+        return self.connection
 
     def check_connection(self) -> StatusResponse:
         response = StatusResponse(False)
-        checking = PyPI.is_connected()
-        if checking["status"]:
-            response.success = True
-        else:
-            response.error_message = checking["message"]
-
-        self.is_connected = True
-
-        return response
-
-    def connect(self) -> PyPI:
-        """making the connectino object
-
-        Returns:
-            PyPI: pypi class as the returned value
-        """
-        self.connection = PyPI
-        return self.connection
+        if self.connection and not self.connection.is_connected():
+            return Response(
+                RESPONSE_TYPE.ERROR,
+                error_message="Client not connected"
+            )
+        return Response(RESPONSE_TYPE.OK)
 
     def native_query(self, query: str) -> StatusResponse:
-        """Receive and process a raw query.
-
-        Parameters
-        ----------
-        query : str
-            query in a native format
-
-        Returns
-        -------
-        StatusResponse
-            Request status
-        """
         ast = parse_sql(query, dialect="mindsdb")
         return self.query(ast)

a46f43a549f88bda631ce512ff1b076f3415b848
Tanmai Kamat <40868910+Tanmai2002@users.noreply.github.com>
2023-10-21 15:41:54 +0000

    fix: Optimized the Advance Query

--- mindsdb/integrations/handlers/influxdb_handler/influxdb_tables.py
+++ mindsdb/integrations/handlers/influxdb_handler/influxdb_tables.py
@@ -30,24 +30,23 @@ class InfluxDBTables(APITable):
             If the query contains an unsupported condition
         """
         
-        influxdb_tables_df = self.handler.call_influxdb_tables()
+        table_name=self.handler.connection_data['influxdb_table_name']
+        head_influx_table = self.handler.call_influxdb_tables(f"SELECT * FROM {table_name} LIMIT 1")
         select_statement_parser = SELECTQueryParser(
             query,
-            self.handler.connection_data['influxdb_table_name'],
-            self.get_columns(influxdb_tables_df)
+            "tables",
+            self.get_columns(head_influx_table)
         )
-        selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
-
-        select_statement_executor = SELECTQueryExecutor(
-            influxdb_tables_df,
-            selected_columns,
-            where_conditions,
-            order_by_conditions,
-            result_limit
-        )
-        influxdb_tables_df  = select_statement_executor.execute_query()
-
+        selected_columns, where_conditions, order_by_conditions, _ = select_statement_parser.parse_query()
         
+        try:
+            selected_columns.remove("name")
+            selected_columns.remove("tags")
+        except Exception as e:
+            logger.warn(e)
+
+        formatted_query=self.get_select_query(table_name,selected_columns,where_conditions,order_by_conditions,query.limit)
+        influxdb_tables_df  = self.handler.call_influxdb_tables(formatted_query)
 
         return influxdb_tables_df
 
@@ -60,3 +59,22 @@ class InfluxDBTables(APITable):
         """
 
         return list(dataframe.columns)
+    def get_select_query(self,table_name,selected_columns, where_conditions, order_by_conditions, result_limit):
+        """Gets Well formed Query
+        Returns
+        -------
+        str
+        """
+        columns=", ".join([f'"{column}"' for column in selected_columns])
+        query=f'SELECT {columns} FROM "{table_name}"'
+        if(where_conditions is not None and len(where_conditions)>0):
+            query+=" WHERE "
+            query+=" AND ".join([f"{i[1]} {i[0]} {i[2]}" for i in where_conditions])
+        if (order_by_conditions!={} and order_by_conditions['columns'] is not None and len(order_by_conditions['columns'])>0):
+            query+=" ORDER BY "
+            query+=", ".join([f'{column_name} {"ASC"if asc else "DESC"}' for column_name,asc in zip(order_by_conditions['columns'],order_by_conditions['ascending'])] )
+        if(result_limit is not None):
+            query+=f" LIMIT {result_limit}"
+        query+=";"
+        return query
+

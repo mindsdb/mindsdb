047ad5f52ae81c4a6990867e67162c55e539b4b6
Varun Dalal <varun_dalal@epam.com>
2023-10-21 06:31:45 +0530

    Add Shared Exceptions for APIHandler

--- mindsdb/__init__.py
+++ mindsdb/__init__.py
@@ -1,1 +1,4 @@
 from mindsdb.__about__ import __version__  # noqa: F401
+from mindsdb.integrations.handlers import exceptions
+
+__all__ = [__version__, exceptions]
---
+++ mindsdb/exceptions.py
@@ -0,0 +1,14 @@
+class TableNotFound(Exception):
+    pass
+
+class ConnectionFailed(Exception):
+    pass
+
+class InvalidNativeQuery(Exception):
+    pass
+
+class TableAlreadyExists(Exception):
+    pass
+
+class MissingConnectionParams(Exception):
+    pass

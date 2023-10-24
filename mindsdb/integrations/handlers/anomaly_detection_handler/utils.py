a1cf34275ac788d0cd04485ded1d9a693223aea2
Sam Miller <sam@atlasai.co.uk>
2023-10-24 14:58:54 +0100

    remove untested code

--- mindsdb/integrations/handlers/anomaly_detection_handler/utils.py
+++ mindsdb/integrations/handlers/anomaly_detection_handler/utils.py
@@ -20,19 +20,3 @@ def train_supervised(X_train, y_train):
     clf = CatBoostClassifier(logging_level="Silent")
     clf.fit(X_train, y_train)
     return clf
-
-
-def get_predictions(clf, X, model_is_pyod):
-    y_pred = clf.predict(X)
-    if model_is_pyod:
-        y_scores = clf.decision_function(X)
-    else:
-        y_scores = clf.predict_proba(X)[:, 1]
-    return y_pred, y_scores
-
-
-def print_results(classifier_name, y_train, y_test, y_train_scores, y_test_scores):
-    print("\nOn Training Data:")
-    evaluate_print(classifier_name, y_train, y_train_scores)
-    print("\nOn Test Data:")
-    evaluate_print(classifier_name, y_test, y_test_scores)

498753f0d7d3ff87f36441ade22d9425f28dd961
sekhar kumar dash <sekharkumardash229@gmail.com>
2023-10-25 23:15:37 +0530

    updated stripe_tables.py

--- mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
+++ mindsdb/integrations/handlers/stripe_handler/stripe_tables.py
@@ -1,10 +1,16 @@
 import pandas as pd
-from typing import Text, List, Dict
-
+import stripe
+from typing import Text, List, Dict, Any
+from mindsdb_sql.parser import ast
+from mindsdb.integrations.libs.api_handler import APITable
+from mindsdb.integrations.handlers.utilities.query_utilities import INSERTQueryParser, DELETEQueryParser, UPDATEQueryParser, DELETEQueryExecutor, UPDATEQueryExecutor
 from mindsdb_sql.parser import ast
 from mindsdb.integrations.libs.api_handler import APITable
 
 from mindsdb.integrations.handlers.utilities.query_utilities.select_query_utilities import SELECTQueryParser, SELECTQueryExecutor
+from mindsdb.utilities.log import get_log
+
+logger = get_log("integrations.stripe_handler")
 
 
 class CustomersTable(APITable):
@@ -109,7 +115,7 @@ class ProductsTable(APITable):
 
 class PaymentIntentsTable(APITable):
     """The Stripe Payment Intents Table implementation"""
-
+    
     def select(self, query: ast.Select) -> pd.DataFrame:
         """
         Pulls Stripe Payment Intents data.
@@ -147,6 +153,126 @@ class PaymentIntentsTable(APITable):
         payment_intents_df = select_statement_executor.execute_query()
 
         return payment_intents_df
+    
+   
+
+    def delete(self, query: ast.Delete) -> None:
+        """
+        Deletes data from the Stripe "DELETE /v1/payment_intents/:id" API endpoint.
+
+        Parameters
+        ----------
+        query : ast.Delete
+           Given SQL DELETE query
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+        delete_statement_parser = DELETEQueryParser(query)
+        where_conditions = delete_statement_parser.parse_query()
+
+        payment_intents_df = pd.json_normalize(self.get_payment_intents())
+
+        delete_query_executor = DELETEQueryExecutor(
+            payment_intents_df,
+            where_conditions
+        )
+
+        payment_intents_df = delete_query_executor.execute_query()
+
+        payment_intent_ids = payment_intents_df['id'].tolist()
+        self.delete_payment_intents(payment_intent_ids)
+        
+    def delete_payment_intents(self, payment_intent_ids: list) -> None:
+        for payment_intent_id in payment_intent_ids:
+         stripe.PaymentIntent.delete(payment_intent_id)
+
+    
+    def update(self, query: 'ast.Update') -> None:
+        """
+        Updates data in Stripe "POST /v1/payment_intents/:id" API endpoint.
+
+        Parameters
+        ----------
+        query : ast.Update
+            Given SQL UPDATE query
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+        update_statement_parser = UPDATEQueryParser(query)
+        values_to_update, where_conditions = update_statement_parser.parse_query()
+
+        payment_intents_df = pd.json_normalize(self.get_payment_intents())
+        update_query_executor = UPDATEQueryExecutor(
+            payment_intents_df,
+            where_conditions
+        )
+
+        payment_intents_df = update_query_executor.execute_query()
+        payment_intent_ids = payment_intents_df['id'].tolist()
+        self.update_payment_intents(payment_intent_ids, values_to_update)
+
+    def update_payment_intents(self, payment_intent_ids: list, values_to_update: dict) -> None:
+        for payment_intent_id in payment_intent_ids:
+            stripe.PaymentIntent.modify(payment_intent_id, **values_to_update)
+
+
+    def get_columns(self) -> List[Text]:
+        return pd.json_normalize(self.get_payment_intents(limit=1)).columns.tolist()
+
+    def get_payment_intents(self, **kwargs) -> List[Dict]:
+        payment_intents = stripe.PaymentIntent.list(**kwargs)
+        return [payment_intent for payment_intent in payment_intents.data]
+
+    
+    
+    def insert(self, query: 'ast.Insert') -> None:
+        """
+        Inserts data into Stripe "POST /v1/payment_intents" API endpoint.
+
+        Parameters
+        ----------
+        query : ast.Insert
+            Given SQL INSERT query
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the query contains an unsupported condition
+        """
+        insert_statement_parser = INSERTQueryParser(
+            query,
+            supported_columns=['amount', 'currency', 'description', 'payment_method_types'],
+            mandatory_columns=['amount', 'currency'],
+            all_mandatory=True
+        )
+        payment_intent_data = insert_statement_parser.parse_query()
+        self.create_payment_intent(payment_intent_data)
+
+    def create_payment_intent(self, payment_intent_data: list) -> None:
+        for data in payment_intent_data:
+            stripe.PaymentIntent.create(**data)
+        
+
+    
+
 
     def get_columns(self) -> List[Text]:
         return pd.json_normalize(self.get_payment_intents(limit=1)).columns.tolist()

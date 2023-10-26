8499f596c179c8e37da0384ea546da09c6f6b8ab
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-10-26 11:40:41 +0100

    fix merge conflicts

--- mindsdb/integrations/handlers/chromadb_handler/chromadb_handler.py
+++ mindsdb/integrations/handlers/chromadb_handler/chromadb_handler.py
@@ -1,4 +1,3 @@
-import sys
 from collections import OrderedDict
 from typing import List, Optional
 
@@ -17,6 +16,7 @@ from mindsdb.integrations.libs.vectordatabase_handler import (
 )
 from mindsdb.utilities import log
 
+import sys
 __import__("pysqlite3")
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb  # noqa: E402

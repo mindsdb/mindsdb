2afd019439283956328fc2cb46b36c3c4bd5a33e
Emmanuel Excellent <68669639+Daddypastor@users.noreply.github.com>
2023-10-28 04:55:30 +0100

    Update paypal_tables.py
    Implement the Payout table for paypal

--- mindsdb/integrations/handlers/paypal_handler/paypal_tables.py
+++ mindsdb/integrations/handlers/paypal_handler/paypal_tables.py
@@ -6,7 +6,7 @@ from mindsdb_sql.parser import ast
 from mindsdb.integrations.libs.api_handler import APITable
 
 from mindsdb.integrations.handlers.utilities.query_utilities import SELECTQueryParser, SELECTQueryExecutor
-
+from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
 
 class PaymentsTable(APITable):
 
@@ -111,21 +111,19 @@ class SubscriptionsTable(APITable):
         subscriptions = paypalrestsdk.BillingPlan.all(kwargs, api=connection)
         return [subscription.to_dict() for subscription in subscriptions['plans']]
 
-
-#The task is to extend this implementation to include the Payouts table.
 class PayoutsTable(APITable):
 
     def select(self, query: ast.Select) -> pd.DataFrame:
         """
-        Pulls PayPal Payouts data.
+        Pulls PayPal payouts data.
         Parameters
         ----------
         query : ast.Select
-           Given SQL SELECT query
+            Given SQL SELECT query
         Returns
         -------
         pd.DataFrame
-            PayPal Payouts matching the query
+            PayPal payouts matching the query
         Raises
         ------
         ValueError
@@ -139,22 +137,65 @@ class PayoutsTable(APITable):
         )
         selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
 
-        payouts_df = pd.json_normalize(self.get_payouts(count=result_limit))
+        conditions = extract_comparison_conditions(query.where)
+
+        payout_batch_id = ""
+
+        for a_where in conditions:
+            if a_where[1] == "payout_batch_id":
+                if a_where[0] != "=":
+                    raise ValueError("Unsupported where operation for state")
+
+                payout_batch_id = a_where[2]
+
+        payouts_data = self.get_payout(payout_batch_id)  # Get the data
+        payouts_df = pd.DataFrame(payouts_data)  # Create a DataFrame
+
         select_statement_executor = SELECTQueryExecutor(
             payouts_df,
             selected_columns,
             where_conditions,
             order_by_conditions
         )
+
         payouts_df = select_statement_executor.execute_query()
 
         return payouts_df
 
     def get_columns(self) -> List[Text]:
-        return pd.json_normalize(self.get_payouts(count=1)).columns.tolist()
+        return [
+            "payout_batch_id",
+            "batch_status",
+            "time_created",
+            "time_completed",
+            "sender_batch_id",
+            "email_subject",
+            "email_message",
+            "funding_source",
+            "amount_currency",
+            "amount_value",
+            "fees_currency",
+            "fees_value",
+        ]
 
-    def get_payouts(self, **kwargs) -> List[Dict]:
+    def get_payout(self, payout_batch_id:str) -> List[Dict]:
         connection = self.handler.connect()
-        payouts = paypalrestsdk.Payout.find('12345678', api=connection)
-        return [payout.to_dict() for payout in payouts['payout']]
+        endpoint = f"v1/payments/payouts/{payout_batch_id}"
+        payout = connection.get(endpoint)
 
+        payout_data = {
+            "payout_batch_id": payout['batch_header']['payout_batch_id'],
+            "batch_status": payout['batch_header']['batch_status'],
+            "time_created": payout['batch_header']['time_created'],
+            "time_completed": payout['batch_header']['time_completed'],
+            "sender_batch_id": payout['batch_header']['sender_batch_header']['sender_batch_id'],
+            "email_subject": payout['batch_header']['sender_batch_header']['email_subject'],
+            "email_message": payout['batch_header']['sender_batch_header']['email_message'],
+            "funding_source": payout['batch_header']['funding_source'],
+            "amount_currency": payout['batch_header']['amount']['currency'],
+            "amount_value": payout['batch_header']['amount']['value'],
+            "fees_currency": payout['batch_header']['fees']['currency'],
+            "fees_value": payout['batch_header']['fees']['value'],
+        }
+        
+        return [payout_data]

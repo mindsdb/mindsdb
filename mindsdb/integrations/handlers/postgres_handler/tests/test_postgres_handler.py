f223150b4b1710fd3b4d2631b30717c50db06a66
Tori Green <tori@neonsurge.com>
2023-10-31 12:39:13 +1300

    Postgres Handler Tests

--- mindsdb/integrations/handlers/postgres_handler/tests/test_postgres_handler.py
+++ mindsdb/integrations/handlers/postgres_handler/tests/test_postgres_handler.py
@@ -58,8 +58,21 @@ def handler(request):
     container.kill()
     docker_client.close()
 
+def get_table_names(handler):
+    res = handler.get_tables()
+    tables = res.data_frame
+    assert tables is not None, "expected to have some tables in the db, but got None"
+    assert 'table_name' in tables, f"expected to get 'table_name' column in the response:\n{tables}"
+    return list(tables['table_name'])
 
-class TestPostgresHandler:
+def check_valid_response(res):
+    if res.resp_type == RESPONSE_TYPE.TABLE:
+        assert res.data_frame is not None, "expected to have some data, but got None"
+    assert res.error_code == 0, f"expected to have zero error_code, but got {res.error_code}"
+    assert res.error_message is None, f"expected to have None in error message, but got {res.error_message}"
+
+
+class TestPostgresConnection:
     def test_connect(self, handler):
         handler.connect()
         assert handler.is_connected, "connection error"
@@ -68,6 +81,8 @@ class TestPostgresHandler:
         res = handler.check_connection()
         assert res.success, res.error_message
 
+
+class TestPostgresQuery:
     def test_native_query_show_dbs(self, handler):
         dbs = handler.native_query("SELECT datname FROM pg_database;")
         dbs = dbs.data_frame
@@ -77,52 +92,54 @@ class TestPostgresHandler:
         expected_db = HANDLER_KWARGS["connection_data"]["database"]
         assert expected_db in dbs, f"expected to have {expected_db} db in response: {dbs}"
 
+    def test_select_query(self, handler):
+        limit = 5
+        query = f"SELECT * FROM rentals WHERE number_of_rooms = 2 LIMIT {limit}"
+        res = handler.query(query)
+        check_valid_response(res)
+        got_rows = res.data_frame.shape[0]
+        want_rows = limit
+        assert got_rows == want_rows, f"expected to have {want_rows} rows in response but got: {got_rows}"
+
+
+class TestPostgresTables:
     def test_get_tables(self, handler):
-        tables = self.get_table_names(handler)
+        tables = get_table_names(handler)
         assert "rentals" in tables, f"expected to have 'rentals' table in the db but got: {tables}"
 
-    def test_describe_table(self, handler):
+    def test_create_table(self, handler):
+        new_table = "test_mdb"
+        res = handler.native_query(f"CREATE TABLE IF NOT EXISTS {new_table} (test_col INT)")
+        check_valid_response(res)
+        tables = get_table_names(handler)
+        assert new_table in tables, f"expected to have {new_table} in database, but got: {tables}"
+
+    def test_drop_table(self, handler):
+        drop_table = "test_md"
+        res = handler.native_query(f"DROP TABLE IF EXISTS {drop_table}")
+        check_valid_response(res)
+        tables = get_table_names(handler)
+        assert drop_table not in tables
+
+
+class TestPostgresColumns:
+    def test_get_columns(self, handler):
         described = handler.get_columns("rentals")
         describe_data = described.data_frame
-        self.check_valid_response(described)
+        check_valid_response(described)
         got_columns = list(describe_data.iloc[:, 0])
         want_columns = ["number_of_rooms", "number_of_bathrooms",
                         "sqft", "location", "days_on_market",
                         "initial_price", "neighborhood", "rental_price"]
         assert sorted(got_columns) == sorted(want_columns), f"expected to have next columns in rentals table:\n{want_columns}\nbut got:\n{got_columns}"
 
-    def test_create_table(self, handler):
-        new_table = "test_mdb"
-        res = handler.native_query(f"CREATE TABLE IF NOT EXISTS {new_table} (test_col INT)")
-        self.check_valid_response(res)
-        tables = self.get_table_names(handler)
-        assert new_table in tables, f"expected to have {new_table} in database, but got: {tables}"
 
-    def test_drop_table(self, handler):
-        drop_table = "test_md"
-        res = handler.native_query(f"DROP TABLE IF EXISTS {drop_table}")
-        self.check_valid_response(res)
-        tables = self.get_table_names(handler)
-        assert drop_table not in tables
+class TestPostgresDisconnect:
+    def test_disconnect(self, handler):
+        handler.disconnect()
+        assert handler.is_connected == False, "failed to disconnect"
 
-    def test_select_query(self, handler):
-        limit = 5
-        query = f"SELECT * FROM rentals WHERE number_of_rooms = 2 LIMIT {limit}"
-        res = handler.query(query)
-        self.check_valid_response(res)
-        got_rows = res.data_frame.shape[0]
-        want_rows = limit
-        assert got_rows == want_rows, f"expected to have {want_rows} rows in response but got: {got_rows}"
+    def test_check_connection(self, handler):
+        res = handler.check_connection()
+        assert res.success, res.error_message
 
-    def check_valid_response(self, res):
-        if res.resp_type == RESPONSE_TYPE.TABLE:
-            assert res.data_frame is not None, "expected to have some data, but got None"
-        assert res.error_code == 0, f"expected to have zero error_code, but got {res.error_code}"
-        assert res.error_message is None, f"expected to have None in error message, but got {res.error_message}"
-
-    def get_table_names(self, handler):
-        res = handler.get_tables()
-        tables = res.data_frame
-        assert tables is not None, "expected to have some tables in the db, but got None"
-        assert 'table_name' in tables, f"expected to get 'table_name' column in the response:\n{tables}"
-        return list(tables['table_name'])

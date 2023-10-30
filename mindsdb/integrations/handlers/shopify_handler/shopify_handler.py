1090d108e7910b242a83c800ad8e06d80a282d50
dalalvarun <70593335+dalalvarun@users.noreply.github.com>
2023-10-30 17:42:20 +0530

    Feature/7753 shared exceptions for handlers (#8153)
    * Add Shared Exceptions for APIHandler
    
    * Use Exceptions in APIHandler
    
    * Use exceptions in Shopify Handler
    
    * Fix linting issues
    
    * Fix path for exceptions
    
    * Change as per feedback
    
    * Add missing line

--- mindsdb/integrations/handlers/shopify_handler/shopify_handler.py
+++ mindsdb/integrations/handlers/shopify_handler/shopify_handler.py
@@ -76,9 +76,11 @@ class ShopifyHandler(APIHandler):
         if self.is_connected is True:
             return self.connection
 
-        if kwargs.get("connection_data") is None:
+        if self.kwargs.get("connection_data") is None:
             raise MissingConnectionParams(f"Incomplete parameters passed to Shopify Handler")
-            
+
+        api_session = shopify.Session(self.connection_data['shop_url'], '2021-10', self.connection_data['access_token'])
+        
         self.yotpo_app_key = self.connection_data['yotpo_app_key'] if 'yotpo_app_key' in self.connection_data else None
         self.yotpo_access_token = self.connection_data['yotpo_access_token'] if 'yotpo_access_token' in self.connection_data else None
 

d859396995a1dc174ce3b986c3a9c158ff1aeec9
Abhi <abhi@Abhis-MacBook-Air.local>
2023-10-31 16:13:50 +0530

    added remove text table

--- mindsdb/integrations/handlers/clipdrop_handler/clipdrop.py
+++ mindsdb/integrations/handlers/clipdrop_handler/clipdrop.py
@@ -9,14 +9,14 @@ class ClipdropClient:
         self.base_endpoint = "https://clipdrop-api.co/"
         
     def write_to_file(self, img):
-        f_name = self.dir_to_save + str(uuid.uuid4())
+        f_name = self.dir_to_save + str(uuid.uuid4()) + ".png"
         with open(f_name, "wb") as f:
             f.write(img)
         return f_name
 
     def make_request(self, url, files={}):
         headers = {"x-api-key": self.api_key}
-        resp = requests.get(url, headers=headers, files=files)
+        resp = requests.post(url, headers=headers, files=files)
         content = {}
         if resp.status_code == 200:
             saved_path = self.write_to_file(resp.content)
@@ -26,18 +26,20 @@ class ClipdropClient:
         return content
 
     def image_extension_check(self, url):
-        return url.endswith("jpeg") or url.endswith("jpg") or url.endswith("png")
+        ext = url.split(".")[-1]
+        if ext in ["jpeg", "jpg", "png"]:
+            return ext
+        raise Exception("Unknown image format. Currently jpg, jpeg & png are supported.")
     
     def download_image(self, url):
-        if self.image_extension_check(url):
-            res = requests.get(url)
-            return res.content
-        raise Exception("Unknown image format. Currently jpg, jpeg & png are supported.")
+        img_ext = self.image_extension_check(url)
+        res = requests.get(url)
+        return {"img_ext": img_ext, "content": res.content}
 
-    def remove_background(self, img_url):
+    def remove_text(self, img_url):
         url = f'{self.base_endpoint}remove-text/v1'
         img_content = self.download_image(img_url)
         files = {
-            'image_file': ('image.jpg', img_content, 'image/jpeg')
+            'image_file': ('image.jpg', img_content["content"], f'image/{img_content["img_ext"]}')
         }
         return self.make_request(url, files=files)
--- mindsdb/integrations/handlers/clipdrop_handler/clipdrop_handler.py
+++ mindsdb/integrations/handlers/clipdrop_handler/clipdrop_handler.py
@@ -1,11 +1,7 @@
 from collections import OrderedDict
 
 from mindsdb.integrations.handlers.clipdrop_handler.clipdrop_tables import (
-    DockerHubRepoImagesSummaryTable,
-    DockerHubRepoImagesTable,
-    DockerHubRepoTagTable,
-    DockerHubRepoTagsTable,
-    DockerHubOrgSettingsTable
+    RemoveTextTable
 )
 from mindsdb.integrations.handlers.clipdrop_handler.clipdrop import ClipdropClient
 from mindsdb.integrations.libs.api_handler import APIHandler
@@ -37,11 +33,12 @@ class ClipdropHandler(APIHandler):
         connection_data = kwargs.get("connection_data", {})
         self.connection_data = connection_data
         self.kwargs = kwargs
-        self.client = ClipdropClient()
+        self.client = ClipdropClient(connection_data.get("api_key"), 
+                                     connection_data.get(""))
         self.is_connected = False
 
-        repo_images_stats_data = ClipdropRepoImagesSummaryTable(self)
-        self._register_table("repo_images_summary", repo_images_stats_data)
+        remove_text_data = RemoveTextTable(self)
+        self._register_table("remove_text", remove_text_data)
 
     def connect(self) -> StatusResponse:
         """Set up the connection required by the handler.
@@ -51,14 +48,10 @@ class ClipdropHandler(APIHandler):
         StatusResponse
             connection object
         """
-        resp = StatusResponse(False)
-        status = self.client.login(self.connection_data.get("username"), self.connection_data.get("password"))
-        if status["code"] != 200:
-            resp.success = False
-            resp.error_message = status["error"]
-            return resp
-        self.is_connected = True
-        return resp
+        # FYI - No way currently to establish a connection with the given key
+        # without making a requests that is chargeable
+        # TODO - Find a way to solve FYI
+        return StatusResponse(True)
 
     def check_connection(self) -> StatusResponse:
         """Check connection to the handler.
@@ -68,24 +61,10 @@ class ClipdropHandler(APIHandler):
         StatusResponse
             Status confirmation
         """
-        response = StatusResponse(False)
-
-        try:
-            status = self.client.login(self.connection_data.get("username"), self.connection_data.get("password"))
-            if status["code"] == 200:
-                current_user = self.connection_data.get("username")
-                logger.info(f"Authenticated as user {current_user}")
-                response.success = True
-            else:
-                response.success = False
-                logger.info("Error connecting to dockerhub. " + status["error"])
-                response.error_message = status["error"]
-        except Exception as e:
-            logger.error(f"Error connecting to Clipdrop API: {e}!")
-            response.error_message = e
-
-        self.is_connected = response.success
-        return response
+        # FYI - No way currently to establish a connection with the given key
+        # without making a requests that is chargeable
+        # TODO - Find a way to solve FYI
+        return StatusResponse(True)
 
     def native_query(self, query: str) -> StatusResponse:
         """Receive and process a raw query.
@@ -110,7 +89,7 @@ connection_args = OrderedDict(
         "description": "Clipdrop API key",
         "required": True,
         "label": "api_key",
-    }
+    },
     dir_to_save={
         "type": ARG_TYPE.STR,
         "description": "The local directory to save Clipdrop API response",

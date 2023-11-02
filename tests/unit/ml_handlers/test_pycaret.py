a8ceac8e5829025d27f3e833886ac22c05ce1a94
Aditya Azad <adityaazad121@gmail.com>
2023-11-01 22:36:51 -0400

    Switch to mock data

--- tests/unit/ml_handlers/test_pycaret.py
+++ tests/unit/ml_handlers/test_pycaret.py
@@ -47,8 +47,14 @@ class TestPyCaret(BaseExecutorTest):
 
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
     def test_classifier(self, mock_handler):
-        iris_df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/iris.csv')
-        self.set_handler(mock_handler, name='pg', tables={'iris': iris_df})
+        df = pd.DataFrame({
+            'sepal_length': [5.1, 4.9, 4.7, 4.6, 6.4, 6.9, 5.5, 6.5, 7.7, 6.3, 6.7, 7.2],
+            'sepal_width': [3.5, 3.0, 3.2, 3.1, 3.2, 3.1, 2.3, 2.8, 2.8, 2.7, 3.3, 3.2],
+            'petal_length': [1.4, 4.0, 1.3, 1.5, 4.5, 4.9, 4.0, 4.6, 6.7, 4.9, 5.7, 6.0],
+            'petal_width': [0.2, 0.2, 0.2, 0.2, 1.5, 1.5, 1.3, 1.5, 2.0, 1.8, 2.1, 1.8],
+            'species': ['Iris-setosa', 'Iris-setosa', 'Iris-setosa', 'Iris-setosa', 'Iris-versicolor', 'Iris-versicolor', 'Iris-versicolor', 'Iris-versicolor', 'Iris-virginica', 'Iris-virginica', 'Iris-virginica', 'Iris-virginica']
+        })
+        self.set_handler(mock_handler, name='pg', tables={'iris': df})
 
         # create project
         self.run_sql('create database proj;')
@@ -63,7 +69,8 @@ class TestPyCaret(BaseExecutorTest):
               engine = 'pycaret',
               model_type = 'classification',
               model_name = 'xgboost',
-              setup_session_id = 123;
+              setup_session_id = 123,
+              setup_fold = 2;
         ''')
         self.wait_predictor('proj', 'my_pycaret_class_model')
 
@@ -74,13 +81,21 @@ class TestPyCaret(BaseExecutorTest):
             JOIN proj.my_pycaret_class_model AS m;
         ''')
 
-        # rough estimate from testing
         assert ret['prediction_label'].iloc[0] == 'Iris-setosa'
 
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
     def test_regression(self, mock_handler):
-        insurance_df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/insurance.csv')
-        self.set_handler(mock_handler, name='pg', tables={'insurance': insurance_df})
+        df = pd.DataFrame({
+            'age': [19, 18, 28, 33, 32, 31, 46, 37],
+            'sex': ['female', 'male', 'male', 'male', 'male', 'female', 'female', 'female'],
+            'bmi': [27.9, 33.77, 33, 22.705, 28.88, 25.74, 33.44, 27.74],
+            'children': [0, 1, 3, 0, 0, 0, 1, 3],
+            'smoker': ['yes', 'no', 'no', 'no', 'no', 'no', 'no', 'no'],
+            'region': ['southwest', 'southeast', 'southeast', 'northwest', 'northwest', 'southeast', 'southeast', 'northwest'],
+            'charges': [16884.924, 1725.5523, 4449.462, 21984.47061, 3866.8552, 3756.6216, 8240.5896, 7281.5056]
+        })
+
+        self.set_handler(mock_handler, name='pg', tables={'insurance': df})
 
         # create project
         self.run_sql('create database proj;')
@@ -95,7 +110,8 @@ class TestPyCaret(BaseExecutorTest):
               engine = 'pycaret',
               model_type = 'regression',
               model_name = 'xgboost',
-              setup_session_id = 123;
+              setup_session_id = 123,
+              setup_fold = 2;
         ''')
         self.wait_predictor('proj', 'my_pycaret_regr_model')
 
@@ -106,14 +122,25 @@ class TestPyCaret(BaseExecutorTest):
             JOIN proj.my_pycaret_regr_model AS m;
         ''')
 
-        # rough estimate from testing
-        assert int(ret['prediction_label'].iloc[0]) > 16000
+        assert int(ret['prediction_label'].iloc[0]) == 3822
 
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
     @pytest.mark.skip(reason="MindsDB recognizes 'Anomaly' as a keyword so it fails to fetch Anomaly column")
     def test_anomaly(self, mock_handler):
-        anomaly_df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/anomaly.csv')
-        self.set_handler(mock_handler, name='pg', tables={'anomaly': anomaly_df})
+        df = pd.DataFrame({
+            'Col1': [0.263995357, 0.764928588, 0.13842355, 0.935242061, 0.605866573, 0.518789697, 0.912225161, 0.608234451, 0.723781923, 0.73359095],
+            'Col2': [0.546092303, 0.65397459, 0.065575135, 0.227771913, 0.845269445, 0.837065879, 0.272378939, 0.331678698, 0.429296975, 0.367422001],
+            'Col3': [0.336714104, 0.538842451, 0.192801069, 0.553562822, 0.074514511, 0.332993162, 0.365792205, 0.861309323, 0.899016587, 0.088600152],
+            'Col4': [0.092107835, 0.995016662, 0.014465045, 0.176370646, 0.241530075, 0.514723634, 0.562208164, 0.158963258, 0.073715215, 0.208463224],
+            'Col5': [0.325261175, 0.805967636, 0.957033424, 0.331664957, 0.307923366, 0.355314772, 0.50189852, 0.558449452, 0.885169295, 0.182754409],
+            'Col6': [0.212464853, 0.780304761, 0.458443656, 0.634508561, 0.373030452, 0.465650668, 0.413997158, 0.013080054, 0.570250227, 0.736672363],
+            'Col7': [0.258565714, 0.437317789, 0.559647989, 0.109202597, 0.994553306, 0.896994183, 0.488468506, 0.251942977, 0.017265143, 0.538513303],
+            'Col8': [0.869236755, 0.277978893, 0.42307639, 0.11247202, 0.183727053, 0.034959735, 0.111113968, 0.249329646, 0.550683376, 0.049843054],
+            'Col9': [0.197077957, 0.843918225, 0.24339588, 0.281278233, 0.329148141, 0.73458152, 0.191947043, 0.927804425, 0.71326865, 0.891548497],
+            'Col10': [0.292984504, 0.70343162, 0.43962138, 0.107867968, 0.922947409, 0.25345779, 0.29565178, 0.355286799, 0.980911322, 0.308864217]
+        })
+
+        self.set_handler(mock_handler, name='pg', tables={'anomaly': df})
 
         # create project
         self.run_sql('create database proj;')
@@ -128,8 +155,8 @@ class TestPyCaret(BaseExecutorTest):
               engine = 'pycaret',
               model_type = 'anomaly',
               model_name = 'iforest',
-              setup_session_id = 123;
-
+              setup_session_id = 123,
+              setup_fold = 2;
         ''')
         self.wait_predictor('proj', 'my_pycaret_anom_model')
 
@@ -141,13 +168,18 @@ class TestPyCaret(BaseExecutorTest):
             JOIN proj.my_pycaret_anom_model AS m;
         ''')
 
-        # rough estimate from testing
         assert int(ret['Anomaly'].iloc[0]) == 0
 
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
     def test_cluster(self, mock_handler):
-        jewellery_df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/jewellery.csv')
-        self.set_handler(mock_handler, name='pg', tables={'jewellery': jewellery_df})
+        df = pd.DataFrame({
+            'Age': [58, 59, 62, 59, 87, 29, 54, 87],
+            'Income': [77769, 81799, 74751, 74373, 17760, 13157, 76500, 42592],
+            'SpendingScore': [0.7913287771988531, 0.7910820467274178, 0.7026569520102857, 0.7656795619984281, 0.3487775484305076, 0.8470341025128374, 0.7851978501165687, 0.3552896820382753],
+            'Savings': [6559.8299230048315, 5417.661426197439, 9258.992965034067, 7346.334503537976, 16869.507130301474, 3535.5143522162816, 6878.884248553975, 18086.287157859304]
+        })
+
+        self.set_handler(mock_handler, name='pg', tables={'jewellery': df})
 
         # create project
         self.run_sql('create database proj;')
@@ -173,16 +205,17 @@ class TestPyCaret(BaseExecutorTest):
             JOIN proj.my_pycaret_cluster_model AS m;
         ''')
 
-        # rough estimate from testing
-        assert ret['Cluster'].iloc[0] == "Cluster 2"
+        assert ret['Cluster'].iloc[0] == "Cluster 0"
 
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
     def test_timeseries(self, mock_handler):
-        airline_df = pd.read_csv('https://raw.githubusercontent.com/sktime/sktime/main/sktime/datasets/data/Airline/Airline.csv')
-        airline_df[['Year', 'Month']] = airline_df['Date'].str.split('-', expand=True)
-        airline_df['Year'] = airline_df['Year'].astype(int)
-        airline_df['Month'] = airline_df['Month'].astype(int)
-        self.set_handler(mock_handler, name='pg', tables={'airline': airline_df})
+        df = pd.DataFrame({
+            'Year': [1949, 1949, 1949, 1949, 1949, 1949, 1949, 1949, 1949, 1949, 1949, 1949, 1950, 1950, 1950, 1950, 1950, 1950, 1950, 1950],
+            'Month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
+            'Passengers': [112, 118, 132, 129, 121, 135, 148, 148, 136, 119, 104, 118, 115, 126, 141, 135, 125, 149, 170, 170]
+        })
+
+        self.set_handler(mock_handler, name='pg', tables={'airline': df})
 
         # create project
         self.run_sql('create database proj;')
@@ -210,5 +243,4 @@ class TestPyCaret(BaseExecutorTest):
             JOIN proj.my_pycaret_timeseries_model AS m;
         ''')
 
-        # rough estimate from testing
-        assert int(ret['y_pred'].iloc[0]) > 500
+        assert int(ret['y_pred'].iloc[0]) == 125

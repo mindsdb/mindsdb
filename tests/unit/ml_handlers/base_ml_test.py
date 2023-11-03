dcbafaf247378b34857dcabcc598f46f23f33a63
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-11-03 14:53:01 +0530

    added the base ML API test class

--- tests/unit/ml_handlers/base_ml_test.py
+++ tests/unit/ml_handlers/base_ml_test.py
@@ -1,3 +1,4 @@
+import os
 import time
 import pandas as pd
 
@@ -8,7 +9,7 @@ class BaseMLTest(BaseExecutorTest):
     """
     Base test class for ML engines
     """
-    def wait_predictor(self, project, name, timeout=100):
+    def wait_predictor(self, project: str, name: str, timeout:int = 100) -> None:
         """Wait for the predictor to be created, raising an exception if predictor creation fails or exceeds timeout"""
         for attempt in range(timeout):
             ret = self.run_sql(f"select * from {project}.models where name='{name}'")
@@ -21,11 +22,21 @@ class BaseMLTest(BaseExecutorTest):
             time.sleep(0.5)
         raise RuntimeError("Predictor wasn't created")
     
-    def run_sql(self, sql):
+    def run_sql(self, sql: str) -> pd.DataFrame:
         """Execute SQL and return a DataFrame, raising an AssertionError if an error occurs"""
         ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
         assert ret.error_code is None, f"SQL execution failed with error: {ret.error_code}"
         if ret.data is not None:
             columns = [col.alias if col.alias else col.name for col in ret.columns]
             return pd.DataFrame(ret.data, columns=columns)
+        
+
+class BaseMLAPITest(BaseMLTest):
+    """
+    Base test class for API-based ML engines
+    """
+    @staticmethod
+    def get_api_key(env_var: str):
+        """Retrieve API key from environment variables"""
+        return os.environ.get(env_var)
 

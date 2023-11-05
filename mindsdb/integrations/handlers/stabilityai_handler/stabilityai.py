4c87d8870de58f6deef629b4c51f71d4e30bee10
Abhi <abhi@Abhis-MacBook-Air.local>
2023-11-05 22:42:38 +0530

    added docstring

--- mindsdb/integrations/handlers/stabilityai_handler/stabilityai.py
+++ mindsdb/integrations/handlers/stabilityai_handler/stabilityai.py
@@ -1,12 +1,25 @@
 from stability_sdk import client
 from stability_sdk.client import generation
 from PIL import Image
-import io, requests
+import io
+import requests
+
 
 class StabilityAPIClient:
-    
-    def __init__(self, api_key, dir_to_save, 
+
+    def __init__(self, api_key, dir_to_save,
                  engine="stable-diffusion-xl-1024-v1-0", upscale_engine="esrgan-v1-x2plus"):
+        """Initialize the stability wrapper api client.
+
+        Args:
+            api_key: Stability AI API Key
+            dir_to_save: The local directory path to save the response images from the API
+            engine (str, optional): Stability AI engine to use. Defaults to "stable-diffusion-xl-1024-v1-0".
+            upscale_engine (str, optional): Stability AI upscaling engine to use. Defaults to "esrgan-v1-x2plus".
+
+        Raises:
+            ValueError: For unknown engine or upscale engine
+        """
         self.api_key = api_key
         self.STABILITY_HOST = 'grpc.stability.ai:443'
         self.save_dir = dir_to_save + "/" if not dir_to_save.endswith("/") else dir_to_save
@@ -15,20 +28,36 @@ class StabilityAPIClient:
             raise ValueError("Unknown engine. The available engines are - " + self.available_engines)
         if not self._is_valid_engine(upscale_engine):
             raise ValueError("Unknown upscale engine. The available engines are - " + self.available_engines)
-        
-        self.stability_api = client.StabilityInference(host=self.STABILITY_HOST, 
+
+        self.stability_api = client.StabilityInference(host=self.STABILITY_HOST,
                                                        key=self.api_key,
                                                        engine=engine,
                                                        upscale_engine=upscale_engine,
                                                        verbose=True)
-        
+
     def save_image(self, artifact):
+        """Save the binary image in the artifact to the local directory
+
+        Args:
+            artifact: Artifact returned by the API
+
+        Returns:
+            The local image path.
+        """
         img = Image.open(io.BytesIO(artifact.binary))
-        path = self.save_dir + str(artifact.seed)+ ".png"
+        path = self.save_dir + str(artifact.seed) + ".png"
         img.save(path)
         return path
-    
+
     def _process_artifacts(self, artifacts):
+        """Process the artificats returned by the API
+
+        Args:
+            artifacts :Artifact returned by the API
+
+        Returns:
+            The saved image paths
+        """
         saved_image_paths = []
         for resp in artifacts:
             for artifact in resp.artifacts:
@@ -39,10 +68,15 @@ class StabilityAPIClient:
                 if artifact.type == generation.ARTIFACT_IMAGE:
                     saved_image_paths.append(self.save_image(artifact))
         return saved_image_paths
-    
+
     def get_existing_engines(self):
+        """Get the existing engines
+
+        Returns: Engines supported by the API
+        """
         url = "https://api.stability.ai/v1/engines/list"
-        if self.api_key is None: raise Exception("Missing Stability API key.")
+        if self.api_key is None:
+            raise Exception("Missing Stability API key.")
 
         response = requests.get(url, headers={
             "Authorization": f"Bearer {self.api_key}"
@@ -52,19 +86,46 @@ class StabilityAPIClient:
             raise Exception("Non-200 response: " + str(response.text))
 
         payload = response.json()
-        
-        return {engine["id"]:engine for engine in payload}
-    
+
+        return {engine["id"]: engine for engine in payload}
+
     def _is_valid_engine(self, engine_id):
+        """Validates the given engine id against the supported engines by Stability
+
+        Args:
+            engine_id: The engine id to check
+
+        Returns:
+            True if valid engine else False
+        """
         return engine_id in self.available_engines
-    
+
     def _read_image_url(self, image_url):
+        """Downloads the given image url.
+
+        Args:
+            image_url: The image url to download
+
+        Returns:
+            Downloaded image
+        """
         return Image.open(requests.get(image_url, stream=True).raw)
-        
-    def text_to_image(self, prompt, seed=121245125, 
-                      height=1024, width=1024, 
+
+    def text_to_image(self, prompt, seed=121245125,
+                      height=1024, width=1024,
                       steps=50):
-        
+        """Converts the given text to image using stability API.
+
+        Args:
+            prompt: The given text
+            seed (int, optional): Random seed. Defaults to 121245125.
+            height (int, optional): Height of the image. Defaults to 1024.
+            width (int, optional): Width of the image. Defaults to 1024.
+            steps (int, optional): Number of steps. Defaults to 50.
+
+        Returns:
+            The local saved paths of the generated images
+        """
         answers = self.stability_api.generate(
             prompt=prompt,
             seed=seed,
@@ -73,7 +134,7 @@ class StabilityAPIClient:
             steps=steps,
             sampler=generation.SAMPLER_K_DPMPP_2M
         )
-        
+
         saved_images = self._process_artifacts(answers)
-        
+
         return saved_images
--- mindsdb/integrations/handlers/stabilityai_handler/stabilityai_handler.py
+++ mindsdb/integrations/handlers/stabilityai_handler/stabilityai_handler.py
@@ -10,24 +10,25 @@ from mindsdb.utilities.log import get_log
 
 logger = get_log("integrations.stabilityai_handler")
 
+
 class StabilityAIHandler(BaseMLEngine):
     name = "stabilityai"
 
     @staticmethod
     def create_validation(target, args=None, **kwargs):
         args = args['using']
-        
+
         if 'api_key' not in args:
             raise Exception('api_key has to be specified')
 
         if 'input_column' not in args:
             raise Exception('input_column has to be specified')
-        
+
         if 'local_directory_path' not in args:
             raise Exception('local_directory_path has to be specified')
-        
+
         client = StabilityAPIClient(args["api_key"], args["local_directory_path"])
-        
+
         if 'stability_engine_id' in args:
             if not client._is_valid_engine(args["stability_engine_id"]):
                 raise Exception("Unknown engine. The available engines are - " + list(client.available_engines.keys()))
@@ -36,30 +37,30 @@ class StabilityAIHandler(BaseMLEngine):
         if 'using' not in args:
             raise Exception("Stability AI Inference engine requires a USING clause! Refer to its documentation for more details.")
         self.generative = True
-        
+
         self.model_storage.json_set('args', args["using"])
 
     def predict(self, df, args=None):
-        
+
         args = self.model_storage.json_get('args')
         api_key = self._get_stability_api_key(args)
 
         input_column = args['input_column']
-        
+
         if input_column not in df.columns:
             raise RuntimeError(f'Column "{input_column}" not found in input data')
-        
+
         local_directory_path = args["local_directory_path"]
         stability_engine_id = args.get('stability_engine_id', "stable-diffusion-xl-1024-v1-0")
-        
+
         client = StabilityAPIClient(api_key=api_key, dir_to_save=local_directory_path, engine=stability_engine_id)
-        
+
         result_df = pd.DataFrame()
-        
+
         result_df['predictions'] = df[input_column].apply(self.generate_image, client=client)
-        
+
         result_df = result_df.rename(columns={'predictions': args['target']})
-        
+
         return result_df
 
     def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
@@ -68,18 +69,18 @@ class StabilityAIHandler(BaseMLEngine):
         stability_engine_id = args["stability_engine_id"]
         response = client.available_engines.get(stability_engine_id)
         return pd.json_normalize(response)
-    
+
     def _get_stability_api_key(self, args):
         if 'api_key' in args:
             return args['api_key']
 
         connection_args = self.engine_storage.get_connection_args()
-        
+
         if 'api_key' in connection_args:
             return connection_args['api_key']
 
         raise Exception("Missing API key 'api_key'. Either re-create this ML_ENGINE specifying the `api_key` parameter\
                  or re-create this model and pass the API key with `USING` syntax.")
-        
+
     def generate_image(self, text, client):
         return client.text_to_image(prompt=text)

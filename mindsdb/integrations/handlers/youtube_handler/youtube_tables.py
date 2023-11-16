1c2b8c7dc1c2b95ad28919d850f50f00db347f7f
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-11-17 00:15:55 +0530

    simplified the SELECT for comments using query utilities

--- mindsdb/integrations/handlers/youtube_handler/youtube_tables.py
+++ mindsdb/integrations/handlers/youtube_handler/youtube_tables.py
@@ -37,27 +37,17 @@ class YoutubeCommentsTable(APITable):
         ValueError
             If the query contains an unsupported condition
         """
-        conditions = extract_comparison_conditions(query.where)
+        select_statement_parser = SELECTQueryParser(query, "comments", self.get_columns())
 
-        order_by_conditions = {}
-
-        if query.order_by and len(query.order_by) > 0:
-            order_by_conditions["columns"] = []
-            order_by_conditions["ascending"] = []
-
-            for an_order in query.order_by:
-                if an_order.field.parts[-1] in self.get_columns():
-                    order_by_conditions["columns"].append(an_order.field.parts[-1])
-
-                    if an_order.direction == "ASC":
-                        order_by_conditions["ascending"].append(True)
-                    else:
-                        order_by_conditions["ascending"].append(False)
-                else:
-                    raise ValueError(f"Order by unknown column {an_order.field.parts[-1]}")
+        (
+            selected_columns,
+            where_conditions,
+            order_by_conditions,
+            result_limit,
+        ) = select_statement_parser.parse_query()
 
         channel_id, video_id = None, None
-        for a_where in conditions:
+        for a_where in where_conditions:
             if a_where[1] == "video_id":
                 if a_where[0] != "=":
                     raise NotImplementedError("Only '=' operator is supported for video_id column.")
@@ -68,41 +58,26 @@ class YoutubeCommentsTable(APITable):
                     raise NotImplementedError("Only '=' operator is supported for channel_id column.")
                 else:
                     channel_id = a_where[2]
-            else:
-                raise NotImplementedError(f"Unsupported where condition {a_where}")
+
+        if not video_id and not channel_id:
+            raise ValueError("Either video_id or channel_id has to be present in where clause.")
 
         if video_id and channel_id:
             raise ValueError("Only one of video_id or channel_id can be present in where clause.")
             
-        youtube_comments_df = self.get_comments(video_id=video_id, channel_id=channel_id)
+        comments_df = self.get_comments(video_id=video_id, channel_id=channel_id)
 
-        selected_columns = []
-        for target in query.targets:
-            if isinstance(target, ast.Star):
-                selected_columns = self.get_columns()
-                break
-            elif isinstance(target, ast.Identifier):
-                selected_columns.append(target.parts[-1])
-            else:
-                raise ValueError(f"Unknown query target {type(target)}")
+        select_statement_executor = SELECTQueryExecutor(
+            comments_df, 
+            selected_columns, 
+            [where_condition for where_condition in where_conditions if where_condition[1] not in ['video_id', 'channel_id']], 
+            order_by_conditions, 
+            result_limit if query.limit else None
+        )
 
-        if len(youtube_comments_df) == 0:
-            youtube_comments_df = pd.DataFrame([], columns=selected_columns)
-        else:
-            youtube_comments_df.columns = self.get_columns()
-            for col in set(youtube_comments_df.columns).difference(set(selected_columns)):
-                youtube_comments_df = youtube_comments_df.drop(col, axis=1)
+        comments_df = select_statement_executor.execute_query()
 
-            if len(order_by_conditions.get("columns", [])) > 0:
-                youtube_comments_df = youtube_comments_df.sort_values(
-                    by=order_by_conditions["columns"],
-                    ascending=order_by_conditions["ascending"],
-                )
-
-        if query.limit:
-            youtube_comments_df = youtube_comments_df.head(query.limit.value)
-
-        return youtube_comments_df
+        return comments_df
     
     def insert(self, query: ast.Insert) -> None:
         """Inserts data into the YouTube POST /commentThreads API endpoint.

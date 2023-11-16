2031685ab845368900aafacee8d665e88b4a67c3
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-11-16 08:27:28 +0300

    Updated confluence_handler.py

--- mindsdb/integrations/handlers/confluence_handler/confluence_handler.py
+++ mindsdb/integrations/handlers/confluence_handler/confluence_handler.py
@@ -11,7 +11,6 @@ from mindsdb.integrations.libs.api_handler import APIHandler
 from mindsdb.integrations.libs.response import (
     HandlerStatusResponse as StatusResponse,
 )
-from mindsdb.utilities.log import get_log
 from mindsdb_sql import parse_sql
 from mindsdb.utilities import log
 

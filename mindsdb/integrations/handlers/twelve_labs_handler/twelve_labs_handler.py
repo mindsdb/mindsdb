3cac9c06ef8b9cd071c759fb5f8012964f45e4b9
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-11-22 12:27:43 +0530

    added a skeleton for the create() method with other associated methods

--- mindsdb/integrations/handlers/twelve_labs_handler/twelve_labs_handler.py
+++ mindsdb/integrations/handlers/twelve_labs_handler/twelve_labs_handler.py
@@ -5,6 +5,7 @@ from typing import Optional, Dict
 import pandas as pd
 
 from mindsdb.integrations.libs.base import BaseMLEngine
+from mindsdb.integrations.utilities.handler_utils import get_api_key
 
 
 class TwelveLabsHandler(BaseMLEngine):
@@ -19,6 +20,39 @@ class TwelveLabsHandler(BaseMLEngine):
         pass
 
     def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
+        # check for USING clause
+        if 'using' not in args:
+            # TODO: update Exception to InsufficientParametersException
+            raise Exception("Twelve Labs engine requires a USING clause! Refer to its documentation for more details.")
+
+        # get USING args and add target
+        args = args['using']
+        args['target'] = target
+
+        # get api key
+        api_key = get_api_key(
+            api_name=self.name,
+            create_args=args,
+            engine_storage=self.engine_storage,
+        )
+
+        # if index_name is not provided, create an index
+
+        # create video indexing tasks for all video files or video urls
+        # video urls will be given precedence
+
+        # poll for video indexing tasks to complete
+
+        # store args in model_storage
+        self.model_storage.json_set('args', args)
+
+    def _create_index(self, index_name: str, engine_id: str, index_options: List[str]) -> str:
+        pass
+
+    def _create_video_indexing_task(self, index_id: str, video_url: str, video_file: str) -> str:
+        pass
+
+    def _get_video_indexing_task(self, task_id: str) -> Dict:
         pass
 
     def predict(self, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:

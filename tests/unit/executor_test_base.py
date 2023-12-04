5ca016e7e3c7c53ee0100df53cb818232204f012
dusvyat <13661123+dusvyat@users.noreply.github.com>
2023-12-04 14:06:26 +0000

    hf sentence transformer handler update

--- tests/unit/executor_test_base.py
+++ tests/unit/executor_test_base.py
@@ -171,7 +171,13 @@ class BaseExecutorTest(BaseUnitTest):
         super().setup_method()
         self.set_executor()
 
-    def set_executor(self, mock_lightwood=False, mock_model_controller=False, import_dummy_ml=False):
+    def set_executor(
+        self,
+        mock_lightwood=False,
+        mock_model_controller=False,
+        import_dummy_ml=False,
+        import_dummy_llm=False,
+    ):
         # creates executor instance with mocked model_interface
         from mindsdb.api.mysql.mysql_proxy.controllers.session_controller import (
             SessionController,
@@ -204,6 +210,18 @@ class BaseExecutorTest(BaseUnitTest):
 
             if not integration_controller.handlers_import_status['dummy_ml']['import']['success']:
                 error = integration_controller.handlers_import_status['dummy_ml']['import']['error_message']
+                raise Exception(f"Can not import: {str(handler_dir)}: {error}")
+
+        if import_dummy_llm:
+
+            test_handler_path = os.path.dirname(__file__)
+            sys.path.append(test_handler_path)
+
+            handler_dir = Path(test_handler_path) / 'dummy_llm_handler'
+            integration_controller.import_handler('', handler_dir)
+
+            if not integration_controller.handlers_import_status['dummy_llm']['import']['success']:
+                error = integration_controller.handlers_import_status['dummy_llm']['import']['error_message']
                 raise Exception(f"Can not import: {str(handler_dir)}: {error}")
 
         if mock_lightwood:
@@ -337,6 +355,16 @@ class BaseExecutorDummyML(BaseExecutorTest):
     def setup_method(self):
         super().setup_method()
         self.set_executor(import_dummy_ml=True)
+
+
+class BaseExecutorDummyLLM(BaseExecutorTest):
+    """
+    Set up executor: mock LLM handler
+    """
+
+    def setup_method(self):
+        super().setup_method()
+        self.set_executor(import_dummy_llm=True)
 
 
 class BaseExecutorMockPredictor(BaseExecutorTest):

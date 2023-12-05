0d7da29756c37ecd2eb9f28089d72f307cc9561a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-05 18:08:25 +0530

    implemented the ChatMessagesTable

--- mindsdb/integrations/handlers/ms_teams_handler/ms_teams_tables.py
+++ mindsdb/integrations/handlers/ms_teams_handler/ms_teams_tables.py
@@ -302,7 +302,7 @@ class ChatsTable(APITable):
                     raise NotImplementedError("Only '=' operator is supported for id column.")
                 
         if id:
-            chats_df = pd.json_normalize(self.get_chats(id))
+            chats_df = pd.json_normalize(self.get_chats(id), sep='_')
             where_conditions = [where_condition for where_condition in where_conditions if where_condition[1] not in ['id']]
         else:
             chats_df = pd.json_normalize(self.get_chats(), sep='_')
@@ -375,7 +375,99 @@ class ChatMessagesTable(APITable):
         ValueError
             If the query contains an unsupported condition
         """
-        pass
+        select_statement_parser = SELECTQueryParser(
+            query,
+            'chat_messages',
+            self.get_columns()
+        )
+
+        selected_columns, where_conditions, order_by_conditions, result_limit = select_statement_parser.parse_query()
+
+        chat_id, message_id = None, None
+        for op, arg1, arg2 in where_conditions:
+            if arg1 == 'id':
+                if op == "=":
+                    message_id = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for id column.")
+                
+            if arg1 == 'chatId':
+                if op == "=":
+                    chat_id = arg2
+                else:
+                    raise NotImplementedError("Only '=' operator is supported for chatId column.")
+                
+        if message_id and chat_id:
+            messages_df = pd.json_normalize(self.get_messages(chat_id, message_id), sep='_')
+            where_conditions = [where_condition for where_condition in where_conditions if where_condition[1] not in ['id', 'chatId']]
+        else:
+            messages_df = pd.json_normalize(self.get_messages(chat_id), sep='_')
+
+        select_statement_executor = SELECTQueryExecutor(
+            messages_df,
+            selected_columns,
+            where_conditions,
+            order_by_conditions,
+            result_limit if query.limit else None
+        )
+
+        messages_df = select_statement_executor.execute_query()
+
+        return messages_df
+    
+    def get_messages(self, chat_id = None, message_id = None) -> List[Dict[Text, Any]]:
+        api_client = self.handler.connect()
+
+        if message_id and chat_id:
+            return [api_client.get_chat_message(chat_id, message_id)]
+        elif chat_id:
+            return api_client.get_chat_messages(chat_id)
+        else:
+            return api_client.get_all_chat_messages()
+        
+    def get_columns(self) -> list:
+        return [
+            "id",
+            "replyToId",
+            "etag",
+            "messageType",
+            "createdDateTime",
+            "lastModifiedDateTime",
+            "lastEditedDateTime",
+            "deletedDateTime",
+            "subject",
+            "summary",
+            "chatId",
+            "importance",
+            "locale",
+            "webUrl",
+            "channelIdentity",
+            "policyViolation",
+            "eventDetail",
+            "attachments",
+            "mentions",
+            "reactions",
+            "from_application",
+            "from_device",
+            "from_user_@odata.type",
+            "from_user_id",
+            "from_user_displayName",
+            "from_user_userIdentityType",
+            "from_user_tenantId",
+            "body_contentType",
+            "body_content",
+            "from",
+            "eventDetail_@odata.type",
+            "eventDetail_visibleHistoryStartDateTime",
+            "eventDetail_members",
+            "eventDetail_initiator_application",
+            "eventDetail_initiator_device",
+            "eventDetail_initiator_user_@odata.type",
+            "eventDetail_initiator_user_id",
+            "eventDetail_initiator_user_displayName",
+            "eventDetail_initiator_user_userIdentityType",
+            "eventDetail_initiator_user_tenantId",
+        ]
 
     def insert(self, query: ASTNode) -> None:
         """Inserts data into the Microsoft Teams "POST /chats/{chat_id}/messages" API endpoint.
@@ -394,4 +486,21 @@ class ChatMessagesTable(APITable):
         ValueError
             If the query contains an unsupported condition
         """
-        pass
+        insert_statement_parser = INSERTQueryParser(
+            query,
+            supported_columns=["chatId", "subject", "body_content"],
+            mandatory_columns=["chatId", "body_content"],
+        )
+
+        messages_to_send = insert_statement_parser.parse_query()
+
+        self.send_messages(messages_to_send)
+
+    def send_messages(self, messages_to_send: List[Dict[Text, Any]]) -> None:
+        api_client = self.handler.connect()
+        for message in messages_to_send:
+            api_client.send_chat_message(
+                chat_id=message["chatId"],
+                message=message["body_content"],
+                subject=message.get("subject")
+            )

0a225096dab373a8669a242e76665a87da2eae6a
tmichaeldb <tyler@mindsdb.com>
2023-12-11 10:22:04 -0800

    Pydantic v2 compliance

--- mindsdb/integrations/handlers/chromadb_handler/settings.py
+++ mindsdb/integrations/handlers/chromadb_handler/settings.py
@@ -35,7 +35,7 @@ class ChromaHandlerConfig(BaseModel):
                     raise ValueError(f"Unexpected parameter '{key}'.")
         return values
 
-    @root_validator(allow_reuse=True)
+    @root_validator(allow_reuse=True, skip_on_failure=True)
     def check_config(cls, values):
         """Check if config is valid."""
 

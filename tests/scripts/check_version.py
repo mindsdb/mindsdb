072c2c686d72a41ca52ee5a527c26383747fe423
hamishfagg <ivdata+github@ivdata.net>
2023-12-11 15:02:42 +1300

    fix version check

--- tests/scripts/check_version.py
+++ tests/scripts/check_version.py
@@ -8,14 +8,14 @@ RELEASE_PATTERN = "^\d+(\.\d+)*$"  # noqa: W605
 PRERELEASE_PATTERN = "^\d+(\.\d+)*(a|b|rc)\d+$"  # noqa: W605
 
 version_str = sys.argv[1].replace('v', '')
-release_type = sys.argv[2]
+is_prerelease = sys.argv[2] == "true"
 
-if release_type == "release":
-    if re.match(RELEASE_PATTERN, version_str) is None:
-        raise Exception("Invalid release version: %s" % version_str)
-elif release_type == "prerelease":
+if is_prerelease:
     if re.match(PRERELEASE_PATTERN, version_str) is None:
         raise Exception("Invalid prerelease version: %s" % version_str)
+elif re.match(RELEASE_PATTERN, version_str) is None:
+    raise Exception("Invalid release version: %s" % version_str)
+
 
 if version_str != __version__:
     raise Exception("Version mismatch between __about__.py and release tag: %s != %s" % (version_str, __version__))

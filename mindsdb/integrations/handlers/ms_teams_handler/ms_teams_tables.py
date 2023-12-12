af3b634cc480050e6b000ec8076a96c1b9f60044
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-12 18:28:50 +0530

    updated the ChannelMessagesTable

--- mindsdb/integrations/handlers/ms_teams_handler/ms_teams_tables.py
+++ mindsdb/integrations/handlers/ms_teams_handler/ms_teams_tables.py
@@ -1,42 +1,47 @@
-from mindsdb_sql.parser.ast import ASTNode
 import pandas as pd
 from typing import Text, List, Dict, Any
 
+from mindsdb.utilities import log
 from mindsdb_sql.parser import ast
+from mindsdb_sql.parser.ast import ASTNode
 from mindsdb.integrations.libs.api_handler import APITable
+
+from mindsdb.integrations.handlers.ms_teams_handler.settings import ms_teams_handler_config
 
 from mindsdb.integrations.handlers.utilities.query_utilities.insert_query_utilities import INSERTQueryParser
 from mindsdb.integrations.handlers.utilities.query_utilities.select_query_utilities import SELECTQueryParser, SELECTQueryExecutor
-
-from mindsdb.utilities import log
 
 logger = log.getLogger(__name__)
 
 
 class ChannelMessagesTable(APITable):
-    """The Microsoft Teams Messages Table implementation"""
+    """
+    The Microsoft Teams Channel Messages Table implementation.       
+    """
      
-    def select(self, query: ASTNode) -> pd.DataFrame:
-        """Pulls data from the Microsoft Teams "GET /teams/{group_id}/channels/{channel_id}/messages" API endpoint.
+    def select(self, query: ast.Select) -> pd.DataFrame:
+        """
+        Pulls data from the "GET /teams/{group_id}/channels/{channel_id}/messages" and "GET /teams/{group_id}/channels/{channel_id}/messages/{message_id}" Microsoft Graph API endpoints.
 
         Parameters
         ----------
-        query : ast.Select
-           Given SQL SELECT query
+        query: ast.Select
+            Given SQL SELECT query.
 
         Returns
         -------
         pd.DataFrame
-            Microsoft Teams Messages matching the query
+            Microsoft Teams Channel Messages matching the query.
 
         Raises
         ------
-        ValueError
-            If the query contains an unsupported condition
+        NotImplementedError
+            If the query contains an unsupported condition.
         """
+
         select_statement_parser = SELECTQueryParser(
             query,
-            'messages',
+            'channel_messages',
             self.get_columns()
         )
 
@@ -80,7 +85,23 @@ class ChannelMessagesTable(APITable):
 
         return messages_df
     
-    def get_messages(self, team_id = None, channel_id = None, message_id= None) -> List[Dict[Text, Any]]:
+    def get_messages(self, team_id: Text = None, channel_id: Text = None, message_id: Text = None) -> List[Dict[Text, Any]]:
+        """
+        Calls the API client to get the messages from the Microsoft Graph API.
+        If all parameters are None, it will return all the messages from all the channels from all the teams.
+
+        Parameters
+        ----------
+        team_id: str
+            The team id to get the messages from.
+
+        channel_id: str
+            The channel id to get the messages from.
+
+        message_id: str
+            The message id to get the message from.
+        """
+
         api_client = self.handler.connect()
         # TODO: Should these records be filtered somehow?
         if message_id and channel_id and team_id:
@@ -88,13 +109,14 @@ class ChannelMessagesTable(APITable):
         else:
             return api_client.get_channel_messages()
     
-    def insert(self, query: ASTNode) -> None:
-        """Inserts data into the Microsoft Teams "POST /teams/{group_id}/channels/{channel_id}/messages" API endpoint.
+    def insert(self, query: ast.Insert) -> None:
+        """
+        Inserts data into the "POST /teams/{group_id}/channels/{channel_id}/messages" Microsoft Graph API endpoint.
 
         Parameters
         ----------
         query : ast.Insert
-           Given SQL INSERT query
+           Given SQL INSERT query.
 
         Returns
         -------
@@ -102,9 +124,16 @@ class ChannelMessagesTable(APITable):
 
         Raises
         ------
-        ValueError
-            If the query contains an unsupported condition
+        UnsupportedColumnException
+            If the query contains an unsupported column.
+
+        MandatoryColumnException
+            If the query is missing a mandatory column.
+
+        ColumnCountMismatchException
+            If the number of columns does not match the number of values.
         """
+
         insert_statement_parser = INSERTQueryParser(
             query,
             supported_columns=["channelIdentity_teamId", "channelIdentity_channelId", "subject", "body_content"],
@@ -116,6 +145,23 @@ class ChannelMessagesTable(APITable):
         self.send_messages(messages_to_send)
 
     def send_messages(self, messages_to_send: List[Dict[Text, Any]]) -> None:
+        """
+        Calls the API client to send the messages to the Microsoft Graph API.
+        
+        Parameters
+        ----------
+        messages_to_send: List[Dict[Text, Any]]
+            The messages to send to the Microsoft Graph API.
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        None
+        """
+
         api_client = self.handler.connect()
         for message in messages_to_send:
             api_client.send_channel_message(
@@ -125,57 +171,17 @@ class ChannelMessagesTable(APITable):
                 subject=message.get("subject")
             )
     
-    def get_columns(self) -> list:
-        return [
-            "id",
-            "replyToId",
-            "etag",
-            "messageType",
-            "createdDateTime",
-            "lastModifiedDateTime",
-            "lastEditedDateTime",
-            "deletedDateTime",
-            "subject",
-            "summary",
-            "chatId",
-            "importance",
-            "locale",
-            "webUrl",
-            "policyViolation",
-            "eventDetail",
-            "attachments",
-            "mentions",
-            "reactions",
-            "from_application",
-            "from_device",
-            "from_user_@odata.type",
-            "from_user_id",
-            "from_user_displayName",
-            "from_user_userIdentityType",
-            "from_user_tenantId",
-            "body_contentType",
-            "body_content",
-            "channelIdentity_teamId",
-            "channelIdentity_channelId",
-            "from",
-            "eventDetail_@odata.type",
-            "eventDetail_visibleHistoryStartDateTime",
-            "eventDetail_members",
-            "eventDetail_initiator_device",
-            "eventDetail_initiator_user",
-            "eventDetail_initiator_application_@odata.type",
-            "eventDetail_initiator_application_id",
-            "eventDetail_initiator_application_displayName",
-            "eventDetail_initiator_application_applicationIdentityType",
-            "eventDetail_channelId",
-            "eventDetail_channelDisplayName",
-            "eventDetail_initiator_application",
-            "eventDetail_initiator_user_@odata.type",
-            "eventDetail_initiator_user_id",
-            "eventDetail_initiator_user_displayName",
-            "eventDetail_initiator_user_userIdentityType",
-            "eventDetail_initiator_user_tenantId"
-        ]
+    def get_columns(self) -> List[Text]:
+        """
+        Returns the columns of the Channel Messages Table.
+
+        Returns
+        -------
+        list
+            The columns of the Channel Messages Table.
+        """
+
+        return ms_teams_handler_config.CHANNEL_MESSAGES_TABLE_COLUMNSS
 
 class ChannelMessageRepliesTable(APITable):
     """The Microsoft Teams Message Replies Table implementation"""

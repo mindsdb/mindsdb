972a5cb5e1656afc80af071167ab5e89dc8940c7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-13 11:57:47 +0530

    improved the handler implementation with type hints, comments etc.

--- mindsdb/integrations/handlers/ms_teams_handler/ms_teams_handler.py
+++ mindsdb/integrations/handlers/ms_teams_handler/ms_teams_handler.py
@@ -1,24 +1,17 @@
 import msal  # noqa, required by MSGraphAPIAuthManager
-
-from .ms_graph_api_teams_client import MSGraphAPITeamsClient
-from mindsdb.integrations.handlers.utilities.auth_utilities import MSGraphAPIAuthManager
-
-from mindsdb.integrations.handlers.ms_teams_handler.ms_teams_tables import ChannelsTable, ChannelMessagesTable, ChatsTable, ChatMessagesTable
-from mindsdb.integrations.libs.api_handler import APIChatHandler
-from mindsdb.integrations.libs.response import (
-    HandlerStatusResponse as StatusResponse,
-)
+from typing import Text, Dict
 
 from mindsdb.utilities import log
 from mindsdb_sql import parse_sql
+from mindsdb.integrations.handlers.utilities.auth_utilities import MSGraphAPIAuthManager
+from mindsdb.integrations.handlers.ms_teams_handler.settings import ms_teams_handler_config
+from mindsdb.integrations.handlers.ms_teams_handler.ms_graph_api_teams_client import MSGraphAPITeamsClient
 
-DEFAULT_SCOPES = [
-    'https://graph.microsoft.com/User.Read',
-    'https://graph.microsoft.com/Group.Read.All',
-    'https://graph.microsoft.com/ChannelMessage.Send',
-    'https://graph.microsoft.com/Chat.Read',
-    'https://graph.microsoft.com/ChatMessage.Send',
-]
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+)
+from mindsdb.integrations.libs.api_handler import APIChatHandler
+from mindsdb.integrations.handlers.ms_teams_handler.ms_teams_tables import ChannelsTable, ChannelMessagesTable, ChatsTable, ChatMessagesTable
 
 logger = log.getLogger(__name__)
 
@@ -37,6 +30,7 @@ class MSTeamsHandler(APIChatHandler):
             name (str): name of particular handler instance
             **kwargs: arbitrary keyword arguments.
         """
+
         super().__init__(name)
 
         connection_data = kwargs.get("connection_data", {})
@@ -59,28 +53,33 @@ class MSTeamsHandler(APIChatHandler):
         chat_messages_data = ChatMessagesTable(self)
         self._register_table("chat_messages", chat_messages_data)
 
-    def connect(self):
+    def connect(self) -> MSGraphAPITeamsClient:
         """
         Set up the connection required by the handler.
+
         Returns
         -------
-        StatusResponse
-            connection object
+        MSGraphAPITeamsClient
+            Client object for accessing the Microsoft Graph API.
         """
+
         if self.is_connected and self.connection.check_connection():
             return self.connection
 
+        # initialize the auth manager for the Microsoft Graph API
         ms_graph_api_auth_manager = MSGraphAPIAuthManager(
             handler_storage=self.handler_storage,
-            scopes=self.connection_data.get('scopes', DEFAULT_SCOPES),
+            scopes=self.connection_data.get('scopes', ms_teams_handler_config.DEFAULT_SCOPES),
             client_id=self.connection_data["client_id"],
             client_secret=self.connection_data["client_secret"],
             tenant_id=self.connection_data["tenant_id"],
             code=self.connection_data.get('code')
         )
 
+        # get access token from the auth manager for the Microsoft Graph API
         access_token = ms_graph_api_auth_manager.get_access_token()
 
+        # pass the access token to the client for access to the Microsoft Graph API
         self.connection = MSGraphAPITeamsClient(access_token)
 
         self.is_connected = True
@@ -90,8 +89,11 @@ class MSTeamsHandler(APIChatHandler):
     def check_connection(self) -> StatusResponse:
         """
         Check connection to the handler.
-        Returns:
-            HandlerStatusResponse
+
+        Returns
+        -------
+        StatusResponse
+            Response object with the status of the connection.
         """
 
         response = StatusResponse(False)
@@ -110,21 +112,35 @@ class MSTeamsHandler(APIChatHandler):
 
         return response
 
-    def native_query(self, query: str) -> StatusResponse:
-        """Receive and process a raw query.
+    def native_query(self, query: Text) -> StatusResponse:
+        """
+        Receive and process a raw query.
+
         Parameters
         ----------
-        query : str
-            query in a native format
+        query: Text
+            Query in the native format.
+
         Returns
         -------
         StatusResponse
-            Request status
+            Response object with the result of the query.
         """
+
         ast = parse_sql(query, dialect="mindsdb")
         return self.query(ast)
     
-    def get_chat_config(self):
+    def get_chat_config(self) -> Dict:
+        """
+        Get the configuration for the chatbot.
+        This method is required for the implementation of the chatbot.
+
+        Returns
+        -------
+        Dict
+            Configuration for the chatbot.
+        """
+
         params = {
             'polling': {
                 'type': 'message_count',
@@ -140,9 +156,20 @@ class MSTeamsHandler(APIChatHandler):
                 'time_col': 'createdDateTime',
             }
         }
+
         return params
     
-    def get_my_user_name(self):
+    def get_my_user_name(self) -> Text:
+        """
+        Get the name of the signed in user.
+        This method is required for the implementation of the chatbot.
+
+        Returns
+        -------
+        Text
+            Name of the signed in user.
+        """        
+
         connection = self.connect()
         user_profile = connection.get_user_profile()
         return user_profile['displayName']

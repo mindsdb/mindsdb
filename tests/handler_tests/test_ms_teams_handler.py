5983237909918890896693420634cb414d9f928e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-14 15:53:11 +0530

    added a few tests for the other tables

--- tests/handler_tests/test_ms_teams_handler.py
+++ tests/handler_tests/test_ms_teams_handler.py
@@ -1,14 +1,9 @@
 import unittest
-import pandas as pd
-from unittest.mock import Mock, patch
-
-from mindsdb_sql.parser import ast
-from mindsdb_sql.parser.ast.select.star import Star
-from mindsdb_sql.parser.ast.select.identifier import Identifier
+from unittest.mock import Mock
 
 from mindsdb.integrations.handlers.ms_teams_handler.ms_teams_handler import MSTeamsHandler
 from mindsdb.integrations.handlers.ms_teams_handler.settings import ms_teams_handler_config
-from mindsdb.integrations.handlers.ms_teams_handler.ms_teams_tables import ChatsTable, ChannelsTable
+from mindsdb.integrations.handlers.ms_teams_handler.ms_teams_tables import ChatsTable, ChatMessagesTable, ChannelsTable, ChannelMessagesTable
 
 
 class TestChatsTable(unittest.TestCase):
@@ -32,6 +27,75 @@ class TestChatsTable(unittest.TestCase):
         chats_table = ChatsTable(self.api_handler)
 
         self.assertListEqual(chats_table.get_columns(), ms_teams_handler_config.CHATS_TABLE_COLUMNS)
+
+
+class TestChatMessagesTable(unittest.TestCase):
+    """
+    Tests for the ChatMessagesTable class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the tests.
+        """
+
+        cls.api_handler = Mock(MSTeamsHandler)
+
+    def test_get_columns_returns_all_columns(self):
+        """
+        Test that get_columns returns all columns.
+        """
+
+        chat_messages_table = ChatMessagesTable(self.api_handler)
+
+        self.assertListEqual(chat_messages_table.get_columns(), ms_teams_handler_config.CHAT_MESSAGES_TABLE_COLUMNS)    
+
+
+class TestChannelsTable(unittest.TestCase):
+    """
+    Tests for the ChannelsTable class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the tests.
+        """
+
+        cls.api_handler = Mock(MSTeamsHandler)
+
+    def test_get_columns_returns_all_columns(self):
+        """
+        Test that get_columns returns all columns.
+        """
+
+        channels_table = ChannelsTable(self.api_handler)
+
+        self.assertListEqual(channels_table.get_columns(), ms_teams_handler_config.CHANNELS_TABLE_COLUMNS)
+
+
+class TestChannelMessagesTable(unittest.TestCase):
+    """
+    Tests for the ChannelMessagesTable class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the tests.
+        """
+
+        cls.api_handler = Mock(MSTeamsHandler)
+
+    def test_get_columns_returns_all_columns(self):
+        """
+        Test that get_columns returns all columns.
+        """
+
+        channel_messages_table = ChannelMessagesTable(self.api_handler)
+
+        self.assertListEqual(channel_messages_table.get_columns(), ms_teams_handler_config.CHANNEL_MESSAGES_TABLE_COLUMNS)       
 
 
 if __name__ == "__main__":

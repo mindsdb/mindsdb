f9a51b752e8291db43a820561d3469d47d1df7fa
tcchawla <tc.chawla2000@gmail.com>
2023-12-31 16:17:37 -0500

    added describe

--- mindsdb/integrations/handlers/leonardoai_handler/leonardo_ai_handler.py
+++ mindsdb/integrations/handlers/leonardoai_handler/leonardo_ai_handler.py
@@ -1,10 +1,14 @@
 import os
 import json
 import contextlib
+from pandas.core.api import DataFrame as DataFrame
 import requests
 import time
 from mindsdb_sql.parser import ast
 from typing import Dict, Optional
+from mindsdb_sql.planner.utils import query_traversal
+from mindsdb.integrations.utilities.sql_utils import extract_comparison_conditions
+from mindsdb.integrations.libs.llm_utils import get_completed_prompts
 
 import pandas as pd
 
@@ -31,32 +35,56 @@ class LeonardoAIHandler(BaseMLEngine):
         self.base_api = LEONARDO_API_BASE
         self.rate_limit = 50
         self.max_batch_size = 5
-        
-    # @staticmethod
-    # @contextlib.contextmanager
-    # def _leonardo_base_api(key='LEONARDO_API_BASE'):
-    #     os.environ['LEONARDO_API_BASE'] = LEONARDO_API_BASE
-    #     try:
-    #         yield
-    #     except KeyError:
-    #         logger.exception('Error getting API key')
             
     def create(self, target: str, args=None, **kwargs):
         
+        # args = args['using']
+        # args['target'] = target
+        
+        print("args: ", args)
         if "using" not in args:
             raise Exception(
                 "Leornardo Engine requires a USING clause!"
             )
-        
-        if "model" not in args["using"]:
-            args["using"]["model"] = self.default_model
-        
+    
+        # elif args["using"]["model"] not in self._get_platform_model(args):
+        #     raise Exception("Leornardo Engine requires a valid model")
+        print("ARGS B: ", args)
         self.model_storage.json_set("args", args)
         
-    def predict(self, df: pd.DataFrame, args: Optional[Dict] = None) -> pd.DataFrame:
+        available_models = self._get_platform_model(args)
+        print("ARGS: ", args)
         
+        if not args['using']['model']:
+            args['using']['model'] = self.default_model
+        elif args['using']['model'] not in available_models:
+            raise Exception(f"Invalid model name. Please use a valid Model")
+        
+    @staticmethod
+    @contextlib.contextmanager
+    def create_validation(self, args=None, **kwargs):
+        if 'using' not in args:
+            raise Exception("LangChain engine requires a USING clause!")
+        else:
+            args = args['using']
+    
+    def predict(self, df: pd.DataFrame, args: Optional[Dict] = None, **kwargs) -> pd.DataFrame:
+        
+        pred_args = args['predict_params'] if args else {}
         args = self.model_storage.json_get("args")
         api_key = self._get_leonardo_api_key(args, self.engine_storage)
+        print("PRED_ARGS: ", pred_args)
+        prompt_template = pred_args.get('prompt_template', args.get('prompt_template', 'Generate a picture of {{{{text}}}}'))
+        
+        # prepare prompts
+        prompts, empty_prompt_id = get_completed_prompts(prompt_template, df)
+        df['__mdb_prompt'] = prompts
+        print(args.values())
+        
+        print("PROMPTS: ", prompts)
+        
+        # input_column = args["using"]["text"]
+        # print("Testing print: ", input_column)
         
         self.connection = requests.get(
             "https://cloud.leonardo.ai/api/rest/v1/me", 
@@ -68,7 +96,7 @@ class LeonardoAIHandler(BaseMLEngine):
         
         print(self.connection.json)
         
-        result_df = pd.DataFrame(self.predict_answer(args))
+        result_df = pd.DataFrame(self.predict_answer(prompts))
         
         # result_df['predictions'] = self.predict_answer(args)
         
@@ -117,8 +145,11 @@ class LeonardoAIHandler(BaseMLEngine):
                  or re-create this model and pass the API key with `USING` syntax.'
             )
             
-    def _get_platform_model(self, api_key):
+    def _get_platform_model(self, args):
         model_ids = []
+        
+        args = self.model_storage.json_get('args')
+        api_key = self._get_leonardo_api_key(args, self.engine_storage)
         
         self.connection = requests.get(
             "https://cloud.leonardo.ai/api/rest/v1/platformModels", 
@@ -127,14 +158,15 @@ class LeonardoAIHandler(BaseMLEngine):
                 "authorization": f"Bearer {api_key}"
             }
         )
+        print(self.connection.json())
         
-        models = self.connection["custom_models"]
+        models = self.connection.json()
         
-        model_ids = [model[id] for model in models]
+        model_ids = [model['id'] for model in models['custom_models']]
         
         return model_ids
             
-    def predict_answer(self, args: Optional[Dict] = None):
+    def predict_answer(self, prompts, **kwargs):
         
         args = self.model_storage.json_get('args')
         print(f"Args: {args}")
@@ -142,7 +174,7 @@ class LeonardoAIHandler(BaseMLEngine):
         print(f"API key: {api_key}")
         generation_id = ''
         model = '6bef9f1b-29cb-40c7-b9df-32b51c1f67d3',
-        prompt = 'Oil Painting of a dog' # text
+        # prompt = 'Oil Painting of a dog' # text
         # Endpoint URLs
         generation_url = "https://cloud.leonardo.ai/api/rest/v1/generations"
         
@@ -160,7 +192,7 @@ class LeonardoAIHandler(BaseMLEngine):
         generation_payload = {
             "height": 512,
             "modelId": args['using']['model'],
-            "prompt": f"{prompt}",
+            "prompt": f"{prompts}",
             "width": 512
         }
         
@@ -213,3 +245,14 @@ class LeonardoAIHandler(BaseMLEngine):
         print("\nDict : ", url_dicts)
         img_urls = pd.DataFrame(url_dicts, columns=['url'])
         return img_urls
+    
+    def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
+        args = self.model_storage.json_get('args')
+        model, target = args['model'], args['target']
+        prompt_template = args.get('prompt_template', 'Generate a picture of {{{{text}}}}')
+        
+        if attribute == "features":
+            return pd.DataFrame([[target, prompt_template]], columns=['target_column', 'mindsdb_prompt_template'])
+        
+        else:
+            pass

f8af681554f58ff4cc12e4c3a96421ac89377322
andrew <elkin.andr@gmail.com>
2024-01-19 16:39:29 +0300

    sentence-transformers to exceptions

--- tests/scripts/check_requirements.py
+++ tests/scripts/check_requirements.py
@@ -46,7 +46,8 @@ MAIN_RULE_IGNORES = {
 # THe following packages need exceptions because they are optional deps of some other packages. e.g. langchain CAN use openai
 # (pysqlite3 is imported in an unusual way in the chromadb handler and needs to be excluded too)
 # pypdf and openpyxl are optional deps of langchain, that are used for the file handler
-OPTIONAL_HANDLER_DEPS = ["pysqlite3", "torch", "openai", "tiktoken", "wikipedia", "anthropic", "pypdf", "openpyxl"]
+OPTIONAL_HANDLER_DEPS = ["pysqlite3", "torch", "openai", "tiktoken", "wikipedia", "anthropic", "pypdf", "openpyxl",
+                         "sentence-transformers"]
 
 # List of rules we can ignore for specific packages
 # Here we ignore any packages in the main requirements.txt for "listed but not used" errors, because they will be used for the core code but not necessarily in a given handler

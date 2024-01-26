b042e44e1d7fa77b5cef95e9e5595003846bb714
tmichaeldb <tyler@mindsdb.com>
2024-01-25 18:16:28 -0800

    Fix test_huggingface_api

--- tests/unit/ml_handlers/test_huggingface_api.py
+++ tests/unit/ml_handlers/test_huggingface_api.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 import os
 import pytest
 import pandas as pd
+import time
 
 from mindsdb_sql import parse_sql
 
@@ -9,6 +10,25 @@ from .base_ml_test import BaseMLAPITest
 
 @pytest.mark.skipif(os.environ.get('HUGGINGFACE_API_KEY') is None, reason='Missing Huggingface API key!')
 class TestHuggingFaceAPI(BaseMLAPITest):
+    def wait_predictor(self, project, name):
+        # wait
+        done = False
+        for attempt in range(200):
+            ret = self.run_sql(
+                f"select * from {project}.models where name='{name}'"
+            )
+            # print(ret['STATUS'][0])
+            if not ret.empty:
+                if ret['STATUS'][0] == 'complete':
+                    done = True
+                    break
+                elif ret['STATUS'][0] == 'error':
+                    # print(f"{ret['ERROR'][0]}")
+                    break
+            time.sleep(1)
+        if not done:
+            raise RuntimeError("predictor wasn't created")
+
     def run_sql(self, sql):
         ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
         assert ret.error_code is None
@@ -35,6 +55,8 @@ class TestHuggingFaceAPI(BaseMLAPITest):
               input_column = 'text'
             """
         )
+
+        self.wait_predictor('proj', 'test_hfapi_text_classification')
 
         result_df = self.run_sql(
             """

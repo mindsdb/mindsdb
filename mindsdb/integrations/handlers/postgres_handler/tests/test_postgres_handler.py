043ae314be4aab6b16df072cd15bd5297f1588f9
hamishfagg <ivdata+github@ivdata.net>
2024-02-02 15:59:00 +1300

    update response_type import

--- mindsdb/integrations/handlers/postgres_handler/tests/test_postgres_handler.py
+++ mindsdb/integrations/handlers/postgres_handler/tests/test_postgres_handler.py
@@ -6,7 +6,7 @@ import time
 from mindsdb.integrations.handlers.postgres_handler.postgres_handler import (
     PostgresHandler,
 )
-from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
+from mindsdb.api.executor.data_types.response_type import RESPONSE_TYPE
 
 HANDLER_KWARGS = {
     "connection_data": {

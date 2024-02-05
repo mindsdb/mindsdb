db0e898cb345b6f01edb305ac5ed84bc6ae306bc
ZoranPandovski <zoran.pandovski@gmail.com>
2024-02-05 17:12:09 +0100

    Init psql tests

---
+++ tests/load/tests_start.py
@@ -0,0 +1,22 @@
+import logging
+from locust import between, HttpUser
+from load.test_mysql import MySQLConnectionBehavior
+from load.test_postgresql import PostgreSQLConnectionBehavior
+from utils.config import get_value_from_json_env_var
+
+class DBConnectionUser(HttpUser):
+    tasks = [PostgreSQLConnectionBehavior, MySQLConnectionBehavior]  
+    wait_time = between(5, 15)
+    config = get_value_from_json_env_var("INTEGRATIONS_CONFIG", "mindsdb_cloud")
+    host = config['host']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            response = self.client.post('/cloud/login', json={
+                'email': self.config['user'],
+                'password': self.config['password']
+            })
+            response.raise_for_status()
+        except Exception as e:
+            logging.error('Logging failed: ', e)

b3e67b2bcd57663f7194d86d5d7f2ab8ca02ad4a
Tyler.S <tyler@mindsdb.com>
2024-03-11 13:25:16 -0700

    Move HTTPHelperMixin to utils where it belongs

--- tests/integration_tests/flows/test_home_rental_prices_tutorial.py
+++ tests/integration_tests/flows/test_home_rental_prices_tutorial.py
@@ -5,7 +5,7 @@ import pytest
 
 from mindsdb.api.executor.data_types.response_type import RESPONSE_TYPE
 from .conftest import CONFIG_PATH
-from .http_test_helpers import HTTPHelperMixin
+from tests.utils.http_test_helpers import HTTPHelperMixin
 
 
 # used by mindsdb_app fixture in conftest

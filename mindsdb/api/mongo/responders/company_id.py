9317c49c4c34eff8cb56b31a3c5a8b0ad494f36d
Max Stepanov <stpmax@yandex.ru>
2024-03-17 18:06:44 +0300

    mongo api encryption key exchange

--- mindsdb/api/mongo/responders/company_id.py
+++ mindsdb/api/mongo/responders/company_id.py
@@ -7,13 +7,18 @@ class Responce(Responder):
         return 'company_id' in query
 
     def result(self, query, request_env, mindsdb_env, session):
+        user_id = query.get('user-id')
         company_id = query.get('company_id')
         user_class = query.get('user_class', 0)
+        encryption_key = query.get('encryption_key')
         email_confirmed = query.get('email_confirmed', 1)
+
         need_response = query.get('need_response', False)
 
+        ctx.user_id = user_id
         ctx.company_id = company_id
         ctx.user_class = user_class
+        ctx.encryption_key = encryption_key
         ctx.email_confirmed = email_confirmed
 
         if need_response:

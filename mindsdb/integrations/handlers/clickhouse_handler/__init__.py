68ded8274efd60a7ca394b19017fcaf371c6094b
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-19 16:28:19 +0530

    improved the titles for ClickHouse, Luma and SharePoint

--- mindsdb/integrations/handlers/clickhouse_handler/__init__.py
+++ mindsdb/integrations/handlers/clickhouse_handler/__init__.py
@@ -12,7 +12,7 @@ except Exception as e:
     Handler = None
     import_error = e
 
-title = 'CliclHouse'
+title = 'ClickHouse'
 name = 'clickhouse'
 type = HANDLER_TYPE.DATA
 icon_path = 'icon.svg'

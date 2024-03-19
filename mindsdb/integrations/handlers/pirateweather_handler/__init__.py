8619549d5ca30944800c535af9ec7241170fcf0e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-19 16:59:35 +0530

    improved titles for Hacker News and Pirate Weather

--- mindsdb/integrations/handlers/pirateweather_handler/__init__.py
+++ mindsdb/integrations/handlers/pirateweather_handler/__init__.py
@@ -12,7 +12,7 @@ except Exception as e:
     Handler = None
     import_error = e
 
-title = "Pirate Weather API"
+title = "Pirate Weather"
 name = "pirateweather"
 type = HANDLER_TYPE.DATA
 icon_path = "icon.png"

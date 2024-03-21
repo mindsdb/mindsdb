c32d8a7bedf494999f994b301511411fda1b1e6a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-21 16:29:50 +0530

    added parsing for JSON credentials

--- mindsdb/integrations/handlers/utilities/auth_utilities/google/google_service_account_oauth_utilities.py
+++ mindsdb/integrations/handlers/utilities/auth_utilities/google/google_service_account_oauth_utilities.py
@@ -1,4 +1,6 @@
+import json
 import requests
+from typing import Union
 from google.oauth2 import service_account
 
 from mindsdb.utilities import log
@@ -10,17 +12,23 @@ logger = log.getLogger(__name__)
 
 
 class GoogleServiceAccountOAuth2Manager:
-    def __init__(self, credentials_url: str = None, credentials_file: str = None, credentials_json: dict = None) -> None:
+    def __init__(self, credentials_url: str = None, credentials_file: str = None, credentials_json: Union[dict, str] = None) -> None:
         # if no credentials provided, raise an exception
         if not any([credentials_url, credentials_file, credentials_json]):
             raise NoCredentialsException('No valid Google Service Account credentials provided.')
         self.credentials_url = credentials_url
         self.credentials_file = credentials_file
-        self.credentials_json = credentials_json
+        if isinstance(credentials_json, str):
+            # convert to JSON
+            self.credentials_json = json.loads(credentials_json)
+        else:
+            # unescape new lines in private_key
+            credentials_json['private_key'] = credentials_json['private_key'].replace('\\n', '\n')
+            self.credentials_json = credentials_json
 
     def get_oauth2_credentials(self):
         try:
-            if self.credenetials_url:
+            if self.credentials_url:
                 creds = service_account.Credentials.from_service_account_info(self._download_credentials_file())
                 return creds
 

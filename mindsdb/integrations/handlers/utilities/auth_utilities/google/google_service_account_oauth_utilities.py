f31c7ba6858e24027404610e4b164683ae942cd6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-03-21 13:14:44 +0530

    raised NoCredentialsException if no creds provided

--- mindsdb/integrations/handlers/utilities/auth_utilities/google/google_service_account_oauth_utilities.py
+++ mindsdb/integrations/handlers/utilities/auth_utilities/google/google_service_account_oauth_utilities.py
@@ -1,6 +1,8 @@
-from google.oauth2 import service_account
-
 from mindsdb.utilities import log
+
+from ..exceptions import NoCredentialsException
+
+from google.oauth2 import service_account
 
 
 logger = log.getLogger(__name__)
@@ -8,6 +10,9 @@ logger = log.getLogger(__name__)
 
 class GoogleServiceAccountOauth2Utilities:
     def __init__(self, credentials_url: str = None, credentials_file: str = None, credentials_json: dict = None) -> None:
+        # if no credentials provided, raise an exception
+        if not any([credentials_url, credentials_file, credentials_json]):
+            raise NoCredentialsException('No valid Google Service Account credentials provided.')
         self.credentials_url = credentials_url
         self.credentials_file = credentials_file
         self.credentials_json = credentials_json

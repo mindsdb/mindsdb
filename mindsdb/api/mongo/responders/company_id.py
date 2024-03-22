049f578549541e161f03c1600777adb7b8914446
Max Stepanov <stpmax@yandex.ru>
2024-03-22 18:03:14 +0300

    mongo protocol warning

--- mindsdb/api/mongo/responders/company_id.py
+++ mindsdb/api/mongo/responders/company_id.py
@@ -1,5 +1,10 @@
 from mindsdb.api.mongo.classes import Responder
+from mindsdb.utilities import log
+from mindsdb.utilities.config import Config
 from mindsdb.utilities.context import context as ctx
+
+
+logger = log.getLogger(__name__)
 
 
 class Responce(Responder):
@@ -20,6 +25,10 @@ class Responce(Responder):
         ctx.user_class = user_class
         ctx.encryption_key = encryption_key
         ctx.email_confirmed = email_confirmed
+
+        if Config().get("cloud", False) is True and ctx.encryption_key is None:
+            logger.warn("Got cloud Mongo request with missed encryption key")
+            # TODO Raise exception here after cloud migrations
 
         if need_response:
             return {'ok': 1}

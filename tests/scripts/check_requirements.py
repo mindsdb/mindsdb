54127b4cd832b441db24f1c0dd5e9d228a2751cd
ZoranPandovski <zoran.pandovski@gmail.com>
2024-03-27 15:23:02 +0100

    Exclude grpc from requirements script test

--- tests/scripts/check_requirements.py
+++ tests/scripts/check_requirements.py
@@ -23,7 +23,6 @@ def get_requirements_from_file(path):
 MAIN_REQS_PATH = "requirements/requirements.txt"
 DEV_REQS_PATH = "requirements/requirements-dev.txt"
 TEST_REQS_PATH = "requirements/requirements-test.txt"
-GRPC_REQS_PATH = "requirements/requirements-grpc.txt"
 DOCKER_REQS_PATH = "docker/handler_discovery/requirements.txt"
 
 HANDLER_REQS_PATHS = list(
@@ -52,7 +51,7 @@ OPTIONAL_HANDLER_DEPS = ["pysqlite3", "torch", "openai", "tiktoken", "wikipedia"
 # List of rules we can ignore for specific packages
 # Here we ignore any packages in the main requirements.txt for "listed but not used" errors, because they will be used for the core code but not necessarily in a given handler
 MAIN_REQUIREMENTS_DEPS = get_requirements_from_file(MAIN_REQS_PATH) + get_requirements_from_file(
-    TEST_REQS_PATH) + get_requirements_from_file(GRPC_REQS_PATH)
+    TEST_REQS_PATH)
 
 BYOM_HANLDER_DEPS = ["pyarrow"]
 
@@ -269,7 +268,7 @@ def check_requirements_imports():
 
     # Run against the main codebase
     errors = run_deptry(
-        ','.join([MAIN_REQS_PATH, GRPC_REQS_PATH, DOCKER_REQS_PATH]),
+        ','.join([MAIN_REQS_PATH, DOCKER_REQS_PATH]),
         get_ignores_str(MAIN_RULE_IGNORES),
         ".",
         f"--extend-exclude \"{'|'.join(MAIN_EXCLUDE_PATHS)}\"",

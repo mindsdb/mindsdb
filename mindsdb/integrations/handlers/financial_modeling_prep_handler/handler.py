b28b29c02e677e867b00616b59e62dfb837e9702
RiyanaD <riyana.duttaa@gmail.com>
2024-04-04 22:23:38 -0700

    set up handler.py

---
+++ mindsdb/integrations/handlers/financial_modeling_prep_handler/handler.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from typing import Dict
+
+from mindsdb.integrations.libs.api_handler import APIHandler
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse,
+    HandlerResponse as Response,
+)
+from mindsdb.utilities import log
+from mindsdb_sql import parse_sql

8abf1c17e1ea8dc512f42aa3b438b1cda57ec5d7
Daniel Usvyat <usvyat@gmail.com>
2024-04-19 18:17:52 +0100

    Minor tweaks to retrieval skill and file ingestion (#9093)
    * Update default params and handle missing embedding args
    
    The default chunk size and overlap values in file_handler have been increased for better performance. In langchain_handler, checks have been added to use the same provider and model for embeddings if 'embedding_model_args' are not provided in input params. Skill_tool's retriever_config has been renamed to 'config' for clarity.
    
    * Handle invoke errors in langchain_handler
    
    This update modifies the langchain handler to accommodate errors during the invocation process. Specifically, it implements exception handling for the agent_executor's invoke method. If an error occurs, instead of crashing, it will now return the error message. However, if the error doesn't match a specific format, the exception will still be raised.
    
    * Update file_handler.py
    
    * Add logging and refactor default vector store in langchain handler
    
    This change introduces logging for situations where 'vector_store_config' is not present in the `langchain_handler` tool configuration. It also modifies the condition that checks for the absence of this property to add a persisting directory. Furthermore, it refactors the code in `langchain_handler.py` related to the absence of 'embedding_model_args'.
    
    * Remove default vector store from langchain handler
    
    The default vector store setup was removed from the langchain handler. The 'vector_store_config' is no longer automatically assigned, and an alert for this missing parameter is no longer logged. The code was adjusted to operate without the previously used 'mindsdb_path'.

--- mindsdb/integrations/handlers/langchain_handler/tools.py
+++ mindsdb/integrations/handlers/langchain_handler/tools.py
@@ -19,6 +19,9 @@ from langchain.chains import ReduceDocumentsChain, MapReduceDocumentsChain
 from mindsdb.integrations.utilities.rag.rag_pipeline_builder import RAG
 from mindsdb.integrations.utilities.rag.settings import RAGPipelineModel
 from mindsdb.interfaces.skills.skill_tool import skill_tool, SkillType
+from mindsdb.utilities import log
+
+logger = log.getLogger(__name__)
 
 # Individual tools
 # Note: all tools are defined in a closure to pass required args (apart from LLM input) through it, as custom tools don't allow custom field assignment.  # noqa
@@ -165,16 +168,10 @@ def _build_retrieval_tool(tool: dict, pred_args: dict):
 
     tools_config = tool['config']
 
-    mindsdb_path = pred_args['mindsdb_path']
-
     # we update the config with the pred_args to allow for custom config
     tools_config.update(pred_args)
 
     rag_params = _get_rag_params(tools_config)
-
-    if 'vector_store_config' not in rag_params:
-
-        rag_params['vector_store_config'] = {'persist_directory': mindsdb_path('persisted_chroma')}
 
     rag_config = RAGPipelineModel(**rag_params)
 
@@ -187,6 +184,7 @@ def _build_retrieval_tool(tool: dict, pred_args: dict):
         name=tool['name'],
         description=tool['description']
     )
+
 
 def langchain_tool_from_skill(skill, pred_args):
     # Makes Langchain compatible tools from a skill

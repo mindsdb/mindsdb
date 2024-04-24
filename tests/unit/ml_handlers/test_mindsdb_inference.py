8997213aacece3db4e9f69fcded1262dde6dc8c6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-24 15:29:48 +0530

    improved comments and docstrings

--- tests/unit/ml_handlers/test_mindsdb_inference.py
+++ tests/unit/ml_handlers/test_mindsdb_inference.py
@@ -24,17 +24,19 @@ class TestMindsDBInference(unittest.TestCase):
 
     def test_create_validation(self):
         """
-        Test if `create_validation` method raises an Exception when `using` is not present in args.
+        Test if model creation is validated correctly.
         """
+
         with self.assertRaises(Exception):
             self.handler.create_validation('target', args={}, handler_storage=None)
 
     @patch('mindsdb.integrations.handlers.openai_handler.helpers.OpenAI')
     def test_create(self, mock_openai):
         """
-        Test if `create` method returns a MindsDBInferenceHandler object.
+        Test if model creation runs without raising an Exception.
         """
 
+        # Mock the models.list method of the OpenAI client
         mock_models_list = MagicMock()
         mock_models_list.data = [
             MagicMock(id='dummy_model_name')
@@ -44,18 +46,18 @@ class TestMindsDBInference(unittest.TestCase):
 
         self.handler.create('dummy_target', args={'using': {'model_name': 'dummy_model_name', 'prompt_template': 'dummy_prompt_template'}})
 
-    @patch('mindsdb.integrations.handlers.mindsdb_inference.mindsdb_inference_handler.MindsDBInferenceHandler._get_supported_models')
-    @patch('mindsdb.integrations.handlers.openai_handler.openai_handler.OpenAIHandler._get_client')
-    def test_predict_sentiment_analysis(self, mock_get_client, mock_get_models):
+    @patch('mindsdb.integrations.handlers.mindsdb_inference.mindsdb_inference_handler.openai.OpenAI')
+    @patch('mindsdb.integrations.handlers.openai_handler.openai_handler.OpenAI')
+    def test_predict_sentiment_analysis(self, mock_openai_openai_handler, mock_openai_mdb_inference_handler):
         """
-        Test if `predict` method returns a DataFrame.
+        Test if model prediction returns the expected result for a sentiment analysis task.
         """
 
-        # Create a list of mock objects each with an `id` attribute
+        # Mock the models.list method of the OpenAI client (for the MindsDB Inference handler)
         mock_supported_models = [MagicMock(id='mistral-7b')]
-        mock_get_models.return_value = mock_supported_models
-
-        # Define a return value for the `json_get` method of the mock model storage
+        mock_openai_mdb_inference_handler.return_value.models.list.return_value = mock_supported_models
+        
+        # Mock the json_get method of the model storage
         self.handler.model_storage.json_get.return_value = {
             'model_name': 'mistral-7b',
             'prompt_template': 'Classify the sentiment of the following text as one of `positive`, `neutral` or `negative`: {{text}}',
@@ -64,10 +66,9 @@ class TestMindsDBInference(unittest.TestCase):
             'api_base': 'https://llm.mdb.ai/'
         }
 
-        df = pandas.DataFrame({'text': ['I love MindsDB!']})
-
-        mock_openai = MagicMock()
-        mock_openai.chat.completions.create.return_value = MagicMock(
+        # Mock the chat.completions.create method of the OpenAI client (for the OpenAI handler)
+        mock_openai_client = MagicMock()
+        mock_openai_client.chat.completions.create.return_value = MagicMock(
             choices=[
                 MagicMock(
                     message=MagicMock(
@@ -77,8 +78,9 @@ class TestMindsDBInference(unittest.TestCase):
             ]
         )
 
-        mock_get_client.return_value = mock_openai
+        mock_openai_openai_handler.return_value = mock_openai_client
 
+        df = pandas.DataFrame({'text': ['I love MindsDB!']})
         result = self.handler.predict(df, args={})
 
         self.assertIsInstance(result, pandas.DataFrame)

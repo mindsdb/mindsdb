115125d81f6b3a7fb0ec862b904f3a897a193dcd
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-26 16:55:36 +0530

    cleaned up the check_connection and disconnect methods

--- mindsdb/integrations/handlers/snowflake_handler/snowflake_handler.py
+++ mindsdb/integrations/handlers/snowflake_handler/snowflake_handler.py
@@ -15,6 +15,7 @@ from mindsdb.utilities import log
 
 logger = log.getLogger(__name__)
 
+
 class SnowflakeHandler(DatabaseHandler):
     """
     This handler handles connection and execution of the Snowflake statements.
@@ -29,52 +30,86 @@ class SnowflakeHandler(DatabaseHandler):
         self.connection = None
 
     def connect(self):
+        """
+        Establishes a connection to a Snowflake account.
+
+        Raises:
+            snowflake.connector.errors.Error: If an error occurs while connecting to the Snowflake account.
+
+        Returns:
+            snowflake.connector.connection.SnowflakeConnection: A connection object to the Snowflake account.
+        """
+
         if self.is_connected is True:
             return self.connection
-        con = connector.connect(
-            host=self.connection_data['host'],
-            user=self.connection_data['user'],
-            password=self.connection_data['password'],
-            account=self.connection_data['account'],
-            warehouse=self.connection_data['warehouse'],
-            database=self.connection_data['database'],
-            schema=self.connection_data['schema'],
-            protocol=self.connection_data['protocol'],
-            port=self.connection_data['port'],
-            application='MindsDB'
-        )
-        self.is_connected = True
-        self.connection = con
-        return self.connection
+        
+        # Mandatory connection parameters
+        if not all(key in self.connection_data for key in ['account', 'user', 'password']):
+            raise ValueError('Required parameters (account, user, password) must be provided.')
+
+        config = {
+            'account': self.connection_data.get('account'),
+            'user': self.connection_data.get('user'),
+            'password': self.connection_data.get('password')
+        }
+
+        # Optional connection parameters
+        if 'database' in self.connection_data:
+            config['database'] = self.connection_data.get('database')
+
+        if 'schema' in self.connection_data:
+            config['schema'] = self.connection_data.get('schema')
+
+        if 'warehouse' in self.connection_data:
+            config['warehouse'] = self.connection_data.get('warehouse')
+
+        if 'role' in self.connection_data:
+            config['role'] = self.connection_data.get('role')
+        
+        try:
+            self.connection = connector.connect(**config)
+            self.is_connected = True
+            return self.connection
+        except connector.errors.Error as e:
+            logger.error(f'Error connecting to Snowflake, {e}!')
+            raise
 
     def disconnect(self):
+        """
+        Closes the connection to the Snowflake account if it's currently open.
+        """
+
         if self.is_connected is False:
             return
         self.connection.close()
         self.is_connected = False
-        return 
 
     def check_connection(self) -> StatusResponse:
         """
-        Check the connection
-        :return: success status and error message if error occurs
+        Checks the status of the connection to the Snowflake account.
+
+        Returns:
+            StatusResponse: An object containing the success status and an error message if an error occurs.
         """
+
         response = StatusResponse(False)
         need_to_close = self.is_connected is False
         try:
+            # Execute a simple query to test the connection
             connection = self.connect()
             with connection.cursor() as cur:
                 cur.execute('select 1;')
             response.success = True
         except connector.errors.Error as e:
-            logger.error(f'Error connecting to Snowflake {self.connection_data["database"]}, {e}!')
-            response.error_message = e
+            logger.error(f'Error connecting to Snowflake, {e}!')
+            response.error_message = str(e)
 
-        if response.success is True and need_to_close:
+        if response.success and need_to_close:
             self.disconnect()
 
-        if response.success is False and self.is_connected is True:
+        elif not response.success and self.is_connected:
             self.is_connected = False
+            
         return response
 
     def native_query(self, query: str) -> Response:

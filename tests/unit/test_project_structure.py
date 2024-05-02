82a8ea984c76fda24f535c38455cae2a77bcb298
andrew <elkin.andr@gmail.com>
2024-05-02 12:28:29 +0300

    tests

--- tests/unit/test_project_structure.py
+++ tests/unit/test_project_structure.py
@@ -1116,6 +1116,34 @@ class TestJobs(BaseExecutorDummyML):
         ret = self.run_sql('select * from models where name="pred"')
         assert len(ret) == 1
 
+    def test_model_column_maping(self):
+        df = pd.DataFrame([
+            {'a': 10, 'c': 30},
+            {'a': 20, 'c': 40},
+        ])
+        self.set_data('tbl', df)
+
+        self.run_sql(
+            '''
+                CREATE model mindsdb.pred
+                PREDICT p
+                using engine='dummy_ml',
+                join_learn_process=true
+            '''
+        )
+        ret = self.run_sql('''
+            select * from dummy_data.tbl t
+            join pred m on m.input = t.a
+        ''')
+        assert ret['output'][0] == 10
+
+        # without aliases
+        ret = self.run_sql('''
+            select * from dummy_data.tbl
+            join pred on pred.input = tbl.c
+        ''')
+        assert ret['output'][0] == 30
+
     def test_schema(self, scheduler):
 
         # --- create objects + describe ---

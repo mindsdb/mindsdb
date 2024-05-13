55ca7461c93b9086139eda12f867b108ed2928ca
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-13 21:36:02 +0530

    added more config vars to settings

--- mindsdb/integrations/handlers/anyscale_endpoints_handler/settings.py
+++ mindsdb/integrations/handlers/anyscale_endpoints_handler/settings.py
@@ -1,3 +1,4 @@
+from typing import Text, List
 from pydantic_settings import BaseSettings
 
 from mindsdb.integrations.handlers.openai_handler.constants import OPENAI_API_BASE
@@ -10,20 +11,47 @@ class AnyscaleHandlerConfig(BaseSettings):
     Attributes
     ----------
 
-    ANYSCALE_API_BASE : str
+    ANYSCALE_API_BASE : Text
         Base URL for the Anyscale API.
 
+    OPENAI_API_BASE : Text
+        Base URL for the OpenAI API.
+
     MIN_FT_VAL_LEN : int
+        Minimum number of validation chats required for fine-tuning.
     
     MIN_FT_DATASET_LEN : int
+        Minimum number of training and validation chats required for fine-tuning.
 
+    DEFAULT_MODEL : Text
+        Default model to use for models.
+
+    DEFAULT_MODE : Text
+        Default mode to use for models. Can be 'default', 'conversational', or 'conversational-full'.
+
+    SUPPORTED_MODES : List
+        List of supported modes. Can be 'default', 'conversational', or 'conversational-full'.
+
+    RATE_LIMIT : int
+        Number of requests per minute.
+
+    MAX_BATCH_SIZE : int
+        Maximum batch size for requests.
+
+    DEFAULT_MAX_TOKENS : int
+        Default maximum tokens for requests.
     """
 
-    ANYSCALE_API_BASE = 'https://api.endpoints.anyscale.com/v1'
-    OPENAI_API_BASE = OPENAI_API_BASE
-    # TODO: Are these comments correct? Update the attribute definitions in the above docstring as well.
-    MIN_FT_VAL_LEN = 20  # anyscale checks for at least 20 validation chats
-    MIN_FT_DATASET_LEN = MIN_FT_VAL_LEN * 2  # we ask for 20 training chats as well
+    ANYSCALE_API_BASE: Text = 'https://api.endpoints.anyscale.com/v1'
+    OPENAI_API_BASE: Text = OPENAI_API_BASE
+    MIN_FT_VAL_LEN: int = 20
+    MIN_FT_DATASET_LEN: int = MIN_FT_VAL_LEN * 2
+    DEFAULT_MODEL: Text = 'meta-llama/Llama-2-7b-chat-hf'
+    DEFAULT_MODE: Text = 'default'
+    SUPPORTED_MODES: List = ['default', 'conversational', 'conversational-full']
+    RATE_LIMIT: int = 25
+    MAX_BATCH_SIZE: int = 20
+    DEFAULT_MAX_TOKENS: int = 100
 
 
 anyscale_handler_config = AnyscaleHandlerConfig()

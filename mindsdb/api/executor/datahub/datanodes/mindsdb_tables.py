3597bb206aed6dcb16d5733ecae03c76d1f735fd
Max Stepanov <stpmax@yandex.ru>
2024-05-14 15:17:07 +0300

    hide sensitive args in models

--- mindsdb/api/executor/datahub/datanodes/mindsdb_tables.py
+++ mindsdb/api/executor/datahub/datanodes/mindsdb_tables.py
@@ -65,38 +65,36 @@ class ModelsTable(MdbTable):
     ]
 
     @classmethod
-    def get_data(cls, inf_schema, **kwargs):
+    def get_data(cls, session, inf_schema, **kwargs):
         data = []
         for project_name in inf_schema.get_projects_names():
             project = inf_schema.database_controller.get_project(name=project_name)
-            project_models = project.get_models(active=None)
+            project_models = project.get_models(active=None, with_secrets=session.show_secrets)
             for row in project_models:
                 table_name = row["name"]
                 table_meta = row["metadata"]
 
-                data.append(
-                    [
-                        table_name,
-                        table_meta["engine"],
-                        project_name,
-                        table_meta["active"],
-                        table_meta["version"],
-                        table_meta["status"],
-                        table_meta["accuracy"],
-                        table_meta["predict"],
-                        table_meta["update_status"],
-                        table_meta["mindsdb_version"],
-                        table_meta["error"],
-                        table_meta["select_data_query"],
-                        to_json(table_meta["training_options"]),
-                        table_meta["current_training_phase"],
-                        table_meta["total_training_phases"],
-                        table_meta["training_phase_name"],
-                        table_meta["label"],
-                        row["created_at"],
-                        table_meta["training_time"],
-                    ]
-                )
+                data.append([
+                    table_name,
+                    table_meta["engine"],
+                    project_name,
+                    table_meta["active"],
+                    table_meta["version"],
+                    table_meta["status"],
+                    table_meta["accuracy"],
+                    table_meta["predict"],
+                    table_meta["update_status"],
+                    table_meta["mindsdb_version"],
+                    table_meta["error"],
+                    table_meta["select_data_query"],
+                    to_json(table_meta["training_options"]),
+                    table_meta["current_training_phase"],
+                    table_meta["total_training_phases"],
+                    table_meta["training_phase_name"],
+                    table_meta["label"],
+                    row["created_at"],
+                    table_meta["training_time"],
+                ])
             # TODO optimise here
             # if target_table is not None and target_table != project_name:
             #     continue
--- mindsdb/integrations/handlers/openai_handler/__init__.py
+++ mindsdb/integrations/handlers/openai_handler/__init__.py
@@ -2,6 +2,7 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 
 from .__about__ import __version__ as version, __description__ as description
 from .creation_args import creation_args
+from .model_using_args import model_using_args
 try:
     from .openai_handler import OpenAIHandler as Handler
     import_error = None
@@ -16,5 +17,6 @@ icon_path = 'icon.svg'
 permanent = False
 
 __all__ = [
-    'Handler', 'version', 'name', 'type', 'title', 'description', 'import_error', 'icon_path', 'creation_args'
+    'Handler', 'version', 'name', 'type', 'title', 'description',
+    'import_error', 'icon_path', 'creation_args', 'model_using_args'
 ]

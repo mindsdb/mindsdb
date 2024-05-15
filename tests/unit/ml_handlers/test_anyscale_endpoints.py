3b6f9c48683a44d0bf4f781407fc933af471c8fe
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-15 18:35:12 +0530

    added unit tests for predict()

--- tests/unit/ml_handlers/test_anyscale_endpoints.py
+++ tests/unit/ml_handlers/test_anyscale_endpoints.py
@@ -114,6 +114,101 @@ class TestAnyscaleEndpoints(unittest.TestCase):
 
         self.handler.create('dummy_target', args={'using': {'model_name': 'dummy_model_name', 'prompt_template': 'dummy_prompt_template'}})
 
+    @patch('mindsdb.integrations.handlers.openai_handler.openai_handler.OpenAI')
+    def test_predict_runs_no_errors_on_default_mode_prompt_completion(self, mock_openai_handler_openai_client):
+        """
+        Test if model prediction returns the expected result for a sentiment analysis task.
+        """
+
+        mock_openai_client = MagicMock()
+
+        # Mock the models.list method of the OpenAI client
+        mock_models_list = [
+            MagicMock(
+                id='dummy_model_name',
+            )
+        ]
+
+        mock_openai_client.models.list.return_value = mock_models_list
+
+        # Mock the json_get method of the model storage
+        self.handler.model_storage.json_get.return_value = {
+            'model_name': 'dummy_model_name',
+            'prompt_template': 'Classify the sentiment of the following text as one of `positive`, `neutral` or `negative`: {{text}}',
+            'target': 'sentiment',
+            'mode': 'default'
+        }
+
+        # Mock the completions.create method of the OpenAI client (for the OpenAI handler)
+        mock_openai_client.completions.create.return_value = MagicMock(
+            choices=[
+                MagicMock(
+                    text='Positive'
+                )
+            ]
+        )
+
+        mock_openai_handler_openai_client.return_value = mock_openai_client
+
+        df = pandas.DataFrame({'text': ['I love MindsDB!']})
+        result = self.handler.predict(df, args={})
+
+        self.assertIsInstance(result, pandas.DataFrame)
+        self.assertTrue('sentiment' in result.columns)
+
+        pandas.testing.assert_frame_equal(result, pandas.DataFrame({'sentiment': ['Positive']}))
+
+    @patch('mindsdb.integrations.handlers.openai_handler.openai_handler.OpenAI')
+    def test_predict_runs_no_errors_on_default_mode_question_answering(self, mock_openai_handler_openai_client):
+        """
+        Test if model prediction returns the expected result for a question answering task.
+        """
+
+        mock_openai_client = MagicMock()
+
+        # Mock the models.list method of the OpenAI client
+        mock_models_list = [
+            MagicMock(
+                id='dummy_model_name',
+                rayllm_metadata={
+                    'engine_config': {
+                        'model_type': 'text-generation'
+                    }
+                }
+            )
+        ]
+
+        mock_openai_client.models.list.return_value = mock_models_list
+
+        # Mock the json_get method of the model storage
+        self.handler.model_storage.json_get.return_value = {
+            'model_name': 'dummy_model_name',
+            'question_column': 'question',
+            'target': 'answer',
+            'mode': 'default'
+        }
+
+        # Mock the chat.completions.create method of the OpenAI client (for the OpenAI handler)
+        mock_openai_client.chat.completions.create.return_value = MagicMock(
+            choices=[
+                MagicMock(
+                    message=MagicMock(
+                        content='Sweden'
+                    )
+                )
+            ]
+        )
+
+        mock_openai_handler_openai_client.return_value = mock_openai_client
+
+        df = pandas.DataFrame({'question': ['Where is Stockholm located?']})
+        result = self.handler.predict(df, args={})
+
+        self.assertIsInstance(result, pandas.DataFrame)
+        self.assertTrue('answer' in result.columns)
+
+        pandas.testing.assert_frame_equal(result, pandas.DataFrame({'answer': ['Sweden']}))
+
 
 if __name__ == '__main__':
     unittest.main()

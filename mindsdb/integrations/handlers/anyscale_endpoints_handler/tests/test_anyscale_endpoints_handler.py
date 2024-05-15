33937d6092fd32ccdffe72cada95507832eb4183
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-15 10:06:49 +0530

    fixed the select() integration tests

--- mindsdb/integrations/handlers/anyscale_endpoints_handler/tests/test_anyscale_endpoints_handler.py
+++ mindsdb/integrations/handlers/anyscale_endpoints_handler/tests/test_anyscale_endpoints_handler.py
@@ -7,7 +7,7 @@ from tests.unit.ml_handlers.base_ml_test import BaseMLAPITest
 
 
 @pytest.mark.skipif(os.environ.get('MDB_TEST_ANYSCALE_ENDPOINTS_API_KEY') is None, reason='Missing API key!')
-class TestMindsDBInference(BaseMLAPITest):
+class TestAnyscaleEndpoints(BaseMLAPITest):
     """
     Integration tests for Anyscale Endpoints AI engine.
     """
@@ -82,65 +82,67 @@ class TestMindsDBInference(BaseMLAPITest):
         with pytest.raises(Exception):
             self.wait_predictor("proj", "test_anyscale_invalid_operation_mode")
 
-    def test_select_runs_no_errors_on_chat_completion_question_answering_single(self):
+    def test_select_runs_no_errors_on_completion_sentiment_analysis_single(self):
         """
-        Test for a valid answer to a question answering task (chat completion).
+        Test for a valid response to a sentiment analysis task (completion).
         """
 
         self.run_sql(
-            f"""
-            CREATE MODEL proj.test_anyscale_single_qa
-            PREDICT answer
+            """
+            CREATE MODEL proj.test_anyscale_single_sa
+            PREDICT sentiment
             USING
                 engine='anyscale_endpoints_engine',
-                question_column='question';
+                model_name = 'mistralai/Mistral-7B-Instruct-v0.1',
+                prompt_template = 'Classify the sentiment of the following text as one of `positive`, `neutral` or `negative`: {{text}}';
             """
         )
-        self.wait_predictor("proj", "test_anyscale_single_qa")
+        self.wait_predictor("proj", "test_anyscale_single_sa")
 
         result_df = self.run_sql(
             """
-            SELECT answer
-            FROM proj.test_anyscale_single_qa
-            WHERE question = 'What is the capital of Sweden?';
+            SELECT sentiment
+            FROM proj.test_anyscale_single_sa
+            WHERE text = 'I love machine learning!';
             """
         )
 
-        assert "stockholm" in result_df["answer"].iloc[0].lower()
+        assert "positive" in result_df["sentiment"].iloc[0].lower()
 
     @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
-    def test_select_runs_no_errors_on_chat_completion_question_answering_bulk(self, mock_postgres_handler):
+    def test_select_runs_no_errors_on_completion_sentiment_analysis_bulk(self, mock_postgres_handler):
         """
-        Test for valid answers to bulk questions in a question answering task (chat completion).
+        Test for valid reponses to bulk questions in a sentiment analysis task (completion).
         """
 
-        df = pd.DataFrame.from_dict({"question": [
-            "What is the capital of Sweden?",
-            "What is the second planet in the solar system?"
+        df = pd.DataFrame.from_dict({"text": [
+            "I love machine learning!",
+            "I hate slow internet connections!"
         ]})
         self.set_handler(mock_postgres_handler, name="pg", tables={"df": df})
 
         self.run_sql(
-            f"""
-            CREATE MODEL proj.test_anyscale_bulk_qa
-            PREDICT answer
+            """
+            CREATE MODEL proj.test_anyscale_bulk_sa
+            PREDICT sentiment
             USING
                 engine='anyscale_endpoints_engine',
-                question_column='question';
+                model_name = 'mistralai/Mistral-7B-Instruct-v0.1',
+                prompt_template = 'Classify the sentiment of the following text as one of `positive`, `neutral` or `negative`: {{text}}';
             """
         )
-        self.wait_predictor("proj", "test_anyscale_bulk_qa")
+        self.wait_predictor("proj", "test_anyscale_bulk_sa")
 
         result_df = self.run_sql(
             """
-            SELECT p.answer
+            SELECT p.sentiment
             FROM pg.df as t
-            JOIN proj.test_anyscale_bulk_qa as p;
+            JOIN proj.test_anyscale_bulk_sa as p;
         """
         )
 
-        assert "stockholm" in result_df["answer"].iloc[0].lower()
-        assert "venus" in result_df["answer"].iloc[1].lower()
+        assert "positive" in result_df["sentiment"].iloc[0].lower()
+        assert "negative" in result_df["sentiment"].iloc[1].lower()
 
 
 if __name__ == '__main__':

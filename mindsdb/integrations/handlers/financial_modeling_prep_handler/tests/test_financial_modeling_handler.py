994be9855ca3e09d39dc5c2064324fc8c08865e7
RiyanaD <riyana.duttaa@gmail.com>
2024-05-15 20:49:56 -0700

    test for limit

--- mindsdb/integrations/handlers/financial_modeling_prep_handler/tests/test_financial_modeling_handler.py
+++ mindsdb/integrations/handlers/financial_modeling_prep_handler/tests/test_financial_modeling_handler.py
@@ -11,3 +11,8 @@ class FinancialModelingHandlerTest(unittest.TestCase):
         query = "SELECT * FROM my_table.daily_chart_table WHERE symbol = 'AAPL'"
         result = self.handler.query(query)
         assert result.type is RESPONSE_TYPE.TABLE
+
+    def test_1_select_limit_query(self):
+        query = "SELECT * FROM my_table.daily_chart_table WHERE symbol = 'AAPL' LIMIT 5"
+        result = self.handler.query(query)
+        assert result.shape[0] == 5 

3216c894f26f24bdc8f716e0a031784a7eea1bb0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-22 19:23:24 +0530

    added integration tests for default mode

--- mindsdb/integrations/handlers/openai_handler/tests/test_openai_handler.py
+++ mindsdb/integrations/handlers/openai_handler/tests/test_openai_handler.py
@@ -16,7 +16,6 @@ class TestOpenAI(BaseMLAPITest):
         """
         Setup test environment by creating a project and an OpenAI engine.
         """
-
         super().setup_method()
         self.run_sql("CREATE DATABASE proj")
         self.run_sql(
@@ -32,7 +31,6 @@ class TestOpenAI(BaseMLAPITest):
         """
         Test if CREATE MODEL raises an exception with an unsupported model.
         """
-
         self.run_sql(
             f"""
             CREATE MODEL proj.test_openaai_unsupported_model_model
@@ -47,6 +45,125 @@ class TestOpenAI(BaseMLAPITest):
             self.wait_predictor("proj", "test_openaai_unsupported_model_model")
 
         assert "Invalid model name." in str(excinfo.value)
+
+    def test_single_full_flow_in_default_mode_runs_no_errors_with_question_column(self):
+        """
+        Test the full flow in default mode with a question column for a single prediction.
+        """
+        self.run_sql(
+            f"""
+            CREATE MODEL proj.test_openai_single_full_flow_default_mode_question_column
+            PREDICT answer
+            USING
+                engine='openai_engine';
+                question_column='question';
+            """
+        )
+
+        self.wait_predictor("proj", "test_openai_single_full_flow_default_mode_question_column")
+
+        result_df = self.run_sql(
+            """
+            SELECT answer
+            FROM proj.test_openai_single_full_flow_default_mode_question_column
+            WHERE question='What is the capital of Sweden?'
+            """
+        )
+        assert "stockholm" in result_df["answer"].iloc[0].lower()
+
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_bulk_full_flow_in_default_mode_runs_no_errors_with_question_column(self, mock_handler):
+        """
+        Test the full flow in default mode with a question column for bulk predictions.
+        """
+        df = pd.DataFrame.from_dict({"question": [
+            "What is the capital of Sweden?",
+            "What is the second planet of the solar system?"
+        ]})
+        self.set_handler(mock_handler, name="pg", tables={"df": df})
+
+        self.run_sql(
+            f"""
+            CREATE MODEL proj.test_openai_bulk_full_flow_default_mode_question_column
+            PREDICT answer
+            USING
+                engine='openai_engine',
+                question_column='question';
+            """
+        )
+
+        self.wait_predictor("proj", "test_openai_bulk_full_flow_default_mode_question_column")
+
+        result_df = self.run_sql(
+            """
+            SELECT p.answer
+            FROM pg.df as t
+            JOIN proj.test_openai_full_flow_default_mode_question_column as p;
+            """
+        )
+        assert "stockholm" in result_df["answer"].iloc[0].lower()
+        assert "venus" in result_df["answer"].iloc[1].lower()
+
+    def test_single_full_flow_in_default_mode_runs_no_errors_with_prompt_template(self):
+        """
+        Test the full flow in default mode with a prompt template for a single prediction.
+        """
+        self.run_sql(
+            f"""
+            CREATE MODEL proj.test_openai_single_full_flow_default_mode_prompt_template
+            PREDICT answer
+            USING
+                engine='openai_engine',
+                prompt_template='Answer this question and add "Boom!" to the end of the answer: {{{{question}}}}';
+            """
+        )
+
+        self.wait_predictor("proj", "test_openai_single_full_flow_default_mode_prompt_template")
+
+        result_df = self.run_sql(
+            """
+            SELECT answer
+            FROM proj.test_openai_single_full_flow_default_mode_prompt_template
+            WHERE question='What is the capital of Sweden?'
+            """
+        )
+        assert "stockholm" in result_df["answer"].iloc[0].lower()
+        assert "boom!" in result_df["answer"].iloc[0].lower()
+
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_bulk_full_flow_in_default_mode_runs_no_errors_with_prompt_template(self, mock_handler):
+        """
+        Test the full flow in default mode with a prompt template for bulk predictions.
+        """
+        df = pd.DataFrame.from_dict({"question": [
+            "What is the capital of Sweden?",
+            "What is the second planet of the solar system?"
+        ]})
+        self.set_handler(mock_handler, name="pg", tables={"df": df})
+
+        self.run_sql(
+            f"""
+            CREATE MODEL proj.test_openai_bulk_full_flow_default_mode_prompt_template
+            PREDICT answer
+            USING
+                engine='openai_engine',
+                prompt_template='Answer this question and add "Boom!" to the end of the answer: {{{{question}}}}';
+            """
+        )
+
+        self.wait_predictor("proj", "test_openai_bulk_full_flow_default_mode_prompt_template")
+
+        result_df = self.run_sql(
+            """
+            SELECT p.answer
+            FROM pg.df as t
+            JOIN proj.test_openai_bulk_full_flow_default_mode_prompt_template as p;
+            """
+        )
+        assert "stockholm" in result_df["answer"].iloc[0].lower()
+        assert "boom!" in result_df["answer"].iloc[0].lower()
+        assert "venus" in result_df["answer"].iloc[1].lower()
+        assert "boom!" in result_df["answer"].iloc[1].lower()
 
 
 if __name__ == "__main__":

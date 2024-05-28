89cec99899a5d65fa551b2295fa03e6a5f5966af
andrew <elkin.andr@gmail.com>
2024-05-28 17:57:10 +0300

    fix unit tests

--- tests/unit/executor_test_base.py
+++ tests/unit/executor_test_base.py
@@ -397,7 +397,7 @@ class BaseExecutorDummyML(BaseExecutorTest):
                 col.alias if col.alias is not None else col.name
                 for col in ret.columns
             ]
-            return pd.DataFrame(ret.data, columns=columns)
+            return pd.DataFrame(ret.data).set_axis(columns, axis=1)
 
 
 class BaseExecutorDummyLLM(BaseExecutorTest):

d5c722bebee1f20a2277817d8d6cd13227397aca
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-07 16:46:35 +0530

    added an initial module for integration testing

--- mindsdb/integrations/handlers/bigquery_handler/tests/test_bigquery_handler.py
+++ mindsdb/integrations/handlers/bigquery_handler/tests/test_bigquery_handler.py
@@ -1,24 +1,72 @@
-import unittest
+import os
+import pytest
+import pandas as pd
+from google.cloud import bigquery
+
 from mindsdb.integrations.handlers.bigquery_handler.bigquery_handler import BigQueryHandler
-from mindsdb.integrations.libs.response import RESPONSE_TYPE
+from mindsdb.integrations.utilities.handlers.auth_utilities import GoogleServiceAccountOAuth2Manager
+from mindsdb.integrations.libs.response import (
+    HandlerResponse as Response,
+    RESPONSE_TYPE,
+)
+
+HANDLER_KWARGS = {
+    "connection_data": {
+        "project_id": os.environ.get("MDB_TEST_BIGQUERY_PROJECT_ID"),
+        "dataset": os.environ.get("MDB_TEST_BIGQUERY_DATASET"),
+        "service_account_json": os.environ.get("MDB_TEST_BIGQUERY_SERVICE_ACCOUNT_JSON"),
+    }
+}
 
 
-class BigQueryHandlerTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        connection_data = {
-            "project_id": "tough-future-332513",
-            "service_account_keys": "/home/bigq/tough-future-332513-6699d9b39601.json"
-        }
-        cls.handler = BigQueryHandler('test_bigquery_handler', connection_data)
+@pytest.fixture(scope="class")
+def bigquery_handler():
+    """
+    Create a BigQuery instance for testing.
+    """
+    seed_db()
+    handler = BigQueryHandler("test_bigquery_handler", **HANDLER_KWARGS)
+    yield handler
+    handler.disconnect()
 
-    def test_0_check_connection(self):
-        assert self.handler.check_connection()
 
-    def test_1_list_tables(self):
-        datasets = self.handler.get_tables('Rentals')
-        assert datasets.type is not RESPONSE_TYPE.ERROR
+def seed_db():
+    """
+    Seed the test DB by running the queries in the seed.sql file.
+    """
+    # Connect to the BigQuery warehouse to run seed queries
+    google_sa_oauth2_manager = GoogleServiceAccountOAuth2Manager(
+        credentials_json=HANDLER_KWARGS["connection_data"]["service_account_json"]
+    )
+    credentials = google_sa_oauth2_manager.get_oauth2_credentials()
 
-    def test_2_list_table_columns(self):
-        datasets = self.handler.get_columns('Rentals', 'home_rentals')
-        assert datasets.type is not RESPONSE_TYPE.ERROR
+    client = bigquery.Client(
+        project=HANDLER_KWARGS["connection_data"]["project_id"],
+        credentials=credentials
+    )
+
+    with open("mindsdb/integrations/handlers/bigquery_handler/tests/seed.sql", "r") as f:
+        for line in f.readlines():
+            query = client.query(line)
+            query.result()
+
+
+@pytest.mark.usefixtures("bigquery_handler")
+class TestBigQueryHandlerConnect:
+    def test_connect(self, bigquery_handler):
+        """
+        Tests the `connect` method to ensure it connects to the BigQuery warehouse.
+        """
+        bigquery_handler.connect()
+        assert bigquery_handler.is_connected, "the handler has failed to connect"
+
+    def test_check_connection(self, bigquery_handler):
+        """
+        Tests the `check_connection` method to verify that it returns a StatusResponse object and accurately reflects the connection status.
+        """
+        res = bigquery_handler.check_connection()
+        assert res.success, res.error_message
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

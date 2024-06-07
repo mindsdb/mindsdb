f78defc63b1f5e57559b4c600d878a46deb9b08a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-07 17:00:06 +0530

    added the rest of the integration tests

--- mindsdb/integrations/handlers/bigquery_handler/tests/test_bigquery_handler.py
+++ mindsdb/integrations/handlers/bigquery_handler/tests/test_bigquery_handler.py
@@ -51,6 +51,37 @@ def seed_db():
             query.result()
 
 
+def check_valid_response(res):
+    """
+    Utility function to check if the response is valid.
+    """
+
+    if res.resp_type == RESPONSE_TYPE.TABLE:
+        assert res.data_frame is not None, "expected to have some data, but got None"
+    assert (
+        res.error_code == 0
+    ), f"expected to have zero error_code, but got {res.error_code}"
+    assert (
+        res.error_message is None
+    ), f"expected to have None in error message, but got {res.error_message}"
+
+
+def get_table_names(snowflake_handler):
+    """
+    Utility function to get the table names from the Snowflake account.
+    """
+
+    res = snowflake_handler.get_tables()
+    tables = res.data_frame
+
+    assert tables is not None, "expected to have some tables in the db, but got None"
+    assert (
+        "table_name" in tables
+    ), f"expected to get 'table_name' column in the response:\n{tables}"
+
+    return list(tables["table_name"])
+
+
 @pytest.mark.usefixtures("bigquery_handler")
 class TestBigQueryHandlerConnect:
     def test_connect(self, bigquery_handler):
@@ -66,6 +97,130 @@ class TestBigQueryHandlerConnect:
         """
         res = bigquery_handler.check_connection()
         assert res.success, res.error_message
+
+
+@pytest.mark.usefixtures("bigquery_handler")
+class TestBigQueryHandlerTables:
+    table_for_creation = "TEST_MDB"
+
+    def test_get_tables(self, bigquery_handler):
+        """
+        Tests the `get_tables` method to confirm it correctly calls `native_query` with the appropriate SQL commands.
+        """
+        res = bigquery_handler.get_tables()
+        assert res.type == RESPONSE_TYPE.TABLE, "expected a TABLE"
+        assert len(res.data_frame) > 0, "expected > O columns"
+
+        tables = res.data_frame
+
+        assert (
+            tables is not None
+        ), "expected to have some tables in the db, but got None"
+        assert (
+            "table_name" in tables
+        ), f"expected to get 'table_name' in the response but got: {tables}"
+        assert (
+            "TEST" in tables["table_name"].values
+        ), "expected to have 'test' in the response."
+
+    def test_get_columns(self, bigquery_handler):
+        """
+        Tests if the `get_columns` method correctly constructs the SQL query and if it calls `native_query` with the correct query.
+        """
+        res = bigquery_handler.get_columns("TEST")
+        assert res.type == RESPONSE_TYPE.TABLE, "expected a TABLE"
+        assert len(res.data_frame) > 0, "expected > O columns"
+
+        views = res.data_frame
+
+        expected_columns = {
+            "Field": ["COL_ONE", "COL_FOUR", "COL_TWO", "COL_THREE"],
+            "Type": ["INT64", "STRING", "INT64", "FLOAT64"],
+        }
+        expected_df = pd.DataFrame(expected_columns)
+
+        # Sort both DataFrames by all columns before comparing
+        views = views.sort_values(by=list(res.data_frame.columns)).reset_index(drop=True)
+        expected_df = expected_df.sort_values(by=list(expected_df.columns)).reset_index(drop=True)
+
+        assert views.equals(
+            expected_df
+        ), "response does not contain the expected columns"
+
+    def test_create_table(self, bigquery_handler):
+        """
+        Tests a table creation query to ensure it creates a table in the Snowflake account.
+        """
+
+        query = f"""
+            CREATE TABLE IF NOT EXISTS {self.table_for_creation} (
+                test_col INT
+            );
+        """
+        res = bigquery_handler.native_query(query)
+        check_valid_response(res)
+
+        tables = get_table_names(bigquery_handler)
+
+        assert (
+            self.table_for_creation in tables
+        ), f"expected to have {self.table_for_creation} in database, but got: {tables}"
+
+    def test_drop_table(self, bigquery_handler):
+        """
+        Tests a table drop query to ensure it drops a table in the Snowflake account.
+        """
+
+        query = f"DROP TABLE IF EXISTS {self.table_for_creation}"
+        res = bigquery_handler.native_query(query)
+        check_valid_response(res)
+
+        tables = get_table_names(bigquery_handler)
+
+        assert self.table_for_creation not in tables
+
+
+@pytest.mark.usefixtures("bigquery_handler")
+class TestBigQueryHandlerQuery:
+    def test_select_native_query(self, bigquery_handler):
+        """
+        Tests the `native_query` method to ensure it executes a SQL query using a mock cursor and returns a Response object.
+        """
+
+        query = "SELECT * FROM test"
+        res = bigquery_handler.native_query(query)
+
+        assert type(res) is Response
+        assert res.resp_type == RESPONSE_TYPE.TABLE
+
+        expected_data = {
+            "COL_ONE": [1, 2, 3],
+            "COL_TWO": [-1, -2, -3],
+            "COL_THREE": [0.1, 0.2, 0.3],
+            "COL_FOUR": ["A", "B", "C"],
+        }
+        expected_df = pd.DataFrame(expected_data)
+
+        assert res.data_frame.equals(
+            expected_df
+        ), "response does not contain the expected data"
+
+    def test_select_query(self, bigquery_handler):
+        """
+        Tests the `query` method to ensure it executes a SQL query and returns a Response object.
+        """
+
+        limit = 3
+        query = "SELECT * FROM test"
+        res = bigquery_handler.query(query)
+        check_valid_response(res)
+
+        got_rows = res.data_frame.shape[0]
+        want_rows = limit
+
+        assert (
+            got_rows == want_rows
+        ), f"expected to have {want_rows} rows in response but got: {got_rows}"
 
 
 if __name__ == "__main__":

a0ba9ad29d231d346d66d7118d0ff5335bccb826
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-13 14:25:36 +0530

    added an initial set of integration tests

--- mindsdb/integrations/handlers/redshift_handler/tests/test_redshift_handler.py
+++ mindsdb/integrations/handlers/redshift_handler/tests/test_redshift_handler.py
@@ -1,36 +1,77 @@
-import unittest
+import os
+import pytest
+import psycopg
+
+from mindsdb.integrations.handlers.postgres_handler.tests.test_postgres_handler import (
+    TestPostgresConnection,
+    TestPostgresQuery,
+    TestPostgresTables,
+    TestPostgresColumns,
+    TestPostgresDisconnect
+)
+
 from mindsdb.integrations.handlers.redshift_handler.redshift_handler import RedshiftHandler
-from mindsdb.api.executor.data_types.response_type import RESPONSE_TYPE
 
 
-class RedshiftHandlerTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.kwargs = {
-            "host": "examplecluster.abc123xyz789.us-west-1.redshift.amazonaws.com",
-            "port": 5439,
-            "database": "dev",
-            "user": "awsuser",
-            "password": "my_password"
-        }
-        cls.handler = RedshiftHandler('test_redshift_handler', cls.kwargs)
-
-    def test_0_check_connection(self):
-        assert self.handler.check_connection()
-
-    def test_1_native_query_select(self):
-        query = "SELECT * FROM sales"
-        result = self.handler.native_query(query)
-        assert result.type is RESPONSE_TYPE.TABLE
-
-    def test_2_get_tables(self):
-        tables = self.handler.get_tables()
-        assert tables.type is not RESPONSE_TYPE.ERROR
-
-    def test_4_get_columns(self):
-        columns = self.handler.get_columns('sales')
-        assert columns.type is not RESPONSE_TYPE.ERROR
+HANDLER_KWARGS = {
+    "connection_data": {
+        "host": os.environ.get("MDB_TEST_REDSHIFT_HOST", "127.0.0.1"),
+        "port": os.environ.get("MDB_TEST_REDSHIFT_PORT", "5439"),
+        "user": os.environ.get("MDB_TEST_REDSHIFT_USER", "redshift"),
+        "password": os.environ.get("MDB_TEST_REDSHIFT_PASSWORD", "supersecret"),
+        "database": os.environ.get("MDB_TEST_REDSHIFT_DATABASE", "redshift_db_handler_test").lower()
+    }
+}
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture(scope="module")
+def handler():
+    seed_db()
+    handler = RedshiftHandler("test_redshift_handler", **HANDLER_KWARGS)
+    yield handler
+    handler.disconnect()
+
+
+def seed_db():
+    """
+    Seed the test DB by running the queries in the seed.sql file.
+    """
+    # Connect to the sample database to run seed queries
+    conn_info = HANDLER_KWARGS["connection_data"].copy()
+    del conn_info["database"]
+    conn_info["dbname"] = "dev"
+    db = psycopg.connect(**conn_info)
+    db.autocommit = True
+
+    cursor = db.cursor()
+
+    # Check if the database exists, if not create it
+    cursor.execute(f"SELECT 1 FROM pg_database WHERE datname = '{HANDLER_KWARGS['connection_data']['database']}'")
+    result = cursor.fetchone()
+    if not result:
+        cursor.execute(f"CREATE DATABASE {HANDLER_KWARGS['connection_data']['database']}")
+
+    # Reconnect to the new database
+    conn_info["dbname"] = HANDLER_KWARGS['connection_data']['database']
+    db = psycopg.connect(**conn_info)
+    db.autocommit = True
+    cursor = db.cursor()
+
+    with open("mindsdb/integrations/handlers/redshift_handler/tests/seed.sql", "r") as f:
+        for line in f.readlines():
+            cursor.execute(line)
+    cursor.close()
+    db.close()
+
+
+@pytest.mark.redshift
+class TestRedshiftConnection(TestPostgresConnection):
+    def test_connect(self, handler):
+        super().test_connect(handler)
+
+    def test_check_connection(self, handler):
+        super().test_check_connection(handler)
+
+
+if __name__ == "__main__":
+    pytest.main(["-m", "redshift", __file__])

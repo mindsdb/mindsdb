6fa38fd5ae28aa16c76243ac9f8143956959b14c
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-13 12:37:54 +0530

    added the docstring for the insert() statement

--- mindsdb/integrations/handlers/redshift_handler/redshift_handler.py
+++ mindsdb/integrations/handlers/redshift_handler/redshift_handler.py
@@ -24,18 +24,27 @@ class RedshiftHandler(PostgresHandler):
         """
         Initialize the handler.
         Args:
-            name (str): name of particular handler instance
+            name (str): name of particular handler instance.
             **kwargs: arbitrary keyword arguments.
         """
         super().__init__(name,**kwargs)
 
     def insert(self, table_name: str, df: pd.DataFrame):
+        """
+        Handles the execution of INSERT statements.
+
+        Args:
+            table_name (str): name of the table to insert the data into.
+            df (pd.DataFrame): data to be inserted into the table.        
+        """
         need_to_close = not self.is_connected
 
         connection = self.connect()
 
+        # Replace NaN values with None
         df = df.replace({np.nan: None})
 
+        # Build the query to insert the data
         columns = ', '.join([f'"{col}"' if ' ' in col else col for col in df.columns])
         values = ', '.join(['%s' for _ in range(len(df.columns))])
         query = f'INSERT INTO {table_name} ({columns}) VALUES ({values})'

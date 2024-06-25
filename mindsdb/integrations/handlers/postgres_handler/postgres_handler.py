7b4904bb0ae5a1da817f2ef59f62db09fb8b045d
dusvyat <usvyat@gmail.com>
2024-06-25 11:54:50 +0300

    Update Postgres handler to include schema in table query
    The handler now fetches the 'schema' from connection arguments, defaulting to 'public' if not provided. The schema is then used in the table query, allowing for more accurate query execution in PostgreSQL.

--- mindsdb/integrations/handlers/postgres_handler/postgres_handler.py
+++ mindsdb/integrations/handlers/postgres_handler/postgres_handler.py
@@ -286,7 +286,7 @@ class PostgresHandler(DatabaseHandler):
 
         if not table_name or not isinstance(table_name, str):
             raise ValueError("Invalid table name provided.")
-
+        schema = self.connection_args.get('schema', 'public')
         query = f"""
             SELECT
                 column_name as "Field",
@@ -295,6 +295,7 @@ class PostgresHandler(DatabaseHandler):
                 information_schema.columns
             WHERE
                 table_name = '{table_name}'
+                and table_schema = '{schema}'
         """
         return self.native_query(query)
 

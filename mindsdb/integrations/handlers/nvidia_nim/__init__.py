637464366525491f83514bb8f5c5568888cda07a
ZoranPandovski <zoran.pandovski@gmail.com>
2024-06-28 18:46:10 +0200

    Initial nim integration

---
+++ mindsdb/integrations/handlers/nvidia_nim/__init__.py
@@ -0,0 +1,23 @@
+from mindsdb.integrations.libs.const import HANDLER_TYPE
+from mindsdb.utilities import log
+
+logger = log.getLogger(__name__)
+
+from .__about__ import __description__ as description
+from .__about__ import __version__ as version
+
+try:
+    from .nim_handler import NvidiaNimHandler as Handler
+
+    import_error = None
+except Exception as e:
+    Handler = None
+    import_error = e
+
+title = "Nvidia Nim"
+name = "nvidia_nim"
+type = HANDLER_TYPE.ML
+#icon_path = 'icon.svg'
+permanent = False
+
+__all__ = ["Handler", "version", "name", "type", "title", "description", "import_error", "icon_path"]

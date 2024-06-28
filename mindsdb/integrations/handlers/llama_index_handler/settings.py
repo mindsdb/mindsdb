f37030164bdbea37af5778a71b7a83feae0d2db5
ZoranPandovski <zoran.pandovski@gmail.com>
2024-06-28 15:24:51 +0200

    Add validation

---
+++ mindsdb/integrations/handlers/llama_index_handler/settings.py
@@ -0,0 +1,58 @@
+from typing import List, Any, Optional
+from pydantic import validator, BaseModel
+from pydantic_settings import BaseSettings
+
+class LlamaIndexConfig(BaseSettings):
+    """
+    Model for LlamaIndexHandler settings.
+
+    Attributes:
+        default_index_class (str): Default index class.
+        supported_index_class (List[str]): Supported index classes.
+        default_reader (str): Default reader. Note this is custom data frame reader.
+        supported_reader (List[str]): Supported readers.
+    """
+    DEFAULT_INDEX_CLASS : str = "VectorStoreIndex"
+    SUPPORTED_INDEXES : List[str] = ["VectorStoreIndex"]
+    DEFAULT_READER: str = "DFReader"
+    SUPPORTED_READERS : List[str] = ["DFReader", "SimpleWebPageReader"]
+
+
+llama_index_config = LlamaIndexConfig()
+
+class LlamaIndexModel(BaseModel):
+    """
+    Model for LlamaIndexHandler.
+
+    Attributes:
+        reader (str): Reader.
+        index_class (str): Index class.
+        index (Any): Index.
+        reader_params (Any): Reader parameters.
+        index_params (Any): Index parameters.
+    """
+    reader: Optional[str] = None
+    index_class: Optional[str] = None
+    input_column: str
+    openai_api_key: Optional[str] = None
+    input_column: Optional[str]
+    mode: Optional[str] = None
+    user_column: Optional[str] = None
+    assistant_column: Optional[str] = None
+
+    @validator('reader')
+    def validate_reader(cls, value):
+        if value not in llama_index_config.SUPPORTED_READERS:
+            raise ValueError(f"Reader {value} is not supported.")
+
+    @validator('index_class')
+    def validate_index_class(cls, value):
+        if value not in llama_index_config.SUPPORTED_INDEXES:
+            raise ValueError(f"Index class {value} is not supported.")
+    
+    @validator('mode')
+    def validate_mode_class(cls, value, values):
+        if value == "conversational" and all([values.get('user_column'), values.get('assistant_column')]):
+            raise ValueError(f"Conversational mode requires user_column and assistant_column parameter")
+
+   

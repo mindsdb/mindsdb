1ac50deb1de465edf357a672a325c50c25109229
Minura Punchihewa <49385643+MinuraPunchihewa@users.noreply.github.com>
2024-07-02 12:08:42 +0530

    Updated  /<handler_name>/install Route to Parse Dependencies Recursively (#9408)

--- mindsdb/integrations/utilities/install.py
+++ mindsdb/integrations/utilities/install.py
@@ -1,9 +1,19 @@
 import os
 import sys
 import subprocess
+from typing import Text, List
 
 
-def install_dependencies(dependencies):
+def install_dependencies(dependencies: List[Text]) -> dict:
+    """
+    Installs the dependencies for a handler by calling the `pip install` command via subprocess.
+
+    Args:
+        dependencies (List[Text]): List of dependencies for the handler.
+
+    Returns:
+        dict: A dictionary containing the success status and an error message if an error occurs.
+    """
     outs = b''
     errs = b''
     result = {
@@ -11,35 +21,10 @@ def install_dependencies(dependencies):
         'error_message': None
     }
 
-    # get the path to this script
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    split_dependencies = []
-    for dependency in dependencies:
-        # ignore standalone comments
-        if dependency.startswith('#'):
-            continue
+    # split the dependencies by parsing the contents of the requirements.txt file
+    split_dependencies = parse_dependencies(dependencies)
 
-        # remove inline comments
-        if '#' in dependency:
-            dependency = dependency.split('#')[0].strip()
-
-        # check if the dependency is a path to a requirements file
-        if dependency.startswith('-r'):
-            # split the string into '-r' and the path
-            r_flag, req_path = dependency.split(' ')
-            # create the absolute path to the requirements file
-            abs_req_path = os.path.abspath(os.path.join(script_path, req_path.replace('mindsdb/integrations', '..')))
-
-            # check if the file exists
-            if os.path.exists(abs_req_path):
-                split_dependencies.append(r_flag)
-                split_dependencies.append(abs_req_path)
-            else:
-                raise FileNotFoundError(f"Requirements file not found: {req_path}")
-
-        else:
-            split_dependencies.append(dependency)
-
+    # run the pip install command
     try:
         sp = subprocess.Popen(
             [sys.executable, '-m', 'pip', 'install', *split_dependencies],
@@ -51,6 +36,7 @@ def install_dependencies(dependencies):
     except Exception as e:
         result['error_message'] = str(e)
 
+    # return the result of the installation
     if code != 0:
         output = ''
         if isinstance(outs, bytes) and len(outs) > 0:
@@ -64,3 +50,72 @@ def install_dependencies(dependencies):
         result['success'] = True
 
     return result
+
+
+def parse_dependencies(dependencies: List[Text]) -> List[Text]:
+    """
+    Recursively parses dependencies from a list of dependencies given in a requirements.txt file for a handler.
+    This function will perform the following:
+    1. Ignore standalone comments.
+    2. Remove inline comments.
+    3. Check if the dependency is a path to a requirements file and recursively parse the dependencies from that file.
+
+    Args:
+        dependencies (List[Text]): List of dependencies for a handler as read from the requirements.txt file.
+
+    Returns:
+        List[Text]: List of parsed dependencies for the handler.
+    """
+    # get the path to this script
+    script_path = os.path.dirname(os.path.realpath(__file__))
+
+    split_dependencies = []
+    for dependency in dependencies:
+        # ignore standalone comments
+        if dependency.startswith('#'):
+            continue
+
+        # remove inline comments
+        if '#' in dependency:
+            dependency = dependency.split('#')[0].strip()
+
+        # check if the dependency is a path to a requirements file
+        if dependency.startswith('-r'):
+            # get the path to the requirements file
+            req_path = dependency.split(' ')[1]
+            # create the absolute path to the requirements file
+            abs_req_path = os.path.abspath(os.path.join(script_path, req_path.replace('mindsdb/integrations', '..')))
+            # check if the file exists
+            if os.path.exists(abs_req_path):
+                inner_dependencies, inner_split_dependencies = [], []
+                # read the dependencies from the file
+                inner_dependencies = read_dependencies(abs_req_path)
+                # recursively split the dependencies
+                inner_split_dependencies = parse_dependencies(inner_dependencies)
+                # add the inner dependencies to the split dependencies
+                split_dependencies.extend(inner_split_dependencies)
+            else:
+                raise FileNotFoundError(f"Requirements file not found: {req_path}")
+
+        else:
+            split_dependencies.append(dependency)
+
+    return split_dependencies
+
+
+def read_dependencies(path: Text) -> List[Text]:
+    """
+    Reads the dependencies for a handler from the relevant requirements.txt file and returns them as a list.
+
+    Args:
+        path (Text): Path to the requirements.txt file for the handler.
+
+    Returns:
+        List[Text]: List of dependencies for the handler.
+    """
+    dependencies = []
+    # read the dependencies from the file
+    with open(str(path), 'rt') as f:
+        dependencies = [x.strip(' \t\n') for x in f.readlines()]
+        dependencies = [x for x in dependencies if len(x) > 0]
+    return dependencies

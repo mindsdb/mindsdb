fa4466c8036c7b795a9a840a963067b12729d53a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-02 15:51:37 +0530

    updated the connection args

--- mindsdb/integrations/handlers/s3_handler/connection_args.py
+++ mindsdb/integrations/handlers/s3_handler/connection_args.py
@@ -6,36 +6,28 @@ from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_T
 connection_args = OrderedDict(
     aws_access_key_id={
         'type': ARG_TYPE.STR,
-        'description': 'The access key for the AWS account.'
+        'description': 'The access key for the AWS account.',
+        'required': True
     },
     aws_secret_access_key={
         'type': ARG_TYPE.STR,
         'description': 'The secret key for the AWS account.',
-        'secret': True
+        'secret': True,
+        'required': True
+    },
+    aws_session_token={
+        'type': ARG_TYPE.STR,
+        'description': 'The session token for the AWS account.',
+        'required': False
     },
     region_name={
         'type': ARG_TYPE.STR,
-        'description': 'The AWS region where the S3 bucket is located.'
-    },
-    bucket={
-        'type': ARG_TYPE.STR,
-        'description': 'The name of the S3 bucket.'
-    },
-    key={
-        'type': ARG_TYPE.STR,
-        'description': 'The key of the object to be queried.'
-    },
-    input_serialization={
-        'type': ARG_TYPE.STR,
-        'description': 'The format of the data in the object that is to be queried.'
+        'description': 'The AWS region.',
+        'required': False
     }
 )
 
 connection_args_example = OrderedDict(
     aws_access_key_id='PCAQ2LJDOSWLNSQKOCPW',
     aws_secret_access_key='U/VjewPlNopsDmmwItl34r2neyC6WhZpUiip57i',
-    region_name='us-east-1',
-    bucket='mindsdb-bucket',
-    key='iris.csv',
-    input_serialization="{'CSV': {'FileHeaderInfo': 'NONE'}}",
 )

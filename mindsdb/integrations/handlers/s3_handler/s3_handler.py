375ca1683a0df006fa1e3fbc188b412443224fd0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-02 15:48:36 +0530

    configured mandatory and optional params in connect

--- mindsdb/integrations/handlers/s3_handler/s3_handler.py
+++ mindsdb/integrations/handlers/s3_handler/s3_handler.py
@@ -55,17 +55,32 @@ class S3Handler(APIHandler):
         """
         if self.is_connected is True:
             return self.connection
+        
+        # Mandatory connection parameters.
+        if not all(key in self.connection_data for key in ['aws_access_key_id', 'aws_secret_access_key']):
+            raise ValueError('Required parameters (aws_access_key_id, aws_secret_access_key) must be provided.')
+
+        config = {
+            'aws_access_key_id': self.connection_data.get('aws_access_key_id'),
+            'aws_secret_access_key': self.connection_data.get('aws_secret_access_key')
+        }
+
+        # Optional connection parameters.
+        optional_params = ['aws_session_token', 'region_name']
+        for param in optional_params:
+            if param in self.connection_data:
+                config[param] = self.connection_data[param]
 
         try:
             self.connection = boto3.client(
                 's3',
-                aws_access_key_id=self.connection_data['aws_access_key_id'],
-                aws_secret_access_key=self.connection_data['aws_secret_access_key'],
-                region_name=self.connection_data['region_name']
+                **config
             )
             self.is_connected = True
             return self.connection
-        except KeyError as e:
+        # No exception is raised when invalid credentials are provided at this point.
+        # Therefore, it is not possible to catch a more specific exception.
+        except Exception as e:
             logger.error(f'Error connecting to AWS, {e}!')
             raise
 

c5e50361d4f432551cd1fdeba2c181201d732efa
andrew <elkin.andr@gmail.com>
2024-07-05 17:03:19 +0300

    email handler test

--- tests/unit/executor/test_api_handler.py
+++ tests/unit/executor/test_api_handler.py
@@ -4,6 +4,8 @@ import types
 from unittest.mock import patch
 import datetime as dt
 
+import pandas as pd
+
 from tests.unit.executor_test_base import BaseExecutorDummyML
 
 from dataclasses import dataclass
@@ -26,7 +28,7 @@ class TestApiHandler(BaseExecutorDummyML):
         Test for APIResource
         """
 
-        # create
+        # --- create ---
         self.run_sql('''
              CREATE DATABASE gh
                 WITH
@@ -37,7 +39,7 @@ class TestApiHandler(BaseExecutorDummyML):
                   }
         ''')
 
-        # select
+        # --- select ---
         @dataclass
         class User:
             login: str = 'user1'
@@ -82,7 +84,7 @@ class TestApiHandler(BaseExecutorDummyML):
         assert len(ret) == 1
         assert ret['number'][0] == 125
 
-        # insert
+        # --- insert ---
         self.run_sql('''
             insert into gh.issues (title, body)
              values ('feature', 'do better')
@@ -93,3 +95,70 @@ class TestApiHandler(BaseExecutorDummyML):
 
         assert args[0] == 'feature'
         assert kwargs['body'] == 'do better'
+
+    @patch("mindsdb.integrations.handlers.email_handler.email_handler.EmailClient")
+    def test_email(self, EmailClient):
+        """
+        Test for APIResource
+        """
+
+        # --- create ---
+        self.run_sql('''
+            CREATE DATABASE em
+                WITH ENGINE = 'email',
+                PARAMETERS = {
+                  "email": "e@mail.com",
+                  "password": "-"
+            }
+        ''')
+
+        # --- select ---
+        search_email = EmailClient().search_email
+
+        mock_df = pd.DataFrame({
+            'date': ["Wed, 02 Feb 2022 15:30:00 +0000",
+                     "Thu, 10 Mar 2022 10:45:15 +0530",
+                     "Fri, 16 Dec 2022 20:15:30 -0400"
+                     ],
+            'body_content_type': ['text', 'text', 'text'],
+            "body": ["info", "info", "info"],
+            "from_field": ["x1@m.com", "x2@m.com", "x3@m.com"],
+            "id": ["2", "3", "4"],
+            "to_field": ["a@m.com", "a@m.com", "b@m.com"],
+            "subject": ["info", "info", "info"],
+        })
+
+        search_email.return_value = mock_df
+
+        ret = self.run_sql('''
+            SELECT to_field, max(from_field) from_field
+            FROM em.emails
+            WHERE subject = 'info'
+             and id > 1
+            group by to_field
+            order by to_field
+        ''')
+
+        args = search_email.call_args_list[0][0]
+
+        # check input to search_email
+        assert args[0].subject == 'info'
+        assert args[0].since_email_id == 2
+
+        # check response
+        assert len(ret) == 2
+        assert ret['from_field'][0] == 'x2@m.com'
+
+        # --- insert ---
+        self.run_sql('''
+          INSERT INTO em.emails(to_field, subject, body)
+          VALUES ("toemail@email.com", "MindsDB", "Hello from MindsDB!");
+        ''')
+
+        func_call = EmailClient().send_email.call_args_list[0]
+        args = func_call[0]
+        kwargs = func_call[1]
+
+        assert args[0] == "toemail@email.com"
+        assert kwargs['subject'] == 'MindsDB'
+        assert kwargs['body'] == "Hello from MindsDB!"

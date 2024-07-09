102fac986e5e98121010a79b3fb3e44bfe7a6aa0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-09 18:39:38 +0530

    added the project files for the Bedrock handler

---
+++ mindsdb/integrations/handlers/bedrock_handler/bedrock_handler.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from typing import Optional, Dict
+
+from mindsdb.utilities import log
+from mindsdb.integrations.libs.base import BaseMLEngine
+
+
+logger = log.getLogger(__name__)
+
+
+class AmazonBedrockHandler(BaseMLEngine):
+    """
+    Twelve Labs API handler implementation.
+    """
+
+    name = 'twelve_labs'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.generative = True
+
+    @staticmethod
+    def create_validation(target: str, args: Dict = None, **kwargs: Dict) -> None:
+        pass
+
+    def create(self, target: str, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
+        pass
+
+    def predict(self, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
+        pass
+
+    def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
+        pass

d2573ddf24a9cdfe3cf48d4145ccec3cdbc3aa30
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-11 16:50:19 +0530

    added an incomplete config model for validating models

--- mindsdb/integrations/handlers/bedrock_handler/settings.py
+++ mindsdb/integrations/handlers/bedrock_handler/settings.py
@@ -1,6 +1,8 @@
 from typing import Text, Optional, Any
 from botocore.exceptions import ClientError
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, field_validator
+
+from mindsdb.interfaces.storage.model_fs import HandlerStorage
 
 from mindsdb.integrations.handlers.bedrock_handler.utilities import create_amazon_bedrock_client
 from mindsdb.integrations.utilities.handlers.validation_utilities import ParameterValidationUtilities
@@ -28,7 +30,6 @@ class AmazonBedrockEngineConfig(BaseModel):
     aws_secret_access_key: Text
     region_name: Text
     aws_session_token: Optional[Text]
-
 
     class Config:
         extra = "forbid"
@@ -80,3 +81,75 @@ class AmazonBedrockEngineConfig(BaseModel):
             bedrock_client.list_foundational_models()
         except ClientError as e:
             raise ValueError(f"Invalid Amazon Bedrock credentials: {e}")
+        
+
+class AmazonBedrockModelConfig(BaseModel):
+    """
+    Configuration model for Amazon Bedrock models.
+
+    Attributes
+    ----------
+    model_id: Text
+        Amazon Bedrock model ID.
+
+        
+    engine: HandlerStorage
+        The handler storage from the engine of the model. This is not provided by the user. It is used for validating the model ID.
+    """
+    model_id: Text
+
+    engine: HandlerStorage
+
+    class Config:
+        extra = "forbid"
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_param_typos(cls, values: Any) -> Any:
+        """
+        Root validator to check if there are any typos in the parameters.
+
+        Parameters
+        ----------
+        values : Dict
+            Dictionary containing the attributes of the model.
+
+        Raises
+        ------
+        ValueError
+            If there are any typos in the parameters.
+        """
+        ParameterValidationUtilities.validate_parameter_spelling(cls, values)
+
+        return values
+    
+    @model_validator(mode="before")
+    @classmethod
+    def check_for_valid_model_id(cls, values: Any) -> Any):
+        """
+        Root validator to check if the model ID provided is valid.
+
+        Parameters
+        ----------
+        values : Dict
+            Dictionary containing the attributes of the model.
+
+        Raises
+        ------
+        ValueError
+            If the model ID provided is not valid.
+        """
+        connection_args = values["engine"].get_connection_args()
+
+        bedrock_client = create_amazon_bedrock_client(
+            connection_args["aws_access_key_id"],
+            connection_args["aws_secret_access_key"],
+            connection_args["region_name"],
+            connection_args["aws_session_token"]
+        )
+
+        try:
+            bedrock_client.get_foundational_model(values["model_id"])
+        except ClientError as e:
+            raise ValueError(f"Invalid Amazon Bedrock model ID: {e}")
+

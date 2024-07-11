f5ff67b9745cfbe1070ae388e7f2ca4192afa2b7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-11 16:28:05 +0530

    added create_engine() for validating engines

--- mindsdb/integrations/handlers/bedrock_handler/bedrock_handler.py
+++ mindsdb/integrations/handlers/bedrock_handler/bedrock_handler.py
@@ -4,6 +4,8 @@ from typing import Optional, Dict
 from mindsdb.utilities import log
 from mindsdb.integrations.libs.base import BaseMLEngine
 
+from mindsdb.integrations.handlers.bedrock_handler.settings import AmazonBedrockEngineConfig
+
 
 logger = log.getLogger(__name__)
 
@@ -18,6 +20,22 @@ class AmazonBedrockHandler(BaseMLEngine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.generative = True
+
+    def create_engine(self, connection_args: Dict) -> None:
+        """
+        Validate the OpenAI API credentials on engine creation.
+
+        Args:
+            connection_args (Dict): Parameters for the engine.
+
+        Raises:
+            Exception: If the handler is not configured with valid API credentials.
+
+        Returns:
+            None
+        """
+        connection_args = {k.lower(): v for k, v in connection_args.items()}
+        AmazonBedrockEngineConfig(**connection_args)
 
     @staticmethod
     def create_validation(target: str, args: Dict = None, **kwargs: Dict) -> None:

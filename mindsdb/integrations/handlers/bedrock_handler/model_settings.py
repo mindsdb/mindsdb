506546f6cc91e754f0ce6bb262b311b112f34f6b
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-12 23:16:39 +0530

    added a func to get model config from provider and output

--- mindsdb/integrations/handlers/bedrock_handler/model_settings.py
+++ mindsdb/integrations/handlers/bedrock_handler/model_settings.py
@@ -1,5 +1,25 @@
+import sys
+import inspect
 from pydantic import BaseModel
-from typing import Text, List, Optional
+from typing import Text, List, Optional, ClassVar
+
+
+def find_config_for_model(provider: Text, output_modalities: List[Text]):
+    """
+    Find the configuration model for the specified provider and output modalities.
+
+    Args:
+        provider (Text): The provider of the model.
+        output_modalities (List[Text]): The output modalities of the model.
+
+    Returns:
+        BaseModel: The configuration model for the specified provider and output modalities.
+    """
+    for _, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj) and issubclass(obj, BaseModel) and hasattr(obj, 'provider') and hasattr(obj, 'output_modalities'):
+            if obj.provider == provider and set(obj.output_modalities) == set(output_modalities):
+                return obj
+    return None
 
 
 class AmazonBedrockTitanTextConfig(BaseModel):
@@ -21,6 +41,9 @@ class AmazonBedrockTitanTextConfig(BaseModel):
     stop : Optional[List[Text]]
         Specify a character sequence to indicate where the model should stop. Maps to `stopSequences` in the Amazon Bedrock API.
     """
+    provider: ClassVar = 'Amazon'
+    output_modalities: ClassVar = ['TEXT']
+
     temperature: Optional[float]
     top_p: Optional[float]
     max_tokens: Optional[int]
@@ -29,13 +52,25 @@ class AmazonBedrockTitanTextConfig(BaseModel):
     # TODO: Add validations for the attributes.
 
     def __dict__(self):
+        text_generation_config = {}
+
+        if self.temperature:
+            text_generation_config['temperature'] = self.temperature
+
+        if self.top_p:
+            text_generation_config['topP'] = self.top_p
+
+        if self.max_tokens:
+            text_generation_config['maxTokenCount'] = self.max_tokens
+
+        if self.stop:
+            text_generation_config['stopSequences'] = self.stop
+
+        if not text_generation_config:
+            return None
+        
         return {
             'parameters': {
-                'textGenerationConfig': {
-                    'temperature': self.temperature,
-                    'topP': self.top_p,
-                    'maxTokenCount': self.max_tokens,
-                    'stopSequences': self.stop
-                }
+                'textGenerationConfig': text_generation_config
             }
         }

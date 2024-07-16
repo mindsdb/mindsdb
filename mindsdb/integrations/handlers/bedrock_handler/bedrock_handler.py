70575927edca71a738acfac1efc2ce757853e2f1
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-16 16:04:14 +0530

    implemented predict() for default mode with question_col

--- mindsdb/integrations/handlers/bedrock_handler/bedrock_handler.py
+++ mindsdb/integrations/handlers/bedrock_handler/bedrock_handler.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from typing import Optional, Dict, Any
+from typing import Text, Dict, List, Optional, Any
 
 from mindsdb.utilities import log
 
 from mindsdb.integrations.libs.base import BaseMLEngine
 from mindsdb.integrations.libs.api_handler_exceptions import MissingConnectionParams
+from mindsdb.integrations.handlers.bedrock_handler.utilities import create_amazon_bedrock_runtime_client
 from mindsdb.integrations.handlers.bedrock_handler.handler_settings import AmazonBedrockHandlerEngineConfig, AmazonBedrockHandlerModelConfig
 
 
@@ -64,10 +65,56 @@ class AmazonBedrockHandler(BaseMLEngine):
             self.model_storage.json_set('args', args)
 
     def predict(self, df: Optional[pd.DataFrame] = None, args: Optional[Dict] = None) -> None:
-        pass
+        args = self.model_storage.json_get('args')
+        handler_model_params = args['handler_model_params']
+        mode = args['handler_model_params']['mode']
+        model_id = handler_model_params['model_id']
+        inference_config = handler_model_params.get('inference_config')
+        target = args['target']
 
-    def _predict_for_default_mode(self, df: pd.DataFrame, args: Dict) -> pd.DataFrame:
-        pass
+        # Run predict for the default mode.
+        if mode == 'default':
+            prompts = self._prepare_data_for_default_mode(df, args)
+            predictions = self._predict_for_default_mode(model_id, prompts, inference_config)
 
-    def describe(self, attribute: Optional[str] = None) -> pd.DataFrame:
+        pred_df = pd.DataFrame(predictions, columns=[target])
+        return pred_df
+
+    def _prepare_data_for_default_mode(self, df: pd.DataFrame, args: Dict) -> Dict:
+        handler_model_params = args['handler_model_params']
+
+        # Prepare the parameters + data for the prediction.
+        # Question column.
+        if handler_model_params.get('question_column') is not None:
+            if handler_model_params['question_column'] not in df.columns:
+                raise ValueError(f"Column {handler_model_params['question_column']} not found in the dataframe!")
+            
+            questions = list(df[args['question_column']].apply(lambda x: str(x)))
+            prompts = [{"role": "user", "content": [{"text": question}]} for question in questions]
+
+        # Prompt template.
+        if handler_model_params.get('prompt_template') is not None:
+            pass
+
+        return prompts
+
+    def _predict_for_default_mode(self, model_id: Text, prompts: List[Text], inference_config: Dict) -> Dict:
+        predictions = []
+        bedrock_runtime_client = create_amazon_bedrock_runtime_client(
+            **self.engine_storage.get_connection_args()
+        )
+
+        for prompt in prompts:
+            response = bedrock_runtime_client.converse(
+                modelId=model_id,
+                messages=[prompt],
+                inferenceConfig=inference_config
+            )
+            predictions.append(
+                response["output"]["message"]["content"][0]["text"]
+            )
+
+        return predictions
+
+    def describe(self, attribute: Optional[Text] = None) -> pd.DataFrame:
         pass

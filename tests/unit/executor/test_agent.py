e425df94a7ad57fadf513d3a356bb3caa59c412b
andrew <elkin.andr@gmail.com>
2024-07-24 11:27:51 +0300

    unit tests

--- tests/unit/executor/test_agent.py
+++ tests/unit/executor/test_agent.py
@@ -1,5 +1,7 @@
 from tests.unit.executor_test_base import BaseExecutorDummyML
 
+from unittest.mock import patch
+
 
 class TestAgent(BaseExecutorDummyML):
 
@@ -27,6 +29,64 @@ class TestAgent(BaseExecutorDummyML):
              provider='mindsdb',
              model_name = "base_model", -- <
              prompt_template="Answer the user input in a helpful way"
+         ''')
+        ret = self.run_sql("select * from my_agent where question = 'hi'")
+
+        assert agent_response in ret.answer[0]
+
+    @patch('openai.resources.chat.completions.Completions.create')
+    def test_openai_provider(self, mock_chat_completion):
+        agent_response = 'how can I assist you today?'
+
+        mock_chat_completion.return_value = {
+            'choices': [{
+                'message': {
+                    'role': 'system',
+                    'content': agent_response
+                }
+            }]
+        }
+
+        self.run_sql('''
+            CREATE AGENT my_agent
+            USING
+             provider='openai',
+             model_name = "gpt-3.5-turbo",
+             openai_api_key='--',
+             prompt_template="Answer the user input in a helpful way"
+         ''')
+        ret = self.run_sql("select * from my_agent where question = 'hi'")
+
+        assert agent_response in ret.answer[0]
+
+    @patch('openai.resources.chat.completions.Completions.create')
+    def test_openai_provider_with_model(self, mock_chat_completion):
+        agent_response = 'how can I assist you today?'
+
+        mock_chat_completion.return_value = {
+            'choices': [{
+                'message': {
+                    'role': 'system',
+                    'content': agent_response
+                }
+            }]
+        }
+
+        self.run_sql('CREATE ML_ENGINE langchain FROM langchain')
+
+        self.run_sql('''
+            CREATE MODEL lang_model
+                PREDICT answer USING
+            engine = "langchain",
+            model_name = "gpt-3.5-turbo",
+            openai_api_key='--',
+            prompt_template="Answer the user input in a helpful way";
+         ''')
+
+        self.run_sql('''
+            CREATE AGENT my_agent
+            USING
+              model='lang_model'
          ''')
         ret = self.run_sql("select * from my_agent where question = 'hi'")
 

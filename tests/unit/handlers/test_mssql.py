b4418e208648e31fc581bcc579b28b06058627b3
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-07-31 13:05:09 +0530

    added the setUp method to MSSQL unit tests with required attrs

--- tests/unit/handlers/test_mssql.py
+++ tests/unit/handlers/test_mssql.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import pymssql
 import unittest
 from unittest.mock import patch
 
@@ -8,33 +9,37 @@ from tests.unit.handlers.base_db_test import BaseDBTest
 
 class TestMSSQLHandler(BaseDBTest, unittest.TestCase):
 
-    dummy_connection_data = OrderedDict(
-        host='127.0.0.1',
-        port=1433,
-        user='example_user',
-        password='example_pass',
-        database='example_db',
-    )
+    def setUp(self):
+        self.dummy_connection_data = OrderedDict(
+            host='127.0.0.1',
+            port=1433,
+            user='example_user',
+            password='example_pass',
+            database='example_db',
+        )
 
-    get_tables_query = f"""
-        SELECT
-            table_schema,
-            table_name,
-            table_type
-        FROM {dummy_connection_data['database']}.INFORMATION_SCHEMA.TABLES
-        WHERE TABLE_TYPE in ('BASE TABLE', 'VIEW');
-    """
+        self.err_to_raise_on_connect_failure = pymssql.OperationalError("Connection Failed")
 
-    table_name = 'mock_table'
-    get_columns_query = f"""
-        SELECT
-            column_name as "Field",
-            data_type as "Type"
-        FROM
-            information_schema.columns
-        WHERE
-            table_name = '{table_name}'
-    """
+        self.get_tables_query = f"""
+            SELECT
+                table_schema,
+                table_name,
+                table_type
+            FROM {self.dummy_connection_data['database']}.INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_TYPE in ('BASE TABLE', 'VIEW');
+        """
+
+        self.get_columns_query = f"""
+            SELECT
+                column_name as "Field",
+                data_type as "Type"
+            FROM
+                information_schema.columns
+            WHERE
+                table_name = '{self.mock_table}'
+        """
+
+        return super().setUp()
 
     def create_handler(self):
         return SqlServerHandler('mssql', connection_data=self.dummy_connection_data)

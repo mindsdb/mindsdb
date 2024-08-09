cc308de8d08b71de2996db2e5f11896165037b3d
Minura Punchihewa <49385643+MinuraPunchihewa@users.noreply.github.com>
2024-08-09 17:19:39 +0530

    Amazon DynamoDB Integration Improvements (#9593)

--- mindsdb/integrations/handlers/dynamodb_handler/connection_args.py
+++ mindsdb/integrations/handlers/dynamodb_handler/connection_args.py
@@ -6,16 +6,29 @@ from mindsdb.integrations.libs.const import HANDLER_CONNECTION_ARG_TYPE as ARG_T
 connection_args = OrderedDict(
     aws_access_key_id={
         'type': ARG_TYPE.STR,
-        'description': 'The access key for the AWS account.'
+        'description': 'The AWS access key that identifies the user or IAM role.',
+        'required': True,
+        'label': 'AWS Access Key'
     },
     aws_secret_access_key={
         'type': ARG_TYPE.STR,
-        'description': 'The secret key for the AWS account.',
-        'secret': True
+        'description': 'The AWS secret access key that identifies the user or IAM role.',
+        'secret': True,
+        'required': True,
+        'label': 'AWS Secret Access Key'
     },
     region_name={
         'type': ARG_TYPE.STR,
-        'description': 'The AWS region where the DynamoDB tables are created.'
+        'description': 'The AWS region to connect to.',
+        'required': True,
+        'label': 'AWS Region'
+    },
+    aws_session_token={
+        'type': ARG_TYPE.STR,
+        'description': 'The AWS session token that identifies the user or IAM role. This becomes necessary when using temporary security credentials.',
+        'secret': True,
+        'required': False,
+        'label': 'AWS Session Token'
     }
 )
 

d5746b9fda8bcb90d81b950e6403332477c7ace6
ZoranPandovski <zoran.pandovski@gmail.com>
2024-08-27 14:20:21 +0200

    Simplify check

--- mindsdb/utilities/security.py
+++ mindsdb/utilities/security.py
@@ -64,8 +64,7 @@ def validate_crawling_sites(urls):
     if isinstance(urls, str):
         urls = [urls]
     # Check if all provided URLs are from the allowed sites
-    valid = all(
-        any(urlparse(url).netloc == allowed_netloc for allowed_netloc in allowed_netlocs)
-        for url in urls
-    )
+    valid = all(urlparse(url).netloc in allowed_netlocs for url in urls)
     return valid
+
+

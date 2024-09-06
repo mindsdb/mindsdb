c4183e2cedfee511ee2eff72a68c33f5634c0aa8
andrew <elkin.andr@gmail.com>
2024-09-06 17:56:49 +0300

    show warning if cert defined but not exists

--- mindsdb/api/mysql/mysql_proxy/mysql_proxy.py
+++ mindsdb/api/mysql/mysql_proxy/mysql_proxy.py
@@ -880,6 +880,8 @@ class MysqlProxy(SocketServer.BaseRequestHandler):
             cert_path = tempfile.mkstemp(prefix="mindsdb_cert_", text=True)[1]
             make_ssl_cert(cert_path)
             atexit.register(lambda: os.remove(cert_path))
+        elif not os.path.exists(cert_path):
+            logger.error(f"Certificate not exists: {cert_path}")
 
         # TODO make it session local
         server_capabilities.set(CAPABILITIES.CLIENT_SSL, config["api"]["mysql"]["ssl"])

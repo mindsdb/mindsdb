aa369988aa46998112f78c6085bb9ce678e49b49
Mohamed <mohamed.abderrahim44@gmail.com>
2024-09-13 14:45:04 +0100

    add normal flow test

--- mindsdb/integrations/handlers/unify_handler/tests/test_unify_handler.py
+++ mindsdb/integrations/handlers/unify_handler/tests/test_unify_handler.py
@@ -1,77 +1,74 @@
 import os
 import pytest
 import pandas as pd
-from unittest.mock import patch
 
-from .base_ml_test import BaseMLAPITest
+from mindsdb_sql import parse_sql
+
+from tests.unit.executor_test_base import BaseExecutorTest
+
+class TestUnify(BaseExecutorTest):
+    # def wait_predictor(self, project, name):
+    #     # wait
+    #     done = False
+    #     for _ in range(200):
+    #         ret = self.run_sql(f"select * from {project}.models where name='{name}'")
+    #         if not ret.empty:
+    #             if ret["STATUS"][0] == "complete":
+    #                 done = True
+    #                 break
+    #             elif ret["STATUS"][0] == "error":
+    #                 break
+    #         time.sleep(0.5)
+    #     if not done:
+    #         raise RuntimeError("predictor wasn't created")
+    
+    def run_sql(self, sql):
+        ret = self.command_executor.execute_command(parse_sql(sql, dialect="mindsdb"))
+        assert ret.error_code is None
+        if ret.data is not None:
+            return ret.data.to_df()
 
 
-# @pytest.mark.skipif(os.environ.get('UNIFY_API_KEY') is None, reason='Missing API key!')
-class TestUnify(BaseMLAPITest):
     """
     Integration tests for the Unify handler.
     """
-
+    @pytest.fixture(autouse=True, scope="function")
     def setup_method(self):
         """
         Setup test environment, creating a project
         """
         super().setup_method()
-        self.run_sql("CREATE database proj")
+        self.run_sql("create database proj")
+        # self.run_sql(
+        #     f"""
+        #     CREATE ML_ENGINE unify-engine
+        #     FROM unify
+        #     USING
+        #     unify_api_key = '{os.environ.get('UNIFY_API_KEY')}';
+        #     """
+        # )
+    
+    @pytest.mark.skipif(os.environ.get('UNIFY_API_KEY') is None, reason='Missing API key!')
+    def test_unify_correct_flow(self):
         self.run_sql(
-            f"""
-            CREATE ML_ENGINE unify_engine
-            FROM unify
-            USING
-            unify_api_key = '{self.get_api_key('UNIFY_API_KEY')}';
-            """
+        f"""
+        CREATE ML_ENGINE unify_engine
+        FROM unify
+        USING
+            unify_api_key = '{os.environ.get('UNIFY_API_KEY')}';
+        """
         )
-
-    def test_unsupported_model(self):
-        """Test for unsupported model in Unify"""
+        
         self.run_sql(
-            f"""
-            CREATE MODEL proj.test_unify_unsupported_model
-            PREDICT output
-            USING
-                engine='unify-engine',
-                column='text',
-                model='this-model-does-not-exist',
-                api_key='{self.get_api_key('ANTHROPIC_API_KEY')}';
-            """
-        )
-        with pytest.raises(Exception):
-            self.wait_predictor("proj", "test_unsupported_model")
-
-    def test_unknown_model_argument(self):
-        """Test for unknown argument when creating a Unify model"""
-        self.run_sql(
-            f"""
-            CREATE MODEL proj.test_unify_unknown_argument
-            PREDICT output
-            USING
-                engine='unify-engine',
-                column='text',
-                api_key='{self.get_api_key('UNIFY_API_KEY')}',
-                unknown_argument='wrong value';
-            """
-        )
-        with pytest.raises(Exception):
-            self.wait_predictor("proj", "test_unify_unknown_argument")
-            
-    def test_correct_flow_in_default_mode(self):
-        """Test for correct flow: model to give answer to given instruction"""
-        self.run_sql(
-            f"""
-            CREATE MODEL proj.test_unify_correct_flow
-            PREDICT output
-            USING
-                engine='unify_engine',
-                model = 'llama-3-8b-chat',
-                provider = 'together-ai',
-                column = 'text',
-                api_key='{self.get_api_key('UNIFY_API_KEY')}';
-            """
+        f"""
+        CREATE MODEL proj.test_unify_correct_flow
+        PREDICT output
+        USING
+            engine='unify_engine',
+            model = 'llama-3-8b-chat',
+            provider = 'together-ai',
+            column = 'text';
+        """
         )
         self.wait_predictor("proj", "test_unify_correct_flow")
 
@@ -82,4 +79,4 @@ class TestUnify(BaseMLAPITest):
             WHERE text = 'Hello';
         """
         )
-        assert "Hello" in result_df["answer"].iloc[0].lower()
+        assert "hello" in result_df["output"].iloc[0].lower()

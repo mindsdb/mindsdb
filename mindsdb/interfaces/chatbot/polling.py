b1e8c3b0e38bcd7bab007740898b010383c98676
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-09-27 19:40:51 +0530

    updated real time polling to support multiple tables

--- mindsdb/interfaces/chatbot/polling.py
+++ mindsdb/interfaces/chatbot/polling.py
@@ -18,11 +18,13 @@ class BasePolling:
     def start(self, stop_event):
         raise NotImplementedError
 
-    def send_message(self, message: ChatBotMessage):
+    def send_message(self, message: ChatBotMessage, table_name=None):
         chat_id = message.destination
         text = message.text
 
-        t_params = self.params["chat_table"]
+        t_params = self.params["chat_table"] if table_name is None else next(
+            (t["chat_table"] for t in self.params if t["chat_table"]["name"] == table_name)
+        )
         chat_id_cols = t_params["chat_id_col"] if isinstance(t_params["chat_id_col"], list) else [t_params["chat_id_col"]]
 
         ast_query = Insert(
@@ -46,22 +48,27 @@ class MessageCountPolling(BasePolling):
     def run(self, stop_event):
         while True:
             try:
-                chat_ids = self.check_message_count()
-                logger.debug(f"number of chat ids found: {len(chat_ids)}")
-                for chat_id in chat_ids:
-                    try:
-                        chat_memory = self.chat_task.memory.get_chat(chat_id)
-                    except Exception as e:
-                        logger.error(f"Problem retrieving chat memory: {e}")
+                for chat_params in self.params:
+                    chat_ids = self.check_message_count(chat_params)
+                    logger.debug(f"number of chat ids found: {len(chat_ids)}")
 
-                    try:
-                        message = self.get_last_message(chat_memory)
-                    except Exception as e:
-                        logger.error(f"Problem getting last message: {e}")
-                        message = None
+                    for chat_id in chat_ids:
+                        try:
+                            chat_memory = self.chat_task.memory.get_chat(
+                                chat_id,
+                                table_name=chat_params["chat_table"]["name"],
+                            )
+                        except Exception as e:
+                            logger.error(f"Problem retrieving chat memory: {e}")
 
-                    if message:
-                        self.chat_task.on_message(chat_memory, message)
+                        try:
+                            message = self.get_last_message(chat_memory)
+                        except Exception as e:
+                            logger.error(f"Problem getting last message: {e}")
+                            message = None
+
+                        if message:
+                            self.chat_task.on_message(chat_memory, message, table_name=chat_params["chat_table"]["name"])
 
             except Exception as e:
                 logger.error(e)
@@ -84,8 +91,8 @@ class MessageCountPolling(BasePolling):
             return
         return last_message
 
-    def check_message_count(self):
-        p_params = self.params["polling"]
+    def check_message_count(self, chat_params):
+        p_params = chat_params["polling"]
 
         chat_ids = []
 

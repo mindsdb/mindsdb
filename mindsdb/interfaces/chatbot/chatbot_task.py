c49032c2e02e6cae64dd3494e562c25e0fac781c
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-09-27 18:36:01 +0530

    converted params to messag count polling to lists

--- mindsdb/interfaces/chatbot/chatbot_task.py
+++ mindsdb/interfaces/chatbot/chatbot_task.py
@@ -53,8 +53,8 @@ class ChatBotTask(BaseTask):
 
         polling = chat_params['polling']['type']
         if polling == 'message_count':
-            self.chat_pooling = MessageCountPolling(self, chat_params)
-            self.memory = HandlerMemory(self, chat_params)
+            self.chat_pooling = MessageCountPolling(self, chat_params if isinstance(chat_params, list) else [chat_params])
+            self.memory = HandlerMemory(self, chat_params if isinstance(chat_params, list) else [chat_params])
 
         elif polling == 'realtime':
             self.chat_pooling = RealtimePolling(self, chat_params)

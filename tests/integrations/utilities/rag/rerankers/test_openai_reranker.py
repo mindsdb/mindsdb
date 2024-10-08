bd509b92f127bbc2da703456b8d022b9340d1e60
Fedor Shabashev <fedor.shabashev@gmail.com>
2024-10-08 23:07:00 +0100

    minor fix: updating texts used for unit test

--- tests/integrations/utilities/rag/rerankers/test_openai_reranker.py
+++ tests/integrations/utilities/rag/rerankers/test_openai_reranker.py
@@ -9,9 +9,9 @@ async def test_openai_reranker():
     openai_reranker = OpenAIReranker()
     openai_reranker.model_post_init(None)
     results = await openai_reranker.compress_documents(
-        documents=[Document(page_content="Trump declared that he likes cats more than dogs"),
-                   Document(page_content="Trump declared that he likes AI")],
-        query="Trump opinion on animals",
+        documents=[Document(page_content="Jack declared that he likes cats more than dogs"),
+                   Document(page_content="Jack declared that he likes AI")],
+        query="Jack's opinion on animals",
     )
     assert len(results) == 1
     assert "cats" in results[0].page_content

fb5d392820cc68c31010592a9430abf8887adbf9
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-10-09 21:16:05 +0530

    added the skeleton for ContactsTable

---
+++ mindsdb/integrations/handlers/salesforce_handler/salesforce_tables.py
@@ -0,0 +1,46 @@
+from typing import Dict, List, Text
+
+from mindsdb_sql.parser.ast import Select
+import pandas as pd
+
+from mindsdb.integrations.libs.api_handler import APIResource
+from mindsdb.utilities import log
+
+
+logger = log.getLogger(__name__)
+
+
+class ContactsTable(APIResource):
+    """
+    This is the table abstraction for the Contacts resource of the Salesforce API.
+    """
+
+    def select(self, query: Select) -> pd.DataFrame:
+        """
+        Executes a SELECT SQL query represented by an ASTNode object on the Salesforce Contacts resource and retrieves the data (if any).
+
+        Args:
+            query (ASTNode): An ASTNode object representing the SQL query to be executed.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the data retrieved from the Salesforce Contacts resource.
+        """
+        pass
+
+    def add(self, contact: Dict) -> None:
+        """
+        Adds a new contact to the Salesforce Contacts resource.
+
+        Args:
+            contact (Dict): The data to be inserted into the Salesforce Contacts resource.
+        """
+        pass
+
+    def get_columns(self) -> List[Text]:
+        """
+        Retrieves the attributes (columns) of the Salesforce Contacts resource.
+
+        Returns:
+            List[Text]: A list of Attributes (columns) of the Salesforce Contacts resource.
+        """
+        pass

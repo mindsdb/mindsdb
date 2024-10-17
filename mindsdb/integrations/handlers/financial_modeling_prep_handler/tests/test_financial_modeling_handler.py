89ba819116bc68bda5cfd641ddf47f140de76be2
RiyanaD <riyana.duttaa@gmail.com>
2024-10-17 15:29:30 -0700

    fixing test table name

--- mindsdb/integrations/handlers/financial_modeling_prep_handler/tests/test_financial_modeling_handler.py
+++ mindsdb/integrations/handlers/financial_modeling_prep_handler/tests/test_financial_modeling_handler.py
@@ -10,18 +10,18 @@ class FinancialModelingHandlerTest(unittest.TestCase):
             "api_key": "TdP0vQC4NIfRFIl6d31ZbhpbWLsSEccA"
         }
         cls.handler = FinancialModelingHandler('test_financial_handler', connection_data)
-    
+
     def test_0_select_query(self):
-        query = "SELECT * FROM my_table.historical_prices WHERE symbol = 'AAPL'"
+        query = "SELECT * FROM fmp_datasource.historical_prices WHERE symbol = 'AAPL'"
         result = self.handler.query(query)
         print(result)
         assert result.type is RESPONSE_TYPE.TABLE
 
     def test_1_select_limit_query(self):
-        query = "SELECT * FROM my_table.historical_prices WHERE symbol = 'AAPL' LIMIT 5"
+        query = "SELECT * FROM fmp_datasource.historical_prices WHERE symbol = 'AAPL' LIMIT 5"
         result = self.handler.query(query)
         print(result)
-        assert result.data_frame.shape[0] == 5 
+        assert result.data_frame.shape[0] == 5
 
 
 if __name__ == '__main__':

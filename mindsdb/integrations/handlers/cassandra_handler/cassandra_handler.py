666c6ca7d8003a20b532bf18f9e64959ffcefd3f
tino097 <konstantin.sivakov@gmail.com>
2024-10-24 10:29:51 +0200

    Implement `get_tables` method to return correct table names

--- mindsdb/integrations/handlers/cassandra_handler/cassandra_handler.py
+++ mindsdb/integrations/handlers/cassandra_handler/cassandra_handler.py
@@ -1,4 +1,9 @@
 from mindsdb.integrations.handlers.scylla_handler import Handler as ScyllaHandler
+from mindsdb.integrations.libs.response import (
+    HandlerResponse as Response,
+    RESPONSE_TYPE
+)
+import pandas as pd
 
 
 class CassandraHandler(ScyllaHandler):
@@ -10,3 +15,16 @@ class CassandraHandler(ScyllaHandler):
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+        self.connection_data = kwargs.get('connection_data', {})
+        self.keyspace = self.connection_data.get('keyspace')
+
+    def get_tables(self) -> Response:
+        """
+        Get the list of tables in the connected Cassandra database.
+
+        :return: List of table names.
+        """
+        tables = self.session.execute("SELECT table_name FROM system_schema.tables WHERE keyspace_name = '%s'" % self.keyspace)
+        response = Response(RESPONSE_TYPE.TABLE,
+                            pd.DataFrame([dict(table_name=row.table_name) for row in tables]))
+        return response

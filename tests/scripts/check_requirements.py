0cc1b7d94bac75083afe588c2e32338f260bdd95
Minura Punchihewa <49385643+MinuraPunchihewa@users.noreply.github.com>
2024-10-30 21:56:34 +0530

    added missing comma to package mapping

--- tests/scripts/check_requirements.py
+++ tests/scripts/check_requirements.py
@@ -127,7 +127,7 @@ PACKAGE_NAME_MAP = {
     "auto-ts": ["auto_ts"],
     "llama-index-readers-web": ["llama_index"],
     "llama-index-embeddings-openai": ["llama_index"],
-    "unifyai": ["unify"]
+    "unifyai": ["unify"],
     "botframework-connector": ["botframework"],
     "botbuilder-schema": ["botbuilder"],
     "opentelemetry-api": ["opentelemetry"],

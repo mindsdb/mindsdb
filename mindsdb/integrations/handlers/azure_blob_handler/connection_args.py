1310c72e863a778d6158b9d90a05f29e5fe27521
Fabian Jevon <Shar_evonz@hotmail.com>
2024-10-30 17:58:05 +0700

    remove access key param

--- mindsdb/integrations/handlers/azure_blob_handler/connection_args.py
+++ mindsdb/integrations/handlers/azure_blob_handler/connection_args.py
@@ -10,13 +10,6 @@ connection_args = OrderedDict(
         'required': True,
         'label': 'Storage Account Name'
     },
-    account_access_key={
-        'type': ARG_TYPE.STR,
-        'description': 'Account Access Key',
-        'required': True,
-        'label': 'Account Access Key',
-        'secret': True
-    },
     container_name={
         'type': ARG_TYPE.STR,
         'description': 'The name of your storage service account Container Name',
@@ -35,7 +28,6 @@ connection_args = OrderedDict(
 
 connection_args_example = OrderedDict(
     storage_account_name='',
-    account_access_key='',
     container_name='',
     connection_string=''
 )

46124226d10a6f8f148f6cbe82d1012f0a68a0be
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-11-01 13:11:51 +0530

    added the skeleton for the MS Graph API client for One Drive

---
+++ mindsdb/integrations/handlers/ms_one_drive_handler/ms_graph_api_one_drive_client.py
@@ -0,0 +1,11 @@
+from typing import Text, List, Dict, Optional
+from mindsdb.integrations.utilities.handlers.api_utilities.microsoft.ms_graph_api_utilities import MSGraphAPIBaseClient
+
+
+class MSGraphAPIOneDriveClient(MSGraphAPIBaseClient):
+    """
+    The Microsoft Graph API client for the Microsoft OneDrive handler.
+    This client is used for accessing the Microsoft OneDrive specific endpoints of the Microsoft Graph API.
+    Several common methods for submitting requests, fetching data, etc. are inherited from the base class.
+    """
+    pass

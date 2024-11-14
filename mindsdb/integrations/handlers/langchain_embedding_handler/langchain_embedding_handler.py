af3e5e0ec3c24f42bedd97c4a6b828502df509dd
AverellGatton <gattonian@gmail.com>
2024-11-13 20:24:46 -0800

    user args specify embedding keyword

--- mindsdb/integrations/handlers/langchain_embedding_handler/langchain_embedding_handler.py
+++ mindsdb/integrations/handlers/langchain_embedding_handler/langchain_embedding_handler.py
@@ -164,8 +164,13 @@ class LangchainEmbeddingHandler(BaseMLEngine):
             )
 
         # convert each row into a document
-        df_texts = df[input_columns].apply(self.row_to_document, axis=1)
-        embeddings = model.embed_documents(df_texts.tolist())
+        if user_args["task"] == "embed":
+            df_texts = df[input_columns].apply(self.row_to_document, axis=1)
+            embeddings = model.embed_documents(df_texts.tolist())
+        elif user_args["task"] == "query":
+            df_texts = df[input_columns].apply(self.row_to_document, axis=1)
+            embeddings = model.embed_query(df_texts.tolist())
+
 
         # create a new dataframe with the embeddings
         df_embeddings = df.copy().assign(**{target: embeddings})

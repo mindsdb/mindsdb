11d19b99fe42d939f4246b93c271cd9e356aa20c
Max Stepanov <stpmax@yandex.ru>
2024-11-19 10:42:35 +0300

    update migration

--- mindsdb/migrations/versions/2024-11-19_0f89b523f346_agent_skills_parameters.py
+++ mindsdb/migrations/versions/2024-11-19_0f89b523f346_agent_skills_parameters.py
@@ -1,7 +1,7 @@
 """agent_skills_parameters
 
 Revision ID: 0f89b523f346
-Revises: 6c57ed39a82b
+Revises: 9d559f68d535
 Create Date: 2024-11-13 15:24:39.796947
 
 """
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import sqlite  # noqa
 
 # revision identifiers, used by Alembic.
 revision = '0f89b523f346'
-down_revision = '6c57ed39a82b'
+down_revision = '9d559f68d535'
 branch_labels = None
 depends_on = None
 

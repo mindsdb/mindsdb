76130506b73795b95dbcc170544991ef6f750aed
Max Stepanov <stpmax@yandex.ru>
2024-12-05 13:53:53 +0300

    Raise an exception in `create_tmp_handler` (#10244)

--- mindsdb/integrations/libs/process_cache.py
+++ mindsdb/integrations/libs/process_cache.py
@@ -223,24 +223,15 @@ class ProcessCache:
 
         if config['ml_task_queue']['type'] != 'redis':
             if is_cloud:
-                try:
-                    lightwood_handler = integration_controller.get_handler_module('lightwood')
-                except ImportError:
-                    lightwood_handler = None
+                lightwood_handler = integration_controller.get_handler_module('lightwood')
                 if lightwood_handler is not None and lightwood_handler.Handler is not None:
                     preload_handlers[lightwood_handler.Handler] = 4 if is_cloud else 1
 
-                try:
-                    huggingface_handler = integration_controller.get_handler_module('huggingface')
-                except ImportError:
-                    huggingface_handler = None
-                    if huggingface_handler is not None and huggingface_handler.Handler is not None:
-                        preload_handlers[huggingface_handler.Handler] = 1
+                huggingface_handler = integration_controller.get_handler_module('huggingface')
+                if huggingface_handler is not None and huggingface_handler.Handler is not None:
+                    preload_handlers[huggingface_handler.Handler] = 1
 
-                try:
-                    openai_handler = integration_controller.get_handler_module('openai')
-                except ImportError:
-                    openai_handler = None
+                openai_handler = integration_controller.get_handler_module('openai')
                 if openai_handler is not None and openai_handler.Handler is not None:
                     preload_handlers[openai_handler.Handler] = 1
 

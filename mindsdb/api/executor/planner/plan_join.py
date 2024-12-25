1dbfceea75802c8598b33b46f7ae05710b3664f1
andrew <elkin.andr@gmail.com>
2024-12-25 17:42:48 +0300

    return old planing, to not fix tests

--- mindsdb/api/executor/planner/plan_join.py
+++ mindsdb/api/executor/planner/plan_join.py
@@ -571,8 +571,7 @@ class PlanJoinTablesQuery:
                 else:
                     model_params[param.lower()] = value
 
-            # partition_size is handled directly by project_datanode.predict
-            # partition_size = model_params.pop('partition_size', None)
+            partition_size = model_params.pop('partition_size', None)
 
         predictor_step = ApplyPredictorStep(
             namespace=item.integration,

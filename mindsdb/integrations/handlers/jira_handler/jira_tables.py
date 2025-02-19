19613816edaaf225f5964a2d8671bb7f9274ad0a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-02-19 13:15:32 +0530

    added support for IN conditions

--- mindsdb/integrations/handlers/jira_handler/jira_tables.py
+++ mindsdb/integrations/handlers/jira_handler/jira_tables.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from atlassian import Jira
 import pandas as pd
 
 from mindsdb.integrations.libs.api_handler import APIResource
@@ -18,13 +19,16 @@ class JiraProjectsTable(APIResource):
         sort: List[SortColumn] = None,
         targets: List[str] = None,
         **kwargs
-    ):
+    ) -> pd.DataFrame:
         client = self.handler.connect()
 
         projects = []
         for condition in conditions:
-            if (condition.column in ('id', 'key') and condition.op == FilterOperator.EQUAL):
-                projects = [client.get_project(condition.value)]
+            if condition.column in ('id', 'key'): 
+                if condition.op == FilterOperator.EQUAL:
+                    projects = [client.get_project(condition.value)]
+                elif condition.op == FilterOperator.IN:
+                    projects = [client.get_project(project_id) for project_id in condition.value]
                 condition.applied = True
 
         if not projects:
@@ -61,38 +65,58 @@ class JiraIssuesTable(APIResource):
         sort: List[SortColumn] = None,
         targets: List[str] = None,
         **kwargs
-    ):
+    ) -> pd.DataFrame:
         client = self.handler.connect()
 
         issues = []
+        are_conditions_valid = False
         for condition in conditions:
-            if (condition.column in ('id', 'key') and condition.op == FilterOperator.EQUAL):
-                issues = [client.get_issue(condition.value)]
+            if condition.column in ('id', 'key'): 
+                if condition.op == FilterOperator.EQUAL:
+                    issues = [client.get_issue(condition.value)]
+                elif condition.op == FilterOperator.IN:
+                    issues = [client.get_issue(issue_id) for issue_id in condition.value]
                 condition.applied = True
+                are_conditions_valid = True
 
-            if condition.column in ('project_id', 'project_key', 'project_name') and condition.op == FilterOperator.EQUAL:
-                issues = client.get_all_project_issues(condition.value)
+            elif condition.column in ('project_id', 'project_key', 'project_name'):
+                if condition.op == FilterOperator.EQUAL:
+                    issues = client.get_all_project_issues(condition.value, limit=limit)
+                elif condition.op == FilterOperator.IN:
+                    for project_id in condition.value:
+                        if limit:
+                            # NOTE: Does this have a limit? Paging?
+                            issues.extend(client.get_all_project_issues(project_id, limit=limit - len(issues)))
+                            if len(issues) >= limit:
+                                break
+                        else:
+                            issues.extend(client.get_all_project_issues(project_id))
+
                 condition.applied = True
+                are_conditions_valid = True
 
-        if not issues:
+        if not are_conditions_valid:
             raise ValueError("Either the issue 'id', 'key' or one of 'project_id', 'project_key', 'project_name' must be provided.")
 
-        issues_df = pd.json_normalize(issues)
-        issues_df.rename(columns={
-            "fields.project.id": "project_id",
-            "fields.project.key": "project_key",
-            "fields.project.name": "project_name",
-            "fields.summary": "summary",
-            "fields.priority.name": "priority",
-            "fields.creator.displayName": "creator",
-            "fields.assignee.displayName": "assignee",
-            "fields.status.name": "status",
-        }, inplace=True)
-        issues_df = issues_df[self.get_columns()]
+        if issues:
+            issues_df = pd.json_normalize(issues)
+            issues_df.rename(columns={
+                "fields.project.id": "project_id",
+                "fields.project.key": "project_key",
+                "fields.project.name": "project_name",
+                "fields.summary": "summary",
+                "fields.priority.name": "priority",
+                "fields.creator.displayName": "creator",
+                "fields.assignee.displayName": "assignee",
+                "fields.status.name": "status",
+            }, inplace=True)
+            issues_df = issues_df[self.get_columns()]
+        else:
+            issues_df = pd.DataFrame([], columns=self.get_columns())
 
         return issues_df
 
-    def get_columns(self):
+    def get_columns(self) -> List[str]:
         return [
             "id",
             "key",

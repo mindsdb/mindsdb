8bcecd1253cbf49dc33d287dca01d1f254878bbc
andrew <elkin.andr@gmail.com>
2025-02-28 19:06:44 +0300

    generate tables from api

--- mindsdb/integrations/handlers/jira_handler/__init__.py
+++ mindsdb/integrations/handlers/jira_handler/__init__.py
@@ -2,20 +2,9 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 from .__about__ import __version__ as version, __description__ as description
 
 try:
-    # from .jira_handler import JiraHandler as Handler
-    # import_error = None
-    from mindsdb.integrations.libs.api_handler_generator import APIHandlerGenerator, APIResourceGenerator, OpenAPISpecParser
-
-    openapi_spec_parser = OpenAPISpecParser(
-        "https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json"
-    )
-    api_handler_generator = APIHandlerGenerator(openapi_spec_parser)
-    api_resource_generator = APIResourceGenerator(openapi_spec_parser)
-
-    resources = api_resource_generator.generate_api_resources()
-    Handler = api_handler_generator.generate_api_handler(resources)
-
+    from .jira_handler import JiraHandler as Handler
     import_error = None
+
 except Exception as e:
     Handler = None
     import_error = e

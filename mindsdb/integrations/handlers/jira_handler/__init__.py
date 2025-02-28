939f7b5de51b927617184bb041637d5949a022c1
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2025-02-28 16:33:26 +0530

    removed dummy handler

--- mindsdb/integrations/handlers/jira_handler/__init__.py
+++ mindsdb/integrations/handlers/jira_handler/__init__.py
@@ -2,7 +2,21 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 from .__about__ import __version__ as version, __description__ as description
 
 try:
-    from .jira_handler import JiraHandler as Handler
+    # from .jira_handler import JiraHandler as Handler
+    # import_error = None
+    from mindsdb.integrations.libs.api_handler_generator import APIHandlerGenerator, APIResourceGenerator
+
+
+    api_handler_generator = APIHandlerGenerator(
+        "https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json"
+    )
+    api_resource_generator = APIResourceGenerator(
+        "https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json"
+    )
+
+    resources = api_resource_generator.generate_api_resources()
+    Handler = api_handler_generator.generate_api_handler(resources)
+
     import_error = None
 except Exception as e:
     Handler = None

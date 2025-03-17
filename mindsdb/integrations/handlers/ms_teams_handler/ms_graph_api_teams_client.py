1ec4c0a89470daf0fa80e4e9f719f5fac18f3a79
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-03-17 12:52:48 -0700

    added the client methods for retrieving channel messages

--- mindsdb/integrations/handlers/ms_teams_handler/ms_graph_api_teams_client.py
+++ mindsdb/integrations/handlers/ms_teams_handler/ms_graph_api_teams_client.py
@@ -49,8 +49,7 @@ class MSGraphAPITeamsClient(MSGraphAPIBaseClient):
         Returns:
             Dict: The channel data.
         """
-        api_url = self._get_api_url(f"teams/{group_id}/channels/{channel_id}")
-        channel = self.fetch_data_json(api_url)
+        channel = self.fetch_data_json(f"teams/{group_id}/channels/{channel_id}")
         # Add the group ID to the channel data.
         channel.update({"teamId": group_id})
 
@@ -115,3 +114,55 @@ class MSGraphAPITeamsClient(MSGraphAPIBaseClient):
         channels = self.get_all_channels_across_all_groups()
 
         return [channel for channel in channels if channel["id"] in channel_ids]
+    
+    def get_message_in_channel_by_id(self, group_id: Text, channel_id: Text, message_id: Text) -> Dict:
+        """
+        Get a message by its ID, the ID of the group that it belongs to, and the ID of the channel that it belongs to.
+
+        Args:
+            group_id (Text): The ID of the group that the channel belongs to.
+            channel_id (Text): The ID of the channel that the message belongs to.
+            message_id (Text): The ID of the message.
+
+        Returns:
+            Dict: The message data.
+        """
+        return self.fetch_data_json(f"teams/{group_id}/channels/{channel_id}/messages/{message_id}")
+    
+    def get_messages_in_channel_by_ids(self, group_id: Text, channel_id: Text, message_ids: List[Text]) -> List[Dict]:
+        """
+        Get messages by their IDs, the ID of the group that they belong to, and the ID of the channel that they belong to.
+
+        Args:
+            group_id (Text): The ID of the group that the channel belongs to.
+            channel_id (Text): The ID of the channel that the messages belong to.
+            message_ids (List[Text]): The IDs of the messages.
+
+        Returns:
+            List[Dict]: The messages data.
+        """
+        messages = []
+        for message_id in message_ids:
+            messages.append(self.get_message_in_channel_by_id(group_id, channel_id, message_id))
+
+        return messages
+
+    def get_all_messages_in_channel(self, group_id: Text, channel_id: Text, limit: int = None) -> List[Dict]:
+        """
+        Get messages of a channel by its ID and the ID of the group that it belongs to.
+
+        Args:
+            group_id (Text): The ID of the group that the channel belongs to.
+            channel_id (Text): The ID of the channel.
+
+        Returns:
+            List[Dict]: The messages data.
+        """
+        messages = []
+        for messages_batch in self.fetch_paginated_data(f"teams/{group_id}/channels/{channel_id}/messages"):
+            messages += messages_batch
+
+            if limit and len(messages) >= limit:
+                break
+
+        return messages[:limit]

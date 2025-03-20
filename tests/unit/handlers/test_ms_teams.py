1a89d64835bb5c9e1cf5a575762ed144b2b38ba9
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-03-19 20:47:45 -0700

    added test for connect with cache

--- tests/unit/handlers/test_ms_teams.py
+++ tests/unit/handlers/test_ms_teams.py
@@ -46,9 +46,19 @@ class TestMSTeamsHandler(BaseHandlerTestSetup, unittest.TestCase):
             handler_storage=mock_handler_storage
         )
     
-    def create_handler_without_code(self):
+    def create_handler_without_code_without_cache(self):
         mock_handler_storage = MagicMock()
         mock_handler_storage.file_get.side_effect = FileNotFoundError
+
+        return MSTeamsHandler(
+            'teams',
+            connection_data=self.dummy_connection_data_without_code,
+            handler_storage=mock_handler_storage
+        )
+    
+    def create_handler_without_code_with_cache(self):
+        mock_handler_storage = MagicMock()
+        mock_handler_storage.file_get.side_effect = b'mock_file_content'
 
         return MSTeamsHandler(
             'teams',
@@ -63,7 +73,7 @@ class TestMSTeamsHandler(BaseHandlerTestSetup, unittest.TestCase):
         """
         Test if `connect` method successfully returns a redirect URL when the authentication code is not provided.
         """
-        self.handler = self.create_handler_without_code()
+        self.handler = self.create_handler_without_code_without_cache()
 
         mock_msal = MagicMock()
         mock_msal.get_accounts.return_value = []
@@ -77,6 +87,8 @@ class TestMSTeamsHandler(BaseHandlerTestSetup, unittest.TestCase):
 
         self.assertFalse(self.handler.is_connected)
 
+        # TODO: Assert connect() is only called once.
+
     @patch('msal.SerializableTokenCache')
     def test_connect_with_valid_code_returns_access_token(self, mock_token_cache):
         """"
@@ -86,6 +98,36 @@ class TestMSTeamsHandler(BaseHandlerTestSetup, unittest.TestCase):
         mock_msal.get_accounts.return_value = []
 
         mock_msal.acquire_token_by_authorization_code.return_value = {
+            "access_token": "mock_access_token"
+        }
+
+        self.mock_connect.return_value = mock_msal
+
+        mock_token_cache.has_state_changed = True
+
+        connection = self.handler.connect()
+
+        self.assertIsNotNone(connection)
+        self.assertTrue(self.handler.is_connected)
+        # TODO: Assert connect() is called only once.
+
+        # TODO: Asserts _register_table() is called for each table.
+
+    @patch('msal.SerializableTokenCache')
+    def test_connect_with_cache_returns_access_code(self, mock_token_cache):
+        """"
+        Test if `connect` method successfully returns an access token when some content is available in the cache.
+        """
+        self.handler = self.create_handler_without_code_with_cache()
+
+        mock_msal = MagicMock()
+        mock_msal.get_accounts.return_value = [
+            {
+                "mock_key": "mock_value"
+            }
+        ]
+
+        mock_msal.acquire_token_silent.return_value = {
             "access_token": "mock_access_token"
         }
 

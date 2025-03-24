21fbc69c3d49d5877ccb087a93c61db3f3ea92c7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-03-24 12:11:47 +0530

    added tests for check_connection()

--- tests/unit/handlers/test_salesforce.py
+++ tests/unit/handlers/test_salesforce.py
@@ -2,8 +2,13 @@ from collections import OrderedDict
 import unittest
 from unittest.mock import patch, MagicMock
 
+from salesforce_api.exceptions import AuthenticationError
+
 from base_handler_test import BaseHandlerTestSetup
 from mindsdb.integrations.handlers.salesforce_handler.salesforce_handler import SalesforceHandler
+from mindsdb.integrations.libs.response import (
+    HandlerStatusResponse as StatusResponse
+)
 
 
 class TestSalesforceHandler(BaseHandlerTestSetup, unittest.TestCase):
@@ -33,6 +38,27 @@ class TestSalesforceHandler(BaseHandlerTestSetup, unittest.TestCase):
         self.assertIsNotNone(connection)
         self.assertTrue(self.handler.is_connected)
         self.mock_connect.assert_called_once()
+
+    def test_check_connection_success(self):
+        """
+        Test that the `check_connection` method returns a StatusResponse object and accurately reflects the connection status on a successful connection.
+        """
+        response = self.handler.check_connection()
+
+        self.assertTrue(response.success)
+        assert isinstance(response, StatusResponse)
+        self.assertFalse(response.error_message)
+
+    def test_check_connection_failure(self):
+        """
+        Test that the `check_connection` method returns a StatusResponse object and accurately reflects the connection status on a failed connection.
+        """
+        self.mock_connect.side_effect = AuthenticationError('Invalid credentials')
+        response = self.handler.check_connection()
+
+        self.assertFalse(response.success)
+        assert isinstance(response, StatusResponse)
+        self.assertTrue(response.error_message)
 
 
 if __name__ == '__main__':

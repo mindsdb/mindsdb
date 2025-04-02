df9d440bb08d6a2d60a7c71dbff047f70a6cf595
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-04-02 13:46:52 +0530

    improved logic for identifying extension

--- mindsdb/interfaces/functions/to_markdown.py
+++ mindsdb/interfaces/functions/to_markdown.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 
 import fitz  # PyMuPDF
 from markitdown import MarkItDown
+import mimetypes
 from openai import OpenAI
 import requests
 
@@ -66,7 +67,21 @@ class ToMarkdown:
         """
         parsed_url = urlparse(file_path_or_url)
         if parsed_url.scheme in ('http', 'https'):
-            return os.path.splitext(parsed_url.path)[1]
+            try:
+                # Make a HEAD request to get headers without downloading the file.
+                response = requests.head(file_path_or_url, allow_redirects=True)
+                content_type = response.headers.get('Content-Type', '')
+                if content_type:
+                    ext = mimetypes.guess_extension(content_type.split(';')[0].strip())
+                    if ext:
+                        return ext
+
+                # Fallback to extracting extension from the URL path
+                ext = os.path.splitext(parsed_url.path)[1]
+                if ext:
+                    return ext
+            except requests.RequestException:
+                raise RuntimeError(f'Unable to retrieve file extension from URL: {file_path_or_url}')
         else:
             return os.path.splitext(file_path_or_url)[1]
 

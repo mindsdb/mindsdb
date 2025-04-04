27b5e627dfd10d7c2936e17d6bfd775d71caeeb1
Elkin Andrew <elkin@raketa.im>
2025-04-04 14:19:00 +0300

    test ingestion

--- tests/integration_tests/knowledge_bases/test_kb_sql.py
+++ tests/integration_tests/knowledge_bases/test_kb_sql.py
@@ -1,4 +1,10 @@
+import time
+import datetime as dt
+
 import mindsdb_sdk
+from threading import Thread
+
+import pandas as pd
 
 
 class Test:
@@ -16,7 +22,6 @@ class Test:
             }
 
         self.con = mindsdb_sdk.connect(**mindsdb_server)
-
 
     def create_vector_db(self, connection_args):
         engine = connection_args.pop('engine')
@@ -38,7 +43,7 @@ class Test:
         return name
 
     def create_db(self):
-        name = f'example_db'
+        name = 'example_db'
 
         try:
             self.con.databases.get(name)
@@ -69,17 +74,16 @@ class Test:
 
         if engine == 'openai':
             model = self.con.models.create(
-               name, predict='embedding', engine=engine,
-               options=params,
+                name, predict='embedding', engine=engine,
+                options=params,
             )
         elif engine == 'langchain_embedding':
             model = self.con.models.create(
-               name, predict='embedding', engine=engine,
-               options=params,
+                name, predict='embedding', engine=engine,
+                options=params,
             )
         model.wait_complete()
         return model.name
-
 
     def run_sql(self, sql):
         print('>>>', sql)
@@ -98,6 +102,26 @@ class Test:
         # connect database
         self.db_name = self.create_db()
 
+
+    def create_kb(self, name):
+        self.run_sql(f'drop knowledge base if exists {name}')
+
+        self.run_sql(f'''
+                    create knowledge base {name}
+                    using  model={self.emb_model},
+                    storage={self.vectordb_name}.crm_table
+                ''')
+
+        # clean
+        try:
+            self.run_sql(f'delete from kb_crm where id in (select id from {name})')
+        except Exception:
+            ...
+
+        ret = self.run_sql(f'describe knowledge base {name}')
+        assert len(ret) == 1
+
+
     def check_syntax_create_update_drop(self):
         # TODO, what is "Confirm data persistence and integrity in vector store"
 
@@ -105,22 +129,7 @@ class Test:
             Verify successful setup and operation
             Test creation of Knowledge Bases
         '''
-        self.run_sql('drop knowledge base if exists kb_crm')
-
-        self.run_sql(f'''
-            create knowledge base kb_crm
-            using  model={self.emb_model},
-            storage={self.vectordb_name}.crm_table
-        ''')
-
-        # clean
-        try:
-            self.run_sql('delete from kb_crm where id in (select id from kb_crm)')
-        except:
-            ...
-
-        ret = self.run_sql('describe knowledge base kb_crm')
-        assert len(ret) == 1
+        self.create_kb('kb_crm')
 
         # -------------- insert --------
         print('insert from table')
@@ -137,9 +146,9 @@ class Test:
         print('insert from values')
         for i in range(2):
             # do it twice second time it will be updated
-            self.run_sql(f"""
+            self.run_sql("""
                 insert into kb_crm (id, content) values
-                (1000, 'Help'), (1001, 'Thank you')           
+                (1000, 'Help'), (1001, 'Thank you')
             """)
         count_rows += 2
 
@@ -161,29 +170,29 @@ class Test:
         # ---------- selecting options --------
 
         print('Limit')
-        ret = self.run_sql(f"select id, chunk_content from kb_crm limit 4")
+        ret = self.run_sql("select id, chunk_content from kb_crm limit 4")
         assert len(ret) == 4
 
         print('Limit with content')
-        ret = self.run_sql(f"select id, chunk_content from kb_crm where content = 'help' limit 4")
+        ret = self.run_sql("select id, chunk_content from kb_crm where content = 'help' limit 4")
         assert len(ret) == 4
         assert ret['id'][0] == '1000'  # id is string
 
         print('filter by id')
-        ret = self.run_sql(f"select id, chunk_content from kb_crm where id = '1001'")
+        ret = self.run_sql("select id, chunk_content from kb_crm where id = '1001'")
         assert len(ret) == 1
         assert ret['chunk_content'][0] == 'Thank you'
 
-        ret = self.run_sql(f"select id, chunk_content from kb_crm where id != '1000' limit 4")
+        ret = self.run_sql("select id, chunk_content from kb_crm where id != '1000' limit 4")
         assert len(ret) == 4
         assert '1000' not in ret['id']
 
         # in, not in
-        ret = self.run_sql(f"select id, chunk_content from kb_crm where id in ('1001', '1000')")
+        ret = self.run_sql("select id, chunk_content from kb_crm where id in ('1001', '1000')")
         assert len(ret) == 2
         assert set(ret['id']) == {'1000', '1001'}
 
-        ret = self.run_sql(f"select id, chunk_content from kb_crm where id not in ('1001', '1000') limit 4")
+        ret = self.run_sql("select id, chunk_content from kb_crm where id not in ('1001', '1000') limit 4")
         assert len(ret) == 4
         assert '1000' not in ret['id']
 
@@ -191,21 +200,21 @@ class Test:
             # some operators don't work with chromadb
 
             # like / not like
-            ret = self.run_sql(f"select id, chunk_content from kb_crm where id like '100%')")
+            ret = self.run_sql("select id, chunk_content from kb_crm where id like '100%')")
             assert len(ret) == 2
             assert '1001' in ret['id']
 
-            ret = self.run_sql(f"select id, chunk_content from kb_crm where id not like '100%' limit 4")
+            ret = self.run_sql("select id, chunk_content from kb_crm where id not like '100%' limit 4")
             assert len(ret) == 4
             assert '1001' not in ret['id']
 
         # TODO filtering combination with content
 
         # ------------------- join with table -------------
-        ret = self.run_sql(f"""
+        ret = self.run_sql("""
             select k.chunk_content, t.message_body, k.id, t.pk
             from kb_crm k
-            join example_db.crm_demo t on t.pk = k.id 
+            join example_db.crm_demo t on t.pk = k.id
             where k.content = 'Help' and k.id not in ('1001', '1000')
             limit 4
         """)
@@ -222,28 +231,28 @@ class Test:
         '''
 
         # delete
-        self.run_sql(f"delete from kb_crm where id = '1'")
-        ret = self.run_sql(f"select * from kb_crm where id = '1'")
+        self.run_sql("delete from kb_crm where id = '1'")
+        ret = self.run_sql("select * from kb_crm where id = '1'")
         assert len(ret) == 0
 
-        self.run_sql(f"delete from kb_crm where id in ('1001', '2')")
-        ret = self.run_sql(f"select * from kb_crm where id in ('1001', '2')")
+        self.run_sql("delete from kb_crm where id in ('1001', '2')")
+        ret = self.run_sql("select * from kb_crm where id in ('1001', '2')")
         assert len(ret) == 0
 
-        self.run_sql(f"delete from kb_crm where id in ('1001', '1')")
+        self.run_sql("delete from kb_crm where id in ('1001', '1')")
 
         # insert
-        self.run_sql(f"""
-            insert into kb_crm (id, content) values (2000, 'OK')           
+        self.run_sql("""
+            insert into kb_crm (id, content) values (2000, 'OK')
         """)
-        ret = self.run_sql(f"select * from kb_crm where id = '2000'")
+        ret = self.run_sql("select * from kb_crm where id = '2000'")
         assert len(ret) == 1
         assert ret['chunk_content'][0] == 'OK'
         chunk_id = ret['chunk_id'][0]
 
         # update
         self.run_sql(f"update kb_crm set content = 'FINE' where chunk_id = '{chunk_id}'")
-        ret = self.run_sql(f"select * from kb_crm where id = '2000'")
+        ret = self.run_sql("select * from kb_crm where id = '2000'")
         assert len(ret) == 1
         assert ret['chunk_content'][0] == 'FINE'
 
@@ -270,22 +279,50 @@ class Test:
         Test querying status of Knowledge Base , DESCRIBE
         """
 
-        """
-        Scenario
-            Create KB and start load in thread
-    
-            check queries table
-            check describe kb
-            
-            wait finished
-    
-            Add threads, compare speed
-    
-        Second test
-            load kb using mindsdb jobs
+        def to_date(s):
+            return dt.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f')
 
-        """
-        # TODO
+        def load_kb(batch_size, threads):
+            self.run_sql(f"""
+                insert into kb_crm_part
+                select pk id, message_body content from example_db.crm_demo
+                order by pk
+                using batch_size={batch_size}, track_column={threads}
+            """)
+
+        test_set = [
+            {'batch_size': 10, 'threads': 'false'},
+            {'batch_size': 10, 'threads': 5},
+            {'batch_size': 50, 'threads': 'false'},
+            {'batch_size': 50, 'threads': 5},
+        ]
+
+        results = []
+        for item in test_set:
+            # Create KB and start load in thread
+            self.create_kb('kb_crm_part')
+
+            thread = Thread(target=load_kb, kwargs=item)
+            thread.run()
+
+            print('start loading')
+            while True:
+                time.sleep(1)
+
+                ret = self.run_sql('describe knowledge base kb_crm')
+                record = ret.iloc[0]
+
+                if record['INSERT_FINISHED_AT'] is not None:
+                    print('loading completed')
+                    item['seconds'] = (to_date(record['INSERT_FINISHED_AT']) - to_date(record['INSERT_STARTED_AT'])).seconds
+                    results.append(item)
+                    break
+
+                if record['ERROR'] is not None:
+                    raise RuntimeError(record['ERROR'])
+        print('========= Tests summary ==========')
+        print(pd.DataFrame(results))
+
 
     def check_relevance(self):
         """
@@ -295,4 +332,3 @@ class Test:
 
         """
         # TODO
-

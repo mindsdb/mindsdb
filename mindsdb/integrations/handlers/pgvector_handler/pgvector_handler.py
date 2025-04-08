2749143018fead67f92a9754ee0065c4404710f1
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-04-08 10:48:16 +0530

    fixed order of SQL code generated

--- mindsdb/integrations/handlers/pgvector_handler/pgvector_handler.py
+++ mindsdb/integrations/handlers/pgvector_handler/pgvector_handler.py
@@ -195,11 +195,6 @@ class PgVectorHandler(PostgresHandler, VectorStoreHandler):
         # given filter conditions, construct where clause
         where_clause = self._construct_where_clause(filter_conditions)
 
-        # construct full after from clause, where clause + offset clause + limit clause
-        after_from_clause = self._construct_full_after_from_clause(
-            where_clause, offset_clause, limit_clause
-        )
-
         # Handle distance column specially since it's calculated, not stored
         modified_columns = []
         has_distance = False
@@ -241,15 +236,15 @@ class PgVectorHandler(PostgresHandler, VectorStoreHandler):
                 if has_distance:
                     targets = f"{targets}, (embeddings {distance_op} '{search_vector}') as distance"
                 
-                return f"SELECT {targets} FROM {table_name} {after_from_clause} ORDER BY embeddings {distance_op} '{search_vector}' ASC"
+                return f"SELECT {targets} FROM {table_name} {where_clause} ORDER BY embeddings {distance_op} '{search_vector}' ASC {limit_clause} {offset_clause} "
 
             else:
                 # if filter conditions, return rows that satisfy the conditions
-                return f"SELECT {targets} FROM {table_name} {after_from_clause}"
+                return f"SELECT {targets} FROM {table_name} {where_clause} {limit_clause} {offset_clause}"
 
         else:
             # if no filter conditions, return all rows
-            return f"SELECT {targets} FROM {table_name} {after_from_clause}"
+            return f"SELECT {targets} FROM {table_name} {limit_clause} {offset_clause}"
 
     def _check_table(self, table_name: str):
         # Apply namespace for a user

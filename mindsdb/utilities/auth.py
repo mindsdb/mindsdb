1c62af70712eae50c7f3defa2e55148d21515c31
hamishfagg <ivdata+github@ivdata.net>
2025-04-15 10:32:46 +1200

    update aws metadata method

--- mindsdb/utilities/auth.py
+++ mindsdb/utilities/auth.py
@@ -16,8 +16,6 @@ def get_aws_meta_data() -> dict:
         'instance-id': None
     }
     aws_token = requests.put("http://169.254.169.254/latest/api/token", headers={'X-aws-ec2-metadata-token-ttl-seconds': '30'}).text
-    print("----------------------------------------------------------------------------")
-    print(aws_token)
     for key in aws_meta_data.keys():
         resp = requests.get(
             f'http://169.254.169.254/latest/meta-data/{key}',

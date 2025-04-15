ed0a88cc63f05ed90bd51097d4330f81b6139b19
andrew <elkin.andr@gmail.com>
2025-04-15 09:57:07 +0300

    replace base_url

--- mindsdb/interfaces/knowledge_base/controller.py
+++ mindsdb/interfaces/knowledge_base/controller.py
@@ -65,7 +65,7 @@ def get_embedding_model_from_params(embedding_model_params: dict):
     if provider == 'azure_openai':
         # Azure OpenAI expects the api_key to be passed as 'openai_api_key'.
         params_copy['openai_api_key'] = api_key
-        params_copy['azure_endpoint'] = params_copy['base_url']
+        params_copy['azure_endpoint'] = params_copy.pop('base_url')
         if 'chunk_size' not in params_copy:
             params_copy['chunk_size'] = 2048
         if 'api_version' in params_copy:

e4473d384b5fae2dd63e2ced3f49b69f52117111
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-04-17 17:33:48 +0530

    included database resolution for DESCRIBE commands

--- mindsdb/api/executor/command_executor.py
+++ mindsdb/api/executor/command_executor.py
@@ -868,13 +868,15 @@ class ExecuteCommands:
             else:
                 raise WrongArgumentError(f'Unknown describe type: {obj_type}')
 
-        name = obj_name.parts[-1]
+        parts = obj_name.parts
+        name = parts[-1]
         where = BinaryOperation(op='=', args=[
             Identifier('name'),
             Constant(name)
         ])
 
         if obj_type in project_objects:
+            database_name = parts[0] if len(parts) > 1 else database_name
             where = BinaryOperation(op='and', args=[
                 where,
                 BinaryOperation(op='=', args=[Identifier('project'), Constant(database_name)])

3f03e942be7f45b8393e48aae16c0ba7e3a01854
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-05-19 12:41:00 +0530

    fixed use of task monitor

--- tests/unit/executor/test_agent.py
+++ tests/unit/executor/test_agent.py
@@ -579,7 +579,7 @@ class TestKB(BaseExecutorDummyML):
 
     @patch('openai.OpenAI')
     @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
-    def test_kb_partitions(self, mock_handler, mock_openai, task_monitor):
+    def test_kb_partitions(self, mock_handler, mock_openai):
         set_openai_models(mock_openai)
 
         df = self._get_ral_table()

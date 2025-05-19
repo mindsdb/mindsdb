c2ea433a8f4a1d71f33f087b8cd8213bdb3729ae
hamishfagg <ivdata+github@ivdata.net>
2025-05-20 11:49:53 +1200

    move unused tests

--- tests/unused/unit/executor/test_udf.py
+++ tests/unused/unit/executor/test_udf.py
@@ -2,6 +2,9 @@ import os
 from textwrap import dedent
 from tempfile import TemporaryDirectory
 
+from unittest.mock import patch
+
+import pandas as pd
 import pytest
 
 from mindsdb_sql_parser.ast.mindsdb import CreateMLEngine
@@ -36,49 +39,49 @@ class TestBYOM(BaseExecutorDummyML):
             )
             assert ret.error_code is None
 
-    # @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
-    # def test_udf(self, data_handler, byom_type):
+    @patch('mindsdb.integrations.handlers.postgres_handler.Handler')
+    def test_udf(self, data_handler, byom_type):
 
-    #     df = pd.DataFrame([
-    #         {'a': 3, 'b': 4, 'c': 'a', 'd': 'b'},
-    #     ])
-    #     self.set_handler(data_handler, name='pg', tables={'sample': df})
+        df = pd.DataFrame([
+            {'a': 3, 'b': 4, 'c': 'a', 'd': 'b'},
+        ])
+        self.set_handler(data_handler, name='pg', tables={'sample': df})
 
-    #     code = dedent("""
-    #         from os import listdir # imported function
+        code = dedent("""
+            from os import listdir # imported function
 
-    #         def fibo(num: int) -> int:
-    #             if num < 2:
-    #                 return num
-    #             return fibo(num - 1) + fibo(num - 2)
+            def fibo(num: int) -> int:
+                if num < 2:
+                    return num
+                return fibo(num - 1) + fibo(num - 2)
 
-    #         # not annotated
-    #         def add1(a, b):
-    #             return a + b
+            # not annotated
+            def add1(a, b):
+                return a + b
 
-    #         # annotated
-    #         def add2(a: int, b: int) -> int:
-    #             return a + b
-    #     """)
+            # annotated
+            def add2(a: int, b: int) -> int:
+                return a + b
+        """)
 
-    #     self._create_engine(name='myml', code=code,
-    #                         type=byom_type, mode='custom_function')
-    #     # convert to explicit types, because duckdb doesn't convert it and fails
-    #     ret = self.run_sql('''
-    #         select myml.fibo(b) x,
-    #                myml.add1(a::char,b::char) y,
-    #                myml.add2(a,b) z
-    #         from pg.sample
-    #     ''')
-    #     assert ret['x'][0] == 3
-    #     assert ret['y'][0] == '34'
-    #     assert ret['z'][0] == 7
+        self._create_engine(name='myml', code=code,
+                            type=byom_type, mode='custom_function')
+        # convert to explicit types, because duckdb doesn't convert it and fails
+        ret = self.run_sql('''
+            select myml.fibo(b) x,
+                   myml.add1(a::char,b::char) y,
+                   myml.add2(a,b) z
+            from pg.sample
+        ''')
+        assert ret['x'][0] == 3
+        assert ret['y'][0] == '34'
+        assert ret['z'][0] == 7
 
-    #     # test without table
-    #     ret = self.run_sql('''
-    #         select myml.fibo(4) x
-    #     ''')
-    #     assert ret['x'][0] == 3
+        # test without table
+        ret = self.run_sql('''
+            select myml.fibo(4) x
+        ''')
+        assert ret['x'][0] == 3
 
     def test_byom(self, byom_type):
 

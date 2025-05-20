ff9705e95e049b7fc1964793d1bdeb39add1a2be
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-05-20 19:06:27 +0530

    updated the data catalog to store column statistics

--- mindsdb/interfaces/data_catalog/data_catalog_loader.py
+++ mindsdb/interfaces/data_catalog/data_catalog_loader.py
@@ -33,6 +33,8 @@ class DataCatalogLoader:
         tables = self._load_table_metadata()
         
         columns = self._load_column_metadata(tables)
+        
+        self._load_column_statistics(tables, columns)
     
     def _load_table_metadata(self):
         """
@@ -93,3 +95,38 @@ class DataCatalogLoader:
         db.session.add_all(columns)
         db.session.commit()
         return columns
+
+    def _load_column_statistics(self, tables: db.MetaTables, columns: db.MetaColumns):
+        """
+        Load the statistics for the specified columns.
+        """
+        logger.debug(f"Loading column statistics for {self.table_names}")
+        response = self.data_handler.get_column_statistics(self.table_names)
+        df = response.data_frame
+
+        return self._add_column_statistics(df, tables, columns)
+
+    def _add_column_statistics(self, df: pd.DataFrame, tables: db.MetaTables, columns: db.MetaColumns):
+        """
+        Add the statistics for the specified columns to the database.
+        """
+        column_statistics = []
+        for row in df.to_dict(orient='records'):
+            table_id = next(
+                (table.id for table in tables if table.name == row.get('table_name'))
+            )
+            column_id = next(
+                (column.id for column in columns if column.name == row.get('column_name') and column.table_id == table_id)
+            )
+            
+            record = db.MetaColumnStatistics(
+                column_id=column_id,
+                most_common_values=row.get('most_common_values'),
+                most_common_frequencies=str(row.get('most_common_frequencies')) if row.get('most_common_frequencies') else None,
+                null_percentage=row.get('null_percentage'),
+                distinct_values_count=row.get('distinct_values_count')
+            )
+            column_statistics.append(record)
+
+        db.session.add_all(column_statistics)
+        db.session.commit()

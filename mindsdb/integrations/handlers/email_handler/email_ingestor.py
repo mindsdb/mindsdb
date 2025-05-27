58fa6fd348a99864b7d755fe82a123893206f821
trickster026 <tricksterr026@proton.me>
2025-05-27 11:56:29 +0530

    bugfix: add fix for undetectable encodings (#10884)

--- mindsdb/integrations/handlers/email_handler/email_ingestor.py
+++ mindsdb/integrations/handlers/email_handler/email_ingestor.py
@@ -41,7 +41,10 @@ class EmailIngestor:
         encoding = None
         if isinstance(body_str, bytes):
             encoding = chardet.detect(body_str)['encoding']
-            if 'windows' in encoding.lower():
+            if encoding is None:
+                # If chardet can't detect the encoding, we default to utf-8.
+                encoding = 'utf-8'
+            elif 'windows' in encoding.lower():
                 # Easier to treat this at utf-8 since str constructor doesn't support all encodings here:
                 # https://chardet.readthedocs.io/en/latest/supported-encodings.html.
                 encoding = 'utf-8'

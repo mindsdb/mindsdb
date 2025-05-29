fe9be12421e3efa33babae384128583c8546c79a
hamishfagg <ivdata+github@ivdata.net>
2025-05-29 18:12:36 +1200

    typo

--- mindsdb/interfaces/file/file_controller.py
+++ mindsdb/interfaces/file/file_controller.py
@@ -124,7 +124,6 @@ class FileController:
         """
         file_reader = FileReader(path=source_path)
         tables = file_reader.get_contents()
-        file_reader.close()
 
         pages_files = {}
         pages_index = {}

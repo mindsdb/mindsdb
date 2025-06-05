f5438a4929ac31fa5c3d8880027332b8b3589593
andrew <elkin.andr@gmail.com>
2025-06-05 14:33:35 +0300

    store inserted columns in `inserted_metadata` check when selecting

--- mindsdb/interfaces/knowledge_base/controller.py
+++ mindsdb/interfaces/knowledge_base/controller.py
@@ -239,7 +239,8 @@ class KnowledgeBaseTable:
                 limit = 100
             query.limit = Constant(limit)
 
-        df = db_handler.dispatch_select(query, conditions)
+        metadata_columns = self._get_metadata_columns()
+        df = db_handler.dispatch_select(query, conditions, allowed_metadata_columns=metadata_columns)
         df = self.addapt_result_columns(df)
 
         logger.debug(f"Query returned {len(df)} rows")
@@ -262,6 +263,12 @@ class KnowledgeBaseTable:
             df = query_df(df, query_copy, session=self.session)
 
         return df
+
+    def _get_metadata_columns(self):
+        user_columns = self._kb.params.get('metadata_columns', [])
+        dynamic_columns = self._kb.params.get('inserted_metadata', [])
+
+        return list(set(user_columns) | set(dynamic_columns))
 
     def score_documents(self, query_text, documents, reranking_model_params):
         reranker = get_reranking_model_from_params(reranking_model_params)
@@ -617,6 +624,13 @@ class KnowledgeBaseTable:
         else:
             # all the rest columns
             metadata_columns = list(set(columns).difference(content_columns))
+
+            # update list of used columns
+            inserted_metadata = set(self._kb.params.get('inserted_metadata', []))
+            inserted_metadata.update(metadata_columns)
+            self._kb.params['inserted_metadata'] = list(inserted_metadata)
+            flag_modified(self._kb, 'params')
+            db.session.commit()
 
         # Add content columns directly (don't combine them)
         for col in content_columns:

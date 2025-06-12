79eb195d8aed59e85a4e9e414d415e3994c712e9
ZoranPandovski <zoran.pandovski@gmail.com>
2025-06-12 17:02:48 +0200

    Fix tests

--- tests/integration/tutorials/test_predict_text_sentiment_huggingface_tutorial.py
+++ tests/integration/tutorials/test_predict_text_sentiment_huggingface_tutorial.py
@@ -1,28 +1,32 @@
 from mindsdb.api.executor.data_types.response_type import RESPONSE_TYPE
 from tests.integration.utils.http_test_helpers import HTTPHelperMixin
+import uuid
+
+random_str = uuid.uuid4().hex[:6]
+example_sentiment_hf_db = f"example_sentiment_huggingface_db{random_str}"
+model_name = f"sentiment_classifier_gpt3_{random_str}"
 
 
 class QueryStorage:
-    create_db = """
-CREATE DATABASE example_sentiment_huggingface_db
+    create_db = f"""
+CREATE DATABASE {example_sentiment_hf_db}
 WITH ENGINE = "postgres",
-PARAMETERS = {
-    "user": "demo_user",
+PARAMETERS = {"user": "demo_user",
     "password": "demo_password",
     "host": "samples.mindsdb.com",
     "port": "5432",
     "database": "demo"
     };
 """
-    check_db_created = """
+    check_db_created = f"""
 SELECT *
-FROM example_sentiment_huggingface_db.demo_data.user_comments LIMIT 3;
+FROM {example_sentiment_hf_db}.demo_data.user_comments LIMIT 3;
 """
-    delete_db = """
-DROP DATABASE example_sentiment_huggingface_db;
+    delete_db = f"""
+DROP DATABASE {example_sentiment_hf_db};
 """
-    create_model = """
-CREATE MODEL sentiment_classifier
+    create_model = f"""
+CREATE MODEL {model_name}
 PREDICT sentiment
 USING engine='huggingface',
   task = 'text-classification',
@@ -30,28 +34,27 @@ USING engine='huggingface',
   input_column = 'comment',
   labels=['negative','neutral','positive'];
 """
-    check_status = """
+    check_status = f"""
 SELECT *
 FROM models
-WHERE name = 'sentiment_classifier';
+WHERE name = {model_name};
 """
-    delete_model = """
+    delete_model = f"""
 DROP MODEL
-  mindsdb.sentiment_classifier;
+  mindsdb.{model_name};
 """
-    prediction = """
-SELECT * FROM sentiment_classifier
+    prediction = f"""
+SELECT * FROM {model_name}
 WHERE comment='It is really easy to do NLP with MindsDB';
 """
     bulk_prediction = """
 SELECT input.comment, model.sentiment
-FROM example_sentiment_huggingface_db.demo_data.user_comments AS input
-JOIN sentiment_classifier AS model;
+FROM {example_sentiment_hf_db}.demo_data.user_comments AS input
+JOIN {model_name} AS model;
 """
 
 
 class TestPredictTextSentimentHuggingface(HTTPHelperMixin):
-
     def setup_class(self):
         self.sql_via_http(self, QueryStorage.delete_db)
         self.sql_via_http(self, QueryStorage.delete_model)
@@ -63,24 +66,24 @@ class TestPredictTextSentimentHuggingface(HTTPHelperMixin):
     def test_db_created(self):
         sql = QueryStorage.check_db_created
         resp = self.sql_via_http(sql, RESPONSE_TYPE.TABLE)
-        assert len(resp['data']) == 3
+        assert len(resp["data"]) == 3
 
     def test_create_model(self, train_finetune_lock):
         with train_finetune_lock.acquire(timeout=600):
             sql = QueryStorage.create_model
             resp = self.sql_via_http(sql, RESPONSE_TYPE.TABLE)
-            assert len(resp['data']) == 1
-            status = resp['column_names'].index('STATUS')
-            assert resp['data'][0][status] == 'generating'
+            assert len(resp["data"]) == 1
+            status = resp["column_names"].index("STATUS")
+            assert resp["data"][0][status] == "generating"
             status = self.await_model_by_query(QueryStorage.check_status, timeout=600)
-            assert status == 'complete'
+            assert status == "complete"
 
     def test_prediction(self):
         sql = QueryStorage.prediction
         resp = self.sql_via_http(sql, RESPONSE_TYPE.TABLE)
-        assert len(resp['data']) == 1
+        assert len(resp["data"]) == 1
 
     def test_bulk_prediciton(self):
         sql = QueryStorage.bulk_prediction
         resp = self.sql_via_http(sql, RESPONSE_TYPE.TABLE)
-        assert len(resp['data']) >= 1
+        assert len(resp["data"]) >= 1

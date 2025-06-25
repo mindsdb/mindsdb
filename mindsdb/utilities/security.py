3a4ebaa6f4409533d1cba78091274fc11e29a25c
Max Stepanov <stpmax@yandex.ru>
2025-06-25 17:02:19 +0300

    modify `validate_urls`

--- mindsdb/utilities/security.py
+++ mindsdb/utilities/security.py
@@ -33,22 +33,38 @@ def clear_filename(filename: str) -> str:
     return filename
 
 
-def validate_urls(urls, allowed_urls):
+def validate_urls(urls: str | list[str], allowed_urls: list[str]) -> bool:
     """
     Checks if the provided URL(s) is/are from an allowed host.
 
-    This function parses the URL(s) and checks the network location part (netloc)
+    This function parses the URL(s) and checks the origin (scheme + netloc)
     against a list of allowed hosts.
+
+    Examples:
+        validate_urls("http://site.com/file", ["site.com"]) -> True
+        validate_urls("http://site.com/file", ["https://site.com"]) -> False
+        validate_urls("site.com/file", ["https://site.com"]) -> False
 
     :param urls: The URL(s) to check. Can be a single URL (str) or a list of URLs (list).
     :param allowed_urls: The list of allowed URLs.
     :return bool:  True if the URL(s) is/are from an allowed host, False otherwise.
     """
-    allowed_netlocs = [urlparse(allowed_url).netloc for allowed_url in allowed_urls]
+    allowed_origins = []
+    for url in allowed_urls:
+        parsed_url = urlparse(url)
+        allowed_origins.append((parsed_url.scheme, parsed_url.netloc))
 
     if isinstance(urls, str):
         urls = [urls]
 
     # Check if all provided URLs are from the allowed sites
-    valid = all(urlparse(url).netloc in allowed_netlocs for url in urls)
-    return valid
+    for url in urls:
+        parsed_url = urlparse(url)
+        scheme = parsed_url.scheme
+        netloc = parsed_url.netloc
+        if (
+            (scheme, netloc) not in allowed_origins
+            and ("", netloc) not in allowed_origins
+        ):
+            return False
+    return True

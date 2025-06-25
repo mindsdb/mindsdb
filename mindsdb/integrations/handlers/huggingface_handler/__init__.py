e4cb05a89feb5918fba06d6de80b5e6a3049b08a
Max Stepanov <stpmax@yandex.ru>
2025-06-25 12:44:33 +0300

    format

--- mindsdb/integrations/handlers/huggingface_handler/__init__.py
+++ mindsdb/integrations/handlers/huggingface_handler/__init__.py
@@ -15,13 +15,11 @@ import_error = """
     The `huggingface_handler` is temporary disabled in current version of MindsDB due to security vulnerability.
 """
 
-title = 'Hugging Face'
-name = 'huggingface'
+title = "Hugging Face"
+name = "huggingface"
 type = HANDLER_TYPE.ML
 icon_path = "icon.svg"
 permanent = False
-execution_method = 'subprocess_keep'
+execution_method = "subprocess_keep"
 
-__all__ = [
-    'Handler', 'version', 'name', 'type', 'title', 'description', 'import_error', 'icon_path'
-]
+__all__ = ["Handler", "version", "name", "type", "title", "description", "import_error", "icon_path"]

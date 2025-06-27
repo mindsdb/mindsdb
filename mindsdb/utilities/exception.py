183083e3b4f4140856ec96fceeb20a0bbce57bfc
Max Stepanov <stpmax@yandex.ru>
2025-06-27 19:13:26 +0300

    make function for db error message formatting

--- mindsdb/utilities/exception.py
+++ mindsdb/utilities/exception.py
@@ -1,3 +1,6 @@
+from textwrap import indent
+
+
 class BaseEntityException(Exception):
     """Base exception for entitys errors
 
@@ -27,3 +30,32 @@ class EntityNotExistsError(BaseEntityException):
         if message is None:
             message = 'Entity does not exists error'
         super().__init__(message, entity_name)
+
+
+def format_db_error_message(db_name: str | None = None, db_type: str | None = None, db_error_msg: str | None = None, failed_query: str | None = None) -> str:
+    """Format the error message for the database query.
+
+    Args:
+        db_name (str | None): The name of the database.
+        db_type (str | None): The type of the database.
+        db_error_msg (str | None): The error message.
+        failed_query (str | None): The failed query.
+
+    Returns:
+        str: The formatted error message.
+    """
+    error_message = "Failed to execute external database query during query processing."
+    if db_name is not None or db_type is not None:
+        error_message += "\n\nDatabase Details:"
+        if db_name is not None:
+            error_message += f"\n- Name: {db_name}"
+        if db_type is not None:
+            error_message += f"\n- Type: {db_type}"
+
+    if db_error_msg is not None:
+        error_message += f"\n\nError:\n{indent(db_error_msg, '    ')}"
+
+    if failed_query is not None:
+        error_message += f"\n\nFailed Query:\n{indent(failed_query, '    ')}"
+
+    return error_message

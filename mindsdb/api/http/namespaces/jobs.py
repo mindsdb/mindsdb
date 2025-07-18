685b6cea75762f735da4b221d55677f80e0e1125
Max Stepanov <stpmax@yandex.ru>
2025-07-18 15:30:07 +0300

    uglify

--- mindsdb/api/http/namespaces/jobs.py
+++ mindsdb/api/http/namespaces/jobs.py
@@ -10,80 +10,68 @@ from mindsdb.metrics.metrics import api_endpoint_metrics
 from mindsdb.interfaces.jobs.jobs_controller import JobsController, parse_job_date
 
 
-@ns_conf.route('/<project_name>/jobs')
+@ns_conf.route("/<project_name>/jobs")
 class JobsResource(Resource):
-    @ns_conf.doc('list_jobs')
-    @api_endpoint_metrics('GET', '/jobs')
+    @ns_conf.doc("list_jobs")
+    @api_endpoint_metrics("GET", "/jobs")
     def get(self, project_name):
-        """ List all jobs in a project """
+        """List all jobs in a project"""
         return ca.jobs_controller.get_list(project_name)
 
-    @ns_conf.doc('create_job')
-    @api_endpoint_metrics('POST', '/jobs')
+    @ns_conf.doc("create_job")
+    @api_endpoint_metrics("POST", "/jobs")
     def post(self, project_name):
         """Create a job in a project"""
 
         # Check for required parameters.
-        if 'job' not in request.json:
-            return http_error(
-                HTTPStatus.BAD_REQUEST,
-                'Missing parameter',
-                'Must provide "job" parameter in POST body'
-            )
+        if "job" not in request.json:
+            return http_error(HTTPStatus.BAD_REQUEST, "Missing parameter", 'Must provide "job" parameter in POST body')
 
-        job = request.json['job']
+        job = request.json["job"]
 
-        name = job.pop('name')
-        if job['start_at'] is not None:
-            job['start_at'] = parse_job_date(job['start_at'])
-        if job['end_at'] is not None:
-            job['end_at'] = parse_job_date(job['end_at'])
+        name = job.pop("name")
+        if job["start_at"] is not None:
+            job["start_at"] = parse_job_date(job["start_at"])
+        if job["end_at"] is not None:
+            job["end_at"] = parse_job_date(job["end_at"])
 
         create_job_name = ca.jobs_controller.add(name, project_name, **job)
 
         return ca.jobs_controller.get(create_job_name, project_name)
 
 
-@ns_conf.route('/<project_name>/jobs/<job_name>')
-@ns_conf.param('project_name', 'Name of the project')
-@ns_conf.param('job_name', 'Name of the job')
+@ns_conf.route("/<project_name>/jobs/<job_name>")
+@ns_conf.param("project_name", "Name of the project")
+@ns_conf.param("job_name", "Name of the job")
 class JobResource(Resource):
-    @ns_conf.doc('get_job')
-    @api_endpoint_metrics('GET', '/jobs/job')
+    @ns_conf.doc("get_job")
+    @api_endpoint_metrics("GET", "/jobs/job")
     def get(self, project_name, job_name):
         """Gets a job by name"""
         job_info = ca.jobs_controller.get(job_name, project_name)
         if job_info is not None:
             return job_info
 
-        return http_error(
-            HTTPStatus.NOT_FOUND,
-            'Job not found',
-            f'Job with name {job_name} does not exist'
-        )
+        return http_error(HTTPStatus.NOT_FOUND, "Job not found", f"Job with name {job_name} does not exist")
 
-    @ns_conf.doc('delete_job')
-    @api_endpoint_metrics('DELETE', '/jobs/job')
+    @ns_conf.doc("delete_job")
+    @api_endpoint_metrics("DELETE", "/jobs/job")
     def delete(self, project_name, job_name):
         """Deletes a job by name"""
         ca.jobs_controller.delete(job_name, project_name)
 
-        return '', HTTPStatus.NO_CONTENT
+        return "", HTTPStatus.NO_CONTENT
 
 
-@ns_conf.route('/<project_name>/jobs/<job_name>/history')
-@ns_conf.param('project_name', 'Name of the project')
-@ns_conf.param('job_name', 'Name of the job')
+@ns_conf.route("/<project_name>/jobs/<job_name>/history")
+@ns_conf.param("project_name", "Name of the project")
+@ns_conf.param("job_name", "Name of the job")
 class JobsHistory(Resource):
-    @ns_conf.doc('job_history')
-    @api_endpoint_metrics('GET', '/jobs/job/history')
+    @ns_conf.doc("job_history")
+    @api_endpoint_metrics("GET", "/jobs/job/history")
     def get(self, project_name, job_name):
         """Get history of job calls"""
         if ca.jobs_controller.get(job_name, project_name) is None:
-            return http_error(
-                HTTPStatus.NOT_FOUND,
-                'Job not found',
-                f'Job with name {job_name} does not exist'
-            )
+            return http_error(HTTPStatus.NOT_FOUND, "Job not found", f"Job with name {job_name} does not exist")
 
         return ca.jobs_controller.get_history(job_name, project_name)
--- mindsdb/api/http/namespaces/tree.py
+++ mindsdb/api/http/namespaces/tree.py
@@ -9,108 +9,105 @@ from mindsdb.api.http.namespaces.configs.tree import ns_conf
 from mindsdb.metrics.metrics import api_endpoint_metrics
 
 
-@ns_conf.route('/')
+@ns_conf.route("/")
 class GetRoot(Resource):
-    @ns_conf.doc('get_tree_root')
-    @api_endpoint_metrics('GET', '/tree')
+    @ns_conf.doc("get_tree_root")
+    @api_endpoint_metrics("GET", "/tree")
     def get(self):
         databases = ca.database_controller.get_list()
-        result = [{
-            'name': x['name'],
-            'class': 'db',
-            'type': x['type'],
-            'engine': x['engine'],
-            'deletable': x['deletable'],
-            'visible': x['visible']
-        } for x in databases]
+        result = [
+            {
+                "name": x["name"],
+                "class": "db",
+                "type": x["type"],
+                "engine": x["engine"],
+                "deletable": x["deletable"],
+                "visible": x["visible"],
+            }
+            for x in databases
+        ]
         return result
 
 
-@ns_conf.route('/<db_name>')
-@ns_conf.param('db_name', "Name of the database")
+@ns_conf.route("/<db_name>")
+@ns_conf.param("db_name", "Name of the database")
 class GetLeaf(Resource):
-    @ns_conf.doc('get_tree_leaf')
-    @api_endpoint_metrics('GET', '/tree/database')
+    @ns_conf.doc("get_tree_leaf")
+    @api_endpoint_metrics("GET", "/tree/database")
     def get(self, db_name):
-        with_schemas = request.args.get('all_schemas')
+        with_schemas = request.args.get("all_schemas")
         if isinstance(with_schemas, str):
-            with_schemas = with_schemas.lower() in ('1', 'true')
+            with_schemas = with_schemas.lower() in ("1", "true")
         else:
             with_schemas = False
         db_name = db_name.lower()
         databases = ca.database_controller.get_dict()
         if db_name not in databases:
-            return http_error(
-                400,
-                "Error",
-                f"There is no element with name '{db_name}'"
-            )
+            return http_error(400, "Error", f"There is no element with name '{db_name}'")
         db = databases[db_name]
-        if db['type'] == 'project':
+        if db["type"] == "project":
             project = ca.database_controller.get_project(db_name)
             tables = project.get_tables()
-            tables = [{
-                'name': key,
-                'schema': None,
-                'class': 'table',
-                'type': val['type'],
-                'engine': val.get('engine'),
-                'deletable': val.get('deletable')
-            } for key, val in tables.items()]
+            tables = [
+                {
+                    "name": key,
+                    "schema": None,
+                    "class": "table",
+                    "type": val["type"],
+                    "engine": val.get("engine"),
+                    "deletable": val.get("deletable"),
+                }
+                for key, val in tables.items()
+            ]
 
             jobs = ca.jobs_controller.get_list(db_name)
-            tables = tables + [{
-                'name': job['name'],
-                'schema': None,
-                'class': 'job',
-                'type': 'job',
-                'engine': 'job',
-                'deletable': True
-                } for job in jobs
+            tables = tables + [
+                {"name": job["name"], "schema": None, "class": "job", "type": "job", "engine": "job", "deletable": True}
+                for job in jobs
             ]
-        elif db['type'] == 'data':
+        elif db["type"] == "data":
             handler = ca.integration_controller.get_data_handler(db_name)
-            if 'all' in inspect.signature(handler.get_tables).parameters:
+            if "all" in inspect.signature(handler.get_tables).parameters:
                 response = handler.get_tables(all=with_schemas)
             else:
                 response = handler.get_tables()
-            if response.type != 'table':
+            if response.type != "table":
                 return []
-            table_types = {
-                'BASE TABLE': 'table',
-                'VIEW': 'view'
-            }
-            tables = response.data_frame.to_dict(orient='records')
+            table_types = {"BASE TABLE": "table", "VIEW": "view"}
+            tables = response.data_frame.to_dict(orient="records")
 
             schemas = defaultdict(list)
 
             for table_meta in tables:
                 table_meta = {key.lower(): val for key, val in table_meta.items()}
-                schama = table_meta.get('table_schema')
-                schemas[schama].append({
-                    'name': table_meta['table_name'],
-                    'class': 'table',
-                    'type': table_types.get(table_meta.get('table_type')),
-                    'engine': None,
-                    'deletable': False
-                })
+                schama = table_meta.get("table_schema")
+                schemas[schama].append(
+                    {
+                        "name": table_meta["table_name"],
+                        "class": "table",
+                        "type": table_types.get(table_meta.get("table_type")),
+                        "engine": None,
+                        "deletable": False,
+                    }
+                )
             if len(schemas) == 1 and list(schemas.keys())[0] is None:
                 tables = schemas[None]
             else:
-                tables = [{
-                    'name': key,
-                    'class': 'schema',
-                    'deletable': False,
-                    'children': val
-                } for key, val in schemas.items()]
-        elif db['type'] == 'system':
+                tables = [
+                    {"name": key, "class": "schema", "deletable": False, "children": val}
+                    for key, val in schemas.items()
+                ]
+        elif db["type"] == "system":
             system_db = ca.database_controller.get_system_db(db_name)
             tables = system_db.get_tree_tables()
-            tables = [{
-                'name': table.name,
-                'class': table.kind,
-                'type': 'system view',
-                'engine': None,
-                'deletable': table.deletable,
-            } for table in tables.values()]
+            tables = [
+                {
+                    "name": table.name,
+                    "class": table.kind,
+                    "type": "system view",
+                    "engine": None,
+                    "deletable": table.deletable,
+                }
+                for table in tables.values()
+            ]
         return tables

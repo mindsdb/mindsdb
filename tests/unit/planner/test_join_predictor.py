87301ca2582415d974e45d7b419bdaa5e38e640c
Max Stepanov <stpmax@yandex.ru>
2025-07-21 14:43:54 +0300

    fix

--- tests/unit/planner/test_join_predictor.py
+++ tests/unit/planner/test_join_predictor.py
@@ -370,7 +370,7 @@ class TestPlanJoinPredictor:
                 ),
                 SubSelectStep(
                     dataframe=Result(2),
-                    query=Select(targets=[Identifier('time')], group_by=[Constant(1)]),
+                    query=Select(targets=[Identifier('`time`')], group_by=[Constant(1)]),
                     table_name='Custom SQL Query'
                 ),
             ],

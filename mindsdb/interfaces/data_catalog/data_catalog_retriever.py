77c1b277276c985fc660c5bb04fa53e98cad2c8e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-08-26 13:25:35 +0530

    implemented the other required methods in the data catalog retriever

--- mindsdb/interfaces/data_catalog/data_catalog_retriever.py
+++ mindsdb/interfaces/data_catalog/data_catalog_retriever.py
@@ -11,7 +11,7 @@ class DataCatalogRetriever(BaseDataCatalog):
     This is different from the DataCatalogReader, which relies on the fact that the metadata is already stored in the database.
     """
 
-    def retrieve_table_metadata(self) -> pd.DataFrame:
+    def retrieve_tables(self) -> pd.DataFrame:
         """
         Retrieve the table metadata from the handler.
         """
@@ -25,8 +25,8 @@ class DataCatalogRetriever(BaseDataCatalog):
             return pd.DataFrame()
         
         return response.data_frame
-    
-    def retrieve_column_metadata(self) -> pd.DataFrame:
+
+    def retrieve_columns(self) -> pd.DataFrame:
         """
         Retrieve the column metadata from the handler.
         """
@@ -37,6 +37,51 @@ class DataCatalogRetriever(BaseDataCatalog):
             return pd.DataFrame()
         elif response.resp_type == RESPONSE_TYPE.OK:
             self.logger.error(f"No columns found for {self.database_name} in the data source.")
+            return pd.DataFrame()
+        
+        return response.data_frame
+
+    def retrieve_column_statistics(self) -> pd.DataFrame:
+        """
+        Retrieve the column statistics from the handler.
+        """
+        self.logger.info(f"Retrieving column statistics for {', '.join(self.table_names) if self.table_names else 'all'} tables for {self.database_name}")
+        response = self.data_handler.meta_get_column_statistics(self.table_names)
+        if response.resp_type == RESPONSE_TYPE.ERROR:
+            self.logger.error(f"Failed to retrieve column statistics for {self.database_name}: {response.error_message}")
+            return pd.DataFrame()
+        elif response.resp_type == RESPONSE_TYPE.OK:
+            self.logger.error(f"No column statistics found for {self.database_name} in the data source.")
+            return pd.DataFrame()
+        
+        return response.data_frame
+
+    def retrieve_primary_keys(self) -> pd.DataFrame:
+        """
+        Retrieve the primary keys from the handler.
+        """
+        self.logger.info(f"Retrieving primary keys for {', '.join(self.table_names) if self.table_names else 'all'} tables for {self.database_name}")
+        response = self.data_handler.meta_get_primary_keys(self.table_names)
+        if response.resp_type == RESPONSE_TYPE.ERROR:
+            self.logger.error(f"Failed to retrieve primary keys for {self.database_name}: {response.error_message}")
+            return pd.DataFrame()
+        elif response.resp_type == RESPONSE_TYPE.OK:
+            self.logger.error(f"No primary keys found for {self.database_name} in the data source.")
+            return pd.DataFrame()
+        
+        return response.data_frame
+
+    def retrieve_foreign_keys(self) -> pd.DataFrame:
+        """
+        Retrieve the foreign keys from the handler.
+        """
+        self.logger.info(f"Retrieving foreign keys for {', '.join(self.table_names) if self.table_names else 'all'} tables for {self.database_name}")
+        response = self.data_handler.meta_get_foreign_keys(self.table_names)
+        if response.resp_type == RESPONSE_TYPE.ERROR:
+            self.logger.error(f"Failed to retrieve foreign keys for {self.database_name}: {response.error_message}")
+            return pd.DataFrame()
+        elif response.resp_type == RESPONSE_TYPE.OK:
+            self.logger.error(f"No foreign keys found for {self.database_name} in the data source.")
             return pd.DataFrame()
         
         return response.data_frame

1ad430ad762d4817cab6c54bc3edfc31f2009807
Hamish Fagg <hamish@mindsdb.com>
2025-09-05 10:41:06 +1200

    lint

--- mindsdb/migrations/env.py
+++ mindsdb/migrations/env.py
@@ -1,6 +1,5 @@
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-import os
 
 from mindsdb.interfaces.storage import db
 from mindsdb.utilities.config import config as app_config

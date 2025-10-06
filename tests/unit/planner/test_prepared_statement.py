20685340edb4157220fee8668a732dbde207a15f
Max Stepanov <stpmax@yandex.ru>
2025-10-06 17:10:25 +0300

    uglify

--- tests/unit/planner/test_prepared_statement.py
+++ tests/unit/planner/test_prepared_statement.py
@@ -13,47 +13,50 @@ from tests.unit.planner import test_ts_predictor
 
 
 class FakeExecutor:
-
     def list_cols_return(self, table_name, columns):
-
-        table_alias = ('int', table_name, table_name)
+        table_alias = ("int", table_name, table_name)
         data = ResultSet()
         for column in columns:
-            data.add_column(Column(
-                name=column['name'],
-                type=column.get('type'),
-                table_name=table_name,
-                table_alias=table_alias
-            ))
+            data.add_column(
+                Column(name=column["name"], type=column.get("type"), table_name=table_name, table_alias=table_alias)
+            )
         return data
 
     def execute(self, step):
-
         if (
-                isinstance(step, steps.ProjectStep)
-                or isinstance(step, steps.FetchDataframeStep)
-                or isinstance(step, steps.UnionStep)
+            isinstance(step, steps.ProjectStep)
+            or isinstance(step, steps.FetchDataframeStep)
+            or isinstance(step, steps.UnionStep)
         ):
-            return [
-                {'id': 1, 'name': 'asdf'},
-                {'id': 2, 'name': 'jkl;'}
-            ]
+            return [{"id": 1, "name": "asdf"}, {"id": 2, "name": "jkl;"}]
         if isinstance(step, steps.GetTableColumns):
-            if step.table in ('tab', 'tab1', 'tab2', 'tab3', 'data.ny_output', 'data', 'yyy.zzz', 'sweat', 'schem.sweat', 'predictors', 'v1'):
+            if step.table in (
+                "tab",
+                "tab1",
+                "tab2",
+                "tab3",
+                "data.ny_output",
+                "data",
+                "yyy.zzz",
+                "sweat",
+                "schem.sweat",
+                "predictors",
+                "v1",
+            ):
                 cols = [
-                    {'name': 'id', 'type': 'int'},
-                    {'name': 'name', 'type': 'str'},
-                    {'name': 'a column with spaces', 'type': 'str'},
-                    {'name': 'column1', 'type': 'str'},
-                    {'name': 'column2', 'type': 'str'},
-                    {'name': 'column3', 'type': 'str'},
-                    {'name': 'col1', 'type': 'str'},
-                    {'name': 'asset', 'type': 'float'},
-                    {'name': 'time', 'type': 'datetime'},
-                    {'name': 'predicted', 'type': 'float'},
-                    {'name': 'target', 'type': 'float'},
-                    {'name': 'sqft', 'type': 'float'},
-                    {'name': 'x', 'type': 'int'},
+                    {"name": "id", "type": "int"},
+                    {"name": "name", "type": "str"},
+                    {"name": "a column with spaces", "type": "str"},
+                    {"name": "column1", "type": "str"},
+                    {"name": "column2", "type": "str"},
+                    {"name": "column3", "type": "str"},
+                    {"name": "col1", "type": "str"},
+                    {"name": "asset", "type": "float"},
+                    {"name": "time", "type": "datetime"},
+                    {"name": "predicted", "type": "float"},
+                    {"name": "target", "type": "float"},
+                    {"name": "sqft", "type": "float"},
+                    {"name": "x", "type": "int"},
                 ]
                 return self.list_cols_return(step.table, cols)
             return None
@@ -62,17 +65,17 @@ class FakeExecutor:
             if name.isdigit():
                 name = step.predictor.parts[-2]
 
-            if name in ('pred', 'tp3', 'pr', 'embedding_model'):
+            if name in ("pred", "tp3", "pr", "embedding_model"):
                 cols = [
-                    {'name': 'id', 'type': 'int'},
-                    {'name': 'value', 'type': 'str'},
-                    {'name': 'predicted', 'type': 'int'},
-                    {'name': 'x1', 'type': 'int'},
-                    {'name': 'x2', 'type': 'int'},
-                    {'name': 'y', 'type': 'int'},
-                    {'name': 'time', 'type': 'datetime'},
-                    {'name': 'price', 'type': 'float'},
-                    {'name': 'target', 'type': 'float'},
+                    {"name": "id", "type": "int"},
+                    {"name": "value", "type": "str"},
+                    {"name": "predicted", "type": "int"},
+                    {"name": "x1", "type": "int"},
+                    {"name": "x2", "type": "int"},
+                    {"name": "y", "type": "int"},
+                    {"name": "time", "type": "datetime"},
+                    {"name": "price", "type": "float"},
+                    {"name": "target", "type": "float"},
                 ]
                 name = step.predictor.parts[-1]
                 return self.list_cols_return(name, cols)
@@ -84,7 +87,6 @@ executor = FakeExecutor()
 
 
 def plan_query_patch(query, **kwargs):
-
     plan = query_planner.QueryPlanner(**kwargs)
 
     steps = []
@@ -118,31 +120,33 @@ test_ts_predictor.plan_query = plan_query_patch
 
 
 class TestPreparedStatement:
-
     def test_from_planner_tests(self):
-
-        for module in (test_integration_select, test_join_predictor, test_join_tables,
-                       test_plan_union, test_select_from_predictor, test_ts_predictor):
-
+        for module in (
+            test_integration_select,
+            test_join_predictor,
+            test_join_tables,
+            test_plan_union,
+            test_select_from_predictor,
+            test_ts_predictor,
+        ):
             for class_name, klass in inspect.getmembers(module, predicate=inspect.isclass):
-                if not class_name.startswith('Test'):
+                if not class_name.startswith("Test"):
                     continue
 
                 tests = klass()
                 for test_name, test_method in inspect.getmembers(tests, predicate=inspect.ismethod):
-                    if not test_name.startswith('test_') or test_name.endswith('_error'):
+                    if not test_name.startswith("test_") or test_name.endswith("_error"):
                         continue
-                    if test_name in ('test_native_query_no_sub_select',):
+                    if test_name in ("test_native_query_no_sub_select",):
                         # skipped tests
                         continue
                     try:
                         test_method()
                     except query_planner.PlanningException as e:
-
-                        if str(e) == 'Predictor must be last table in query':
+                        if str(e) == "Predictor must be last table in query":
                             # TODO replace tables in tests: to predictor as left table
                             pass
-                        elif str(e) == 'Predictor is not at first level':
+                        elif str(e) == "Predictor is not at first level":
                             # TODO make prepared statement planner more sophisticated
                             pass
                         else:

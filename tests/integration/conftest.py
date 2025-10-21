d778476e0e1d693e56ff9d1da2c5e16f75d4da20
Max Stepanov <stpmax@yandex.ru>
2025-10-21 09:47:05 +0300

    debug prints

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,10 +3,13 @@ import os
 from filelock import FileLock
 
 INTERNAL_URL = os.environ.get("INTERNAL_URL", "localhost")
+print(f'INTERNAL_URL={INTERNAL_URL}')
 HTTP_PORT = "80" if "svc.cluster.local" in INTERNAL_URL else "47334"
 
 HTTP_API_ROOT = f"http://{INTERNAL_URL}:{HTTP_PORT}/api"
 MYSQL_API_ROOT = INTERNAL_URL
+print(f'HTTP_API_ROOT={HTTP_API_ROOT}')
+print(f'MYSQL_API_ROOT={MYSQL_API_ROOT}')
 
 lock = FileLock("train_finetune.lock")
 

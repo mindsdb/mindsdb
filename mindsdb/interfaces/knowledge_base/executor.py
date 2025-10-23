b1f80426196729dbbfe712f0d09a8cc868050c65
andrew <elkin.andr@gmail.com>
2025-10-23 09:36:09 +0300

    ruff

--- mindsdb/interfaces/knowledge_base/executor.py
+++ mindsdb/interfaces/knowledge_base/executor.py
@@ -172,7 +172,11 @@ class KnowledgeBaseQueryExecutor:
             # is it json operator on metadata
             if isinstance(arg0, BinaryOperation) and arg0.op in ("->", "->>"):
                 op_arg0, op_arg1 = arg0.args
-                if isinstance(op_arg0, Identifier) and isinstance(op_arg1, Constant) and op_arg0.parts[-1].lower() == 'metadata':
+                if (
+                    isinstance(op_arg0, Identifier)
+                    and isinstance(op_arg1, Constant)
+                    and op_arg0.parts[-1].lower() == "metadata"
+                ):
                     # replace to metadata column
                     condition.args[0] = Identifier(parts=[op_arg1.value])
 

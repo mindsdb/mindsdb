ca0a4f5b8193e703700a55132dd2cdcbd04f6501
tino097 <konstantin.sivakov@gmail.com>
2025-11-05 17:28:04 +0100

    Check the content of the json payload

--- mindsdb/api/http/namespaces/file.py
+++ mindsdb/api/http/namespaces/file.py
@@ -105,12 +105,25 @@ class File(Resource):
                     try:
                         file_object.flush()
                     except (AttributeError, ValueError, OSError):
-                        logger.debug("Failed to flush file_object before closing.", exc_info=True)
+                        logger.debug(
+                            "Failed to flush file_object before closing.", exc_info=True
+                        )
                     file_object.close()
-                Path(file_object.name).rename(Path(file_object.name).parent / data["file"])
+                Path(file_object.name).rename(
+                    Path(file_object.name).parent / data["file"]
+                )
                 file_object = None
         else:
             data = request.json
+
+        if "file" in data:
+            # check for traversal attack
+            if Path(data["file"]).name != data["file"]:
+                return http_error(
+                    400,
+                    "Wrong file name",
+                    f"File name contains path symbols: {data['file']}",
+                )
 
         existing_file_names = ca.file_controller.get_files_names(lower=True)
         if mindsdb_file_name.lower() in existing_file_names:
@@ -131,7 +144,9 @@ class File(Resource):
         if source_type == "url":
             url_file_upload_enabled = config["url_file_upload"]["enabled"]
             if url_file_upload_enabled is False:
-                return http_error(400, "URL file upload is disabled.", "URL file upload is disabled.")
+                return http_error(
+                    400, "URL file upload is disabled.", "URL file upload is disabled."
+                )
 
             if "file" in data:
                 return http_error(
@@ -196,7 +211,9 @@ class File(Resource):
                         )
             with requests.get(url, stream=True) as r:
                 if r.status_code != 200:
-                    return http_error(400, "Error getting file", f"Got status code: {r.status_code}")
+                    return http_error(
+                        400, "Error getting file", f"Got status code: {r.status_code}"
+                    )
                 file_path = os.path.join(temp_dir_path, data["file"])
                 with open(file_path, "wb") as f:
                     for chunk in r.iter_content(chunk_size=8192):
@@ -224,17 +241,23 @@ class File(Resource):
             files = os.listdir(temp_dir_path)
             if len(files) != 1:
                 os.rmdir(temp_dir_path)
-                return http_error(400, "Wrong content.", "Archive must contain only one data file.")
+                return http_error(
+                    400, "Wrong content.", "Archive must contain only one data file."
+                )
             file_path = os.path.join(temp_dir_path, files[0])
             mindsdb_file_name = files[0]
             if not os.path.isfile(file_path):
                 os.rmdir(temp_dir_path)
-                return http_error(400, "Wrong content.", "Archive must contain data file in root.")
+                return http_error(
+                    400, "Wrong content.", "Archive must contain data file in root."
+                )
 
         try:
             if not Path(mindsdb_file_name).suffix == "":
                 return http_error(400, "Error", "File name cannot contain extension.")
-            ca.file_controller.save_file(mindsdb_file_name, file_path, file_name=original_file_name)
+            ca.file_controller.save_file(
+                mindsdb_file_name, file_path, file_name=original_file_name
+            )
         except FileProcessingError as e:
             return http_error(400, "Error", str(e))
         except Exception as e:

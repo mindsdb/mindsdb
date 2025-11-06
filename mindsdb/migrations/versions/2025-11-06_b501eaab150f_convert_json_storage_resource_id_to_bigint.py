0af00a0d10cb81dc3253fb3280e52f9cc6ce9725
Gabriel Bressan <gabriel@talentify.io>
2025-11-06 13:23:46 -0400

    Add migration to convert json_storage resource_id column from Integer to BigInteger

---
+++ mindsdb/migrations/versions/2025-11-06_b501eaab150f_convert_json_storage_resource_id_to_bigint.py
@@ -0,0 +1,44 @@
+"""convert json_storage resource_id to bigint
+
+Revision ID: b501eaab150f
+Revises: a44643042fe8
+Create Date: 2025-11-06 16:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b501eaab150f'
+down_revision = 'a44643042fe8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """
+    Convert resource_id column in json_storage table from Integer to BigInteger.
+    This is necessary to support timestamp-based integration IDs that exceed 32-bit integer limits.
+    """
+    with op.batch_alter_table('json_storage', schema=None) as batch_op:
+        batch_op.alter_column(
+            'resource_id',
+            existing_type=sa.Integer(),
+            type_=sa.BigInteger(),
+            existing_nullable=True
+        )
+
+
+def downgrade():
+    """
+    Revert resource_id column back to Integer.
+    WARNING: This will fail if there are values > 2147483647 in the database.
+    """
+    with op.batch_alter_table('json_storage', schema=None) as batch_op:
+        batch_op.alter_column(
+            'resource_id',
+            existing_type=sa.BigInteger(),
+            type_=sa.Integer(),
+            existing_nullable=True
+        )

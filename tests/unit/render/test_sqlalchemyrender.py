1ee22963805576b9d6dffcb7e1cf6a38960a6e0d
andrew <elkin.andr@gmail.com>
2026-01-15 14:49:15 +0300

    render test

--- tests/unit/render/test_sqlalchemyrender.py
+++ tests/unit/render/test_sqlalchemyrender.py
@@ -16,7 +16,7 @@ from mindsdb_sql_parser import parse_sql
 from mindsdb.utilities.render.sqlalchemy_render import SqlalchemyRender
 
 
-class TestRender:
+class TestMysqlRender:
     def test_create_table(self):
         query = CreateTable(
             name="tbl1",
@@ -77,6 +77,8 @@ class TestRender:
         assert sql == """INSERT INTO tbl1 (a, b) VALUES (%s, %s)"""
         assert params == values
 
+
+class TestPostgresRender:
     def test_alias_in_case(self):
         sql = """
            select case mean when 0 then null else stdev/mean end cov from table1
@@ -184,6 +186,22 @@ class TestRender:
             SELECT * FROM tbl1
             JOIN tbl2 ON tbl1.x = tbl2.x
             JOIN tbl3 ON 1=1
+        """).strip()
+
+        assert rendered.replace("\n", "") == expected.replace("\n", " ")
+
+
+class TestMSSQLRender:
+    def test_mixed_join(self):
+        sql = """
+            select * from car_info order by year limit 10 offset 1
+        """
+        query = parse_sql(sql)
+        rendered = SqlalchemyRender("mssql").get_string(query, with_failback=False)
+
+        expected = dedent("""
+           SELECT * FROM car_info ORDER BY year
+           OFFSET 1 ROWS FETCH FIRST 10 ROWS ONLY
         """).strip()
 
         assert rendered.replace("\n", "") == expected.replace("\n", " ")

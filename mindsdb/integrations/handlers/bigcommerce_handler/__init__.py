3d126c9bb22a2e75332568962b2863b4cf902a99
Max Stepanov <stpmax@yandex.ru>
2026-01-29 15:33:40 +0300

    Mark community supported integrations (#12171)

--- mindsdb/integrations/handlers/bigcommerce_handler/__init__.py
+++ mindsdb/integrations/handlers/bigcommerce_handler/__init__.py
@@ -15,7 +15,7 @@ title = "BigCommerce"
 name = "bigcommerce"
 type = HANDLER_TYPE.DATA
 icon_path = "bigcommerce-black.svg"
-support_level = HANDLER_SUPPORT_LEVEL.MINDSDB
+support_level = HANDLER_SUPPORT_LEVEL.COMMUNITY
 
 __all__ = [
     "Handler",
--- mindsdb/integrations/handlers/github_handler/__init__.py
+++ mindsdb/integrations/handlers/github_handler/__init__.py
@@ -15,7 +15,7 @@ title = "GitHub"
 name = "github"
 type = HANDLER_TYPE.DATA
 icon_path = "icon.svg"
-support_level = HANDLER_SUPPORT_LEVEL.MINDSDB
+support_level = HANDLER_SUPPORT_LEVEL.COMMUNITY
 
 __all__ = [
     "Handler",

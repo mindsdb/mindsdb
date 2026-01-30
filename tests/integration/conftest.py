eb846f9eff994f5f5264ab2c34fa7173dffff71f
Max Stepanov <stpmax@yandex.ru>
2026-01-30 11:21:12 +0300

    debug

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -226,4 +226,4 @@ def create_byom(name: str, target_column: str = "test", company_id=None, user_id
         headers=headers,
     )
     if response.status_code not in (200, 409):
-        raise Exception("Error creating BYOM engine")
+        raise Exception(f"Error creating BYOM engine: {response.body}")

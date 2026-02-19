f03720db06467c5bbed8e61326507b4affcad540
Max Stepanov <stpmax@yandex.ru>
2026-02-19 17:07:32 +0300

    uglify

--- mindsdb/integrations/utilities/install.py
+++ mindsdb/integrations/utilities/install.py
@@ -6,8 +6,8 @@ from typing import Text, List
 
 
 class InstallTool(Enum):
-    pip = sys.executable, '-m', 'pip'
-    uv = 'uv', 'pip'
+    pip = sys.executable, "-m", "pip"
+    uv = "uv", "pip"
 
 
 def install_dependencies(dependencies: List[Text], tool: InstallTool = InstallTool.pip) -> dict:
@@ -21,62 +21,57 @@ def install_dependencies(dependencies: List[Text], tool: InstallTool = InstallTo
     Returns:
         dict: A dictionary containing the success status and an error message if an error occurs.
     """
-    outs = b''
-    errs = b''
-    result = {
-        'success': False,
-        'error_message': None
-    }
+    outs = b""
+    errs = b""
+    result = {"success": False, "error_message": None}
     code = None
 
     try:
         # Split the dependencies by parsing the contents of the requirements.txt file.
         split_dependencies = parse_dependencies(dependencies)
     except FileNotFoundError as file_not_found_error:
-        result['error_message'] = f"Error parsing dependencies, file not found: {str(file_not_found_error)}"
+        result["error_message"] = f"Error parsing dependencies, file not found: {str(file_not_found_error)}"
         return result
     except Exception as unknown_error:
-        result['error_message'] = f"Unknown error parsing dependencies: {str(unknown_error)}"
+        result["error_message"] = f"Unknown error parsing dependencies: {str(unknown_error)}"
         return result
 
     try:
         # Install the dependencies using the selected tool.
         sp = subprocess.Popen(
-            [*tool.value, 'install', *split_dependencies],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            [*tool.value, "install", *split_dependencies], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         code = sp.wait()
         outs, errs = sp.communicate(timeout=1)
     except subprocess.TimeoutExpired as timeout_error:
         sp.kill()
-        result['error_message'] = f"Timeout error while installing dependencies: {str(timeout_error)}"
+        result["error_message"] = f"Timeout error while installing dependencies: {str(timeout_error)}"
         return result
     except FileNotFoundError as e:
-        if e.filename == 'uv':
-            result['error_message'] = "The 'pip' and 'uv' tools are not found. Please install them."
+        if e.filename == "uv":
+            result["error_message"] = "The 'pip' and 'uv' tools are not found. Please install them."
         else:
-            result['error_message'] = f"FileNotFoundError error while installing dependencies: {str(e)}"
+            result["error_message"] = f"FileNotFoundError error while installing dependencies: {str(e)}"
         return result
     except Exception as unknown_error:
-        result['error_message'] = f"Unknown error while installing dependencies: {str(unknown_error)}"
+        result["error_message"] = f"Unknown error while installing dependencies: {str(unknown_error)}"
         return result
 
     # Return the result of the installation if successful, otherwise return an error message.
     if code != 0:
-        output = ''
+        output = ""
         if isinstance(outs, bytes) and len(outs) > 0:
-            output = output + 'Output: ' + outs.decode()
+            output = output + "Output: " + outs.decode()
         if isinstance(errs, bytes) and len(errs) > 0:
             if len(output) > 0:
-                output = output + '\n'
-            output = output + 'Errors: ' + errs.decode()
-        if 'no module named pip' in output.lower() and tool is InstallTool.pip:
+                output = output + "\n"
+            output = output + "Errors: " + errs.decode()
+        if "no module named pip" in output.lower() and tool is InstallTool.pip:
             # try with uv
             return install_dependencies(dependencies, InstallTool.uv)
-        result['error_message'] = output
+        result["error_message"] = output
     else:
-        result['success'] = True
+        result["success"] = True
 
     return result
 
@@ -101,19 +96,19 @@ def parse_dependencies(dependencies: List[Text]) -> List[Text]:
     split_dependencies = []
     for dependency in dependencies:
         # ignore standalone comments
-        if dependency.startswith('#'):
+        if dependency.startswith("#"):
             continue
 
         # remove inline comments
-        if '#' in dependency:
-            dependency = dependency.split('#')[0].strip()
+        if "#" in dependency:
+            dependency = dependency.split("#")[0].strip()
 
         # check if the dependency is a path to a requirements file
-        if dependency.startswith('-r'):
+        if dependency.startswith("-r"):
             # get the path to the requirements file
-            req_path = dependency.split(' ')[1]
+            req_path = dependency.split(" ")[1]
             # create the absolute path to the requirements file
-            abs_req_path = os.path.abspath(os.path.join(script_path, req_path.replace('mindsdb/integrations', '..')))
+            abs_req_path = os.path.abspath(os.path.join(script_path, req_path.replace("mindsdb/integrations", "..")))
             # check if the file exists
             if os.path.exists(abs_req_path):
                 inner_dependencies, inner_split_dependencies = [], []
@@ -144,7 +139,7 @@ def read_dependencies(path: Text) -> List[Text]:
     """
     dependencies = []
     # read the dependencies from the file
-    with open(str(path), 'rt') as f:
-        dependencies = [x.strip(' \t\n') for x in f.readlines()]
+    with open(str(path), "rt") as f:
+        dependencies = [x.strip(" \t\n") for x in f.readlines()]
         dependencies = [x for x in dependencies if len(x) > 0]
     return dependencies

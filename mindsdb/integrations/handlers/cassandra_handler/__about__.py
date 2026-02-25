490c5f0869e4e4082f8827ba6778f97ec5a24d2b
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2026-02-25 14:33:46 +0100

    Removed community handlers

--- mindsdb/integrations/handlers/cassandra_handler/__about__.py
+++
@@ -1,9 +0,0 @@
-__title__ = 'MindsDB Cassandra handler'
-__package_name__ = 'mindsdb_cassandra_handler'
-__version__ = '0.0.1'
-__description__ = "MindsDB handler for CassandraDB"
-__author__ = 'MindsDB Inc'
-__github__ = 'https://github.com/mindsdb/mindsdb'
-__pypi__ = 'https://pypi.org/project/mindsdb/'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2022- mindsdb'

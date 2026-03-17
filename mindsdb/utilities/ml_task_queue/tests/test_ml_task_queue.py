d561eab43d19a6d6a3da641575a42badf2691666
Michael Olawepo <sejuba@gmail.com>
2026-03-17 17:48:29 -0400

    chore: apply ruff formatting to migrated tests

--- mindsdb/utilities/ml_task_queue/tests/test_ml_task_queue.py
+++ mindsdb/utilities/ml_task_queue/tests/test_ml_task_queue.py
@@ -15,18 +15,17 @@ REDIS_HOST = os.environ.get("INTERNAL_URL", "").replace("mindsdb", "redis-master
 
 @pytest.mark.skipif("localhost" in HTTP_API_ROOT or "127.0.0.1" in HTTP_API_ROOT, reason="Requires redis")
 class TestMLTaskQueue(HTTPHelperMixin):
-
     def test_redis_connection(self):
         db = Database(protocol=3, host=REDIS_HOST)
         db.ping()
 
     def test_create_model(self, train_finetune_lock):
-        """ 1. create db connection
-            2. create test dataset
-            3. start to train model in 'async' mode: check status
-            4. start to train model in 'sync' mode: check status
-            5. await model 2 is finished
-            6. 2 messages in redis stream
+        """1. create db connection
+        2. create test dataset
+        3. start to train model in 'async' mode: check status
+        4. start to train model in 'sync' mode: check status
+        5. await model 2 is finished
+        6. 2 messages in redis stream
         """
 
         db_details = {
@@ -37,8 +36,8 @@ class TestMLTaskQueue(HTTPHelperMixin):
                 "port": "5432",
                 "user": "demo_user",
                 "password": "demo_password",
-                "database": "demo"
-            }
+                "database": "demo",
+            },
         }
 
         self.sql_via_http("DROP MODEL IF EXISTS p_test_queue_async;", RESPONSE_TYPE.OK)
@@ -47,7 +46,7 @@ class TestMLTaskQueue(HTTPHelperMixin):
         query = f"""
             CREATE DATABASE IF NOT EXISTS test_demo_queue
             WITH ENGINE = 'postgres',
-            PARAMETERS = {json.dumps(db_details['connection_data'])};
+            PARAMETERS = {json.dumps(db_details["connection_data"])};
         """
         self.sql_via_http(query, RESPONSE_TYPE.OK)
 
@@ -58,8 +57,8 @@ class TestMLTaskQueue(HTTPHelperMixin):
                 predict rental_price;
             """
             response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-            status = response['data'][0][response['column_names'].index('STATUS')]
-            assert status in ('generating', 'training')
+            status = response["data"][0][response["column_names"].index("STATUS")]
+            assert status in ("generating", "training")
 
             query = """
                 create predictor p_test_queue_sync
@@ -68,16 +67,16 @@ class TestMLTaskQueue(HTTPHelperMixin):
                 USING join_learn_process=true;
             """
             response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-            status = response['data'][0][response['column_names'].index('STATUS')]
-            assert status == 'complete'
+            status = response["data"][0][response["column_names"].index("STATUS")]
+            assert status == "complete"
 
-            status = self.await_model('p_test_queue_async')
-            assert status == 'complete'
+            status = self.await_model("p_test_queue_async")
+            assert status == "complete"
 
         db = Database(protocol=3, host=REDIS_HOST)
         assert TASKS_STREAM_NAME in db.keys()
 
-        assert db.type(TASKS_STREAM_NAME) == b'stream'
+        assert db.type(TASKS_STREAM_NAME) == b"stream"
         xlen = db.xlen(TASKS_STREAM_NAME)
         if xlen != 0:
             lol = db.xrange(TASKS_STREAM_NAME)
@@ -85,8 +84,7 @@ class TestMLTaskQueue(HTTPHelperMixin):
         assert db.xlen(TASKS_STREAM_NAME) == 0
 
     def test_predict(self):
-        """ make predict queries to both trained models
-        """
+        """make predict queries to both trained models"""
 
         query = """
             SELECT rental_price,
@@ -95,8 +93,8 @@ class TestMLTaskQueue(HTTPHelperMixin):
             WHERE b = 10;
         """
         response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-        assert len(response['data']) == 1
-        assert len(response['data'][0]) == 2
+        assert len(response["data"]) == 1
+        assert len(response["data"][0]) == 2
 
         query = """
             SELECT rental_price,
@@ -106,15 +104,14 @@ class TestMLTaskQueue(HTTPHelperMixin):
             LIMIT 3;
         """
         response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-        assert len(response['data']) == 3
-        assert len(response['data'][0]) == 2
+        assert len(response["data"]) == 3
+        assert len(response["data"][0]) == 2
 
         db = Database(protocol=3, host=REDIS_HOST)
         assert db.xlen(TASKS_STREAM_NAME) == 0
 
     def test_finetune(self, train_finetune_lock):
-        """ check that finetune is working
-        """
+        """check that finetune is working"""
 
         with train_finetune_lock.acquire(timeout=600):
             query = """
@@ -123,20 +120,20 @@ class TestMLTaskQueue(HTTPHelperMixin):
                 USING join_learn_process=true;
             """
             response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-            status = response['data'][0][response['column_names'].index('STATUS')]
-            assert status == 'complete'
+            status = response["data"][0][response["column_names"].index("STATUS")]
+            assert status == "complete"
 
             query = """
                 FINETUNE p_test_queue_async
                 FROM test_demo_queue (SELECT * FROM demo_data.home_rentals LIMIT 10);
             """
             response = self.sql_via_http(query, RESPONSE_TYPE.TABLE)
-            status = response['data'][0][response['column_names'].index('STATUS')]
+            status = response["data"][0][response["column_names"].index("STATUS")]
             # FINETUNE in this case may be very fast, so add 'complete' to check
-            assert status in ('generating', 'training', 'complete')
+            assert status in ("generating", "training", "complete")
 
-            status = self.await_model('p_test_queue_async', version_number=2)
-            assert status == 'complete'
+            status = self.await_model("p_test_queue_async", version_number=2)
+            assert status == "complete"
 
         db = Database(protocol=3, host=REDIS_HOST)
         assert db.xlen(TASKS_STREAM_NAME) == 0
--- tests/integration/metrics/test_metrics.py
+++ tests/integration/metrics/test_metrics.py
@@ -23,7 +23,7 @@ def _wait_for_metric(name: str, labels: Dict[str, str], timeout: datetime.timede
     start_time = datetime.datetime.now()
     while datetime.datetime.now() - start_time < timeout:
         metrics = _get_metrics()
-        for metrics_line in metrics.split('\n'):
+        for metrics_line in metrics.split("\n"):
             if name not in metrics_line:
                 continue
             # Check labels match metric.
@@ -41,28 +41,31 @@ def _wait_for_metric(name: str, labels: Dict[str, str], timeout: datetime.timede
 
 
 class TestMetrics(HTTPHelperMixin):
-    @pytest.mark.skipif(("localhost" in HTTP_API_ROOT or "127.0.0.1" in HTTP_API_ROOT) and os.getenv('PROMETHEUS_MULTIPROC_DIR') is None, reason="PROMETHEUS_MULTIPROC_DIR environment variable is not set")
+    @pytest.mark.skipif(
+        ("localhost" in HTTP_API_ROOT or "127.0.0.1" in HTTP_API_ROOT)
+        and os.getenv("PROMETHEUS_MULTIPROC_DIR") is None,
+        reason="PROMETHEUS_MULTIPROC_DIR environment variable is not set",
+    )
     def test_http_metrics(self):
         # Make an HTTP request and check for updated metrics.
-        api_metric_labels = {
-            'endpoint': '/util/ping_native',
-            'method': 'GET',
-            'status': '200'
-        }
+        api_metric_labels = {"endpoint": "/util/ping_native", "method": "GET", "status": "200"}
 
         before_metric = _wait_for_metric(
-            'mindsdb_rest_api_latency_seconds_count',
+            "mindsdb_rest_api_latency_seconds_count",
             api_metric_labels,
         )
         print(f"Before metric: {before_metric}")
-        _ = self.api_request('get', '/util/ping_native')
-        assert _wait_for_metric(
-            'mindsdb_rest_api_latency_seconds_count',
-            api_metric_labels,
-        ) == before_metric + 1
+        _ = self.api_request("get", "/util/ping_native")
+        assert (
+            _wait_for_metric(
+                "mindsdb_rest_api_latency_seconds_count",
+                api_metric_labels,
+            )
+            == before_metric + 1
+        )
         # Check multiproc dir is populated.
-        multiproc_dir = os.getenv('PROMETHEUS_MULTIPROC_DIR')
+        multiproc_dir = os.getenv("PROMETHEUS_MULTIPROC_DIR")
         # We can't check this dir if we're running against a remote env.
         if multiproc_dir is not None:
             assert os.path.isdir(multiproc_dir)
-            assert len(os.listdir(os.getenv('PROMETHEUS_MULTIPROC_DIR'))) > 0
+            assert len(os.listdir(os.getenv("PROMETHEUS_MULTIPROC_DIR"))) > 0

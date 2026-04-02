7a34e3fd8f0508003465cf6970fce979b6dfa101
Max Stepanov <stpmax@yandex.ru>
2026-04-02 17:54:33 +0300

    del rag

--- mindsdb/integrations/utilities/rag/retrievers/__init__.py
+++
@@ -1,3 +0,0 @@
-from mindsdb.integrations.utilities.rag.retrievers.multi_hop_retriever import MultiHopRetriever
-
-__all__ = ['MultiHopRetriever']
--- mindsdb/interfaces/knowledge_base/preprocessing/constants.py
+++ mindsdb/interfaces/knowledge_base/preprocessing/constants.py
@@ -1,13 +1,4 @@
-# Default settings for markdown header splitting
-DEFAULT_MARKDOWN_HEADERS = [
-    ("#", "Header 1"),
-    ("##", "Header 2"),
-    ("###", "Header 3"),
-]
-
 # Limits for web crawling
 DEFAULT_CRAWL_DEPTH = None
 DEFAULT_WEB_CRAWL_LIMIT = 1
 DEFAULT_WEB_FILTERS = []
-
-DEFAULT_CONTEXT_DOCUMENT_LIMIT = 50

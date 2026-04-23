3743464bb8bc14600d7fecd28c3fc24a4bdbce17
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2026-04-23 20:53:38 +0200

    Ruff

--- mindsdb/api/http/namespaces/configs/integrations.py
+++ mindsdb/api/http/namespaces/configs/integrations.py
@@ -1,3 +1,3 @@
 from flask_restx import Namespace
 
-ns_conf = Namespace('integrations', description='API for integration-level operations (passthrough, capabilities)')
+ns_conf = Namespace("integrations", description="API for integration-level operations (passthrough, capabilities)")
